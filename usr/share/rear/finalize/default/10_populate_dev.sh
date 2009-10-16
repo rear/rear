0131c314cb22d9ee65e400ef5e5495d6504d663c
Gratien D'haese <gratien.dhaese@gmail.com>
2009-10-16 07:26:52 +0000

    Fix for /dev/shm (on SAP system): results in a file system full error (mkrescue/mkbackup)

--- usr/share/rear/finalize/default/10_populate_dev.sh
+++ usr/share/rear/finalize/default/10_populate_dev.sh
@@ -1,4 +1,5 @@
 # many systems now use udev and thus have an empty /dev
 # this prevents our chrooted grub install later on, so we copy
 # the /dev from our rescue system to the freshly installed system
-cp -a /dev /mnt/local/
+# 	cp -a /dev /mnt/local/    # /dev/shm (SAP) = file system full
+rsync -a --exclude dev/shm/* /dev /mnt/local/
