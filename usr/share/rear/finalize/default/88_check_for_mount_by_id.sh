45ab5e0e8d29a182ad68fc20fb571518b72a4a6b
Schlomo Schapiro <github@schlomo.schapiro.org>
2009-11-18 17:31:42 +0000

    - add sles11 x86_64 validation info - automatically include /usr/lib*/vmware-tools - fix for check_mount_by_id to work on more systems

--- usr/share/rear/finalize/default/88_check_for_mount_by_id.sh
+++ usr/share/rear/finalize/default/88_check_for_mount_by_id.sh
@@ -10,13 +10,27 @@ if grep -q by-id $VAR_DIR/recovery/fstab ; then
 	LogPrint "WARNING ! You are mounting some devices by ID. Please be aware that the IDs"
 	LogPrint "are hardware dependant and that you might have to adjust your fstab to match"
 	LogPrint "the new IDs. Currently your system has the following disks with LUN IDs:"
+	SCSI_ID_HAVE_RESULT=
 	while read major minor size device ; do
 		sysfs="$(tr / \! <<<"$device")"
 		ID_SERIAL=
-		eval "$(scsi_id -g -x -n -s /block/"$sysfs" -d /dev/$device)"
+		# apparently the usage of scsi_id changed over the times, so we try two ways to call it
+		eval "$(scsi_id -g -x -n -s /block/"$sysfs" -d /dev/$device || scsi_id --export --whitelisted -d /dev/$device)"
 		if test "$ID_SERIAL" ; then
 			LogPrint "  $ID_SERIAL  /dev/$device  $((size/1024))MB"
+			SCSI_ID_HAVE_RESULT=1
 		fi
 	done </proc/partitions
 	LogPrint ""
+
+	# sanity check, should be some disks listed here.
+	if ! test "$SCSI_ID_HAVE_RESULT" ; then
+		LogPrint "
+	
+WARNING ! Could not list any LUN IDs with scsi_id. Please tell us how your
+system calls scsi_id. However, some disks just don't have any LUN ID. We
+would like to know more about such systems and how they look.
+
+"
+	fi
 fi
---
+++ usr/share/rear/prep/GNU/Linux/28_include_vmware_tools.sh
@@ -0,0 +1,12 @@
+# $Id$
+#
+# recent vmware tools (or maybe it is just open-vm-tools on SUSE) keep their modules
+# outside the /lib/modules path. To cope with that we add the vmware-tools if vmxnet
+# is loaded but modinfo cannot find it.
+
+if lsmod | grep -q vmxnet ; then
+	if ! modinfo vmxnet >/dev/null 2>&1 ; then
+		COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/lib*/vmware-tools )
+		Log "Including '/usr/lib*/vmware-tools'"
+	fi
+fi
