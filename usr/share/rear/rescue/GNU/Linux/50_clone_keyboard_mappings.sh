f858ad0da9ddc80006dfab7d4713fc756feacb91
Schlomo Schapiro <github@schlomo.schapiro.org>
2010-10-04 11:13:40 +0000

    typo

--- usr/share/rear/rescue/GNU/Linux/50_clone_keyboard_mappings.sh
+++ usr/share/rear/rescue/GNU/Linux/50_clone_keyboard_mappings.sh
@@ -3,4 +3,4 @@ dumpkeys -f1 >$ROOTFS_DIR/etc/dumpkeys.out
 
 # Also include the US keyboard mappings
 # Adding RHEL, SLES and Ubuntu qwerty flavours
-COPY_AS_IS=( "${COPY_AS_IS[@]}" /lib/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/keymaps/i386/qwerty/defkeymap.map.gz )
+COPY_AS_IS=( "${COPY_AS_IS[@]}" /lib/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/ke?maps/i386/qwerty/defkeymap.map.gz )
