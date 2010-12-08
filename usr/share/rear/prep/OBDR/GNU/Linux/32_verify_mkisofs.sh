07cd161cd7ac3941fe5b33d8fd4b55e87e977776
Gratien D'haese <gratien.dhaese@gmail.com>
2010-12-08 11:31:26 +0000

    start cleaning up the OBDR mesh in prep/OBDR

--- usr/share/rear/prep/OBDR/GNU/Linux/32_verify_mkisofs.sh
+++ usr/share/rear/prep/OBDR/GNU/Linux/32_verify_mkisofs.sh
@@ -1,8 +1,1 @@
-# verify that we have a working mkisofs
-#
-# default for ISO_MKISOFS_BIN is to check for mkisofs and genisoimage in the path
-
-test -x "$ISO_MKISOFS_BIN"
-ProgressStopIfError $? "Could not find 'mkisofs' compatible program. Please install 'mkisofs' or 'genisoimage' into your path or manually set ISO_MKISOFS_BIN [$ISO_MKISOFS_BIN]"
-ProgressStep
-Log "Using '$ISO_MKISOFS_BIN' to create ISO images"
+../../../ISO/GNU/Linux/32_verify_mkisofs.sh
