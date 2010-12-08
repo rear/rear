8df1c7e7b48076498a0dab747722dde8f901262d
Gratien D'haese <gratien.dhaese@gmail.com>
2010-12-08 11:38:12 +0000

    due to OBDR changes

--- usr/share/rear/prep/ISO/GNU/Linux/32_verify_mkisofs.sh
+++ usr/share/rear/prep/ISO/GNU/Linux/32_verify_mkisofs.sh
@@ -4,4 +4,5 @@
 
 test -x "$ISO_MKISOFS_BIN"
 ProgressStopIfError $? "Could not find 'mkisofs' compatible program. Please install 'mkisofs' or 'genisoimage' into your path or manually set ISO_MKISOFS_BIN [$ISO_MKISOFS_BIN]"
+ProgressStep
 Log "Using '$ISO_MKISOFS_BIN' to create ISO images"
