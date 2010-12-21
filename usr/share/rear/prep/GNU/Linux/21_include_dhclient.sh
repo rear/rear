6d8f288e0d228b85c1d7f40049cf15235bca2c5f
Gratien D'haese <gratien.dhaese@gmail.com>
2010-12-21 12:49:03 +0000

    add auto dhclient detection in prep/GNU/Linux/21_include_dhclient.sh and fixed a typo and moved some PROGS to Linux.conf

--- usr/share/rear/prep/GNU/Linux/21_include_dhclient.sh
+++ usr/share/rear/prep/GNU/Linux/21_include_dhclient.sh
@@ -1,6 +1,11 @@
 # copy the binaries and config files that we require to use dhclient
 # on the rescue image
-[ -z "USE_DHCLIENT" ] && return	# empty string means no dhcp client support required
+
+# check if current system runs dhclient, if yes, then defined USE_DHCLIENT=yes
+ps -e | grep -q dhclient && USE_DHCLIENT=yes
+
+# check if we defined in our site/local.conf file the variable USE_DHCLIENT
+[ -z "$USE_DHCLIENT" ] && return	# empty string means no dhcp client support required
 
 REQUIRED_PROGS=(
 "${REQUIRED_PROGS[@]}"
@@ -10,5 +15,5 @@ dhclient
 # we made our own /etc/dhclient.conf and /bin/dhclient-script files (no need to copy these
 # from the local Linux system)
 COPY_AS_IS=( "${COPY_AS_IS[@]}" "/etc/localtime" )
-PROGS=( "${PROGS[@]}" arping mktemp ipcalc seq usleep env )
+PROGS=( "${PROGS[@]}" arping ipcalc usleep )
 
