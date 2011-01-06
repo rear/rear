2f47ee36385a8b106d4e75aaf0376e661e9710d9
Gratien D'haese <gratien.dhaese@gmail.com>
2011-01-06 19:15:31 +0000

    small update for rewinding tape drive with OBDR

--- usr/share/rear/prep/TAPE/default/60_rewind_tape_drive.sh
+++ usr/share/rear/prep/TAPE/default/60_rewind_tape_drive.sh
@@ -3,7 +3,8 @@
 # TODO: This might take a long while, we should give the user more feedback about
 # what is going on !
 
-Log "Rewinding tape"
+sleep 5
+LogPrint "Rewinding tape"
 mt -f  "${TAPE_DEVICE}" rewind 
 ProgressStopIfError $? "Problem with rewinding tape device ${TAPE_DEVICE}"
 Log "Finished rewinding tape"
