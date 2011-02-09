c34926df49841a59dbbb80b85511cfc8aaebbc8a
Gratien D'haese <gratien.dhaese@gmail.com>
2011-02-09 08:33:40 +0000

    * add link under finalize from RedHatEnterpriseClient to Fedora as pointed out by Olivier KALOUDOFF * 85_make_USB_bootable.sh: applied patch from Jeroen and Dag (SF#3170685)

--- usr/share/rear/output/USB/default/85_make_USB_bootable.sh
+++ usr/share/rear/output/USB/default/85_make_USB_bootable.sh
@@ -1,10 +1,21 @@
+# Attempt to find the real USB device by trying its parent
+# Return a proper short device name using udev
+TEMP_USB_DEVICE=$(dirname $(my_udevinfo -q path -n "$USB_DEVICE"))
+if [[ -d "/sys/$TEMP_USB_DEVICE" && "$TEMP_USB_DEVICE" =~ "^/block/" ]]; then
+    RAW_USB_DEVICE="/dev/$(my_udevinfo -q name -p "$TEMP_USB_DEVICE")"
+else
+    RAW_USB_DEVICE="/dev/$(my_udevinfo -q name -n "$USB_DEVICE")"
+fi
+
 # Make the USB bootable
-syslinux --stupid ${USB_DEVICE}
-ProgressStopIfError $? "Problem with syslinux --stupid ${USB_DEVICE}"
+syslinux  $USB_DEVICE
+ProgressStopIfError $? "Problem with syslinux  $USB_DEVICE"
 ProgressStep
+
 # Write the USB boot sector
-dd if=$(dirname ${ISO_ISOLINUX_BIN})/mbr.bin of=`echo ${USB_DEVICE} | sed -e 's/[0-9]$//'`
-ProgressStopIfError $? "Problem with writing the mbr.bin to `echo ${USB_DEVICE} | sed -e 's/[0-9]$//'`"
+dd if=$(dirname $ISO_ISOLINUX_BIN)/mbr.bin of=$RAW_USB_DEVICE
+ProgressStopIfError $? "Problem with writing the mbr.bin to $RAW_USB_DEVICE"
 ProgressStep
+
 # Need to flush the buffer for the USB boot sector.
 sync; sync
