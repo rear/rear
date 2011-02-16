068529dc4665dfbe2b9ca3b6b6ed6ef219368705
Schlomo Schapiro <github@schlomo.schapiro.org>
2011-02-16 20:30:51 +0000

    reduced to minimum

--- usr/share/rear/build/default/99_update_os_conf.sh
+++ usr/share/rear/build/default/99_update_os_conf.sh
@@ -1,5 +1,4 @@
 # add os/version info to os.conf in the rescue system so that we don't need to pull lsb into the rescue system
 
 echo -e "#\n# WARNING ! This information was added automatically by the $WORKFLOW workflow !!!" >> $ROOTFS_DIR$CONFIG_DIR/os.conf
-declare -p ARCH OS OS_VERSION OS_VENDOR \
-	OS_VENDOR_VERSION OS_VENDOR_ARCH >> $ROOTFS_DIR$CONFIG_DIR/os.conf 2>/dev/null
+declare -p ARCH OS OS_VERSION OS_VENDOR >> $ROOTFS_DIR$CONFIG_DIR/os.conf 2>/dev/null
