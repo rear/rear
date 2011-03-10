72f98653593bbf8bc993ce06fd44848fb9d56e56
Dag Wieers <dag@wieers.com>
2011-03-10 16:05:57 +0000

    Do not display network for NETFS operations as it could be USB instead

--- usr/share/rear/output/NETFS/default/95_copy_result_files.sh
+++ usr/share/rear/output/NETFS/default/95_copy_result_files.sh
@@ -6,19 +6,19 @@ if [ "$NETFS_PROTO" == "tape" -o "$NETFS_PROTO" == "obdr" ]; then
 	return 0
 fi
 
-test ${#RESULT_FILES[@]} -gt 0 || Error "No files to copy (RESULT_FILES is empty)" 
+test ${#RESULT_FILES[@]} -gt 0 || Error "No files to copy (RESULT_FILES is empty)"
 
 test -d "${BUILD_DIR}/netfs/${NETFS_PREFIX}" || Error "'${BUILD_DIR}/netfs/${NETFS_PREFIX}/' not a directory !"
 
-Log "Copying files '${RESULT_FILES[@]}' to network location"
+Log "Copying files '${RESULT_FILES[@]}' to $NETFS_PROTO location"
 
-ProgressStart "Copying resulting files to network location"
-cp -v "${RESULT_FILES[@]}" "${BUILD_DIR}/netfs/${NETFS_PREFIX}/" 1>&8
-ProgressStopIfError $? "Could not copy files to network location"
+ProgressStart "Copying resulting files to $NETFS_PROTO location"
+cp "${RESULT_FILES[@]}" "${BUILD_DIR}/netfs/${NETFS_PREFIX}/" 1>&8
+ProgressStopIfError $? "Could not copy files to $NETFS_PROTO location"
 echo "$VERSION_INFO" >"${BUILD_DIR}/netfs/${NETFS_PREFIX}/VERSION"
-ProgressStopIfError $? "Could not create VERSION file on network location"
-cp -v $CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt "${BUILD_DIR}/netfs/${NETFS_PREFIX}/README" 1>&8
-ProgressStopOrError $? "Could not copy usage file to network location"
+ProgressStopIfError $? "Could not create VERSION file on $NETFS_PROTO location"
+cp $CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt "${BUILD_DIR}/netfs/${NETFS_PREFIX}/README" 1>&8
+ProgressStopOrError $? "Could not copy usage file to $NETFS_PROTO location"
 if [ -e "$LOGFILE" ]; then
 	contents="$(< $LOGFILE)"
 	case $NETFS_PROTO in
