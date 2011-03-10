4470355db1d7fb4b00774f7d0d2c31651b1866f8
Dag Wieers <dag@wieers.com>
2011-03-10 15:57:32 +0000

    Check if mbr.bin is available as older syslinux versions do not have it.

--- usr/share/rear/prep/ISO/Linux-i386/33_find_isolinux.sh
+++ usr/share/rear/prep/ISO/Linux-i386/33_find_isolinux.sh
@@ -1,14 +1,17 @@
 # find isolinux.bin or abort if it cannot be found
 
 # find isolinux.bin
-if ! test -s "$ISO_ISOLINUX_BIN" ; then
+if [[ ! -s "$ISO_ISOLINUX_BIN" ]]; then
 	for file in /usr/{share,lib,libexec}/*/isolinux.bin ; do
-		if test -s "$file" ; then
+		if [[ -s "$file" ]]; then
 			ISO_ISOLINUX_BIN="$file"
 			break # for loop
 		fi
 	done
 
 fi
-test -s "$ISO_ISOLINUX_BIN" 
+[[ -s "$ISO_ISOLINUX_BIN" ]]
 ProgressStopIfError $? "Could not find 'isolinux.bin'. Maybe you have to set ISO_ISOLINUX_BIN [$ISO_ISOLINUX_BIN] or install the syslinux package ?"
+
+[[ -s "$(dirname $ISO_ISOLINUX_BIN)/mbr.bin" ]]
+ProgressStopIfError $? "Could not find 'mbr.bin' in $(dirname $ISO_ISOLINUX_BIN). Maybe syslinux version is too old ?"
