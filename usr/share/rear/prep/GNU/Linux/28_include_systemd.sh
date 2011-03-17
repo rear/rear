8a63763458baec4477321c967b73f43409ec1f56
Gratien D'haese <gratien.dhaese@gmail.com>
2011-03-17 15:12:08 +0000

    forgot to include the prep script for systemd

---
+++ usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
@@ -0,0 +1,9 @@
+# Fedora 15 is using systemd as init mechanism
+
+if ps ax | grep -q systemd ; then
+	PROGS=( "${PROGS[@]}" systemd agetty systemctl systemd-notify systemd-ask-password dbus-uuidgen dbus-daemon dbus-send )
+	# cgroup stuff - not required for rear
+	#PROGS=( "${PROGS[@]}" cg_annotate cgclear cgcreate cgget cgrulesengd cgset cgdelete cgclassify cgexec )
+	COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /lib/systemd/systemd-* )
+	Log "Including systemd (init replacement) tool-set to bootstrap ReaR"
+fi
