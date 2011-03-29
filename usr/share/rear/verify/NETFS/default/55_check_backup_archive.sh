06cc10fe82ebe444c40e51494ef66da09ea51457
Dag Wieers <dag@wieers.com>
2011-03-29 17:18:34 +0000

    Make the error message somewhat more useful.

--- usr/share/rear/verify/NETFS/default/55_check_backup_archive.sh
+++ usr/share/rear/verify/NETFS/default/55_check_backup_archive.sh
@@ -6,7 +6,7 @@ if [ "$NETFS_PROTO" == "tape" -o "$NETFS_PROTO" == "obdr" ]; then
 fi
 
 if [ ! -s "$backuparchive" ]; then
-	Error "Backup archive '$displayarchive' not found !"
+	Error "Backup archive '$backuparchive' not found !"
 else
 	read size file < <(du -h "$backuparchive")
 	LogPrint "Backup archive size is $size${BACKUP_PROG_COMPRESS_SUFFIX:+ (compressed)}"
