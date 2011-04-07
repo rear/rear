a43305616bda40082355956856ea66115f3f5576
Dag Wieers <dag@wieers.com>
2011-04-07 14:17:10 +0000

    Fix an error related to smartarray ordering in disklayout.conf. Fix an error caused by smartarray being in use, test by reloading cciss driver.
    Developed by Dag Wieers and Jeroen Hoekx for the Belgian Federal Police.

--- usr/share/rear/layout/prepare/default/20_recreate_hpraid.sh
+++ usr/share/rear/layout/prepare/default/20_recreate_hpraid.sh
@@ -1,32 +1,43 @@
 # Ask to recreate HP raid before the actual restoration takes place.
 
+if ! grep -q '^cciss ' /proc/modules; then
+    return
+fi
+
 ORIG_LAYOUT_CODE=$LAYOUT_CODE
 
 LAYOUT_CODE=$VAR_DIR/layout/hpraid.sh
-: > $LAYOUT_CODE
-echo "set -e" >> $LAYOUT_CODE
+
+cat <<EOF > $LAYOUT_CODE
+set -e
+
+# Unload CCISS module to make sure nothing is using it
+if ! rmmod cciss; then
+    Error "CCISS failed to unload, something is still using it !"
+else
+    modprobe cciss
+    sleep 2
+fi
+EOF
 
 restored_controllers=()
 
-while read -u 3 type remainder ; do
-    case $type in
-        smartarray)
-            name=$(echo "$remainder" | cut -d " " -f "1")
-            read -p "To recreate HP SmartArray controller $name, type exactly YES: " -t 20 2>&1
-            if [ "$REPLY" = "YES" ] ; then
-                create_device "$name" "smartarray"
-                restored_controllers=( "${restored_controllers[@]}" $name )
-            fi
-            ;;
-        logicaldrive)
-            name=$(echo "$remainder" | cut -d " " -f "1")
-            ctrl=$(echo "$remainder" | cut -d " " -f "2" | cut -d "|" -f "1")
-            if IsInArray "$ctrl" "${restored_controllers[@]}" ; then
-                create_device "$name" "logicaldrive"
-            fi
-            ;;
-    esac
-done 3< <(grep -E "smartarray|logicaldrive" $LAYOUT_FILE)
+# Start by clearing all controllers
+while read -u 3 type name junk ; do
+    read -p "To recreate HP SmartArray controller $name, type exactly YES: " -t 20 2>&1
+    if [ "$REPLY" = "YES" ] ; then
+        create_device "$name" "smartarray"
+        restored_controllers=( "${restored_controllers[@]}" $name )
+    fi
+done 3< <(grep "^smartarray " $LAYOUT_FILE)
+
+# Now, recreate all logical drives whose controller was cleared.
+while read type name remainder junk ; do
+    ctrl=$(echo "$remainder" | cut -d " " -f1 | cut -d "|" -f1)
+    if IsInArray "$ctrl" "${restored_controllers[@]}" ; then
+        create_device "$name" "logicaldrive"
+    fi
+done < <(grep "^logicaldrive " $LAYOUT_FILE)
 
 echo "set +e" >> $LAYOUT_CODE
 
