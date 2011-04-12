b675a8ec4b0a0eec8b89e32dcad3ce155dfd0bf6
Gratien D'haese <gratien.dhaese@gmail.com>
2011-04-12 09:27:26 +0000

    remove duplicate script to save result files with DP

--- usr/share/rear/output/DP/default/95_dp_save_result_files.sh
+++ usr/share/rear/output/DP/default/95_dp_save_result_files.sh
@@ -4,40 +4,15 @@
 test ${#RESULT_FILES[@]} -gt 0 || Error "No files to copy (RESULT_FILES is empty)"
 
 ProgressStart "Saving result files with DP"
-DP_RESULT_FILES=()
+#DP_RESULT_FILES=()
 
-# decide where to put the result files for saving them with DP
-# if DP_RESULT_FILE_PATH is unset, then save the result files where they are
-# NOTE: Make sure that your DP installation will not silently skip files in /tmp !
-test -z "$DP_RESULT_FILE_PATH" && DP_RESULT_FILE_PATH="$CONFIG_DIR/DP"
+# if DP_RESULT_FILES_PATH is unset, then save the result files where they are
+test -z "$DP_RESULT_FILES_PATH" && DP_RESULT_FILES_PATH="$VAR_DIR/rescue"
 
-if ! test -d "$DP_RESULT_FILE_PATH" ; then
-	 mkdir -v -p "$DP_RESULT_FILE_PATH" 1>&8
-	 ProgressStopIfError $? "Could not create '$DP_RESULT_FILE_PATH'"
+if ! test -d "$DP_RESULT_FILES_PATH" ; then
+	 mkdir -v -p "$DP_RESULT_FILES_PATH" 1>&8
+	 ProgressStopIfError $? "Could not create '$DP_RESULT_FILES_PATH'"
 fi
 
-
-cp -r "$VAR_DIR/recovery" "$DP_RESULT_FILE_PATH"
-
-#if test "$DP_RESULT_FILE_PATH" != "/tmp" ; then
-#	cp -v  "${RESULT_FILES[@]}" "$DP_RESULT_FILE_PATH" 1>&8
-#	ProgressStopIfError $? "Could not copy result files to '$DP_RESULT_FILE_PATH'"
-#	DP_RESULT_FILES=( 
-#		$(
-#			for fname in "${RESULT_FILES[@]}" ; do 
-#				echo "$DP_RESULT_FILE_PATH/$(basename "$fname")"
-#			done
-#		 )
-#	)
-#else
-#	DP_RESULT_FILES=( "${RESULT_FILES[@]}" )
-#fi
-#
-#if test -s "$CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt" ; then
-#	cp -v $CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt "$DP_RESULT_FILE_PATH/README" 1>&8
-#	ProgressStopIfError $? "Could not copy '$CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt'"
-#	DP_RESULT_FILES=( "${DP_RESULT_FILES[@]}" "$DP_RESULT_FILE_PATH"/README )
-#fi
-
-ret=0
-ProgressStopOrError $ret "Could not save result files with dataprotector"
+cp -r "$VAR_DIR/recovery" "$DP_RESULT_FILES_PATH"
+ProgressStopOrError $? "Could not save result files with dataprotector"
