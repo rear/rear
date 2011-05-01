63b5abc23ae4c989988900f8be355e0b07c56898
Schlomo Schapiro <github@schlomo.schapiro.org>
2011-05-01 21:47:19 +0000

    don't take systemd unless it is there

--- usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
+++ usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
@@ -1,6 +1,6 @@
 # Fedora 15 is using systemd as init mechanism
 
-if ps ax | grep -q systemd ; then
+if ps ax | grep -v grep | grep -q systemd ; then
 	PROGS=( "${PROGS[@]}" systemd agetty systemctl systemd-notify systemd-ask-password dbus-uuidgen dbus-daemon dbus-send )
 	# cgroup stuff - not required for rear
 	#PROGS=( "${PROGS[@]}" cg_annotate cgclear cgcreate cgget cgrulesengd cgset cgdelete cgclassify cgexec )
