c081f7aa4097f33e95a72a2f4a5289160eb95880
Schlomo Schapiro <github@schlomo.schapiro.org>
2011-05-09 16:10:41 +0000

    kick udev after disabling lvm2 and md

--- usr/share/rear/recreate/GNU/Linux/09_disable_lvm2_md.sh
+++ usr/share/rear/recreate/GNU/Linux/09_disable_lvm2_md.sh
@@ -4,9 +4,13 @@ type -p lvm >/dev/null || return 0
 
 # just in case, disable LVM and MD
 
-lvm vgchange -a n -v 1>&8
+lvm vgchange -a n -v 1>&2 8>&- 7>&-
 
 while read device junk ; do
 	mdadm --stop /dev/$device 1>&2
 	ProgressStopIfError $? "Could not stop RAID device '$device' !"
 done < <(grep active /proc/mdstat)
+
+# kick udev to deal with the changed devices
+my_udevtrigger
+my_udevsettle
