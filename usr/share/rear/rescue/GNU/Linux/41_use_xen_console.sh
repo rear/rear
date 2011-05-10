f389d702225f94f6f026f93b2e423a9db1fc14e0
Schlomo Schapiro <github@schlomo.schapiro.org>
2011-05-10 18:26:44 +0000

    - older distro do not support autologin and mingetty does not support   --version or other means of finding the version (and I am against   using strings or other hacks). - autologin will therefore be limited to systems that do NOT use old   init but rely on upstart or systemd instead. Probably these fairly new   systems will also sport a mingetty version with autologin support   (found on SLES10)

--- usr/share/rear/rescue/GNU/Linux/41_use_xen_console.sh
+++ usr/share/rear/rescue/GNU/Linux/41_use_xen_console.sh
@@ -2,7 +2,7 @@
 
 if [ -c /dev/xvc0 ] ; then
 	cat <<-EOF >>$ROOTFS_DIR/etc/inittab
-		co:2345:respawn:/bin/mingetty --noclear --autologin root xvc0
+		co:2345:respawn:/bin/mingetty --noclear xvc0
 	EOF
 	Log "XEN PV console support enabled"
 fi
