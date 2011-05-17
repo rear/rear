e40f3af4101a771c6bc95467e92b143bb95d0e15
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2011-05-17 14:54:26 +0000

    Simplify checklayout testing of files.

--- usr/share/rear/layout/compare/default/51_compare_files.sh
+++ usr/share/rear/layout/compare/default/51_compare_files.sh
@@ -1,21 +1,7 @@
 # Compare files that could have an impact on the rescue image
-for config in "${CHECK_CONFIG_FILES[@]}" ; do
-    orig_value=$(cat $VAR_DIR/layout/config/$(basename "$config").md5sum)
-    if [ -z "$orig_value" ] ; then
-        LogPrint "File $config has changed."
+if [ -e $VAR_DIR/layout/config/files.md5sum ] ; then
+    if ! md5sum --check $VAR_DIR/layout/config/files.md5sum >&2; then
+        LogPrint "Configuration files have changed."
         EXIT_CODE=1
     fi
-
-    if [ -e "$config" ] ; then
-        new_value=$(md5sum "$config")
-        if [ "$new_value" != "$orig_value" ] ; then
-            LogPrint "File $config has changed."
-            EXIT_CODE=1
-        fi
-    else
-        if [ "$orig_value" != "0" ] ; then
-            LogPrint "File $config has changed."
-            EXIT_CODE=1
-        fi
-    fi
-done
+fi
--- usr/share/rear/layout/save/default/60_snapshot_files.sh
+++ usr/share/rear/layout/save/default/60_snapshot_files.sh
@@ -3,11 +3,12 @@ if [ "$WORKFLOW" = "checklayout" ] ; then
     return 0
 fi
 
+: > $VAR_DIR/layout/config/files.md5sum
+
 for config in "${CHECK_CONFIG_FILES[@]}" ; do
     if [ -e "$config" ] ; then
-        md5sum "$config" > $VAR_DIR/layout/config/$(basename "$config").md5sum
+        echo "$(md5sum "$config")" >> $VAR_DIR/layout/config/files.md5sum
     else
-        echo 0 > $VAR_DIR/layout/config/$(basename "$config").md5sum
-        continue
+        echo "$(echo 0 | md5sum | cut -d " " -f "1")  $config" >> $VAR_DIR/layout/config/files.md5sum
     fi
 done
