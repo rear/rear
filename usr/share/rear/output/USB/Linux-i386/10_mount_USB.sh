81c6bd9411966397fca09639f92b1fd632b50879
Dag Wieers <dag@wieers.com>
2011-05-26 18:45:11 +0000

    Merge:
     - Make use of StopIfError

--- usr/share/rear/output/USB/Linux-i386/10_mount_USB.sh
+++ usr/share/rear/output/USB/Linux-i386/10_mount_USB.sh
@@ -1,18 +1,20 @@
 # create mount point
-if [ ! -d "$BUILD_DIR/usbfs" ]; then
-	mkdir -p "$BUILD_DIR/usbfs" || Error "Could not mkdir '$BUILD_DIR/usbfs'"
+if [[ ! -d "$BUILD_DIR/usbfs" ]]; then
+	mkdir -p "$BUILD_DIR/usbfs"
+	StopIfError "Could not mkdir '$BUILD_DIR/usbfs'"
 fi
 
 # if a mount command is given, use that instead
 if test "$USB_MOUNTCMD" ; then
 	Log "Mounting with '$USB_MOUNTCMD $BUILD_DIR/usbfs'"
-	$USB_MOUNTCMD "$BUILD_DIR/usbfs" 1>&2 || \
-		Error "Your USB mount command '$USB_MOUNTCMD' failed."
+	$USB_MOUNTCMD "$BUILD_DIR/usbfs" 1>&2
+	StopIfError "Your USB mount command '$USB_MOUNTCMD' failed."
 else
-	[ "$USB_DEVICE" ] || Error $? "USB device (\$USB_DEVICE) is not set."
+	[[ "$USB_DEVICE" ]]
+	StopIfError "USB device (\$USB_DEVICE) is not set."
 	Log "Running 'mount $USB_DEVICE $BUILD_DIR/usbfs'"
-	mount "$USB_DEVICE" "$BUILD_DIR/usbfs" 1>&2 || \
-		Error "Mounting '$USB_DEVICE' '$BUILD_DIR/usbfs' failed."
+	mount "$USB_DEVICE" "$BUILD_DIR/usbfs" 1>&2
+	StopIfError "Mounting '$USB_DEVICE' '$BUILD_DIR/usbfs' failed."
 fi
 
 AddExitTask "umount -fv '$BUILD_DIR/usbfs' 1>&2"
