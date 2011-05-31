ff34105ebf675cef290c05bd33603418d50d2ec4
Dag Wieers <dag@wieers.com>
2011-05-31 16:31:05 +0000

    Improve BEXTRACT_DEVICE logic based on TAPE_DEVICE not ISO_URL

--- usr/share/rear/prep/BACULA/default/03_extract_device.sh
+++ usr/share/rear/prep/BACULA/default/03_extract_device.sh
@@ -1,19 +1,15 @@
 # In case we have bacula, we could use the device from the bacula configuration
 
-# We construct the ISO_URL based on the Bacula device name in the bacula
+# We construct the TAPE_DEVICE based on the Bacula device name in the bacula
 # configuration
 
-if [[ -z "$ISO_URL" ]]; then
-    if [[ -z "$BEXTRACT_DEVICE" ]]; then
-        Error "Configuration is missing ISO_URL or BEXTRACT_DEVICE"
+if [[ -z "$TAPE_DEVICE" ]]; then
+    if [[ "$BEXTRACT_DEVICE" ]]; then
+        TAPE_DEVICE="$(echo cap | btape $BEXTRACT_DEVICE | awk '/^Device name/ { print $3 }')"
+
+        [[ "$TAPE_DEVICE" ]]
+        StopIfError "Either tape device $BEXTRACT_DEVICE is missing, or it has no tape inserted."
     fi
-    tape_device="$(echo cap | btape $BEXTRACT_DEVICE | awk '/^Device name/ { print $3 }')"
-    if [[ -z "$tape_device" ]]; then
-        Error "Either tape device $BEXTRACT_DEVICE is missing, or it has no tape inserted."
-    fi
-    ISO_URL=obdr://${tape_device//\/st/\/nst}
 fi
 
-if [[ -z "$TAPE_DEVICE" ]]; then
-    TAPE_DEVICE=$tape_device
-fi
+TAPE_DEVICE=${TAPE_DEVICE//\/st/\/nst}
