47cbabc944fa8de4f10616f15258c2c65c6b54dd
Dag Wieers <dag@wieers.com>
2011-05-31 16:28:43 +0000

    Make sg3_utils and mt required for OBDR mode

--- usr/share/rear/prep/TAPE/default/40_check_tape_drive.sh
+++ usr/share/rear/prep/TAPE/default/40_check_tape_drive.sh
@@ -1,19 +1,26 @@
 # Check tape drive
-PROGS=( "${PROGS[@]}" mt )
+REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" mt )
 
-# test for cciss driver and include the necessary tools
+# Test for cciss driver and include the necessary tools
 if grep -q '^cciss ' /proc/modules; then
-	PROGS=( "${PROGS[@]}" "${PROGS_OBDR[@]}" )
+    PROGS=( "${PROGS[@]}" "${PROGS_OBDR[@]}" )
+    REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" "${REQUIRED_PROGS_OBDR[@]}" )
 fi
 
-test "${TAPE_DEVICE}"
-ProgressStopIfError $? "No tape device defined: ${TAPE_DEVICE}"
+# Is a tape device provided
+[[ "${TAPE_DEVICE}" ]]
+ProgressStopIfError $? "No tape device (TAPE_DEVICE) defined."
 ProgressStep
 
-mt -f "${TAPE_DEVICE}" status | tee "$TMP_DIR/tape_status" 1>&2
-ProgressStopIfError $PIPESTATUS "Problem with reading tape device ${TAPE_DEVICE}"
+# Write out tape status
+mt -f "${TAPE_DEVICE}" status >"$TMP_DIR/tape_status" 1>&2
+ProgressStopIfError $PIPESTATUS "Problem with reading tape device '${TAPE_DEVICE}'."
 ProgressStep
 
-grep -q WR_PROT "$TMP_DIR/tape_status" && \
-	ProgressStopIfError 1 "Tape is write protected in device ${TAPE_DEVICE}"
+# Log tape status
+cat $TMP_DIR/tape_status
+
+# Check if tape is not write protected
+! grep -q WR_PROT "$TMP_DIR/tape_status"
+ProgressStopIfError $? "Tape in device '${TAPE_DEVICE}' is write protected."
 ProgressStep
