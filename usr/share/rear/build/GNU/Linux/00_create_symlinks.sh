bf8bc1dfeeb0e9533d6cb0a32e2aa4f8dfaed3c7
Dag Wieers <dag@wieers.com>
2011-06-06 11:52:00 +0000

    Use $v for default verbosity (is this going to be a standard practice ?)

--- usr/share/rear/build/GNU/Linux/00_create_symlinks.sh
+++ usr/share/rear/build/GNU/Linux/00_create_symlinks.sh
@@ -19,17 +19,18 @@
 #
 
 
-ln -sfv bin/init $ROOTFS_DIR/init >&2
-ln -sfv bin $ROOTFS_DIR/sbin >&2
-ln -sfv bash $ROOTFS_DIR/bin/sh >&2
-ln -sfv vi $ROOTFS_DIR/bin/vim >&2
-ln -sfv true $ROOTFS_DIR/bin/pam_console_apply >&2 # RH/Fedora with udev needs this
-ln -sfv ../bin $ROOTFS_DIR/usr/bin >&2
-ln -sfv ../bin $ROOTFS_DIR/usr/sbin >&2
-ln -sfv /proc/self/mounts $ROOTFS_DIR/etc/mtab >&2
+ln -sf $v bin/init $ROOTFS_DIR/init >&2
+ln -sf $v bin $ROOTFS_DIR/sbin >&2
+ln -sf $v bash $ROOTFS_DIR/bin/sh >&2
+ln -sf $v vi $ROOTFS_DIR/bin/vim >&2
+ln -sf $v true $ROOTFS_DIR/bin/pam_console_apply >&2 # RH/Fedora with udev needs this
+ln -sf $v ../bin $ROOTFS_DIR/usr/bin >&2
+ln -sf $v ../bin $ROOTFS_DIR/usr/sbin >&2
+ln -sf $v /proc/self/mounts $ROOTFS_DIR/etc/mtab >&2
 test -d $ROOTFS_DIR/etc/sysconfig/network-scripts && ln -sfv /bin/true $ROOTFS_DIR/etc/sysconfig/network-scripts/net.hotplug >&2
 
+Log "Create LVM binary symlinks"
 lvmbins="lvchange lvconvert lvcreate lvdisplay lvextend lvmchange lvmdiskscan lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvresize pvck pvcreate pvdata pvdisplay pvmove pvremove pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit"
 for bin in $lvmbins; do
-    ln -sfv lvm $ROOTFS_DIR/bin/$bin >&2
+    ln -sf $v lvm $ROOTFS_DIR/bin/$bin >&2
 done
