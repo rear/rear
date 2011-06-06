6d32e9f5c40919ea5c87d714a1a822b19cfff8b3
Gratien D'haese <gratien.dhaese@gmail.com>
2011-06-06 13:49:08 +0000

    StopOrError function does not exist - replaced by StopIfError

--- usr/share/rear/output/NETFS/default/95_copy_result_files.sh
+++ usr/share/rear/output/NETFS/default/95_copy_result_files.sh
@@ -19,7 +19,7 @@ echo "$VERSION_INFO" >"${BUILD_DIR}/netfs/${NETFS_PREFIX}/VERSION"
 StopIfError "Could not create VERSION file on $NETFS_PROTO location"
 
 cp -v $CONFIG_DIR/templates/RESULT_usage_$OUTPUT.txt "${BUILD_DIR}/netfs/${NETFS_PREFIX}/README" 1>&8
-StopOrError "Could not copy usage file to $NETFS_PROTO location"
+StopIfError "Could not copy usage file to $NETFS_PROTO location"
 
 cat "$LOGFILE" >"${BUILD_DIR}/netfs/${NETFS_PREFIX}/rear.log"
 StopIfError "Could not copy $LOGFILE to $NETFS_PROTO location"
