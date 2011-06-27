6f0e619c5442fe8567f05689832706f62cf26562
Dag Wieers <dag@wieers.com>
2011-06-27 07:16:00 +0000

    Attempt to improve message when layout recreate failed

--- usr/share/rear/layout/recreate/default/20_run_script.sh
+++ usr/share/rear/layout/recreate/default/20_run_script.sh
@@ -1,29 +1,31 @@
 # Run the actual script
 
 RESTORE_OK=
-while [ -z "$RESTORE_OK" ] ; do
+while [[ -z "$RESTORE_OK" ]]; do
     (
         . $LAYOUT_CODE
     )
 
-    if [ $? -eq 0 ] ; then
+    if (( $? -eq 0 )); then
         RESTORE_OK=y
     else
-        LogPrint "An error has been detected during restore. See $LOGFILE for details."
-        LogPrint "You can fix the error in $LAYOUT_CODE and Retry or choose Abort."
-        LogPrint "Only code that failed will be rerun when choosing Retry."
-        
-        select choice in "Retry" "Abort" ; do
-            if [ "$choice" = "Retry" ] || [ "$choice" = "Abort" ] ; then
+        LogPrint "An error occured during restore. See $LOGFILE for details."
+        LogPrint
+        LogPrint "You can either:"
+        LogPrint " - fix the error in $LAYOUT_CODE on another terminal and Retry here"
+        LogPrint "   (only code-snippets that failed will be rerun when choosing Retry)"
+        LogPrint " - choose Abort and fix the error in $LAYOUT_FILE and rerun 'rear recover'"
+
+        select choice in "Retry" "Abort"; do
+            if [[ "$choice" == "Retry" || "$choice" == "Abort" ]]; then
                 break;
             fi
         done 2>&1
-        
-        if [ "$choice" = "Abort" ] ; then
+
+        if [[ "$choice" == "Abort" ]]; then
             abort_recreate
-        
+
             Error "There was an error restoring the system layout. See $LOGFILE for details."
         fi
-        
     fi
 done
