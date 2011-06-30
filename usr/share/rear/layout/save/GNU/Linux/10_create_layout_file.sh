3e3fa2d0becd25256814c6f69ca770153b1e0fc0
Dag Wieers <dag@wieers.com>
2011-06-30 09:45:40 +0000

    Remove trailing dot, as the other LogPrint's don't

--- usr/share/rear/layout/save/GNU/Linux/10_create_layout_file.sh
+++ usr/share/rear/layout/save/GNU/Linux/10_create_layout_file.sh
@@ -1,6 +1,6 @@
 # Create the layout file
 
-LogPrint "Creating disk layout."
+LogPrint "Creating disk layout"
 Log "Preparing layout directory."
 mkdir -p $v $VAR_DIR/layout >&2
 mkdir -p $v $VAR_DIR/recovery >&2
@@ -9,7 +9,7 @@ mkdir -p $v $VAR_DIR/layout/config >&2
 DISKLAYOUT_FILE=${DISKLAYOUT_FILE:-$VAR_DIR/layout/disklayout.conf}
 
 if [ -e "$DISKLAYOUT_FILE" ] ; then
-    Log "Removing old layout file"
+    Log "Removing old layout file."
 fi
 : > $DISKLAYOUT_FILE
 
