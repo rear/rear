61890386e1e2376beb83d62aead8b801bee3a21a
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2011-07-06 09:57:39 +0000

    Fix mistakes in enforcing of usb output.
    We noticed during testing that it had failed, but forgot to fix it.

--- usr/share/rear/build/USB/default/80_enforce_usb_output.sh
+++ usr/share/rear/build/USB/default/80_enforce_usb_output.sh
@@ -1,12 +1,12 @@
 # Make sure we use the correct OUTPUT in local.conf
 
-real_output=$(source $ROOTFS_DIR/$ETC_DIR/local.conf; echo $OUTPUT)
+real_output=$(source $ROOTFS_DIR/$CONFIG_DIR/local.conf; echo $OUTPUT)
 
-if [[ "$real_output" != "USB" ]]; then
+if [[ "$real_output" == "USB" ]]; then
     return
 fi
 
-cat <<EOF >>$ROOTFS_DIR/$ETC_DIR/local.conf
+cat <<EOF >>$ROOTFS_DIR/$CONFIG_DIR/local.conf
 
 # Added by udev workflow
 OUTPUT=USB
