ca65b794ff9dffe800c15f2383ba9787c6f7d2ff
Gratien D'haese <gratien.dhaese@gmail.com>
2011-09-05 07:55:00 +0000

    add grub2-mkrelpath to please grub, but is not absolute necessary (exec is used to created paths for PXE)

--- usr/share/rear/prep/GNU/Linux/30_include_grub_tools.sh
+++ usr/share/rear/prep/GNU/Linux/30_include_grub_tools.sh
@@ -8,8 +8,8 @@ elif has_binary grub2-probe; then
 fi
 
 PROGS=( "${PROGS[@]}"
-grub-install grub-mkdevicemap grub-probe grub-set-default grub-mkconfig grub-reboot grub-setup grub-mkimage
-grub2-install grub2-mkdevicemap grub2-probe grub2-set-default grub2-mkconfig grub2-reboot grub2-setup grub2-mkimage
+grub-install grub-mkdevicemap grub-probe grub-set-default grub-mkconfig grub-reboot grub-setup grub-mkimage grub-mkrelpath
+grub2-install grub2-mkdevicemap grub2-probe grub2-set-default grub2-mkconfig grub2-reboot grub2-setup grub2-mkimage grub2-mkrelpath
 )
 
 COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/default/grub /etc/grub.d/* /etc/grub*.cfg /boot/grub* /usr/lib/grub* )
