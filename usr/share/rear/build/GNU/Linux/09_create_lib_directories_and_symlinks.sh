57530a4f038254e12f0aceb40ff2645fe88adf37
Dag Wieers <dag@wieers.com>
2011-12-02 13:54:19 +0000

    - Improved logic in case /lib* lists files

--- usr/share/rear/build/GNU/Linux/09_create_lib_directories_and_symlinks.sh
+++ usr/share/rear/build/GNU/Linux/09_create_lib_directories_and_symlinks.sh
@@ -1,8 +1,8 @@
 # Create lib directories
 for libdir in /lib* ; do
 	# $libdir always contains a leading / !
-	if [ -L $libdir ] ; then
-		[ ! -d $ROOTFS_DIR$libdir ]
+	if [[ -L $libdir ]]; then
+		[[ ! -d $ROOTFS_DIR$libdir ]]
 		BugIfError "Cannot create symlink $libdir instead of directory"
 
 		linktarget=$(readlink -f $libdir)
@@ -13,15 +13,18 @@ for libdir in /lib* ; do
 
 		ln -s $v "$linktarget" $ROOTFS_DIR$libdir >&2
 		StopIfError "Could not create symlink '$ROOTFS_DIR$libdir'"
-	elif [ -d $libdir ] ; then
-		[ ! -L $ROOTFS_DIR$libdir ]
+	elif [[ -d $libdir ]]; then
+		[[ ! -L $ROOTFS_DIR$libdir ]]
 		BugIfError "Cannot create directory $libdir instead of symlink"
 
 		mkdir -p $v $ROOTFS_DIR$libdir >&2
 		StopIfError "Could not create directory '$ROOTFS_DIR$libdir'"
+	elif [[ -f $libdir ]]; then
+		Debug "WARNING: unexpected file '$libdir' found, ignoring"
 	else
-		BugError "I never should get here."
+		BugError "Unknown file '$libdir' found, uncertain what to do"
 	fi
+
 	# add relative symlinks under /usr so that a later copy into those symlinks will put the files into the
 	# rescue system and not into the origin system.
 	ln -s $v ..$libdir $ROOTFS_DIR/usr$libdir >&2
