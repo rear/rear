c593412bb31690573268a6abd80a96ad06e99be6
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2012-02-23 15:37:49 +0100

    Implement replacing of disk names in system files for the layout code.

---
+++ usr/share/rear/finalize/GNU/Linux/15_migrate_disk_devices_layout.sh
@@ -0,0 +1,38 @@
+### Apply chosen migrations to files on the disk.
+
+if [[ ! "$USE_LAYOUT" ]] || [[ ! -s "$MAPPING_FILE" ]] ; then
+    return
+fi
+
+### reuse the script in layout/prepare/default/32_apply_mappings.sh
+pushd /mnt/local >&8
+# the funny [] around the first letter make sure that shopt -s nullglob removes this file from the list if it does not exist
+# the files without a [] are mandatory, like fstab
+for file in     [b]oot/{grub.conf,menu.lst,device.map} [e]tc/grub.* [b]oot/grub/{grub.conf,menu.lst,device.map} \
+                [e]tc/sysconfig/grub [e]tc/sysconfig/bootloader \
+                [e]tc/lilo.conf \
+                [e]tc/mtab etc/fstab \
+                [e]tc/mtools.conf \
+                [e]tc/smartd.conf [e]tc/sysconfig/smartmontools \
+                [e]tc/sysconfig/rawdevices \
+                [e]tc/security/pam_mount.conf.xml
+        do
+
+        # sed -i bails on symlinks, so we follow the symlink and patch the result
+        # on dead links we warn and skip them
+        # TODO: maybe we must put this into a chroot so that absolute symlinks will work correctly
+        if [[ -L "$file" ]] ; then
+                if linkdest="$(readlink -f "$file")" ; then
+                        LogPrint "Patching '$linkdest' instead of '$file'"
+                        file="$linkdest"
+                else
+                        LogPrint "Not patching dead link '$file'"
+                        continue
+                fi
+        fi
+
+        tmp_layout=$LAYOUT_FILE
+        LAYOUT_FILE="$file"
+        source $SHARE_DIR/layout/prepare/default/32_apply_mappings.sh
+        LAYOUT_FILE=$tmp_layout
+done
