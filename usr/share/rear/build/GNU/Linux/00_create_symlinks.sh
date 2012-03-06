80f6b33730d5d78fc7b3d16379fa1575c418e572
Gratien D'haese <gratien.dhaese@gmail.com>
2012-03-06 10:07:14 +0100

    udevd-event[xxxx]: run_program: exec of program /sbin/hwup failed

--- usr/share/rear/build/GNU/Linux/00_create_symlinks.sh
+++ usr/share/rear/build/GNU/Linux/00_create_symlinks.sh
@@ -31,6 +31,7 @@ ln -sf $v /proc/self/mounts $ROOTFS_DIR/etc/mtab >&2
 if [[ -d $ROOTFS_DIR/etc/sysconfig/network-scripts ]]; then
     ln -sf $v /bin/true $ROOTFS_DIR/etc/sysconfig/network-scripts/net.hotplug >&2
 fi
+[[ -x /sbin/hwup ]] && ln -sf $v true $ROOTFS_DIR/sbin/hwup >&2 # SUSE with udev needs this
 
 Log "Create LVM binary symlinks"
 lvmbins="lvchange lvconvert lvcreate lvdisplay lvextend lvmchange lvmdiskscan lvmsadc lvmsar lvreduce lvremove lvrename lvresize lvs lvscan pvchange pvresize pvck pvcreate pvdata pvdisplay pvmove pvremove pvs pvscan vgcfgbackup vgcfgrestore vgchange vgck vgconvert vgcreate vgdisplay vgexport vgextend vgimport vgmerge vgmknodes vgreduce vgremove vgrename vgs vgscan vgsplit"
