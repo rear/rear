2d6239fce45a6f75403460711312fa69701788b4
Gratien D'haese <gratien.dhaese@gmail.com>
2012-03-09 10:58:13 +0100

    bring SVN trunk in sync with github:     - rear the rescue.conf file in the system-setup too (as it now contains DHCLIENT variables)     - move the creation of rootfs/etc/rear to the prep phase and remove rescue.conf (which doesn't exist, but to be 100% sure)     - we will always to try to copy dhclient executables so we disable the USE_DHCLIENT being empty

--- usr/share/rear/prep/GNU/Linux/21_include_dhclient.sh
+++ usr/share/rear/prep/GNU/Linux/21_include_dhclient.sh
@@ -28,7 +28,7 @@ dhcp_interfaces_active() {
 		# if we find a dhcp client being active we automatically
 		# include DHCP CLIENT support in Rear
 		Log "Running DHCP client found, enabling USE_DHCLIENT"
-		USE_DHCLIENT=yes
+		USE_DHCLIENT=y
 	fi
 }
 
@@ -57,12 +57,13 @@ dhcp_interfaces_active		# check if one is running (if found define USE_DHCLIENT=
 # suppose that we filled in variable DHCLIENT_BIN in the /etc/rear/local.conf file
 # but forgot to define USE_DHCLIENT=y in the /etc/rear/local.conf file
 # then we should assume that we meant USE_DHCLIENT=y instead of USE_DHCLIENT=
-[ ! -z "$DHCLIENT_BIN" ] && USE_DHCLIENT=yes
-[ ! -z "$DHCLIENT6_BIN" ] && USE_DHCLIENT=yes
+[ ! -z "$DHCLIENT_BIN" ] && USE_DHCLIENT=y
+[ ! -z "$DHCLIENT6_BIN" ] && USE_DHCLIENT=y
 
 # check if we defined in our site/local.conf file the variable USE_DHCLIENT
 # Or, it was defined by function dhcp_interfaces_active if DHCP client was currently running
-[ -z "$USE_DHCLIENT" ] && return	# empty string means no dhcp client support required
+# We will always copy dhclient executables as dhcp could be activated at boot time
+#[ -z "$USE_DHCLIENT" ] && return	# empty string means no dhcp client support required
 
 # Ok, at this point we want DHCP client support to be included in the rescue
 # image of Rear. Check which clients are available.
@@ -88,11 +89,19 @@ COPY_AS_IS=( "${COPY_AS_IS[@]}" "/etc/localtime" )
 PROGS=( "${PROGS[@]}" arping ipcalc usleep )
 
 # At this point we want DHCP client support and found a binary
-# check if binary was defined in /etc/rear/local.conf; if not append it
+# check if binary was defined in /etc/rear/local.conf; if not append it to rescue.conf
 # as we need this variable at recovery time.
+grep -q "^USE_DHCLIENT=" "$CONFIG_DIR/local.conf"
+if [ $? -eq 1 ]; then
+	cat - <<EOF >> "$ROOTFS_DIR$CONFIG_DIR/rescue.conf"
+# line below was automatically added by 21_include_dhclient.sh
+USE_DHCLIENT=$USE_DHCLIENT
+EOF
+fi
+
 grep -q "^DHCLIENT_BIN=" "$CONFIG_DIR/local.conf"
 if [ $? -eq 1 ]; then
-	cat - <<EOF >> "$CONFIG_DIR/local.conf"
+	cat - <<EOF >> "$ROOTFS_DIR$CONFIG_DIR/rescue.conf"
 # line below was automatically added by 21_include_dhclient.sh
 DHCLIENT_BIN=$DHCLIENT_BIN
 EOF
@@ -100,7 +109,7 @@ fi
 
 grep -q "^DHCLIENT6_BIN=" "$CONFIG_DIR/local.conf"
 if [ $? -eq 1 ]; then
-	cat - <<EOF >> "$CONFIG_DIR/local.conf"
+	cat - <<EOF >> "$ROOTFS_DIR$CONFIG_DIR/rescue.conf"
 # line below was automatically added by 21_include_dhclient.sh
 DHCLIENT6_BIN=$DHCLIENT6_BIN
 EOF
