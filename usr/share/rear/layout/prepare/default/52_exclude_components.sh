fa888f1e84d44625b74bcfee75bd7ba9588987cb
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2012-03-29 17:22:02 +0200

    layout: multipath devices need their underlying device present. Assume it is and mark it as done.

--- usr/share/rear/layout/prepare/default/52_exclude_components.sh
+++ usr/share/rear/layout/prepare/default/52_exclude_components.sh
@@ -5,3 +5,20 @@ for component in "${EXCLUDE_RECREATE[@]}" ; do
     mark_as_done "$component"
     mark_tree_as_done "$component"
 done
+
+### Make sure we have all dependencies for multipath devices in place
+while read multipath device slaves junk ; do
+    local -a devices=()
+
+    OIFS=$IFS
+    IFS=","
+    for slave in $slaves ; do
+        devices=( "${devices[@]}" "$slave" )
+    done
+    IFS=$OIFS
+
+    for slave in "${devices[@]}" ; do
+        add_component "$slave"
+        mark_as_done "$slave"
+    done
+done < <(grep ^multipath $LAYOUT_FILE)
