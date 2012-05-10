b7a522bd37aee1126386873eabc3cd3484575cf3
Dag Wieers <dag@wieers.com>
2012-05-10 21:18:17 +0200

    Added fix for DAT320 tape drive, closes #35 (Kai-Olaf Pieth)

--- usr/share/rear/prep/OBDR/default/70_write_OBDR_header.sh
+++ usr/share/rear/prep/OBDR/default/70_write_OBDR_header.sh
@@ -1,4 +1,5 @@
 # write the OBDR header to tape
+
 PROGS=( "${PROGS[@]}" dd )
 
 LogPrint "Writing OBDR header to tape in drive '$TAPE_DEVICE'"
@@ -19,3 +20,7 @@ StopIfError "Could not set block size on tape device '$TAPE_DEVICE'"
 ### Make sure we set a tape label and total padding of 20 blocks of size 512
 printf 'REAR-000%10232s' ' ' | tr ' ' '\0' | dd of=$TAPE_DEVICE bs=512 count=20
 StopIfError "OBDR header could not be written to tape device '$TAPE_DEVICE'"
+
+### Make sure we jump to block 20 before writing (needed for DAT320)
+mt -f "${TAPE_DEVICE}" seek 20
+StopIfError "Could not seek to block 20 on tape device '$TAPE_DEVICE'"
