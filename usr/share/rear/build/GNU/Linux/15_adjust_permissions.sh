5c58cf6ff86665a8ec904fbd581b81f01ef1f429
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2012-05-31 12:53:44 +0200

    Sshd needs root:root on /var/empty.

--- usr/share/rear/build/GNU/Linux/15_adjust_permissions.sh
+++ usr/share/rear/build/GNU/Linux/15_adjust_permissions.sh
@@ -3,7 +3,7 @@
 pushd $ROOTFS_DIR >&8
 # SSH requires this
 chmod $v 0700 root >&2
-chmod $v 0755 var/lib/empty >&2
-chown $v -R root.root root var/lib/empty >&2
+chmod $v 0755 var/empty var/lib/empty >&2
+chown $v -R root.root root var/empty var/lib/empty >&2
 
 popd >&8
