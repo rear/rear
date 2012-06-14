d83b39c3ca609273ea86066b20aee3b98d24fb60
Gratien D'haese <gratien.dhaese@gmail.com>
2012-06-14 16:54:03 +0200

    This fix issue #87 (NBU and xinetd issue)

--- usr/share/rear/rescue/NBU/default/45_prepare_xinetd.sh
+++ usr/share/rear/rescue/NBU/default/45_prepare_xinetd.sh
@@ -7,5 +7,11 @@ COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/xinetd.conf /etc/xinetd.d/bpcd /etc/xinetd.
 cat >$ROOTFS_DIR/etc/scripts/system-setup.d/90-xinetd.sh <<-EOF
 echo "Starting a minimal xinetd daemon ..."
 xinetd
+if [ ! -f /etc/xinetd.d/vnetd ]; then
+        /usr/openv/netbackup/bin/vnetd -standalone
+fi
+if [ ! -f /etc/xinetd.d/bpcd ]; then
+        /usr/openv/netbackup/bin/bpcd -standalone
+fi
 EOF
 chmod $v +x $ROOTFS_DIR/etc/scripts/system-setup.d/90-xinetd.sh >&2
