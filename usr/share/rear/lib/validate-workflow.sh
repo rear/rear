0fd4ff9b3b159ec428202e15822866f68d4b6715
Dag Wieërs <dag@wieers.com>
2012-07-12 20:00:25 +0200

    Improve the text for the validation process

--- usr/share/rear/lib/validate-workflow.sh
+++ usr/share/rear/lib/validate-workflow.sh
@@ -26,32 +26,30 @@ WORKFLOW_validate () {
 
 	Print "
 Thank you for your time and effort to try out $PRODUCT and your
-willingness to report about $PRODUCT in your environment back to us.
+willingness to report about $PRODUCT in your environment back
+to us. This kind of user support is greatly appreciated and helps a lot
+to improve $PRODUCT.
 
-This kind of user support is greatly appreciated and helps a lot to
-improve $PRODUCT.
-
---- Schlomo Schapiro
+--- The $PRODUCT development team
 
 	"
 
 	read -e -p "Press ENTER to continue ..." 2>&1
 
 	Print "
-The purpose of this module is to assist you with informing us about
-the correct functioning of $PRODUCT (or major parts of it)
-to establish a database of validated systems and environments.
+The purpose of submitting validation info is to assist you with informing us
+about the correct functioning of $PRODUCT (or major parts of it) to
+establish a database of validated systems and environments.
 
 Since nobody can test everything it is sufficient if you submit information
 about $PRODUCT in exactly your environment and we will combine such
-reports from all users into the information published on the website
-and within the validation information that you can see at the end of
-'$0 dump'.
+reports from all users into the information published on the website and within
+the validation information that you can see at the end of '$PROGRAM dump'.
 
-Please answer the following questions and email the resulting information
-to Schlomo Schapiro <rear@schlomo.schapiro.org>. Your should include only
-information that you are willing to see published on the $PRODUCT
-website and contained within $PRODUCT.
+Please answer the following questions and email the resulting information to
+the $PRODUCT development team at <contact@relax-and-recover.org>. You
+should only include information that you are willing to see published on the
+$PRODUCT website and contained within $PRODUCT.
 
 	"
 	read -e -p "Press ENTER to continue ..." 2>&1
@@ -61,10 +59,11 @@ website and contained within $PRODUCT.
 -------------------
 Please tell us (if you are willing), who you are and how to reach you. If you
 want to remain anonymous you can put here nothing at all or only your initials
-or no email or ommit your company/organisation etc. However, the more 'personal'
-a testimony to $PRODCUT in your environment, the more credible it will be..
+or no email or omit your company/organisation etc. However, the more 'personal'
+a testimony to $PRODUCT in your environment, the more credible it
+will be...
 
-Example: Your Full Name <your.email@your.domain>, Your Company/Organisation, Country
+Example: Your Name <email-address>, Company/Organisation, Country
 "
 	read -e -p "Submitted By: " 2>&1
 	SUBMITTED_BY="$REPLY"
@@ -86,42 +85,46 @@ Example: LVM, MD, SCSI, $BACKUP, $OUTPUT, EMAIL, ...
 
 3. Comments
 -------------------
-Please let us know any comments you have about $PRODUCT in your environment
-that would help others to better use $PRODUCT or have less trouble installing
-it or maybe simply that you employ $PRODUCT in a very large data centre with
-thousands of servers and that $PRODUCT is the central component of your disaster
-recovery preparations for your data centre.
+Please let us know any comments you have about $PRODUCT in your
+environment that would help others to better use $PRODUCT or have
+less trouble installing it or maybe simply that you employ $PRODUCT
+in a very large data centre with thousands of servers and that
+$PRODUCT is the central component of your disaster recovery
+preparations for your data centre.
 
 Example: Need to install binutils, wodim and syslinux manually
-Example: Works flawless with all features out-of-the-box
-Example: ....
+Example: Works out-of-the-box flawless with all features
+Example: We modified path/to/file in order to support foo-bar better
 "
 	read -e -p "Comments: " 2>&1
 	COMMENTS="$REPLY"
 
 	Print "
 
-Thank you very much for your information. Please copy&paste the part between
-the two lines into an email to rear@schlomo.schapiro.org. Please feel free to
-add anything else you want to tell us about $PRODUCT.
+Thank you very much for your information. Please copy-and-paste the part
+between the two lines into an email to contact@relax-and-recover.org.
+Please feel free to add anything else you want to tell us about
+$PRODUCT.
 
 ----------------------------------8<------------------------------------------
-$PRODUCT Version $VERSION / $RELEASE_DATE
-Validation:	$OS_VENDOR_VERSION_ARCH
-Submitted:	$SUBMITTED_BY
-Date:		$(date +"%Y-%m-%d")
-Features:	$FEATURES
-Comment:	$COMMENTS
+Version:     $PRODUCT $VERSION / $RELEASE_DATE
+Validation:  $OS_VENDOR_VERSION_ARCH
+Submitted:   $SUBMITTED_BY
+Date:        $(date +"%Y-%m-%d")
+Features:    $FEATURES
+Comment:     $COMMENTS
 ---------------------------------->8------------------------------------------
-(you find this information also in /tmp/rear-validate.txt)
+
+You can also find this information in /tmp/rear-validate.txt !
 
 	"
-	echo "$PRODUCT Version $VERSION / $RELEASE_DATE
-Validation:	$OS_VENDOR_VERSION_ARCH
-Submitted:	$SUBMITTED_BY
-Date:		$(date +"%Y-%m-%d")
-Features:	$FEATURES
-Comment:	$COMMENTS" >/tmp/rear-validate.txt
+	cat <<EOF >/tmp/rear-validate.txt
+Version:     $PRODUCT $VERSION / $RELEASE_DATE
+Validation:  $OS_VENDOR_VERSION_ARCH
+Submitted:   $SUBMITTED_BY
+Date:        $(date +"%Y-%m-%d")
+Features:    $FEATURES
+Comment:     $COMMENTS"
+EOF
 
 }
-
