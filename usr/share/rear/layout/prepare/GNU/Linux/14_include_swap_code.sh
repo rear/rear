c3509055796eb55d01a40813d238c9a4e344b31f
Dag Wieërs <dag@wieers.com>
2012-07-24 15:19:19 +0200

    Check if mkswap has the -U option (based on version check)
    This fixes #128

--- usr/share/rear/layout/prepare/GNU/Linux/14_include_swap_code.sh
+++ usr/share/rear/layout/prepare/GNU/Linux/14_include_swap_code.sh
@@ -1,16 +1,26 @@
 # Code to recreate a swap volume
 
+# Test for features in mkswap
+# true if mkswap supports the -U option
+FEATURE_MKSWAP_UUID=
+
+mkswap_version=$(get_version "mkswap --version")
+if version_newer "$mkswap_version" 2.13.1.1; then
+    FEATURE_MKSWAP_UUID="y"
+fi
+
+
 create_swap() {
     local swap device uuid label junk
     read swap device uuid label junk < <(grep "^swap ${1#swap:} " $LAYOUT_FILE)
 
-    if [ -n "${uuid#uuid=}" ] ; then
+    if [[ "$FEATURE_MKSWAP_UUID" && -n "${uuid#uuid=}" ]] ; then
         uuid="-U ${uuid#uuid=} "
     else
         uuid=""
     fi
 
-    if [ -n "${label#label=}" ] ; then
+    if [[ -n "${label#label=}" ]] ; then
         label="-L ${label#label=} "
     else
         label=""
