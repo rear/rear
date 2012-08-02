d479945237cbddb985e32e7cf6f52db96c97b399
Gratien D'haese <gratien.dhaese@gmail.com>
2012-08-02 10:35:09 +0200

    In our skel we keep the systemd service/target files under usr/lib/systemd/system, however, OpenSUSE still uses the lib/systemd/system path. Here we make the link.

---
+++ usr/share/rear/build/SUSE_LINUX/61_link_systemd_lib.sh
@@ -0,0 +1,11 @@
+# Fedora puts systemd stuff into /usr/lib/systemd and SuSe under /lib/systemd
+pushd $ROOTFS_DIR >/dev/null
+	if [[ -d usr/lib/systemd/system ]];  then
+		if [[ ! -d lib/systemd/system ]]; then
+			ln -sf $v ../../usr/lib/systemd/system $ROOTFS_DIR/lib/systemd/system >&2
+		fi
+	else
+		Error "Missing usr/lib/systemd/system - too confused to continue"
+	fi
+popd >/dev/null
+
