637b479d1af5bd934a0185c088772d3119e18392
Dag Wieers <dag@wieers.com>
2012-09-04 11:39:34 +0200

    Include all crypto modules by default (e.g. for cifs)
    This closes #149

--- usr/share/rear/rescue/GNU/Linux/23_storage_and_network_modules.sh
+++ usr/share/rear/rescue/GNU/Linux/23_storage_and_network_modules.sh
@@ -1,30 +1,50 @@
-# keep storage drivers for later use
+# Keep certain drivers for later use
 
-# if we don't have udev then we skip this part and fall through to the old way of taking along
+# If we don't have udev then we skip this part and fall through to the old way of taking along
 # all loaded modules (needed on older Linux, e.g. RHEL4)
 
 have_udev || return 0
+
+# Include storage drivers
 STORAGE_DRIVERS=(
 	$(
-		find /lib/modules/$KERNEL_VERSION/kernel/driver[s]/{block,firewire,ide,ata,md,message,scsi,usb/storage,virtio,xen} -type f -name \*.ko\* -printf "%f\n" | \
+		find /lib/modules/$KERNEL_VERSION/kernel/drivers/{block,firewire,ide,ata,md,message,scsi,usb/storage} -type f -name '*.ko*' -printf '%f\n' | \
 		sed -e 's/^\(.*\)\.ko.*/\1/'
 		#  ^^^^- remove the .ko, faster one sed call than many basename calls or shell code
 	)
 )
 
-# keep network drivers for later use
+# Include network drivers
 NETWORK_DRIVERS=(
 	$(
-		find /lib/modules/$KERNEL_VERSION/kernel/drivers/net -type f -name \*.ko\* -printf "%f\n" | \
+		find /lib/modules/$KERNEL_VERSION/kernel/drivers/net -type f -name '*.ko*' -printf '%f\n' | \
 		sed -e 's/^\(.*\)\.ko.*/\1/'
 		#  ^^^^- remove the .ko, faster one sed call than many basename calls or shell code
 	)
 )
 
-# Also include additional drivers
+# Include crypto drivers
+CRYPTO_DRIVERS=(
+	$(
+		find /lib/modules/$KERNEL_VERSION/kernel/drivers/crypto -type f -name '*.ko*' -printf '%f\n' | \
+		sed -e 's/^\(.*\)\.ko.*/\1/'
+		#  ^^^^- remove the .ko, faster one sed call than many basename calls or shell code
+	)
+)
+
+# Include virtualization drivers
+VIRTUAL_DRIVERS=(
+	$(
+		find /lib/modules/$KERNEL_VERSION/kernel/drivers/{virtio,xen} -type f -name '*.ko*' -printf '%f\n' | \
+		sed -e 's/^\(.*\)\.ko.*/\1/'
+		#  ^^^^- remove the .ko, faster one sed call than many basename calls or shell code
+	)
+)
+
+# Include additional drivers
 EXTRA_DRIVERS=(
 	$(
-		find /lib/modules/$KERNEL_VERSION/{extra,weak-updates} -type f -name \*.ko\* -printf "%f\n" | \
+		find /lib/modules/$KERNEL_VERSION/{extra,weak-updates} -type f -name '*.ko*' -printf '%f\n' | \
 		sed -e 's/^\(.*\)\.ko.*/\1/'
 		#  ^^^^- remove the .ko, faster one sed call than many basename calls or shell code
 	)
--- usr/share/rear/rescue/GNU/Linux/24_kernel_modules.sh
+++ usr/share/rear/rescue/GNU/Linux/24_kernel_modules.sh
@@ -18,17 +18,23 @@
 #
 #
 
-# Note: The STORAGE_DRIVERS and NETWORK_DRIVERS variables are filled in in the
-# previous script !
+# Note: The various DRIVERS list variables are set in the previous script.
 
 # 1. take all kernel modules for network and storage devices
 # 2. collect running kernel modules
-MODULES=( ${MODULES[@]} ${STORAGE_DRIVERS[@]} ${NETWORK_DRIVERS[@]} ${EXTRA_DRIVERS[@]}
-$(lsmod | grep -v '^Modul' | cut -d " " -f 1)
+MODULES=(
+    ${MODULES[@]}
+    ${STORAGE_DRIVERS[@]}
+    ${NETWORK_DRIVERS[@]}
+    ${CRYPTO_DRIVERS[@]}
+    ${VIRTUAL_DRIVERS[@]}
+    ${EXTRA_DRIVERS[@]}
+    $(lsmod | grep -v '^Modul' | cut -d ' ' -f 1)
 )
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}"
-/lib/modules/$KERNEL_VERSION/modules.*
-/etc/modules*
-/etc/modprobe*
+COPY_AS_IS=(
+    "${COPY_AS_IS[@]}"
+    /lib/modules/$KERNEL_VERSION/modules.*
+    /etc/modules*
+    /etc/modprobe*
 )
