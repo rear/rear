a752499bf91470cad9923c6e92231732158b5c35
Jeroen Hoekx <jeroen.hoekx@hamok.be>
2012-09-06 12:36:06 +0200

    Update grub2 includes for newer versions.

--- usr/share/rear/prep/GNU/Linux/30_include_grub_tools.sh
+++ usr/share/rear/prep/GNU/Linux/30_include_grub_tools.sh
@@ -10,6 +10,7 @@ fi
 PROGS=( "${PROGS[@]}"
 grub-install grub-mkdevicemap grub-probe grub-set-default grub-mkconfig grub-reboot grub-setup grub-mkimage grub-mkrelpath
 grub2-install grub2-mkdevicemap grub2-probe grub2-set-default grub2-mkconfig grub2-reboot grub2-setup grub2-mkimage grub2-mkrelpath
+grub-bios-setup grub2-bios-setup
 )
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/default/grub /etc/grub.d/* /etc/grub*.cfg /boot/grub* /usr/lib/grub* )
+COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/default/grub /etc/grub.d/* /etc/grub*.cfg /boot/grub* /usr/lib/grub* /usr/share/grub* )
