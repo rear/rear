9500f61c3f10ece864f9b570d59b5046f3b6a14e
Gratien D'haese <gratien.dhaese@gmail.com>
2012-11-06 11:29:43 +0100

    changes required for systemd on Fedora18 beta

--- usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
+++ usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
@@ -1,7 +1,8 @@
 # Fedora 15 is using systemd as init mechanism
 
 if ps ax | grep -v grep | grep -q systemd ; then
-	PROGS=( "${PROGS[@]}" systemd agetty systemctl systemd-notify systemd-ask-password dbus-uuidgen dbus-daemon dbus-send )
+	PROGS=( "${PROGS[@]}" systemd agetty systemctl systemd-notify systemd-ask-password
+		systemd-udevd systemd-journald dbus-uuidgen dbus-daemon dbus-send )
 	# cgroup stuff - not required for rear
 	#PROGS=( "${PROGS[@]}" cg_annotate cgclear cgcreate cgget cgrulesengd cgset cgdelete cgclassify cgexec )
 	COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /lib/systemd/systemd-* )
