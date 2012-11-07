68f0ed534e557d39ff5a5dbf9a0eb3d79716d35a
Gratien D'haese <gratien.dhaese@gmail.com>
2012-11-07 12:38:47 +0100

    Dag has introduced filesystem detection of USB_DEVICE in 20_check_usb_layout.sh. Therefore, decided if btrfs is found as a filesystem that relabeling is only a small extra task for this script. However, default format is still ext3 if none found. Closes issue #164

--- usr/share/rear/format/USB/default/35_label_usb_disk.sh
+++ usr/share/rear/format/USB/default/35_label_usb_disk.sh
@@ -1,8 +1,22 @@
-USB_LABEL="$(e2label ${RAW_USB_DEVICE}1)"
-if [[ "$USB_LABEL" != "REAR-000" ]]; then
-	LogPrint "Setting filesystem label to REAR-000"
-	e2label ${RAW_USB_DEVICE}1 REAR-000
-	StopIfError "Could not label '${RAW_USB_DEVICE}1' with REAR-000"
-	USB_LABEL="$(e2label ${RAW_USB_DEVICE}1)"
-fi
+case "$ID_FS_TYPE" in
+
+	ext*)
+		USB_LABEL="$(e2label ${RAW_USB_DEVICE}1)"
+		if [[ "$USB_LABEL" != "REAR-000" ]]; then
+			LogPrint "Setting filesystem label to REAR-000"
+			e2label ${RAW_USB_DEVICE}1 REAR-000
+			StopIfError "Could not label '${RAW_USB_DEVICE}1' with REAR-000"
+			USB_LABEL="$(e2label ${RAW_USB_DEVICE}1)"
+		fi
+		;;
+	btrfs)
+		USB_LABEL="$(btrfs filesystem label ${RAW_USB_DEVICE}1)"
+		if [[ "$USB_LABEL" != "REAR-000" ]]; then
+			LogPrint "Setting filesystem label to REAR-000"
+			btrfs filesystem label ${RAW_USB_DEVICE}1 REAR-000
+			StopIfError "Could not label '${RAW_USB_DEVICE}1' with REAR-000"
+			USB_LABEL="$(btrfs filesystem label ${RAW_USB_DEVICE}1)"
+		fi
+		;;
+esac
 Log "Device '${RAW_USB_DEVICE}1' has label $USB_LABEL"
