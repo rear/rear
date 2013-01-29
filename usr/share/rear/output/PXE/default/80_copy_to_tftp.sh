8da21c3d42e6bcf870ca00ca2a812dfda266feb7
Gratien D'haese <gratien.dhaese@gmail.com>
2013-01-29 14:49:46 +0100

    removed the malplaced } close #191

--- usr/share/rear/output/PXE/default/80_copy_to_tftp.sh
+++ usr/share/rear/output/PXE/default/80_copy_to_tftp.sh
@@ -32,7 +32,7 @@ echo "$VERSION_INFO" >"$PXE_TFTP_LOCAL_PATH/$PXE_MESSAGE"
 
 # TODO: umount remote TFTP path
 
-LogPrint "Copied kernel+initrd ($(du -shc $KERNEL_FILE "$TMP_DIR/initrd.cgz"} | tail -n 1 | tr -s "\t " " " | cut -d " " -f 1 )) to $PXE_TFTP_PATH"
+LogPrint "Copied kernel+initrd ($(du -shc $KERNEL_FILE "$TMP_DIR/initrd.cgz" | tail -n 1 | tr -s "\t " " " | cut -d " " -f 1 )) to $PXE_TFTP_PATH"
 
 # Add to result files
 RESULT_FILES=( "${RESULT_FILES[@]}" "$PXE_TFTP_LOCAL_PATH/$PXE_KERNEL" "$PXE_TFTP_LOCAL_PATH/$PXE_INITRD" "$PXE_TFTP_LOCAL_PATH/$PXE_MESSAGE" )
