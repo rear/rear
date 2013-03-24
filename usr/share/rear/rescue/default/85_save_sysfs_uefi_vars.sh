766153e8332ea1de60008e64faf430874c73ddfc
Gratien D'haese <gratien.dhaese@gmail.com>
2013-03-24 17:02:47 +0100

    - Save the UEFI variables we need in recover mode into rescue.conf - made some corrections with finding the UEFI_BOOTLOADER (allow to define your own EFI bootloader in /etc/rear/local.conf)

--- usr/share/rear/rescue/default/85_save_sysfs_uefi_vars.sh
+++ usr/share/rear/rescue/default/85_save_sysfs_uefi_vars.sh
@@ -22,7 +22,7 @@ if [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/vars" ]; then
     done
     # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
     BootCurrent=$(grep BootCurrent $VAR_DIR/recovery/uefi-variables | cut -d: -f2 | awk '{print $1}')	# 0000
-    UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*/data)
+    my_UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*/data)
 
 elif [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/efivars" ]; then
 
@@ -36,16 +36,41 @@ elif [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/efivars" ]; then
     done
     # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
     BootCurrent=$(grep BootCurrent $VAR_DIR/recovery/uefi-variables | cut -d: -f2 | awk '{print $1}')	# 0000
-    UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*)
+    my_UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*)
 
 else
     BugError "UEFI Variables directory $SYSFS_DIR_EFI_VARS is not what I expected"
 fi
 
-# the UEFI_BOOTLOADER contains path in DOS format
-UEFI_BOOTLOADER="/boot/efi"$(echo "$UEFI_BOOTLOADER" | sed -e 's;\\;/;g')
-if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
-    UEFI_BOOTLOADER=$(find /boot/efi -name "grub*.efi" | tail -1)
+# Perhaps we defined UEFI_BOOTLOADER in /etc/rear/local.conf - check this now
+if [[ ! -z "${UEFI_BOOTLOADER}" ]]; then
+    # right, variable is not empty, but is it a file?
+    [[ ! -f ${UEFI_BOOTLOADER} ]] && Error "Cannot find a proper UEFI_BOOTLOADER ($UEFI_BOOTLOADER). 
+Please define it in /etc/rear/local.conf (e.g. UEFI_BOOTLOADER=/boot/efi/EFI/fedora/grubx64.efi)"
+    
+else
+    # the UEFI_BOOTLOADER contains path in DOS format
+    UEFI_BOOTLOADER="/boot/efi"$(echo "$my_UEFI_BOOTLOADER" | sed -e 's;\\;/;g')
+    if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
+        UEFI_BOOTLOADER=$(find /boot/efi -name "grub*.efi" | tail -1)
+    fi
 fi
-[[ ! -f ${UEFI_BOOTLOADER} ]] && Error "Cannot find a proper UEFI_BOOTLOADER ($UEFI_BOOTLOADER). 
-Please define one manual in /etc/rear/local.conf"
+
+# double check it
+if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
+
+    Error "Cannot find a proper UEFI_BOOTLOADER ($UEFI_BOOTLOADER). 
+Please define it in /etc/rear/local.conf (e.g. UEFI_BOOTLOADER=/boot/efi/EFI/fedora/shim.efi)"
+
+else
+
+    Log "Using UEFI_BOOTLOADER=$UEFI_BOOTLOADER"
+
+fi
+
+# save the variables we need in recover mode into the rescue.conf file
+cat - <<EOF >> "$ROOTFS_DIR$CONFIG_DIR/rescue.conf"
+USING_UEFI_BOOTLOADER=$USING_UEFI_BOOTLOADER
+UEFI_BOOTLOADER="$UEFI_BOOTLOADER"
+EOF
+
