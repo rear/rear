e3a80bb2775dd58d8b77a67f658dd7dcfbbf5f99
Gratien D'haese <gratien.dhaese@gmail.com>
2013-04-06 18:26:34 +0200

    avoid /boot/efi to get relabeled by fixfiles (selinux) issue #220 (needs testing)

---
+++ usr/share/rear/restore/NETFS/Linux-i386/51_selinux_fixfiles_exclude_dirs.sh
@@ -0,0 +1,10 @@
+# for UEFI only we should avoid SElinux relabeling vfat filesystem: /boot/efi
+[[ -z "$USING_UEFI_BOOTLOADER" ]] && return  # empty means using BIOS
+
+# check if /mnt/local/boot/efi is mounted
+[[ -d "/mnt/local/boot/efi" ]]
+StopIfError "Could not find directory /mnt/local/boot/efi"
+
+cat > /mnt/local/etc/selinux/fixfiles_exclude_dirs <<EOF
+/boot/efi
+EOF
