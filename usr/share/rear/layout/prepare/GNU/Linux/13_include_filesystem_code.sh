rear/rear

commit 2d5445466d389b331c9ad1a984c66354415f21ea

Updates required to support btrfs subvolumes at recovery time Fixes required for issue #233 Tested with fedora 19 beta release. Will keep it open 'till proven it works like a charm

usr/share/rear/layout/prepare/GNU/Linux/13_include_filesystem_code.sh
@@ -7,10 +7,13 @@ create_fs() {
     label=${label#label=}
     uuid=${uuid#uuid=}
 
+
     case $fstype in
         ext*)
             # File system parameters.
             local blocksize="" reserved_blocks="" max_mounts="" check_interval=""
+            OIFS=$IFS
+            IFS=","
 
             local option name value
             for option in $options ; do
@@ -40,6 +43,7 @@ create_fs() {
                         ;;
                 esac
             done
+            IFS=$OIFS
 cat >> $LAYOUT_CODE <<EOF
 LogPrint "Creating $fstype-filesystem $mp on $device"
 mkfs -t ${fstype}${blocksize}${fragmentsize}${bytes_per_inode} $device >&2
@@ -90,19 +94,32 @@ EOF
         btrfs)
 cat >> $LAYOUT_CODE <<EOF
 LogPrint "Creating $fstype-filesystem $mp on $device"
-mkfs -t $fstype $device
+# if $device is already mounted, skip
+mount | grep -q $device || mkfs -t $fstype -f $device
 EOF
             if [ -n "$label" ] ; then
-                echo "btrfs filesystem label $device $label >&2" >> $LAYOUT_CODE
+                echo "mount | grep -q $device || btrfs filesystem label $device $label >&2" >> $LAYOUT_CODE
             fi
             if [ -n "$uuid" ] ; then
                 # Problem with btrfs is that uuid cannot be set during mkfs! So, we must map it and
                 # change later the /etc/fstab, /boot/grub/menu.lst, etc
-                cat >> $LAYOUT_CODE <<EOF
-                new_uuid=\$(btrfs filesystem show $device | grep -i uuid | cut -d: -f3 | sed -e 's/^ //')
+                cat >> $LAYOUT_CODE <<-EOF
+                new_uuid=\$(btrfs filesystem show $device 2>/dev/null | grep -i uuid | cut -d: -f3 | sed -e 's/^ //')
                 if [ "$uuid" != "\$new_uuid" ] ; then
-                    echo "$uuid \$new_uuid $device" >> $FS_UUID_MAP
-                fi
+                    if [ ! -f $FS_UUID_MAP ]; then 
+                        echo "$uuid \$new_uuid $device" > $FS_UUID_MAP
+                    else
+                        grep -q "${uuid}" $FS_UUID_MAP
+                        if [[ \$? -eq 0 ]]; then
+                            # required when we restart rear recover (via menu) - uuid changed again
+                            old_uuid=\$(grep ${uuid} $FS_UUID_MAP | tail -1 | awk '{print \$2}')
+                            SED_SCRIPT=";/${uuid}/s/\${old_uuid}/\${new_uuid}/g"
+                            sed -i "\$SED_SCRIPT" "$FS_UUID_MAP"
+                        else
+                            echo "$uuid \$new_uuid $device" >> $FS_UUID_MAP
+                        fi
+                    fi
+                fi # end of [ "$uuid" != "\$new_uuid" ]
 EOF
             fi
             ;;
@@ -135,7 +152,7 @@ EOF
     # Extract mount options
     local option mountopts
     for option in $options ; do
-        name=${option%=*}
+        name=${option%%=*}     # options can contain more '=' signs
         value=${option#*=}
 
         case $name in
@@ -150,9 +167,39 @@ EOF
         mountopts=" -o $mountopts"
     fi
 
-cat >> $LAYOUT_CODE <<EOF
-LogPrint "Mounting filesystem $mp"
-mkdir -p /mnt/local$mp
-mount$mountopts $device /mnt/local$mp
-EOF
+    echo "LogPrint \"Mounting filesystem $mp\"" >> $LAYOUT_CODE
+
+    case $fstype in
+        btrfs)
+            # check the $value for subvols (other then root)
+            subvol=$(echo $value |  awk -F, '/subvol=/  { print $NF}') # empty or something like 'subvol=root'
+            if [ -z "$subvol" ]; then
+                echo "mkdir -p /mnt/local$mp" >> $LAYOUT_CODE
+            elif [ "$subvol" = "subvol=root" ]; then
+		echo "# btrfs subvolume 'root' is a special case" >> $LAYOUT_CODE
+		echo "# before we can create subvolumes we must mount a btrfs device on /mnt" >> $LAYOUT_CODE
+		echo "mount | grep btrfs | grep -q '/mnt' || mount /dev/sda3 /mnt" >> $LAYOUT_CODE
+		echo "# create the root btrfs subvolume" >> $LAYOUT_CODE
+		echo "btrfs subvolume create /mnt/root" >> $LAYOUT_CODE
+                echo "mkdir -p /mnt/local$mp" >> $LAYOUT_CODE
+                echo "# umount subvol 0 as it will be remounted as /mnt/local" >> $LAYOUT_CODE
+                echo "umount /mnt" >> $LAYOUT_CODE
+            else
+		echo "# btrfs subvolume creates sub-directory itself" >> $LAYOUT_CODE
+                echo "btrfs subvolume create /mnt/local$mp" >> $LAYOUT_CODE
+                # just mounting it with subvol=xxx will probably fail with an cryptic error:
+                # mount: mount(2) failed: No such file or directory
+                # we need to mount it with its subvol-id - not a joke
+                # even its not yet mounted we can view it - see http://www.funtoo.org/BTRFS_Fun
+                btrfs_id=\$(btrfs subvolume list /mnt/local$mp | tail -1 | awk '{print \$2}') >> $LAYOUT_CODE
+                mountopts=" -o subvolid=\${btrfs_id}" >> $LAYOUT_CODE
+            fi
+            echo "mount$mountopts $device /mnt/local$mp" >> $LAYOUT_CODE
+            ;;
+        *)
+            echo "mkdir -p /mnt/local$mp" >> $LAYOUT_CODE
+            echo "mount$mountopts $device /mnt/local$mp" >> $LAYOUT_CODE
+            ;;
+    esac
+
 }

usr/share/rear/conf/GNU/Linux.conf
@@ -212,7 +212,7 @@ crc32c
 crc32c-intel
 )
 
-COPY_AS_IS=( ${COPY_AS_IS[@]} /dev /etc/inputr[c] /etc/protocols /etc/services /etc/rpc /etc/termcap /etc/terminfo /lib*/terminfo /usr/share/terminfo /etc/netconfig /etc/mke2fs.conf /etc/*-release /lib*/firmware /etc/localtime  /etc/magic /usr/share/misc/magic )
+COPY_AS_IS=( ${COPY_AS_IS[@]} /dev /etc/inputr[c] /etc/protocols /etc/services /etc/rpc /etc/termcap /etc/terminfo /lib*/terminfo /usr/share/terminfo /etc/netconfig /etc/mke2fs.conf /etc/*-release /lib*/firmware /etc/localtime  /etc/magic /usr/share/misc/magic /etc/dracut.conf /etc/dracut.conf.d /usr/lib/dracut )
 
 # exclude /dev/shm/*, due to the way we use tar the leading / should be omitted
 COPY_AS_IS_EXCLUDE=( ${COPY_AS_IS_EXCLUDE[@]} dev/shm/\* )

usr/share/rear/finalize/GNU/Linux/15_migrate_disk_devices_layout.sh
@@ -9,15 +9,17 @@ pushd /mnt/local >&8
 # the funny [] around the first letter make sure that shopt -s nullglob removes this file from the list if it does not exist
 # the files without a [] are mandatory, like fstab
 for file in     [b]oot/{grub.conf,menu.lst,device.map} [e]tc/grub.* [b]oot/grub/{grub.conf,menu.lst,device.map} \
+		[b]oot/grub2/{grub.conf,grub.cfg,menu.lst,device.map} \
                 [e]tc/sysconfig/grub [e]tc/sysconfig/bootloader \
                 [e]tc/lilo.conf \
-                [e]tc/mtab etc/fstab \
+                [e]tc/mtab [e]tc/fstab \
                 [e]tc/mtools.conf \
                 [e]tc/smartd.conf [e]tc/sysconfig/smartmontools \
                 [e]tc/sysconfig/rawdevices \
                 [e]tc/security/pam_mount.conf.xml [b]oot/efi/*/*/grub.cfg
         do
 
+	[[ ! -f $file ]] && continue # skip directory or file not found
         # sed -i bails on symlinks, so we follow the symlink and patch the result
         # on dead links we warn and skip them
         # TODO: maybe we must put this into a chroot so that absolute symlinks will work correctly

usr/share/rear/finalize/GNU/Linux/15_migrate_uuid_tags.sh
@@ -9,7 +9,7 @@ Log "TAG-15-migrate: $FS_UUID_MAP"
 SED_SCRIPT=""
 while read old_uuid new_uuid device ; do
         SED_SCRIPT="$SED_SCRIPT;/${old_uuid}/s/${old_uuid}/${new_uuid}/g"
-done < $FS_UUID_MAP
+done < <(sort -u $FS_UUID_MAP)
 
 # debug line:
 Log "$SED_SCRIPT"
@@ -19,16 +19,18 @@ pushd /mnt/local >&8
 # the funny [] around the first letter make sure that shopt -s nullglob removes this file from the list if it does not exist
 # the files without a [] are mandatory, like fstab
 for file in 	[b]oot/{grub.conf,menu.lst,device.map} [e]tc/grub.* [b]oot/grub/{grub.conf,menu.lst,device.map} \
+		[b]oot/grub2/{grub.conf,grub.cfg,menu.lst,device.map} \
 		[e]tc/sysconfig/grub [e]tc/sysconfig/bootloader \
 		[e]tc/lilo.conf \
-		[e]tc/mtab etc/fstab \
+		[e]tc/mtab [e]tc/fstab \
 		[e]tc/mtools.conf \
 		[e]tc/smartd.conf [e]tc/sysconfig/smartmontools \
 		[e]tc/sysconfig/rawdevices \
 		[e]tc/security/pam_mount.conf.xml [b]oot/efi/*/*/grub.cfg
 	do
 
-	[[ -d "$file" ]] && continue # skip directory
+	#[[ -d "$file" ]] && continue # skip directory
+	[[ ! -f "$file" ]] && continue # skip directory and file not found
 	# sed -i bails on symlinks, so we follow the symlink and patch the result
 	# on dead links we warn and skip them
 	# TODO: maybe we must put this into a chroot so that absolute symlinks will work correctly
@@ -44,6 +46,7 @@ for file in 	[b]oot/{grub.conf,menu.lst,device.map} [e]tc/grub.* [b]oot/grub/{gr
 		fi
 	fi
 
+        LogPrint "Patching file '$file'"
 	sed -i "$SED_SCRIPT" "$file"
 	StopIfError "Patching '$file' with sed failed."
 done

commit 53aff54f86e61eae3f993d5948abd452e237514d

Fix a regression introduced by 2d5445466d389b331c9ad1a984c66354415f21ea A comma as IFS is irrelevant because we need options separated by comma in one block. (eg : default_mount_options) Also, it breaks the loop because spaces are used as separator in disklayout.conf.

usr/share/rear/layout/prepare/GNU/Linux/13_include_filesystem_code.sh
@@ -12,14 +12,11 @@ create_fs() {
         ext*)
             # File system parameters.
             local blocksize="" reserved_blocks="" max_mounts="" check_interval="" default_mount_options=""
-            OIFS=$IFS
-            IFS=","
 
             local option name value
             for option in $options ; do
                 name=${option%=*}
                 value=${option#*=}
-
                 case $name in
                     blocksize)
                         blocksize=" -b $value"
@@ -46,7 +43,6 @@ create_fs() {
                         ;;
                 esac
             done
-            IFS=$OIFS
 cat >> $LAYOUT_CODE <<EOF
 LogPrint "Creating $fstype-filesystem $mp on $device"
 mkfs -t ${fstype}${blocksize}${fragmentsize}${bytes_per_inode} $device >&2