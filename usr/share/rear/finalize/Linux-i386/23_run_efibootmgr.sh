7bab3ab039e1aec6f910be62dabe66982c2dfa25
Yoann Laissus <yoann.laissus@gmail.com>
2013-06-28 14:00:57 +0200

    Fix the bootloader path in the efibootmgr command. Only one backslash is needed.

--- usr/share/rear/finalize/Linux-i386/23_run_efibootmgr.sh
+++ usr/share/rear/finalize/Linux-i386/23_run_efibootmgr.sh
@@ -9,9 +9,9 @@ BootEfiDev="$( mount | grep "boot/efi" | awk '{print $1}' )"
 Dev=$( get_device_name $BootEfiDev )	# /dev/sda1
 Disk=$( echo ${Dev} | sed -e 's/[0-9]//g' )  # /dev/sda
 ParNr=$( echo ${Dev} | sed -e 's/.*\([0-9]\).*/\1/' )  # 1 (must anyway be a low nr <9)
-BootLoader=$( echo $UEFI_BOOTLOADER | cut -d"/" -f4- | sed -e 's;/;\\\\;g' ) # EFI\\fedora\\shim.efi
-Log efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label \"${OS_VENDOR} ${OS_VERSION}\" --loader \"\\\\${BootLoader}\"
-efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label "${OS_VENDOR} ${OS_VERSION}" --loader "\\\\${BootLoader}"
+BootLoader=$( echo $UEFI_BOOTLOADER | cut -d"/" -f4- | sed -e 's;/;\\;g' ) # EFI\fedora\shim.efi
+Log efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label \"${OS_VENDOR} ${OS_VERSION}\" --loader \"\\${BootLoader}\"
+efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label "${OS_VENDOR} ${OS_VERSION}" --loader "\\${BootLoader}"
 LogIfError "Problem occured with creating an efibootmgr entry"
 
 # ok, boot loader has been set-up - tell rear we are done using following var.
