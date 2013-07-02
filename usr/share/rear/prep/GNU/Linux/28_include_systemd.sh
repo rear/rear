5c89f534f7c80ca97bddf7dca78378ca106aa9fb
Gratien D'haese <gratien.dhaese@gmail.com>
2013-07-02 16:16:10 +0200

    added the missing systemd-* binaries on OpenSuse 12.x close #258

--- usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
+++ usr/share/rear/prep/GNU/Linux/28_include_systemd.sh
@@ -5,6 +5,6 @@ if ps ax | grep -v grep | grep -q systemd ; then
 		systemd-udevd systemd-journald journalctl dbus-uuidgen dbus-daemon dbus-send )
 	# cgroup stuff - not required for rear
 	#PROGS=( "${PROGS[@]}" cg_annotate cgclear cgcreate cgget cgrulesengd cgset cgdelete cgclassify cgexec )
-	COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /lib/systemd/systemd-* )
+	COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /usr/lib/systemd/systemd-* /lib/systemd/systemd-* )
 	Log "Including systemd (init replacement) tool-set to bootstrap Relax-and-Recover"
 fi
