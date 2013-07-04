f72bfbed9318016a2a0f34571fadb4233eae94e6
Dag Wieers <dag@wieers.com>
2013-07-05 01:59:22 +0200

    Small rewrite to support 'noefi'
    This fixes #255

--- usr/share/rear/prep/default/31_include_uefi_tools.sh
+++ usr/share/rear/prep/default/31_include_uefi_tools.sh
@@ -3,6 +3,11 @@
 # then add all the required executables, kernel modules, etc...
 # Most likely, only recent OSes will be UEFI capable, such as SLES11, RHEL6, Ubuntu 12.10, Fedora 18
 
+# If noefi is set, we can ignore UEFI altogether
+if grep -qw 'noefi' /proc/cmdline; then
+    return
+fi
+
 # Some distributions don't have a builtin efivars kernel module, so we need to load it.
 # Be aware, efivars is not listed with 'lsmod'
 modprobe -q efivars
@@ -10,38 +15,43 @@ modprobe -q efivars
 # next step, is checking the presence of UEFI variables directory
 # However, we should first check kernel command line to see whether we hide on purpose the UEFI vars with 'noefi'
 SYSFS_DIR_EFI_VARS=
-cat /proc/cmdline | grep -q noefi || {    # 'noefi' option not found, so check for the dir itself
-    if [ -d /sys/firmware/efi/vars ]; then
-        SYSFS_DIR_EFI_VARS=/sys/firmware/efi/vars
-    elif [ -d /sys/firmware/efi/efivars ]; then
-        SYSFS_DIR_EFI_VARS=/sys/firmware/efi/efivars
-    else 
-        return    # when UEFI is enabled the dir is there
-    fi
-    }
+if [[ -d /sys/firmware/efi/vars ]]; then
+    SYSFS_DIR_EFI_VARS=/sys/firmware/efi/vars
+elif [[ -d /sys/firmware/efi/efivars ]]; then
+    SYSFS_DIR_EFI_VARS=/sys/firmware/efi/efivars
+else
+    return    # when UEFI is enabled the dir is there
+fi
 
 # mount-point: efivarfs on /sys/firmware/efi/efivars type efivarfs (rw,nosuid,nodev,noexec,relatime)
-mount | grep -q efivarfs && SYSFS_DIR_EFI_VARS=/sys/firmware/efi/efivars
+if grep -qw efivars /proc/mounts; then
+    SYSFS_DIR_EFI_VARS=/sys/firmware/efi/efivars
+fi
 
 # next step, is checking /boot/efi directory (we need it)
-[[ ! -d /boot/efi ]] && return    # must be mounted
+if [[ ! -d /boot/efi ]]; then
+    return    # must be mounted
+fi
 
 # next step, check filesystem partition type (vfat?)
-UEFI_FS_TYPE=$(mount | grep -i "/boot/efi" | awk '{print $5}')
+UEFI_FS_TYPE=$(awk '/\/boot\/efi/ { print $3 }' /proc/mounts)
 
-[[ "$UEFI_FS_TYPE" != "vfat" ]] && return    # ESP must be type vfat (under Linux)
+# ESP must be type vfat (under Linux)
+if [[ "$UEFI_FS_TYPE" != "vfat" ]]; then
+    return
+fi
 
 # we are still here? Ok, now it is safe to turn on USING_UEFI_BOOTLOADER=1
 USING_UEFI_BOOTLOADER=1
 LogPrint "Using UEFI Boot Loader for Linux (USING_UEFI_BOOTLOADER=1)"
 
 PROGS=( "${PROGS[@]}"
-parted
-gdisk
-efibootmgr
-uefivars
 dosfsck
 dosfslabel
+efibootmgr
+gdisk
+parted
+uefivars
 )
 
 MODULES=( "${MODULES[@]}" efivars )
