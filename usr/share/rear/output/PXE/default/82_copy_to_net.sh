d850c4094238a03c9b926b88d7e1582ecd28af52
Gratien D'haese <gratien.dhaese@gmail.com>
2013-07-17 11:45:47 +0200

    add script from Andreas Lemke to xfer to PXE server

---
+++ usr/share/rear/output/PXE/default/82_copy_to_net.sh
@@ -0,0 +1,36 @@
+# #82_copy_to_net.sh
+
+# Check if we have a target location OUTPUT_URL
+if [[ -z "$OUTPUT_URL" ]]; then
+    return
+fi
+
+local scheme=$(url_scheme $OUTPUT_URL)
+local server=$(url_host $OUTPUT_URL)
+local path=$(url_path $OUTPUT_URL)
+
+case "$scheme" in
+    (nfs|cifs|usb|tape|file)
+        # The ISO has already been transferred by NETFS.
+        return 0
+        ;;
+    (fish|ftp|ftps|hftp|http|https|sftp)
+        LogPrint "Transferring PXE files to $OUTPUT_URL"
+        for i in "${RESULT_FILES[@]}"
+          do
+            LogPrint "Transferring file: $i"
+            lftp -c "open $OUTPUT_URL; mput $i"
+          done
+        StopIfError "Problem transferring PXE files to $OUTPUT_URL"
+        ;;
+    (rsync)
+        LogPrint "Transferring PXE files to $OUTPUT_URL"
+        for i in "${RESULT_FILES[@]}"
+          do
+            LogPrint "Transferring file: $i"
+            rsync -a $v "$i" "$OUTPUT_URL"
+          done
+        StopIfError "Problem transferring PXE files to $OUTPUT_URL"
+        ;;
+    (*) BugError "Support for $scheme is not implemented yet.";;
+esac
