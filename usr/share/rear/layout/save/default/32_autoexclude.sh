e7c14765678d016299fc5318289554e559d00de6
Yoann Laissus <yoann.laissus@gmail.com>
2013-08-09 11:44:37 +0200

    Use double bracket for a if

--- usr/share/rear/layout/save/default/32_autoexclude.sh
+++ usr/share/rear/layout/save/default/32_autoexclude.sh
@@ -1,5 +1,5 @@
 # Automatically exclude multipath devices
-if [ "$AUTOEXCLUDE_MULTIPATH" =~ ^[yY1] ] ; then
+if [[ "$AUTOEXCLUDE_MULTIPATH" =~ ^[yY1] ]] ; then
     while read multipath device devices junk ; do
         Log "Automatically excluding multipath device $device."
         mark_as_done "$device"
