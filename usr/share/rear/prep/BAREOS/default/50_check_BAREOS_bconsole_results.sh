06115c1a9af232f4f431616b588148b24917aa3b
Gratien D'haese <gratien.dhaese@gmail.com>
2013-08-30 10:24:46 +0200

    adding support for BACKUP=BAREOS (Bareos' Bacula implementation)

---
+++ usr/share/rear/prep/BAREOS/default/50_check_BAREOS_bconsole_results.sh
@@ -0,0 +1,47 @@
+# Check that bareos is installed and configured
+#
+# are all  the files/directories present?
+
+if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
+
+   ### Bacule restore using bextract, no need for a director
+   return
+fi
+
+#
+# See if we can ping the director
+#
+# is the director server present? Fetch from /etc/bareos/bconsole.conf file
+BAREOS_DIRECTOR=$(grep -i address /etc/bareos/bconsole.conf | awk '{ print $3 }')
+[ "${BAREOS_DIRECTOR}" ]
+StopIfError "Director not defined in /etc/bareos/bconsole.conf"
+
+if test "$PING"; then
+	ping -c 2 -q  $BAREOS_DIRECTOR >&8
+	StopIfError "Backup host [$BAREOS_DIRECTOR] not reachable."
+else
+	Log "Skipping ping test"
+fi
+
+# does the director allow connections from this client? bconsole knows!
+#
+# We want these two lines to show that we can connect to the director
+# and that the director can connect to the file daemon on this system.
+# "Connecting to Director 'director_name-fd:9101'"
+# "Connecting to Client 'bareos_client_name-fd at FQDN:9102"
+BAREOS_CLIENT=`grep $(hostname -s) /etc/bareos/bareos-fd.conf | grep "\-fd" | awk '{print $3}' | sed -e "s/-fd//g"`
+[ "${BAREOS_CLIENT}" ]
+StopIfError "Client $(hostname -s) not defined in /etc/bareos/bareos-fd.conf"
+
+BAREOS_RESULT=( `echo -e " status client=${BAREOS_CLIENT}-fd" | bconsole |grep Connect ` )
+
+director=${BAREOS_RESULT[3]}
+client=${BAREOS_RESULT[9]}
+
+[ "$director" ]
+StopIfError "Bareos director not reachable."
+
+[ "$client" ]
+StopIfError 1 "Bareos client status unknown on director."
+
+Log "Bareos director = $director, client = $client"
---
+++ usr/share/rear/restore/BAREOS/default/40_restore_backup.sh
@@ -0,0 +1,125 @@
+### Restore from bareos
+###
+
+### Create example bootstrap file
+cat <<EOF >$VAR_DIR/bootstrap.txt
+### Example Bacula bootstrap file
+###
+
+### Only the exact Volume name is required, other keywords are optional.
+### The use of FileIndex and Count keywords speeds up the selection enormously.
+
+### The Volume name to use
+Volume=PLEASE-EDIT-BOOTSTRAP
+
+### A (list of) Client name(s) to be matched on the current Volume
+#Client=$(hostname -s)-fd
+
+### A (list or range of) JobId(s) to be selected from the current Volume
+#JobId=18
+
+### A (list of) Job name(s) to be matched on the current Volume
+#Job=Bkp_Daily.2011-06-16
+
+### A (list or range of) Volume session id(s) to be matched from the current Volume
+#VolSessionId=1
+
+### The Volume session time to be matched from the current Volume
+#VolSessionTime=108927638
+
+### A (list or range of) FileIndex(es) to be selected from the current Volume
+#FileIndex=1-157
+
+### The total number of files that will be restored for this Volume.
+#Count=157
+EOF
+
+if [[ "$BEXTRACT_DEVICE" || "$BEXTRACT_VOLUME" ]]; then
+
+    if [[ -s "$TMP_DIR/restore-exclude-list.txt" ]]; then
+        exclude_list=" -e $TMP_DIR/restore-exclude-list.txt "
+    fi
+
+    if [[ -b "$BEXTRACT_DEVICE" && -d "/backup" ]]; then
+
+        ### Bacula support using bextract and disk archive
+        LogPrint "
+The system is now ready to restore from Bacula. bextract will be started for
+you to restore the required files. It's assumed that you know what is
+necessary to restore - typically it will be a full backup.
+
+Do not exit 'bextract' until all files are restored.
+
+WARNING: The new root is mounted under '/mnt/local'.
+"
+        read -p "Press ENTER to start bextract" 2>&1
+
+        bextract$exclude_list -V$BEXTRACT_VOLUME /backup /mnt/local
+
+        LogPrint "
+Please verify that the backup has been restored correctly to '/mnt/local'
+in the provided shell. When finished, type exit in the shell to continue
+recovery.
+"
+        rear_shell "Did the backup successfully restore to '/mnt/local' ? Ready to continue ?" \
+            "bls -j -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE
+vi bootstrap.txt
+bextract$exclude_list -b bootstrap.txt -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE /mnt/local"
+
+    else
+
+        ### Bacula support using bextract and tape archive
+        LogPrint "$REQUESTRESTORE_TEXT"
+
+        LogPrint "The bextract command looks like:
+
+   bextract$exclude_list -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE /mnt/local
+
+Where \"$BEXTRACT_VOLUME\" is the required Volume name of the tape,
+alternatively, use '*' if you don't know the volume,
+and \"$BEXTRACT_DEVICE\" is the Bacula device name of the tape drive.
+"
+
+        LogPrint "Please restore your backup in the provided shell, use the shell history to
+access the above command and, when finished, type exit in the shell to continue recovery.
+"
+        rear_shell "Did you restore the backup to /mnt/local ? Ready to continue ?" \
+            "bls -j -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE
+vi bootstrap.txt
+bextract$exclude_list -b bootstrap.txt -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE /mnt/local"
+
+    fi
+
+else
+
+    ### Bareos support using bconsole
+
+    # Prompt the user that the system recreation has been done and that
+    # bconsole is about to be started.
+    LogPrint "
+The system is now ready for a restore via Bareos. bconsole will be started for
+you to restore the required files. It's assumed that you know what is necessary
+to restore - typically it will be a full backup.
+
+Do not exit 'bconsole' until all files are restored
+
+WARNING: The new root is mounted under '/mnt/local'.
+
+Press ENTER to start bconsole"
+    read
+
+    bconsole
+
+    LogPrint "
+Please verify that the backup has been restored correctly to '/mnt/local'
+in the provided shell. When finished, type exit in the shell to continue
+recovery.
+"
+    rear_shell "Did the backup successfully restore to '/mnt/local' ? Ready to continue ?" \
+            "bls -j -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE
+vi bootstrap.txt
+bextract$exclude_list -b bootstrap.txt -V$BEXTRACT_VOLUME $BEXTRACT_DEVICE /mnt/local"
+
+fi
+
+# continue with next script
