354486da3191dbfe5c6a4b896e34977be10697d5
test <kreitz@Dev-Server.mhknet.de>
2013-09-02 11:39:52 +0200

    Incrementap Patches

--- usr/share/rear/verify/NETFS/default/07_set_backup_archive.sh
+++ usr/share/rear/verify/NETFS/default/07_set_backup_archive.sh
@@ -1,1 +1,18 @@
-../../../prep/NETFS/default/07_set_backup_archive.sh
+### Determine the name of the backup archive
+### This needs to be after we special case USB devices.
+
+local scheme=$(url_scheme $BACKUP_URL)
+case "$TAPE_DEVICE:$scheme" in
+    (:file|:iso)
+        # define the output path according to the scheme
+        local path=$(url_path $BACKUP_URL)
+        local opath=$(backup_path $scheme $path)
+        backuparchive="${opath}/${BACKUP_PROG_ARCHIVE}${BACKUP_PROG_SUFFIX}${BACKUP_PROG_COMPRESS_SUFFIX}"
+        ;;
+    (:*)
+        backuparchive="${BUILD_DIR}/outputfs/${NETFS_PREFIX}/${BACKUP_PROG_ARCHIVE}${BACKUP_PROG_SUFFIX}${BACKUP_PROG_COMPRESS_SUFFIX}"
+        ;;
+    (*:tape)
+        backuparchive="${TAPE_DEVICE}"
+        ;;
+esac
