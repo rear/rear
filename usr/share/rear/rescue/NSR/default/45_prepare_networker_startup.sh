38bb7835f08ef0e0f71ea1c2c6f0e21278572440
Gratien D'haese <gratien.dhaese@gmail.com>
2013-09-11 21:07:18 +0200

    adding EMC NetWorker (Legato) to rear-1.15 release candidate on request of sponsor

---
+++ usr/share/rear/rescue/NSR/default/45_prepare_networker_startup.sh
@@ -0,0 +1,17 @@
+# 45_prepare_networker_startup.sh
+# make sure nsrecexd gets started up in the rescue image
+
+cat >$ROOTFS_DIR/etc/scripts/system-setup.d/90-networker.sh <<-EOF
+echo "Starting EMC NetWorker nsrexecd daemon ..."
+NSRRC=\$NSR_ROOT_DIR/nsrrc
+NSR_ENVEXEC=/opt/nsr/admin/nsr_envexec
+
+# networkerrc defines environment variables, such as LD_LIBRARY_PATH, required
+# to run NetWorker daemons.
+NETWORKERRC=/opt/nsr/admin/networkerrc
+if [ -f /usr/sbin/nsrexecd ]; then
+    "\$NSR_ENVEXEC" -u "\$NSRRC" -s "\$NETWORKERRC"  "/usr/sbin/nsrexecd"
+fi
+EOF
+chmod +x $ROOTFS_DIR/etc/scripts/system-setup.d/90-networker.sh
+Log "Created the EMC NetWorker nsrexecd start-up script (90-networker.sh) for Rear"
