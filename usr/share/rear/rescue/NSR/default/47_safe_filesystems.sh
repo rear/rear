63540fd1f4437d7b0a555d417bb87eaa8ae7c0ea
Gratien D'haese <gratien.dhaese@gmail.com>
2013-09-27 15:47:23 +0200

    EMC NetWorker commands savefs does not always finds its default NSRSERVER, therefore, we better always add the -s option close #304

--- usr/share/rear/rescue/NSR/default/47_safe_filesystems.sh
+++ usr/share/rear/rescue/NSR/default/47_safe_filesystems.sh
@@ -1,5 +1,5 @@
 # 47_safe_filesystems.sh
-savefs -p 2>&1 | awk -F '(=|,)' '/path/ { printf ("%s ", $2) }' > $VAR_DIR/recovery/nsr_paths
+savefs -p -s $NSRSERVER 2>&1 | awk -F '(=|,)' '/path/ { printf ("%s ", $2) }' > $VAR_DIR/recovery/nsr_paths
 [[ ! -s $VAR_DIR/recovery/nsr_paths ]] && Error "The savefs command could not retrieve the \"save sets\" from this client"
 
 LogPrint "EMC Networker will recover these filesystems: $( cat $VAR_DIR/recovery/nsr_paths )"
