acb6e0727db0897498aabe28c8a58cc88d1b5cec
Gratien D'haese <gratien.dhaese@gmail.com>
2013-10-30 16:04:18 +0100

    started with BOOT_OVER_SAN stuff

---
+++ usr/share/rear/prep/GNU/Linux/24_include_multipath_tools.sh
@@ -0,0 +1,7 @@
+# 24_include_multipath_tools.sh
+# Boot Over SAN executables and other goodies
+
+[[ $BOOT_OVER_SAN != ^[yY1] ]] && return
+
+PROGS=( "${PROGS[@]}" multipath dmsetup kpartx multipathd scsi_id  )
+COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/multipath/bindings /etc/multipath.conf )
