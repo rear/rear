3b4fb21ca8ab07ab6240a63edfa7fd34958b8f8a
Gratien D'haese <gratien.dhaese@gmail.com>
2013-11-25 04:07:34 -0800

    The $CONFIG_DIR variable will be incorrect if rear is called via a relative path. We need to hard-code etc/rear/rescue.conf close #336

--- usr/share/rear/rescue/NETFS/default/60_store_NETFS_variables.sh
+++ usr/share/rear/rescue/NETFS/default/60_store_NETFS_variables.sh
@@ -1,4 +1,4 @@
 # store all NETFS* variables
 # I don't know why it does not work with the full declare -- var=value syntax
 # found out by experiment that I need to remove the declare -- stuff.
-declare -p ${!NETFS*} | sed -e 's/declare .. //' >>$ROOTFS_DIR$CONFIG_DIR/rescue.conf
+declare -p ${!NETFS*} | sed -e 's/declare .. //' >>$ROOTFS_DIR/etc/rear/rescue.conf
