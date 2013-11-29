179fd6493883ddbba740fd64e4851ab322bc39ca
Gratien D'haese <gratien.dhaese@gmail.com>
2013-11-29 07:49:35 -0800

    - added 'kvm' group to CLONE_GROUPS variable;
    - introduced TMPDIR variable to assist (user-defined prefix) the mktemp command. However, we should make sure that in rescue mode the TMPDIR variable is unset so the mktemp creates the BUILD_DIR under /tmp/rear.XXXXX (issue #323 and #157). By setting TMPDIR in local.conf we can choose were the BUILD_DIR resides (as it also contains the large ISO image);
    
    - when we backup to ISO image make sure that the isofs module is copied to the rescue image (issue #326)

---
+++ usr/share/rear/prep/ISO/GNU/Linux/34_add_isofs_module.sh
@@ -0,0 +1,11 @@
+# if $scheme=iso then we must have isofs module in rescue mode so that we can
+# loopback mount the ISO containing the backup
+# BACKUP_URL=iso://backup
+
+local scheme=$(url_scheme $BACKUP_URL)
+
+case "$scheme" in
+    (iso)
+        MODULES=( ${MODULES[@]} isofs )
+        ;;
+esac
