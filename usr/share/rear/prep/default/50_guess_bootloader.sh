67ac463446a5b140ea1eb1d9268e86422f869ebf
Gratien D'haese <gratien.dhaese@gmail.com>
2014-01-22 22:15:04 +0100

    initial attemp to guess bootloader

---
+++ usr/share/rear/prep/default/50_guess_bootloader.sh
@@ -0,0 +1,22 @@
+# purpose is to guess the bootloader in use ans save this into a file
+# /var/lib/rear/recovery/bootloader
+for disk in /sys/block/* ; do
+    if [[ ${disk#/sys/block/} = @(hd*|sd*|cciss*|vd*|xvd*) ]] ; then
+        devname=$(get_device_name $disk)
+        dd if=$devname bs=512 count=4 | strings > $TMP_DIR/bootloader
+        grep -q "EFI" $TMP_DIR/bootloader && {
+        echo "EFI" >$VAR_DIR/recovery/bootloader
+        return
+        }
+        grep -q "GRUB" $TMP_DIR/bootloader && {
+        echo "GRUB" >$VAR_DIR/recovery/bootloader
+        return
+        }
+        grep -q "LILO" $TMP_DIR/bootloader && {
+        echo "LILO" >$VAR_DIR/recovery/bootloader
+        return
+        }
+        cat TMP_DIR/bootloader >&7
+   fi
+done
+
