16fdd254d18f9251160fadafede3a15f2c71fb75
Gratien D'haese <gratien.dhaese@gmail.com>
2014-01-26 16:46:53 +0100

    TSM restore fix for v>=6.4 with dsmc restore as suggested in #356 close #356

--- usr/share/rear/restore/TSM/default/40_restore_with_tsm.sh
+++ usr/share/rear/restore/TSM/default/40_restore_with_tsm.sh
@@ -14,26 +14,26 @@ star ()
 #-----<--------->-------
 
 for num in $TSM_RESTORE_FILESPACE_NUMS ; do
-	filespace="${TSM_FILESPACES[$num]}"
-	# make sure FileSpace has a trailing / (for dsmc)
-	test "${filespace:0-1}" == "/" || filespace="$filespace/"
-	LogPrint "Restoring ${filespace}"
-	TsmProcessed=""
-	Log "Running 'dsmc restore ${filespace}* /mnt/local/$filespace -verbose -subdir=yes -replace=all -tapeprompt=no ${TSM_DSMC_RESTORE_OPTIONS[@]}'"
-	dsmc restore "${filespace}*" "/mnt/local/$filespace" \
-		-verbose -subdir=yes -replace=all \
-		-tapeprompt=no "${TSM_DSMC_RESTORE_OPTIONS[@]}" | \
-	while read Line ; do
-		if test "${Line:0:8}" == "ANS1898I" ; then
-			TsmProcessed="$(echo "${Line:9}" | tr -s '*') "
-			Line="Restoring" # trigger star
-		fi
-		if test "${Line:0:9}" == "Restoring" ; then
-			echo -n "$TsmProcessed"
-			star
-		else
-			echo "$Line"
-		fi
-	done
+    filespace="${TSM_FILESPACES[$num]}"
+    # make sure FileSpace has a trailing / (for dsmc)
+    test "${filespace:0-1}" == "/" || filespace="$filespace/"
+    LogPrint "Restoring ${filespace}"
+    TsmProcessed=""
+    Log "Running 'dsmc restore ${filespace}* /mnt/local/$filespace -verbose -subdir=yes -replace=all -tapeprompt=no ${TSM_DSMC_RESTORE_OPTIONS[@]}'"
+    dsmc restore \""${filespace}"\" \""/mnt/local/${filespace}/"\"  \
+        -verbose -subdir=yes -replace=all \
+        -tapeprompt=no "${TSM_DSMC_RESTORE_OPTIONS[@]}" | \
+    while read Line ; do
+        if test "${Line:0:8}" == "ANS1898I" ; then
+            TsmProcessed="$(echo "${Line:9}" | tr -s '*') "
+            Line="Restoring" # trigger star
+        fi
+        if test "${Line:0:9}" == "Restoring" ; then
+            echo -n "$TsmProcessed"
+            star
+        else
+            echo "$Line"
+        fi
+    done
 done
 
