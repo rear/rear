97c59bdfa512dfd039721eb97920ee5f6e82c726
Gratien D'haese <gratien.dhaese@gmail.com>
2014-02-14 16:21:48 +0100

    checking the bootloader files during savelayout section - see issue #234

---
+++ usr/share/rear/layout/save/default/45_check_bootloader_files.sh
@@ -0,0 +1,15 @@
+# This file is part of Relax and Recover, licensed under the GNU General
+# Public License. Refer to the included LICENSE for full text of license.
+
+# If any of our bootloader files changes then we should trigger a new savelayout or mkrescue
+# prep/default/50_guess_bootloader.sh script created $VAR_DIR/recovery/bootloader file
+myBOOTloader=$( cat $VAR_DIR/recovery/bootloader )
+
+case $myBOOTloader in
+    EFI)  CHECK_CONFIG_FILES=( ${CHECK_CONFIG_FILES[@]} /boot/efi/EFI/*/grub*.cfg )
+        ;;
+    GRUB) CHECK_CONFIG_FILES=( ${CHECK_CONFIG_FILES[@]}  /etc/grub.cfg /etc/grub2.cfg /boot/grub2/grub2.cfg /boot/grub/grub.cfg )
+        ;;
+    LILO) CHECK_CONFIG_FILES=( ${CHECK_CONFIG_FILES[@]} /etc/lilo.conf )
+        ;;
+esac
