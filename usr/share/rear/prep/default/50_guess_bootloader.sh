02266e32ae27a94bf84f2c0769bcd64249d21b36
Gratien D'haese <gratien.dhaese@gmail.com>
2014-02-27 14:01:07 +0100

    improved the bootloader guessing on opensuse/sles (take the /etc/sysconfig/bootloader file)

--- usr/share/rear/prep/default/50_guess_bootloader.sh
+++ usr/share/rear/prep/default/50_guess_bootloader.sh
@@ -1,5 +1,12 @@
 # purpose is to guess the bootloader in use ans save this into a file
 # /var/lib/rear/recovery/bootloader
+if [[ -f /etc/sysconfig/bootloader ]]; then
+    my_bootloader=$( grep LOADER /etc/sysconfig/bootloader |cut -d= -f2 | sed -e 's/"//g' )
+    if [[ ! -z "$my_bootloader" ]]; then
+        echo "$my_bootloader" >$VAR_DIR/recovery/bootloader
+        return
+    fi
+fi
 for disk in /sys/block/* ; do
     if [[ ${disk#/sys/block/} = @(hd*|sd*|cciss*|vd*|xvd*) ]] ; then
         devname=$(get_device_name $disk)
@@ -16,8 +23,8 @@ for disk in /sys/block/* ; do
         echo "LILO" >$VAR_DIR/recovery/bootloader
         return
         }
-        echo "Displaying the raw bootloader info:" >&7
-        cat $TMP_DIR/bootloader >&7
+        Log "Displaying the raw bootloader info:"
+        cat $TMP_DIR/bootloader >&2
    fi
 done
 
