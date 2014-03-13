e8ba45164d251b83e6102a33e3df87dfa652d7d8
Gratien D'haese <gratien.dhaese@gmail.com>
2014-03-13 22:03:12 +0100

    special script to remove the $ISO_DIR/$ISO_PREFIX.iso and $TSM_RESULT_FILE_PATH/$ISO_PREFIX.iso if TSM confirms a successful backup of the $ISO_PREFIX.iso Need variable TSM_RM_ISOFILE=y in local.conf or site.conf file

---
+++ usr/share/rear/output/TSM/default/96_dsmc_verify_isofile.sh
@@ -0,0 +1,17 @@
+# 96_dsmc_verify_isofile.sh
+if [[ ! "$TSM_RM_ISOFILE" =~ [yY1] ]] ; then
+    return
+fi
+
+Log "Verify if the files '$TSM_RESULT_FILE_PATH/$ISO_PREFIX.iso' were archived correctly with dsmc"
+dsmc q archive "$TSM_RESULT_FILE_PATH/$ISO_PREFIX.iso" >&8
+if [[ $? -eq 0 ]]; then
+    Log "Removing the '${TSM_RESULT_FILES[@]}' files to preserve space"
+    rm $v -f ${TSM_RESULT_FILES[@]} >&8
+    Log "Remove the $ISO_DIR/$ISO_PREFIX.iso to preserve space"
+    rm $v -f $ISO_DIR/$ISO_PREFIX.iso >&8
+    LogPrint "The only remaining copy of the ISO file is under TSM:$TSM_RESULT_FILE_PATH/$ISO_PREFIX.iso"
+else
+   LogPrint "TSM did not confirm correctly if the ISO file was stored properly - not remove local ISO files"
+fi
+
