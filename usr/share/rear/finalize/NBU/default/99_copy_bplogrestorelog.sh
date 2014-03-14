624100b6260e0a71aca4754b186fe3552efdacd1
Schlomo Schapiro <github@schlomo.schapiro.org>
2014-03-14 13:39:50 +0100

    Fix pattern to include more log files
    Fixes #379

--- usr/share/rear/finalize/NBU/default/99_copy_bplogrestorelog.sh
+++ usr/share/rear/finalize/NBU/default/99_copy_bplogrestorelog.sh
@@ -2,4 +2,4 @@
 # copy the logfile to the recovered system, at least the part that has been written till now.
 
 mkdir -p /mnt/local/root
-cp -f /tmp/bplog.restore.* /mnt/local/root/
+cp -f /tmp/bplog.restore* /mnt/local/root/
