0e27b84ea434fe046c0309647a5ca02c31195048
Gratien D'haese <gratien.dhaese@gmail.com>
2014-07-04 16:12:18 +0200

    build/DUPLICITY/default/60_create_python_symlink.sh: fix the symbolic link to python

--- usr/share/rear/build/DUPLICITY/default/60_create_python_symlink.sh
+++ usr/share/rear/build/DUPLICITY/default/60_create_python_symlink.sh
@@ -2,11 +2,12 @@
 (
     cd  $ROOTFS_DIR/bin
     for py in $(find . -name "python*" )
-    do 
+    do
         this_py=${py#./*}   # should be without ./
-        case "$this_py" in
-            "python") break ;;
-            "python2*"|"python3*") ln -sf $v "$this_py" python >&2 ;;
+        case $this_py in
+            python) break ;;
+            python2*|python3*) ln -sf $v $this_py python >&2 ;;
         esac
     done
 )
+
