9f4d1e672c8854c2b9d92859cad52ca1896dbfb7
Gratien D'haese <gratien.dhaese@gmail.com>
2014-08-04 13:32:52 +0200

    For issue #440 added a mkdir for $VAR_DIR/output if dir does not exist

--- usr/share/rear/output/PXE/default/80_copy_to_tftp.sh
+++ usr/share/rear/output/PXE/default/80_copy_to_tftp.sh
@@ -24,6 +24,7 @@ PXE_KERNEL="${PXE_TFTP_PREFIX}kernel"
 PXE_INITRD="${PXE_TFTP_PREFIX}initrd.cgz"
 PXE_MESSAGE="${PXE_TFTP_PREFIX}message"
 
+[[ ! -d "$PXE_TFTP_LOCAL_PATH" ]] && mkdir $v -m 750 "$PXE_TFTP_LOCAL_PATH" >&2
 
 cp -pL $v "$KERNEL_FILE" "$PXE_TFTP_LOCAL_PATH/$PXE_KERNEL" >&2
 cp -a $v "$TMP_DIR"/initrd.cgz "$PXE_TFTP_LOCAL_PATH/$PXE_INITRD" >&2
