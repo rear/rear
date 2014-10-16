c5030c134022c9b27e3fce0934de71dff76d0d52
Gratien D'haese <gratien.dhaese@gmail.com>
2014-10-16 11:52:45 +0200

    did not pay enough attention -path was different as well - issue #473

--- usr/share/rear/finalize/default/89_finish_checks.sh
+++ usr/share/rear/finalize/default/89_finish_checks.sh
@@ -13,8 +13,8 @@ that we have failed to install the boot loader correctly.
 Please contribute this code to the $PRODUCT project. To do so
 please take a look at the scripts in $SHARE_DIR/finalize,
 for an example you can use the script for Fedora (and RHEL/CentOS/SL) in
-$SHARE_DIR/finalize/Fedora/i386/21_install_grub.sh or
-$SHARE_DIR/finalize/Fedora/i386/22_install_grub2.sh
+$SHARE_DIR/finalize/Linux-i386/21_install_grub.sh or
+$SHARE_DIR/finalize/Linux-i386/22_install_grub2.sh
 
 --------------------  ATTENTION ATTENTION ATTENTION -------------------
 |                                                                     |
