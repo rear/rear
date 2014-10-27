1d27e04a51b73cc0abfcfffd74c9d868808275b9
tbsky <tbskyd@gmail.com>
2014-10-28 01:08:36 +0800

    fix drbd layout so it can handle /dev/vg/lv format backend and mulitple volumes within single resource

--- usr/share/rear/layout/save/GNU/Linux/25_drbd_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/25_drbd_layout.sh
@@ -4,9 +4,13 @@ if [ -e /proc/drbd ] ; then
     Log "Saving DRBD configuration."
 
     for resource in $(drbdadm sh-resources) ; do
-        dev=$(drbdadm sh-dev $resource)
-        disk=$(drbdadm sh-ll-dev $resource)
+        dev=( $(drbdadm sh-dev $resource) )
+        disk=( $(drbdadm sh-ll-dev $resource) )
 
-        echo "drbd $dev $resource $disk" >> $DISKLAYOUT_FILE
+        for i in ${!dev[*]}; do
+            vol_dev=${dev[$i]}
+            vol_disk=$(get_device_name ${disk[$i]})
+            echo "drbd $vol_dev $resource $vol_disk" >> $DISKLAYOUT_FILE
+        done
     done
 fi
