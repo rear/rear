5e8193f4aa2139ce65f0ce90d6a8711e76b6e108
Gratien D'haese <gratien.dhaese@gmail.com>
2014-11-13 10:35:53 +0100

    remove >&2 from lvreate line as sometimes we get a question line "Wipe it? [y/n]" we rather see the prompt of the question on the screen instead in the logging... related to issue #492

--- usr/share/rear/layout/prepare/GNU/Linux/11_include_lvm_code.sh
+++ usr/share/rear/layout/prepare/GNU/Linux/11_include_lvm_code.sh
@@ -104,6 +104,8 @@ create_lvmvol() {
 
     (
     echo "LogPrint \"Creating LVM volume ${vgrp#/dev/}/$lvname\""
-    echo "lvm lvcreate -l $nrextents -n ${lvname} ${vgrp#/dev/} >&2"
+    #echo "lvm lvcreate -l $nrextents -n ${lvname} ${vgrp#/dev/} >&2"
+    # remove the '>&2' from lvcreate as sometimes we get an question like "Wipe it? [y/n]"
+    echo "lvm lvcreate -l $nrextents -n ${lvname} ${vgrp#/dev/}"
     ) >> "$LAYOUT_CODE"
 }
