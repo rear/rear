dda31b7e17091b0bad659f9ee62943c81bfcb7d5
Gratien D'haese <gratien.dhaese@gmail.com>
2014-11-19 11:55:04 +0100

    adding /etc/multipath/wwids to prep/GNU/Linux/24_include_multipath_tools.sh

--- usr/share/rear/prep/GNU/Linux/24_include_multipath_tools.sh
+++ usr/share/rear/prep/GNU/Linux/24_include_multipath_tools.sh
@@ -4,4 +4,4 @@
 [[ $BOOT_OVER_SAN != ^[yY1] ]] && return
 
 PROGS=( "${PROGS[@]}" multipath dmsetup kpartx multipathd scsi_id  )
-COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/multipath/bindings /etc/multipath.conf )
+COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/multipath/bindings /etc/multipath/wwids /etc/multipath.conf )
