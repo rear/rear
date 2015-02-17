ca41fe5046729fd30a7832ad2985adb18896d631
Gratien D'haese <gratien.dhaese@gmail.com>
2015-02-17 21:05:37 +0100

    * Filled the "Fragment size" (fragmentsize) variable (it was missing - thanks Johannes). * Noticed that label in vfat can contains spaces (substitute ' ' with '\\b'). Why '\\b'? Reading the variable and it becomes '\b' * Modifies scripts usr/share/rear/layout/prepare/GNU/Linux/13_include_filesystem_code.sh and usr/share/rear/layout/save/GNU/Linux/23_filesystem_layout.sh See issue #549

--- usr/share/rear/layout/save/GNU/Linux/23_filesystem_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/23_filesystem_layout.sh
@@ -94,6 +94,8 @@ read_filesystems_command="$read_filesystems_command | sort -t ' ' -k 1,1 -u"
                 # options: blocks, fragments, max_mount, check_interval, reserved blocks, bytes_per_inode
                 blocksize=$( $tunefs -l $device | tr -d '[:blank:]' | grep -oi 'Blocksize:[0-9]*' | cut -d ':' -f 2 )
                 echo -n " blocksize=$blocksize"
+                fragmentsize=$( $tunefs -l $device | tr -d '[:blank:]' | grep -oi 'Fragmentsize:[0-9]*' | cut -d ':' -f 2 )
+                echo -n " fragmentsize=$fragmentsize"
                 nr_blocks=$( $tunefs -l $device | tr -d '[:blank:]' | grep -iv reserved | grep -oi 'Blockcount:[0-9]*' | cut -d ':' -f 2 )
                 reserved_blocks=$( $tunefs -l $device | tr -d '[:blank:]' | grep -oi 'Reservedblockcount:[0-9]*' | cut -d ':' -f 2 )
                 reserved_percentage=$(( reserved_blocks * 100 / nr_blocks ))
@@ -117,8 +119,8 @@ read_filesystems_command="$read_filesystems_command | sort -t ' ' -k 1,1 -u"
                 ;;
             (vfat)
                 # Make sure we don't get any other output from dosfslabel (errors go to stdout :-/)
-                label=$(dosfslabel $device | tail -1)
-		uuid=$(blkid_uuid_of_device $device)
+                label=$(dosfslabel $device | tail -1 | sed -e 's/ /\\\\b/g')  # replace all " " with "\\b"
+                uuid=$(blkid_uuid_of_device $device)
                 echo -n " uuid=$uuid label=$label"
                 ;;
             (xfs)
