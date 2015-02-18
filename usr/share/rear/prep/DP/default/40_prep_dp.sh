0268d2e84f2ad1c09ae933423583dd8733c7b8e9
Gratien D'haese <gratien.dhaese@gmail.com>
2015-02-18 16:23:54 +0100

    removed  "${PROGS_DP[@]}" in prep/DP/default/40_prep_dp.sh (obsolete) - use COPY_AS_IS_DP instead

--- usr/share/rear/prep/DP/default/40_prep_dp.sh
+++ usr/share/rear/prep/DP/default/40_prep_dp.sh
@@ -4,4 +4,4 @@
 
 COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_DP[@]}" )
 COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_DP[@]}" )
-PROGS=( "${PROGS[@]}" "${PROGS_DP[@]}" join head col )
+PROGS=( "${PROGS[@]}" join head col )
