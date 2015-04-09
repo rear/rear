ba384e8025ce20706001d30bfeeb12b85bbef2b0
Gratien D'haese <gratien.dhaese@gmail.com>
2015-04-09 16:24:52 +0200

    * Changed the behavior of SSH_ROOT_PASSWORD - now saved as MD5 hash password. * explained in default.conf how we want it now * rescue/default/50_ssh.sh copies the hashed pw to the rescue /etc/shadow file, * and we still keep the old method too for backward compatability reasons with older releases of rear See issue #560

--- usr/share/rear/rescue/default/50_ssh.sh
+++ usr/share/rear/rescue/default/50_ssh.sh
@@ -19,38 +19,41 @@
 #
 if has_binary sshd; then
 
-	# assume that we have openssh with configs in /etc/ssh
+    # assume that we have openssh with configs in /etc/ssh
 
-	COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/ssh* /root/.s[s]h /root/.shos[t]s )
-	PROGS=(
-	${PROGS[@]}
-	ssh sshd scp sftp
-	$(
-		read subsys sftp file junk < <( grep sftp /etc/sshd_co[n]fig /etc/ssh/sshd_co[n]fig /etc/openssh/sshd_co[n]fig 2>&8 )
-		echo $file
-	)
-	)
+    COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/ssh* /root/.s[s]h /root/.shos[t]s )
+    PROGS=(
+    ${PROGS[@]}
+    ssh sshd scp sftp
+    $(
+        read subsys sftp file junk < <( grep sftp /etc/sshd_co[n]fig /etc/ssh/sshd_co[n]fig /etc/openssh/sshd_co[n]fig 2>&8 )
+        echo $file
+    )
+    )
 
-	# copy ssh user
-	if PASSWD_SSH=$(grep ssh /etc/passwd) ; then
-	# sshd:x:71:65:SSH daemon:/var/lib/sshd:/bin/false
-		echo "$PASSWD_SSH" >>$ROOTFS_DIR/etc/passwd
-		IFS=: read user ex uid gid gecos homedir junk <<<"$PASSWD_SSH"
-		# add ssh group to be collected later
-		CLONE_GROUPS=( "${CLONE_GROUPS[@]}" "$gid" )
-		mkdir -p $v -m 0700 "$ROOTFS_DIR$homedir" >&2
-		chown $v root.root "$ROOTFS_DIR$homedir" >&2
-	fi
+    # copy ssh user
+    if PASSWD_SSH=$(grep ssh /etc/passwd) ; then
+    # sshd:x:71:65:SSH daemon:/var/lib/sshd:/bin/false
+        echo "$PASSWD_SSH" >>$ROOTFS_DIR/etc/passwd
+        IFS=: read user ex uid gid gecos homedir junk <<<"$PASSWD_SSH"
+        # add ssh group to be collected later
+        CLONE_GROUPS=( "${CLONE_GROUPS[@]}" "$gid" )
+        mkdir -p $v -m 0700 "$ROOTFS_DIR$homedir" >&2
+        chown $v root.root "$ROOTFS_DIR$homedir" >&2
+    fi
 
-	echo "ssh:23:respawn:/bin/sshd -D" >>$ROOTFS_DIR/etc/inittab
+    echo "ssh:23:respawn:/bin/sshd -D" >>$ROOTFS_DIR/etc/inittab
 
-	# print a warning if there is no authorized_keys file for root
-	if test ! -f "/root/.ssh/authorized_keys" ; then
-		LogPrint "TIP: To login as root via ssh you need to set up /root/.ssh/authorized_keys or SSH_ROOT_PASSWORD in your configuration file"
-	fi
-	
-	# Set the SSH root password
-	if [[ $SSH_ROOT_PASSWORD ]] ; then
-		echo "root:$(echo $SSH_ROOT_PASSWORD | openssl passwd -1 -stdin):::::::" > $ROOTFS_DIR/etc/shadow
-	fi
+    # print a warning if there is no authorized_keys file for root
+    if test ! -f "/root/.ssh/authorized_keys" ; then
+        LogPrint "TIP: To login as root via ssh you need to set up /root/.ssh/authorized_keys or SSH_ROOT_PASSWORD in your configuration file"
+    fi
+    
+    # Set the SSH root password; if pw is hashed just copy it otherwise use openssl (for backward compatibility)
+    if [[ "$SSH_ROOT_PASSWORD" ]] ; then
+        case "$SSH_ROOT_PASSWORD" in
+        "$1$*") echo "root:$SSH_ROOT_PASSWORD:::::::" > $ROOTFS_DIR/etc/shadow ;;
+        *     ) echo "root:$(echo $SSH_ROOT_PASSWORD | openssl passwd -1 -stdin):::::::" > $ROOTFS_DIR/etc/shadow ;;
+        esac
+    fi
 fi
