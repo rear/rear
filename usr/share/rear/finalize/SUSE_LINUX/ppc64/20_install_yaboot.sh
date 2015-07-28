3a7e5bfca57be59a82779af59c2d8d42c6b9b21f
Dinar Valeev <dvaleev@suse.com>
2015-07-28 10:10:48 -0400

    Install yaboot with lilo script on SLES
    SLES provides lilo script wrapper to install yaboot
    correctly. It takes care about proper setup needed for
    boot partition. In addition mkofboot is not packaged.
    
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

---
+++ usr/share/rear/finalize/SUSE_LINUX/ppc64/20_install_yaboot.sh
@@ -0,0 +1,32 @@
+# Reinstall yaboot boot loader
+LogPrint "Installing PPC PReP Boot partition."
+
+# Find PPC PReP Boot partitions
+if test -f /mnt/local/etc/yaboot.conf; then
+  part=`awk -F '=' '/^boot=/ {print $2}' /mnt/local/etc/yaboot.conf`
+
+  if [ -n "$part" ]; then
+    LogPrint "Boot partion found: $part"
+    chroot /mnt/local /bin/bash --login -c "/sbin/lilo"
+    bootdev=`echo $part | sed -e 's/[0-9]*$//'`
+    LogPrint "Boot device is $bootdev."
+    NOBOOTLOADER=
+  else
+    bootparts=`sfdisk -l 2>&8 | awk '/PPC PReP Boot/ {print $1}'`
+    LogPrint "Boot partitions found: $bootparts."
+    for part in $bootparts
+    do
+      LogPrint "Initializing boot partition $part."
+      chroot /mnt/local /bin/bash --login -c "/sbin/lilo"
+    done
+    bootdev=`for part in $bootparts
+             do
+               echo $part | sed -e 's/[0-9]*$//'
+             done | sort | uniq`
+    LogPrint "Boot device list is $bootdev."
+    NOBOOTLOADER=
+  fi
+else
+  LogPrint "No bootloader configuration found. Install boot partition manually!"
+fi
+
