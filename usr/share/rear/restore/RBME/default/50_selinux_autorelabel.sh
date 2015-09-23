bf0d9d111dcb64826d124bead6251e616d1ef15b
Gratien D'haese <gratien.dhaese@gmail.com>
2015-09-23 09:12:18 +0200

    The 50_selinux_autorelabel.sh script became obsolete in the RBME tree as it has been moved to the default tree (related to issue #650)

--- usr/share/rear/restore/RBME/default/50_selinux_autorelabel.sh
+++
@@ -1,6 +0,0 @@
-# when the variable BACKUP_SELINUX_DISABLE is unset then silently return
-[[ ! "$BACKUP_SELINUX_DISABLE"  =~ ^[yY1] ]] && return
-
-# force relabeling after reboot of the recovered system
-touch /mnt/local/.autorelabel
-Log "Created /.autorelabel file : after reboot SELinux will relabel all files"
