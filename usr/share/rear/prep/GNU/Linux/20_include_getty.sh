cb21fcf4130109592f9318135e1365e97ac2b336
Gratien D'haese <gratien.dhaese@gmail.com>
2015-10-19 14:23:56 +0200

    In order to get rid of mingetty rpm dependency we must deal better we the different getty's in use. In prep/GNU/Linux/20_include_getty.sh we make sure our primary getty executable will be added to the REQUIRED_PROGS array to avoid a missing program in recovery mode. Furthermore, we add common getty programs to the PROGS array (kiss). We renamed the old script 20_include_agetty.sh to 20_include_serial_console.sh which covers the content better. All changes (or additions) are related to issue #661

---
+++ usr/share/rear/prep/GNU/Linux/20_include_getty.sh
@@ -0,0 +1,10 @@
+# 20_include_getty.sh
+# investigate which getty this system has in use and add it to the REQUIRED_PROGS array
+# older Linux distro's use usually mingetty, but newer ones stick to agetty to spawn ttys
+# except debian and ubuntu who are using getty instead
+
+ps ax | grep mingetty | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" mingetty )
+ps ax | grep agetty   | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" agetty )
+ps ax | grep "getty " | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" getty )  # mind the trailing space
+
+# if a REQUIRED_PROGS is missing rear will complain and stop
