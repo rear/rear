8412575e3b239030e282199b175fc46a5a82fd51
Gratien D'haese <gratien.dhaese@gmail.com>
2015-10-20 10:04:48 +0200

    use pgrep instead - issue #661

--- usr/share/rear/prep/GNU/Linux/20_include_getty.sh
+++ usr/share/rear/prep/GNU/Linux/20_include_getty.sh
@@ -3,8 +3,8 @@
 # older Linux distro's use usually mingetty, but newer ones stick to agetty to spawn ttys
 # except debian and ubuntu who are using getty instead
 
-ps ax | grep mingetty | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" mingetty )
-ps ax | grep agetty   | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" agetty )
-ps ax | grep "/getty" | grep -v grep && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" getty )  # mind the / to avoid confusion with agetty
+pgrep -nx mingetty 1>/dev/null && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" mingetty )
+pgrep -nx agetty   1>/dev/null && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" agetty )
+pgrep -nx getty    1>/dev/null && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" getty )
 
 # if a REQUIRED_PROGS is missing rear will complain and stop
