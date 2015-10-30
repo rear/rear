f93719ee8a487e76469d2d38b02f84e8391f6d84
Gratien D'haese <gratien.dhaese@gmail.com>
2015-10-30 21:22:32 +0100

    -new script: usr/share/rear/build/GNU/Linux/45_symlink_mingetty.sh  create a symlink to mingetty if getty or agetty is available  needed for backward compatibility -updated: usr/share/rear/prep/GNU/Linux/20_include_getty.sh  added comments
    See issue #661

--- usr/share/rear/prep/GNU/Linux/20_include_getty.sh
+++ usr/share/rear/prep/GNU/Linux/20_include_getty.sh
@@ -8,3 +8,9 @@ pgrep -nx agetty   1>/dev/null && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" agetty
 pgrep -nx getty    1>/dev/null && REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" getty )
 
 # if a REQUIRED_PROGS is missing rear will complain and stop
+
+# if above commands do not find any getty in the process tables (which is posssible thanks
+# to systemd stuff) then we have a fall back as these getty's are also defined in the
+# PROGS array in conf/GNU/Linux.conf
+# Script build/GNU/Linux/45_symlink_mingetty.sh will link a getty to mingetty to foresee
+# backwards compatibility
