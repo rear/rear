1713fa8e659aa8e90671be23d1078ab795356849
Gratien D'haese <gratien.dhaese@gmail.com>
2015-11-02 13:23:29 +0100

    new script: prep/default/40_save_mountpoint_details.sh updated script: restore/default/90_create_missing_directories.sh Purpose is to list up all directories and permissions so we can recreate any missing dir after recovery which usually the case in combination with backup software (like TSM, bareos,...) See issue #619

---
+++ usr/share/rear/prep/default/40_save_mountpoint_details.sh
@@ -0,0 +1,14 @@
+# 40_save_mountpoint_details.sh
+# Purpose of this script is to save the ownership and permissions of the mount points in a file
+# That file will be read during recovery time to recreate missing directories and set up the proper
+# permissions again (for the moment it is scattered across different flows)
+# Script restore/default/90_create_missing_directories.sh will recreate the missing dirs (all other
+# script may be deleted)
+
+# drwxr-xr-x.  20 root root  4096 Nov  2 07:44 /
+while read junk junk userid groupid junk junk junk junk dir
+do
+    echo $dir $(stat -c %a $dir) $userid $groupid >> "$VAR_DIR/recovery/mountpoint_permissions"
+done < <(ls -ld $(mount | grep -vE '(cgroup|fuse|nfs|/sys/|REAR-000)' | awk '{print $3}'))
+
+# output looks like / 755 root root
