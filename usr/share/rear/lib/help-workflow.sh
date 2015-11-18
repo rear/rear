88e0c104fbf5cdfe277d8f8d473273ba285023b9
Johannes Meixner <jsmeix@suse.com>
2015-11-18 15:49:46 +0100

    Some polishing for the debugscripts option handling and running the help workflow without creating stuff in /tmp so that now for the first time calling it e.g. as   rear --debugscripts 'ue +h -o pipefail' help seems to work well - at least for me and for now.

--- usr/share/rear/lib/help-workflow.sh
+++ usr/share/rear/lib/help-workflow.sh
@@ -21,6 +21,7 @@
 LOCKLESS_WORKFLOWS=( ${LOCKLESS_WORKFLOWS[@]} help )
 
 function WORKFLOW_help () {
+
 cat <<EOF
 Usage: $PROGRAM [-h|--help] [-V|--version] [-dsSv] [-D|--debugscripts SET] [-c DIR] [-r KERNEL] [--] COMMAND [ARGS...]
 
@@ -32,7 +33,7 @@ Available options:
  -c DIR              alternative config directory; instead of /etc/rear
  -d                  debug mode; log debug messages
  -D                  debugscript mode; log every function call (via 'set -x')
- --debugscripts SET  same as -D but with 'set -SET'
+ --debugscripts SET  same as -d -v -D but debugscript mode with 'set -SET'
  -r KERNEL           kernel version to use; current: '$KERNEL_VERSION'
  -s                  simulation mode; show what scripts rear would include
  -S                  step-by-step mode; acknowledge each script individually
@@ -40,23 +41,18 @@ Available options:
  -V --version        version information
 
 List of commands:
-$(
-    for workflow in ${WORKFLOWS[@]} ; do
-        description=WORKFLOW_${workflow}_DESCRIPTION
-        # when description is empty then ${!description} uses an unbound variable:
-        if test -n "$description" ; then
-            if [[ "${!description}" ]] ; then
-                if [[ -z "$RECOVERY_MODE" && "$workflow" != "recover" ]] ; then
-                    printf " %-16s%s\n" $workflow "${!description}"
-                elif [[ "$RECOVERY_MODE" && "$workflow" == "recover" ]] ; then
-                    printf " %-16s%s\n" $workflow "${!description}"
-                fi
-            fi
-        fi
-    done
-)
-
 EOF
+
+for workflow in ${WORKFLOWS[@]} ; do
+    description=WORKFLOW_${workflow}_DESCRIPTION
+    # in some workflows WORKFLOW_${workflow}_DESCRIPTION
+    # is only defined if "$VERBOSE" is set - currently (18. Nov. 2015) for those
+    # WORKFLOW_savelayout_DESCRIPTION WORKFLOW_shell_DESCRIPTION WORKFLOW_udev_DESCRIPTION
+    # so that an empty default is used to avoid that ${!description} is an unbound variable:
+    if test -n "${!description:-}" ; then
+        printf " %-16s%s\n" $workflow "${!description:-}"
+    fi
+done
 
 if test -z "$VERBOSE" ; then
     echo "Use 'rear -v help' for more advanced commands."
