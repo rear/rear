46212bc0d76f306cedb55a654c57c01f68621943
Vladimir Gozora <c@gozora.sk>
2016-04-06 12:18:04 +0200

    Complete removal of efiboot_img_size() function, and move of size detection code to main part of the script.

--- usr/share/rear/output/ISO/Linux-i386/70_create_efibootimg.sh
+++ usr/share/rear/output/ISO/Linux-i386/70_create_efibootimg.sh
@@ -1,48 +1,18 @@
 # 70_create_efibootimg.sh
 is_true $USING_UEFI_BOOTLOADER || return    # empty or 0 means NO UEFI
 
-# function to calculate exact size of EFI virtual image (efiboot.img)
-function efiboot_img_size {
-    # Get size of directory holding EFI virtual image content.
-    # The virtual image must be aligned to 32MiB blocks
-    # therefore the size of directory is measured in 32MiB blocks.
-    efi_img_dir=$1
-    
-    # Specify a minimum EFI virtual image size measured in 32MiB blocks:
-    case "$( basename $UEFI_BOOTLOADER )" in
-        (shim.efi|elilo.efi)
-            # minimum EFI virtual image size for shim and elilo
-            # default: 160MiB = 5 * 32MiB blocks
-            efi_img_min_sz=5
-        ;;
-        (*)
-            # minimum EFI virtual image size for grub
-            # default: 32MiB = 1 * 32MiB block
-            efi_img_min_sz=1
-        ;;
-    esac
-    
-    # Fallback output of the minimum EFI virtual image size measured in 32MiB blocks:
-    test $efi_img_dir || { echo $efi_img_min_sz ; return ; }
-    
-    # The du output is stored in an artificial bash array
-    # so that $efi_img_sz can be simply used to get the first word
-    # which is the efi_img_sz value measured in 32MiB blocks:
-    efi_img_sz=( $( du --block-size=32M --summarize $efi_img_dir ) )
-    
-    # Fallback output of the minimum EFI virtual image size measured in 32MiB blocks:
-    test $efi_img_sz -ge 1 || { echo $efi_img_min_sz ; return ; }
-    
-    # Output at least the minimum EFI virtual image size measured in 32MiB blocks:
-    if test $efi_img_sz -lt $efi_img_min_sz ; then
-        echo $efi_img_min_sz
-    else
-        echo $efi_img_sz
-    fi
-}
+# Calculate exact size of EFI virtual image (efiboot.img):
+# Get size of directory holding EFI virtual image content.
+# The virtual image must be aligned to 32MiB blocks
+# therefore the size of directory is measured in 32MiB blocks.
+# The du output is stored in an artificial bash array
+# so that $efi_img_sz can be simply used to get the first word
+# which is the disk usage of the directory measured in 32MiB blocks:
+efi_img_sz=( $( du --block-size=32M --summarize $TMP_DIR/mnt ) )
+StopIfError "Failed to determine disk usage of EFI virtual image content directory."
 
-# prepare EFI virtual image
-dd if=/dev/zero of=$TMP_DIR/efiboot.img count=$( efiboot_img_size $TMP_DIR/mnt ) bs=32M
+# prepare EFI virtual image aligned to 32MiB blocks:
+dd if=/dev/zero of=$TMP_DIR/efiboot.img count=$efi_img_sz bs=32M
 mkfs.vfat $v -F 16 $TMP_DIR/efiboot.img >&2
 mkdir -p $v $TMP_DIR/efi_virt >&2
 mount $v -o loop -t vfat -o fat=16 $TMP_DIR/efiboot.img $TMP_DIR/efi_virt >&2
