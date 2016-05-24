a4adb74bf23921f0810b7de46f98e205912aed1c
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2016-05-24 10:25:55 +0200

    Additional verification before creating a new grub file. * Add verification of /boot UUID consistency before creating new grub file.

--- usr/share/rear/output/default/94_grub2_rescue.sh
+++ usr/share/rear/output/default/94_grub2_rescue.sh
@@ -104,8 +104,12 @@ if [[ ! -x /etc/grub.d/01_users ]]; then
     chmod 755 /etc/grub.d/01_users
 fi
 
-#Finding UUID of filesystem containing /boot 
+#Finding UUID of filesystem containing /boot
 grub_boot_uuid=$(df /boot | awk 'END {print $1}' | xargs blkid -s UUID -o value)
+
+#Stop if $grub_boot_uuid is not a valid UUID
+blkid -U $grub_boot_uuid > /dev/null 2>&1
+StopIfError "$grub_boot_uuid is not a valid UUID"
 
 #Creating REAR grub menu entry
 echo "#!/bin/bash
