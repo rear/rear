a37f18d2f1032ef93abf33c64eded06555211e28
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2016-06-11 14:55:09 +0200

    Limit ppc64/ppc64le bootlist parameter to 5 entries
    bootlist is not set when more than 5 paths is given.
    This is particulary true when boot device is multipathed.
    We need to check and limit to 5 the number of path given to the bootlist command.

--- usr/share/rear/finalize/Linux-ppc64/22_install_grub2.sh
+++ usr/share/rear/finalize/Linux-ppc64/22_install_grub2.sh
@@ -48,7 +48,18 @@ if [[ -r "$LAYOUT_FILE" ]]; then
         if ! grep -q "PowerNV" /proc/cpuinfo && ! grep -q "emulated by qemu" /proc/cpuinfo ; then
             bootdev=`echo $part | sed -e 's/[0-9]*$//'`
             LogPrint "Boot device is $bootdev."
-            bootlist -m normal $bootdev
+
+            # Test if $bootdev is a multipath device
+            if dmsetup ls --target multipath | grep -w ${bootdev#/dev/mapper/} >/dev/null 2>&1; then
+                LogPrint "Limiting bootlist to 5 entries..."
+                bootlist_path=$(dmsetup deps $bootdev -o devname | awk -F: '{gsub (" ",""); gsub("\\(","/dev/",$2) ; gsub("\\)"," ",$2) ; print $2}' | cut -d" " -f-5)
+                LogPrint "bootlist will be $bootlist_path"
+                bootlist -m normal $bootlist_path
+                LogIfError "Unable to set bootlist. You will have to start in SMS to set it up manually."
+            else
+                LogPrint "bootlist will be $bootdev"
+                bootlist -m normal $bootdev
+            fi
         fi
         NOBOOTLOADER=
     fi
