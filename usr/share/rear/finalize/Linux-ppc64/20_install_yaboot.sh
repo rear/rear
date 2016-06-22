381b4c13afcfd88bd47f08d12fe3902d068fcba1
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2016-06-22 14:40:39 +0200

    CLEANUP : Remove backticks in 20_install_yaboot.sh

--- usr/share/rear/finalize/Linux-ppc64/20_install_yaboot.sh
+++ usr/share/rear/finalize/Linux-ppc64/20_install_yaboot.sh
@@ -12,22 +12,22 @@ part=$( awk -F '=' '/^boot=/ {print $2}' $TARGET_FS_ROOT/etc/yaboot.conf )
 if [ -n "$part" ]; then
   LogPrint "Boot partion found: $part"
   chroot $TARGET_FS_ROOT /bin/bash --login -c "/sbin/mkofboot -b $part --filesystem raw -f"
-  bootdev=`echo $part | sed -e 's/[0-9]*$//'`
+  bootdev=$( echo $part | sed -e 's/[0-9]*$//' )
   LogPrint "Boot device is $bootdev."
   bootlist -m normal $bootdev
   NOBOOTLOADER=
 else
-  bootparts=`sfdisk -l 2>&1 | awk '/PPC PReP Boot/ {print $1}'`
+  bootparts=$( sfdisk -l 2>&1 | awk '/PPC PReP Boot/ {print $1}' )
   LogPrint "Boot partitions found: $bootparts."
   for part in $bootparts
   do
     LogPrint "Initializing boot partition $part."
     chroot $TARGET_FS_ROOT /bin/bash --login -c "/sbin/mkofboot -b $part --filesystem raw -f"
   done
-  bootdev=`for part in $bootparts
+  bootdev=$( for part in $bootparts
            do
              echo $part | sed -e 's/[0-9]*$//'
-           done | sort | uniq`
+         done | sort | uniq )
   LogPrint "Boot device list is $bootdev."
   bootlist -m normal $bootdev
   NOBOOTLOADER=
