815307badaded9d3d7e7d7eba401d9d702815a11
Johannes Meixner <jsmeix@suse.com>
2016-07-21 15:34:26 +0200

    fixed handling when backup directory does not yet exist (issue926)

--- usr/share/rear/prep/NETFS/default/40_automatic_exclude_recreate.sh
+++ usr/share/rear/prep/NETFS/default/40_automatic_exclude_recreate.sh
@@ -14,16 +14,19 @@ case $scheme in
         # if user added path manually then there is no need to do it again
         # FIXME: I <jsmeix@suse.de> have no idea what the above comment line means.
         #
-        # When the backup directory does not yet exist, 'df -P' results nothing on stdout and
-        # when the backup directory does not yet exist it means it will be created at '/'.
-        # But when the backup is stored in a directory on the same filesystem as '/' is
-        # then it results a backup.tar.gz that contains itself in a non-yet-complete state
+        # When the backup is stored in a directory on the same filesystem as '/' is
+        # it results a backup.tar.gz that contains itself (but in a not-yet-complete state)
         # because all of the '/' filesystem is included in the backup.
         # To avoid various weird issues when the backup contains itself
         # a backup directory in the '/' filesystem is simply forbidden
         # regardless that a backup inside itself may not result fatal errors
         # see https://github.com/rear/rear/issues/926
-        test -e "$backup_directory" || Error "URL '$BACKUP_URL' would result the backup directory '$backup_directory' in the '/' filesystem which is forbidden."
+        if ! test -e "$backup_directory" ; then
+            # When the backup directory does not yet exist, 'df -P' results nothing on stdout
+            # which means the backup directory must be created so that 'df -P' can show its mountpoint
+            # to find out whether or not the backup directory would be in the '/' filesystem:
+            mkdir $v -p "$backup_directory" >&2 || Error "Could not create backup directory '$backup_directory' (from URL '$BACKUP_URL')."
+        fi
         test -d "$backup_directory" || Error "URL '$BACKUP_URL' specifies '$backup_directory' which is not a directory."
         backup_directory_mountpoint=$( df -P "$backup_directory" 2>/dev/null | tail -1 | awk '{print $6}' )
         test "/" = "$backup_directory_mountpoint" && Error "URL '$BACKUP_URL' has the backup directory '$backup_directory' in the '/' filesystem which is forbidden."
