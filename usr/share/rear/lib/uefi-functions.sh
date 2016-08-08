b81693f27a41482ed89da36a9af664fe808f8186
gozora <c@gozora.sk>
2016-08-08 18:33:53 +0200

    Corrected bug reported in issue #866 where distributions without grub modules stored in /usr/lib/grub/x86_64-efi got incomplete bootx64.efi loader.
    Corrected bug (not yet reported) when SLES12 SP1 boot ends with
    kernel panic due missing root variable in grub2.

--- usr/share/rear/lib/uefi-functions.sh
+++ usr/share/rear/lib/uefi-functions.sh
@@ -38,7 +38,6 @@ function trim {
 
 function build_bootx86_efi {
     local gmkimage
-    [[ ! -d /usr/lib/grub/x86_64-efi ]] && return
     if has_binary grub-mkimage; then
         gmkimage=grub-mkimage
     elif has_binary grub2-mkimage; then
@@ -47,6 +46,6 @@ function build_bootx86_efi {
         Log "Did not find grub-mkimage (cannot build bootx86.efi)"
         return
     fi
-    $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -d /usr/lib/grub/x86_64-efi -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" part_gpt part_msdos fat ext2 normal chain boot configfile linux linuxefi multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo
+    $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" part_gpt part_msdos fat ext2 normal chain boot configfile linux linuxefi multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs
     StopIfError "Error occurred during $gmkimage of BOOTX64.efi"
 }
