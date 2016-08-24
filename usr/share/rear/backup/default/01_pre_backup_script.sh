9587efd60183ddb1741f40e8a7ada86b5b928f3e
Didac Oliveira <didac@brainupdaters.net>
2016-08-24 13:16:36 +0200

    Solved error with exit tasks on PRE/POST backup scripts

--- usr/share/rear/backup/default/01_pre_backup_script.sh
+++ usr/share/rear/backup/default/01_pre_backup_script.sh
@@ -1,5 +1,5 @@
 if test "$PRE_BACKUP_SCRIPT" ; then
     Log "Running PRE_BACKUP_SCRIPT '${PRE_BACKUP_SCRIPT[@]}'"
-    AddExitTask "$(eval "${POST_BACKUP_SCRIPT[@]}")"
+    AddExitTask "${POST_BACKUP_SCRIPT[@]}"
     eval "${PRE_BACKUP_SCRIPT[@]}"
 fi
