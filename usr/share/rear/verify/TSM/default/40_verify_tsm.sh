3809b0e1831e14e418a47fffff386667d75adb29
Sebastien Termeau <st@tdchosting.dk>
2016-08-29 10:21:57 +0200

    Documentation

--- usr/share/rear/verify/TSM/default/40_verify_tsm.sh
+++ usr/share/rear/verify/TSM/default/40_verify_tsm.sh
@@ -7,7 +7,9 @@
 # 2009-10-12 Schlomo as part of a code review to fix all occurences of [*]
 #
 #
-# read TSM vars from TSM config files
+# read TSM vars from TSM config files ( look a bit like an init file with KEY=VALUE pairs )
+# The keys are loaded into environment variables called TSM_SYS_$KEY
+# Since TSM has several options that have a dot in their name, the dot is replaced by an underscore
 # read dsm.sys
 while read KEY VALUE ; do echo "$KEY" | grep -q '*' && continue ; test -z "$KEY" && continue ; KEY="$(echo "$KEY" | tr a-z. A-Z_)" ; export TSM_SYS_$KEY="${VALUE//\"}" ; done </opt/tivoli/tsm/client/ba/bin/dsm.sys
 # read dsm.opt
