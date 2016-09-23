4b0f7c0ceeff848a783de4ce586b608e241d9d9c
Johannes Meixner <jsmeix@suse.com>
2016-09-23 11:52:38 +0200

    Recreated symbolic links (32_check_cdrom_size.sh 10_check_rsync.sh 15_check_rsync_protocol_version.sh) that had been accidentally rewritten as regular files in previous commit

--- usr/share/rear/prep/OBDR/default/32_check_cdrom_size.sh
+++ usr/share/rear/prep/OBDR/default/32_check_cdrom_size.sh
@@ -1,17 +1,1 @@
-# This file is part of Relax-and-Recover, licensed under the GNU General
-# Public License. Refer to the included LICENSE for full text of license.
-# check_cdrom_size.sh
-while read DEV total used available junk
-do
-	case $DEV in
-	*/*)
-		let available=available/1024 # convert k-blocks to MBytes
-		test "${available}" -gt "${CDROM_SIZE}"
-		StopIfError "Not enough space in ${ISO_DIR} [$DEV]: only ${available} MB free, need ${CDROM_SIZE} MB"
-		Log "ISO Directory '${ISO_DIR}' [$DEV] has $available MB free space"
-	;;
-	*)
-	;;
-	esac
-done < <(df -kP "${ISO_DIR}")
-
+../../ISO/default/32_check_cdrom_size.sh
