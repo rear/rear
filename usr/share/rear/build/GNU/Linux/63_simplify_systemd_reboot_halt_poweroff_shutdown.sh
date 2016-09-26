dd1dac80e389c80a6c01323b647332353e990444
Johannes Meixner <jsmeix@suse.com>
2016-09-26 15:59:38 +0200

    New script build/GNU/Linux/63_simplify_systemd_reboot_halt_poweroff_shutdown.sh replaces in the rear recovery system /bin/reboot /bin/halt /bin/poweroff /bin/shutdown by scripts that basically do umount -a and systemctl --force [reboot halt poweroff] to make reboot halt poweroff and shutdown work simpler and more fail-safe (issue 953).

---
+++ usr/share/rear/build/GNU/Linux/63_simplify_systemd_reboot_halt_poweroff_shutdown.sh
@@ -0,0 +1,36 @@
+
+# In case of systemd simplify how reboot halt poweroff and shutdown works
+# to make it more fail-safe, cf. https://github.com/rear/rear/issues/953
+
+# Skip if systemd is not used:
+test -d $ROOTFS_DIR/usr/lib/systemd/system || return 0
+
+# Replace reboot halt and poweroff by simple scripts that run
+# "umount -a ; systemctl --force [reboot halt poweroff]"
+for command in reboot halt poweroff ; do
+cat <<EOF >$ROOTFS_DIR/bin/$command
+#!/bin/bash
+echo umounting all filesystems
+umount -vfar &>/dev/null
+echo $command in 3 seconds...
+sleep 3
+systemctl --force $command
+EOF
+done
+
+# Because there is no "systemctl shutdown" replace shutdown
+# by a more elaborated script that calls by default poweroff
+# but when '-r' or '--reboot' is specified it calls reboot and
+# when '-H' or '--halt' is specified it calls halt:
+cat <<'EOF' >$ROOTFS_DIR/bin/shutdown
+#!/bin/bash
+command=poweroff
+for arg in "$@" ; do
+    case "$arg" in
+        (-r|--reboot) command=reboot ;;
+        (-H|--halt)   command=halt ;;
+    esac
+done
+$command
+EOF
+
