5f988a7beb7c8f7004f1ff9c16e8c03068bfeccd
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2016-11-06 16:10:54 +0100

    Allow  yaboot bootloader creation with LILO even when yaboot.conf does not exist. + SLES11 ppc64 with /boot in LVM has no yaboot.conf (it is including in the PReP partition). + Scan for PReP Partition and recreate Yaboot bootloader.

--- usr/share/rear/finalize/SUSE_LINUX/ppc64/200_install_yaboot.sh
+++ usr/share/rear/finalize/SUSE_LINUX/ppc64/200_install_yaboot.sh
@@ -3,15 +3,19 @@ LogPrint "Installing PPC PReP Boot partition."
 
 # Find PPC PReP Boot partitions
 if test -f $TARGET_FS_ROOT/etc/yaboot.conf; then
-  part=$( awk -F '=' '/^boot=/ {print $2}' $TARGET_FS_ROOT/etc/yaboot.conf )
+    part=$( awk -F '=' '/^boot=/ {print $2}' $TARGET_FS_ROOT/etc/yaboot.conf )
+fi
 
-  if [ -n "$part" ]; then
+if [ -n "$part" ]; then
     LogPrint "Boot partion found: $part"
     chroot $TARGET_FS_ROOT /bin/bash --login -c "/sbin/lilo"
     bootdev=`echo $part | sed -e 's/[0-9]*$//'`
     LogPrint "Boot device is $bootdev."
     NOBOOTLOADER=
-  else
+else
+    # Allow Yaboot bootloader to be recreated even if there is no yaboot.conf
+    # (SLES11 ppc64 with /boot in LVM does not have /etc/yaboot.conf).
+    LogPrint "Scanning disks for PPC PReP Boot partition..."
     bootparts=`sfdisk -l 2>&8 | awk '/PPC PReP Boot/ {print $1}'`
     LogPrint "Boot partitions found: $bootparts."
     for part in $bootparts
@@ -25,8 +29,8 @@ if test -f $TARGET_FS_ROOT/etc/yaboot.conf; then
              done | sort | uniq`
     LogPrint "Boot device list is $bootdev."
     NOBOOTLOADER=
-  fi
-else
-  LogPrint "No bootloader configuration found. Install boot partition manually!"
 fi
 
+if [[ -n $NOBOOTLOADER ]]; then
+  LogPrint "No bootloader configuration found. Install boot partition manually!"
+fi
