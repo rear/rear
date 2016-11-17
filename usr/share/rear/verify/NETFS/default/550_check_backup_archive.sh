384d869c6a17b31f6dbdf70496f904609dea896a
Johannes Meixner <jsmeix@suse.com>
2016-11-17 15:11:23 +0100

    Fixed syntax error in 550_check_backup_archive.sh

--- usr/share/rear/verify/NETFS/default/550_check_backup_archive.sh
+++ usr/share/rear/verify/NETFS/default/550_check_backup_archive.sh
@@ -24,7 +24,7 @@ for restoreinput in "${RESTORE_ARCHIVES[@]}" ; do
     test "$backuparchive_size" && LogPrint "Backup archive size is $backuparchive_size ${BACKUP_PROG_COMPRESS_SUFFIX:+(compressed)}"
 
     if is_true "$BACKUP_INTEGRITY_CHECK" && test -f $restoreinput.md5 ; then
-        if ! test -f "$backup_splitted_file ; then
+        if ! test -f "$backup_splitted_file" ; then
             LogPrint "Checking integrity of $restoreinput_filename"
             md5sum -c $restoreinput.md5 || Error "Integrity check failed. Restore aborted because BACKUP_INTEGRITY_CHECK is enabled."
         fi
