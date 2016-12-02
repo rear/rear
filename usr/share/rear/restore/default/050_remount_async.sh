81cc19d3af0e90b3879f12afc68547b9e771cfd6
Johannes Meixner <jsmeix@suse.com>
2016-12-02 12:14:32 +0100

    Skip remount sync when systemd is used (issue1097)

--- usr/share/rear/restore/default/050_remount_async.sh
+++ usr/share/rear/restore/default/050_remount_async.sh
@@ -3,12 +3,15 @@
 # otherwise the restore would be terribly slow.
 # Things may have been remounted with sync option in a preceding recover WORKFLOW
 # via finalize/default/900_remount_sync.sh
+# see also https://github.com/rear/rear/issues/1097
 #
-if test "restoreonly" = "$WORKFLOW" ; then
-    while read mountpoint device mountby filesystem junk ; do
-        if ! mount -o remount,async "${device}" $TARGET_FS_ROOT"$mountpoint" ; then
-            LogPrint "Remount async of '${device}' failed which can result very slow restore"
-        fi
-    done < "${VAR_DIR}/recovery/mountpoint_device"
-fi
+
+# Skip if not restoreonly WORKFLOW:
+test "restoreonly" = "$WORKFLOW" || return 0
+
+while read mountpoint device mountby filesystem junk ; do
+    if ! mount -o remount,async "${device}" $TARGET_FS_ROOT"$mountpoint" ; then
+        LogPrint "Remount async of '${device}' failed which can result very slow restore"
+    fi
+done < "${VAR_DIR}/recovery/mountpoint_device"
 
