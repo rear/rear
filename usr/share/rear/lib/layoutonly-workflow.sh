0a755decf6ff6f6bfbd0c56c289c7ea3e9901f24
Johannes Meixner <jsmeix@suse.com>
2016-12-06 12:25:52 +0100

    Documentation for multiple backups (issue 1104)

--- usr/share/rear/lib/layoutonly-workflow.sh
+++ usr/share/rear/lib/layoutonly-workflow.sh
@@ -5,8 +5,11 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-WORKFLOW_layoutonly_DESCRIPTION="recreate only the disk layout"
+WORKFLOW_layoutonly_DESCRIPTION="only recreate the disk layout"
 WORKFLOWS=( ${WORKFLOWS[@]} layoutonly )
+# The layoutonly workflow is a part (a strict subset) of the recover workflow
+# by skipping those part of the recover workflow that are not needed
+# when the task is to only recreate the disk layout (and nothing else):
 function WORKFLOW_layoutonly () {
     SourceStage "setup"
     # SourceStage "verify"
