2be8506a6b3762ffdda3dde071e00ff0e0d60879
Johannes Meixner <jsmeix@suse.com>
2016-12-08 12:31:52 +0100

    New USB_DEVICE_PARTED_LABEL variable and some cleanup of 300_format_usb_disk.sh (issue 1105)

--- usr/share/rear/format/USB/default/300_format_usb_disk.sh
+++ usr/share/rear/format/USB/default/300_format_usb_disk.sh
@@ -6,64 +6,54 @@ umount $REAL_USB_DEVICE &>/dev/null
 
 LogPrint "Repartitioning '$RAW_USB_DEVICE'"
 
-echo "Yes" | parted -s $RAW_USB_DEVICE mklabel msdos >&2
-StopIfError "Could not create initial msdos partition table on '$REAL_USB_DEVICE'"
-
 test "$USB_DEVICE_FILESYSTEM_PERCENTAGE" || USB_DEVICE_FILESYSTEM_PERCENTAGE="100"
 
 if [[ "$EFI" == "y" ]]; then
-    LogPrint "The --efi toggle was used with format - make an EFI bootable device '$REAL_USB_DEVICE'"
-
-    # Prompt user for size of EFI partition on USB disk
-    # Pressing Enter (\n) will use USB_UEFI_PART_SIZE (default in default.conf) or fallback here:
-    test "$USB_UEFI_PART_SIZE" || USB_UEFI_PART_SIZE="100"
-    echo -n "Enter size of EFI partition on '$REAL_USB_DEVICE' in MB [default $USB_UEFI_PART_SIZE MB]: "
-    read efi_part_size
-
-    # Check if user entered unsigned integer larger than 0
-    if [[ "${efi_part_size}" =~ ^[0-9]+$ && ${efi_part_size} > 0 ]]; then
-        USB_UEFI_PART_SIZE=${efi_part_size}
-        LogPrint "Creating EFI partition on '$REAL_USB_DEVICE' with size $USB_UEFI_PART_SIZE MB."
-    # We did not read anything, used defaults
-    elif [[ -z ${efi_part_size} ]]; then
-        LogPrint "Creating EFI partition on '$REAL_USB_DEVICE' with default size $USB_UEFI_PART_SIZE MB."
-    # User input was not correct ...
-    else
-        Error "Bad input for EFI partition size."
-    fi
-
-    echo "Yes" | parted -s $RAW_USB_DEVICE -- mklabel gpt mkpart primary 0 ${USB_UEFI_PART_SIZE}Mib mkpart primary ${USB_UEFI_PART_SIZE}Mib ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% >&2
-
-    StopIfError "Could not create GPT partitions on '$REAL_USB_DEVICE'"
-    # partition 1 is the ESP (vfat partition) on which EFI/BOOT/BOOTX86.EFI resides
+    LogPrint "The --efi toggle was used with format - making an EFI bootable device '$RAW_USB_DEVICE'"
+    # Prompt user for size of EFI system partition on USB disk if no valid value is specified:
+    while ! [[ "$USB_UEFI_PART_SIZE" =~ ^[0-9]+$ && $USB_UEFI_PART_SIZE > 0 ]] ; do
+        # When USB_UEFI_PART_SIZE is empty, do not tell about "Invalid EFI partition size value":
+        test "$USB_UEFI_PART_SIZE" && echo "Invalid EFI system partition size value '$USB_UEFI_PART_SIZE' (must be unsigned integer larger than 0)"
+        echo -n "Enter size for EFI system partition on '$RAW_USB_DEVICE' in MB (plain 'Enter' defaults to 100 MB): "
+        read USB_UEFI_PART_SIZE
+        # Plain 'Enter' defaults to 100 MB (same as the default value in default.conf):
+        test "$USB_UEFI_PART_SIZE" || USB_UEFI_PART_SIZE="100"
+    done
+    LogPrint "Creating GUID partition table (GPT) on '$RAW_USB_DEVICE'"
+    parted -s $RAW_USB_DEVICE mklabel gpt >&2 || Error "Failed to create GPT partition table on '$RAW_USB_DEVICE'"
+    LogPrint "Creating EFI system partition with size $USB_UEFI_PART_SIZE MB on '$RAW_USB_DEVICE'"
+    parted -s $RAW_USB_DEVICE mkpart primary 0 ${USB_UEFI_PART_SIZE}Mib || Error "Failed to create EFI system partition on '$RAW_USB_DEVICE'"
+    LogPrint "Creating ReaR data partition up to ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% on '$RAW_USB_DEVICE'"
+    parted -s $RAW_USB_DEVICE mkpart primary ${USB_UEFI_PART_SIZE}Mib ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% >&2 || Error "Failed to create ReaR data partition on '$RAW_USB_DEVICE'"
+    # partition 1 is the EFI system partition (vfat partition) on which EFI/BOOT/BOOTX86.EFI resides
     ParNr=2
 else
-    echo "Yes" | parted -s $RAW_USB_DEVICE mkpart primary 0 ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% >&2
-    StopIfError "Could not create a primary partition on '$REAL_USB_DEVICE'"
+    test "msdos" = "$USB_DEVICE_PARTED_LABEL" -o "gpt" = "$USB_DEVICE_PARTED_LABEL" || USB_DEVICE_PARTED_LABEL="msdos"
+    LogPrint "Creating partition table of type '$USB_DEVICE_PARTED_LABEL' on '$RAW_USB_DEVICE'"
+    parted -s $RAW_USB_DEVICE mklabel $USB_DEVICE_PARTED_LABEL >&2 || Error "Failed to create $USB_DEVICE_PARTED_LABEL partition table on '$RAW_USB_DEVICE'"
+    LogPrint "Creating ReaR data partition with size ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% on '$RAW_USB_DEVICE'"
+    parted -s $RAW_USB_DEVICE mkpart primary 0 ${USB_DEVICE_FILESYSTEM_PERCENTAGE}% >&2 || Error "Failed to create ReaR data partition on '$RAW_USB_DEVICE'"
     ParNr=1
 fi
 
-echo "Yes" | parted -s $RAW_USB_DEVICE set 1 boot on >&2
-StopIfError "Could not make primary partition bootable on '$REAL_USB_DEVICE'"
+parted -s $RAW_USB_DEVICE set 1 boot on >&2 || Error "Could not make first partition bootable on '$RAW_USB_DEVICE'"
 
 partprobe $RAW_USB_DEVICE
-
 # Wait until udev has had the time to kick in
 sleep 5
 
 if [[ "$EFI" == "y" ]]; then
-    LogPrint "Creating vfat filesystem on EFI system partition (ESP) '${RAW_USB_DEVICE}1'"
-    mkfs.vfat $v -F 16 -n REAR-EFI ${RAW_USB_DEVICE}1 >&2
-
+    LogPrint "Creating vfat filesystem on EFI system partition on '${RAW_USB_DEVICE}1'"
+    mkfs.vfat $v -F 16 -n REAR-EFI ${RAW_USB_DEVICE}1 >&2 || Error "Failed to create vfat filesystem on '${RAW_USB_DEVICE}1'"
     # create link for EFI partition in /dev/disk/by-label
     partprobe $RAW_USB_DEVICE
+    # Wait until udev has had the time to kick in
     sleep 5
 fi
 
-LogPrint "Creating $USB_DEVICE_FILESYSTEM filesystem on '${RAW_USB_DEVICE}${ParNr}'"
-mkfs.$USB_DEVICE_FILESYSTEM -L REAR-000 ${RAW_USB_DEVICE}${ParNr} >&2
-StopIfError "Could not format '${RAW_USB_DEVICE}${ParNr}' with $USB_DEVICE_FILESYSTEM filesystem"
+LogPrint "Creating $USB_DEVICE_FILESYSTEM filesystem with label 'REAR-000' on '${RAW_USB_DEVICE}${ParNr}'"
+mkfs.$USB_DEVICE_FILESYSTEM -L REAR-000 ${RAW_USB_DEVICE}${ParNr} >&2 || Error "Failed to create $USB_DEVICE_FILESYSTEM filesystem on '${RAW_USB_DEVICE}${ParNr}'"
 
-tune2fs -c 0 -i 0 -o acl,journal_data,journal_data_ordered ${RAW_USB_DEVICE}${ParNr} >&2
-StopIfError "Failed to change filesystem characteristics on '${RAW_USB_DEVICE}${ParNr}'"
+LogPrint "Adjusting filesystem parameters on '${RAW_USB_DEVICE}${ParNr}'"
+tune2fs -c 0 -i 0 -o acl,journal_data,journal_data_ordered ${RAW_USB_DEVICE}${ParNr} >&2 || Error "Failed to adjust filesystem parameters on '${RAW_USB_DEVICE}${ParNr}'"
 
