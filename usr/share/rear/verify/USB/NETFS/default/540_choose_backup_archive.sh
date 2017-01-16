550916d79366cd8ce8513c96455252f1f5be8c3f
Johannes Meixner <jsmeix@suse.com>
2017-01-16 14:53:25 +0100

    Make USB backup selection work (issue1166)

--- usr/share/rear/verify/USB/NETFS/default/540_choose_backup_archive.sh
+++ usr/share/rear/verify/USB/NETFS/default/540_choose_backup_archive.sh
@@ -14,39 +14,52 @@ test "usb" = "$scheme" || return
 # test "$USB_SUFFIX" would result true because test " " results true:
 test $USB_SUFFIX && return
 
+# When the RESTORE_ARCHIVES array is not empty, there is no need to disrupt
+# "rear recover" or "rear restoreonly" and ask the user for the backup archive.
+# For the 'test' one must have all array members as a single word i.e. "${name[*]}"
+# because it should succeed when there is any non-empty array member, not necessarily the first one:
+test "${RESTORE_ARCHIVES[*]}" && return
+
 # Detect all backups on the USB device.
-# FIXME: This fails when the backup archive name is not
+# TODO: This fails when the backup archive name is not
 # ${BACKUP_PROG_ARCHIVE}${BACKUP_PROG_SUFFIX}${BACKUP_PROG_COMPRESS_SUFFIX}
 # so that in particular it fails for incremental/differential backups
 # but incremental/differential backups usually require several backup archives
 # to be restored (one full backup plus one differential or several incremental backups)
 # cf. RESTORE_ARCHIVES in usr/share/rear/prep/NETFS/default/070_set_backup_archive.sh
-# and the code below only works for one single backup archive:
+# and the backup selection code below only works to select one single backup archive:
 backups=()
 backup_times=()
-for rear_run in $BUILD_DIR/outputfs/rear/$HOSTNAME/* ;do
-    Debug "Relax-and-Recover run $rear_run detected."
+for rear_run in $BUILD_DIR/outputfs/rear/$HOSTNAME/* ; do
+    Debug "Relax-and-Recover run '$rear_run' detected."
     backup_name=$rear_run/${BACKUP_PROG_ARCHIVE}${BACKUP_PROG_SUFFIX}${BACKUP_PROG_COMPRESS_SUFFIX}
-    if [ -e $backup_name ] ; then
-        Debug "Relax-and-Recover backup $backup_name detected."
-        backups=( "${backups[@]}" "$backup_name")
-        backup_times=( "${backup_times[@]}" "${rear_run##*/}")
+    if test -r "$backup_name" ; then
+        LogPrint "Backup archive $backup_name detected."
+        backups=( "${backups[@]}" "$backup_name" )
+        backup_times=( "${backup_times[@]}" "${rear_run##*/}" )
     fi
 done
 
-# The user has to choose the backup
+# When there is only one backup archive detected use that and do not disrupt
+# "rear recover" or "rear restoreonly" and ask the user for the backup archive:
+if test "1" = "${#backups[@]}" ; then
+    backuparchive=${backups[0]}
+    LogPrint "Using backup archive '$backuparchive'."
+    return
+fi
+
+# Let the user has choose the backup:
 LogPrint "Select a backup archive."
-select choice in "${backup_times[@]}" "Abort"; do
-    [ "$choice" != "Abort" ]
-    StopIfError "User chose to abort recovery."
+select choice in "${backup_times[@]}" "Abort" ; do
+    test "Abort" = "$choice" && Error "User chose to abort recovery."
     n=( $REPLY ) # trim blanks from reply
     let n-- # because bash arrays count from 0
     if [ "$n" -lt 0 ] || [ "$n" -ge "${#backup_times[@]}" ] ; then
-        LogPrint "Invalid choice $REPLY, please try again or abort."
+        LogPrint "Invalid choice $REPLY, try again or abort."
         continue
     fi
-    LogPrint "Backup archive ${backups[$n]} chosen."
     backuparchive=${backups[$n]}
+    LogPrint "Using backup archive '$backuparchive'."
     break
 done 2>&1
 
