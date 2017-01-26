22ca3eef62ee8497957afa3f2ee8fd344632ce73
Johannes Meixner <jsmeix@suse.com>
2017-01-26 15:55:34 +0100

    More meaningful LogPrint texts in 900_create_initramfs.sh (issue1142)

--- usr/share/rear/pack/GNU/Linux/900_create_initramfs.sh
+++ usr/share/rear/pack/GNU/Linux/900_create_initramfs.sh
@@ -5,7 +5,6 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-LogPrint "Creating recovery/rescue system initramfs/initrd"
 # The REAR_INITRD_FILENAME is needed in various subsequent scripts that install the bootloader
 # of the Relax-and-Recover recovery/rescue system during the subsequent 'output' stage.
 pushd "$ROOTFS_DIR" >/dev/null
@@ -13,6 +12,7 @@ case "$REAR_INITRD_COMPRESSION" in
     (lzma)
         # Create initrd.xz with xz and use the lzma compression, see https://github.com/rear/rear/issues/1142
         REAR_INITRD_FILENAME="initrd.xz"
+        LogPrint "Creating recovery/rescue system initramfs/initrd $REAR_INITRD_FILENAME with xz lzma compression"
         if find . ! -name "*~" | cpio -H newc --create --quiet | xz --format=lzma --compress --stdout > "$TMP_DIR/$REAR_INITRD_FILENAME" ; then
             LogPrint "Created $REAR_INITRD_FILENAME with xz lzma compression ($( stat -c%s $TMP_DIR/$REAR_INITRD_FILENAME ) bytes)"
         else
@@ -23,6 +23,7 @@ case "$REAR_INITRD_COMPRESSION" in
     (fast)
         # Create initrd.cgz with gzip --fast compression (fast speed but less compression)
         REAR_INITRD_FILENAME="initrd.cgz"
+        LogPrint "Creating recovery/rescue system initramfs/initrd $REAR_INITRD_FILENAME with gzip fast compression"
         if find . ! -name "*~" | cpio -H newc --create --quiet | gzip --fast > "$TMP_DIR/$REAR_INITRD_FILENAME" ; then
             LogPrint "Created $REAR_INITRD_FILENAME with gzip fast compression ($( stat -c%s $TMP_DIR/$REAR_INITRD_FILENAME ) bytes)"
         else
@@ -33,6 +34,7 @@ case "$REAR_INITRD_COMPRESSION" in
     (best)
         # Create initrd.cgz with gzip --best compression (best compression but slow speed)
         REAR_INITRD_FILENAME="initrd.cgz"
+        LogPrint "Creating recovery/rescue system initramfs/initrd $REAR_INITRD_FILENAME with gzip best compression"
         if find . ! -name "*~" | cpio -H newc --create --quiet | gzip --best > "$TMP_DIR/$REAR_INITRD_FILENAME" ; then
             LogPrint "Created $REAR_INITRD_FILENAME with gzip best compression ($( stat -c%s $TMP_DIR/$REAR_INITRD_FILENAME ) bytes)"
         else
@@ -44,6 +46,7 @@ case "$REAR_INITRD_COMPRESSION" in
         # Create initrd.cgz with gzip default compression by default and also as fallback
         # (no need to error out here if REAR_INITRD_COMPRESSION has an invalid value)
         REAR_INITRD_FILENAME="initrd.cgz"
+        LogPrint "Creating recovery/rescue system initramfs/initrd $REAR_INITRD_FILENAME with gzip default compression"
         if find . ! -name "*~" | cpio -H newc --create --quiet | gzip > "$TMP_DIR/$REAR_INITRD_FILENAME" ; then
             LogPrint "Created $REAR_INITRD_FILENAME with gzip default compression ($( stat -c%s $TMP_DIR/$REAR_INITRD_FILENAME ) bytes)"
         else
