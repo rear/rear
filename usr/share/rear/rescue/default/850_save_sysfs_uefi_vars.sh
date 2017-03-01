5c04d1b6ad19ade4168782a5577c8f72f42e92fb
Johannes Meixner <jsmeix@suse.com>
2017-03-01 13:44:54 +0100

    Trying to make UEFI_BOOTLOADER working more generally as follow up of pull request 1204

--- usr/share/rear/rescue/default/850_save_sysfs_uefi_vars.sh
+++ usr/share/rear/rescue/default/850_save_sysfs_uefi_vars.sh
@@ -1,84 +1,97 @@
-# a simplified uefivars replacement
-is_true $USING_UEFI_BOOTLOADER || return    # empty or 0 means NO UEFI
 
-[[ ! -d $VAR_DIR/recovery ]] && mkdir -p -m 755 $VAR_DIR/recovery
-rm -f $VAR_DIR/recovery/uefi-variables
+# A simplified uefivars replacement
 
-EFIBOOTMGR_OUTPUT=$TMP_DIR/efibootmgr_output
-efibootmgr > "$EFIBOOTMGR_OUTPUT"
+# USING_UEFI_BOOTLOADER empty or 0 means NO UEFI
+is_true $USING_UEFI_BOOTLOADER || return
 
-# depending the directory ends on vars or efivars we need to treat it different
-if [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/vars" ]; then
+# When the user has specified UEFI_BOOTLOADER in /etc/rear/local.conf use it if exists and is a regular file.
+# Double quotes are mandatory here because 'test -f' without any (possibly empty) argument results true:
+if test -f "$UEFI_BOOTLOADER" ; then
 
-    for uefi_dir in $(ls $SYSFS_DIR_EFI_VARS)
-    do
-        uefi_var=$(echo $uefi_dir | cut -d- -f 1)
-        [[ "$uefi_var" = "new_var" ]] && continue
-        [[ "$uefi_var" = "del_var" ]] && continue
-        efi_data="$(efibootmgr_read_var $uefi_var $EFIBOOTMGR_OUTPUT)"
-        [[ -z "$efi_data" ]] && efi_data="$(uefi_read_data $SYSFS_DIR_EFI_VARS/$uefi_dir/data)"
-        efi_attr="$(uefi_read_attributes $SYSFS_DIR_EFI_VARS/$uefi_dir/attributes)"
-        echo "$uefi_var $efi_attr: $efi_data"  >> $VAR_DIR/recovery/uefi-variables
-    done
-    # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
-    BootCurrent=$(grep BootCurrent $VAR_DIR/recovery/uefi-variables | cut -d: -f2 | awk '{print $1}')	# 0000
-    my_UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*/data)
-
-elif [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/efivars" ]; then
-
-    for uefi_file in $(ls $SYSFS_DIR_EFI_VARS)
-    do
-        uefi_var=$(echo $uefi_file | cut -d- -f 1)
-        efi_data="$(efibootmgr_read_var $uefi_var $EFIBOOTMGR_OUTPUT)"
-        [[ -z "$efi_data" ]] && efi_data="$(uefi_read_data $SYSFS_DIR_EFI_VARS/$uefi_file)"
-        echo "$uefi_var $efi_attr: $efi_data"  >> $VAR_DIR/recovery/uefi-variables
-        #TODO: efi_attr how to extract??
-    done
-    # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
-    BootCurrent=$(grep BootCurrent $VAR_DIR/recovery/uefi-variables | cut -d: -f2 | awk '{print $1}')	# 0000
-    my_UEFI_BOOTLOADER=$(uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${BootCurrent}-*)
+    # Show to the user a confirmation when his specified UEFI_BOOTLOADER is actually used:
+    LogPrint "Using UEFI_BOOTLOADER=$UEFI_BOOTLOADER"
 
 else
-    BugError "UEFI Variables directory $SYSFS_DIR_EFI_VARS is not what I expected"
-fi
 
-# Perhaps we defined UEFI_BOOTLOADER in /etc/rear/local.conf - check this now
-if [[ ! -z "${UEFI_BOOTLOADER}" ]]; then
-    # right, variable is not empty, but is it a file?
-    [[ ! -f ${UEFI_BOOTLOADER} ]] && Error "Cannot find a proper UEFI_BOOTLOADER ($UEFI_BOOTLOADER). 
-Please define it in $CONFIG_DIR/local.conf (e.g. UEFI_BOOTLOADER=/boot/efi/EFI/fedora/grubx64.efi)"
+    # When UEFI_BOOTLOADER is not specified or when it does not exist or is no regular file
+    # try to autodetect what to use as UEFI_BOOTLOADER:
+    LogPrint "Autodetecting what to use as UEFI_BOOTLOADER"
 
-else
-    # the UEFI_BOOTLOADER contains path in DOS format
-    UEFI_BOOTLOADER="/boot/efi"$(echo "$my_UEFI_BOOTLOADER" | sed -e 's;\\;/;g')
-    if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
-        UEFI_BOOTLOADER=$(find /boot/efi -name "grub*.efi" | tail -1)
+    test -d "$VAR_DIR/recovery" || mkdir -p -m 755 $VAR_DIR/recovery
+    local uefi_variables_file=$VAR_DIR/recovery/uefi-variables
+    rm -f $uefi_variables_file
+
+    local efibootmgr_output=$TMP_DIR/efibootmgr_output
+    efibootmgr > "$efibootmgr_output"
+
+    local uefi_dir=""
+    local uefi_file=""
+    local uefi_var=""
+    local efi_data=""
+    local efi_attr=""
+    local boot_current=""
+    local uefi_bootloader_DOS_path=""
+    local uefi_bootloader_filename=""
+    # depending the directory ends on vars or efivars we need to treat it different
+    # see prep/default/320_include_uefi_env.sh how SYSFS_DIR_EFI_VARS is set
+    if [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/vars" ] ; then
+
+        for uefi_dir in $( ls $SYSFS_DIR_EFI_VARS ) ; do
+            uefi_var=$( echo $uefi_dir | cut -d- -f 1 )
+            [[ "$uefi_var" = "new_var" ]] && continue
+            [[ "$uefi_var" = "del_var" ]] && continue
+            efi_data="$( efibootmgr_read_var $uefi_var $efibootmgr_output )"
+            [[ -z "$efi_data" ]] && efi_data="$( uefi_read_data $SYSFS_DIR_EFI_VARS/$uefi_dir/data )"
+            efi_attr="$( uefi_read_attributes $SYSFS_DIR_EFI_VARS/$uefi_dir/attributes )"
+            echo "$uefi_var $efi_attr: $efi_data" >> $uefi_variables_file
+        done
+        # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
+        boot_current=$( grep BootCurrent $uefi_variables_file | cut -d: -f2 | awk '{print $1}' ) # 0000
+        uefi_bootloader_DOS_path=$( uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${boot_current}-*/data )
+
+    elif [ "$SYSFS_DIR_EFI_VARS" = "/sys/firmware/efi/efivars" ] ; then
+
+        for uefi_file in $( ls $SYSFS_DIR_EFI_VARS ) ; do
+            uefi_var=$( echo $uefi_file | cut -d- -f 1 )
+            efi_data="$( efibootmgr_read_var $uefi_var $efibootmgr_output )"
+            [[ -z "$efi_data" ]] && efi_data="$( uefi_read_data $SYSFS_DIR_EFI_VARS/$uefi_file )"
+            echo "$uefi_var $efi_attr: $efi_data" >> $uefi_variables_file
+            #TODO: efi_attr how to extract??
+        done
+        # finding the correct EFI bootloader in use (UEFI_BOOTLOADER=)
+        boot_current=$( grep BootCurrent $uefi_variables_file | cut -d: -f2 | awk '{print $1}' ) # 0000
+        uefi_bootloader_DOS_path=$( uefi_extract_bootloader $SYSFS_DIR_EFI_VARS/Boot${boot_current}-* )
+
+    else
+        BugError "UEFI variables directory $SYSFS_DIR_EFI_VARS is neither /sys/firmware/efi/vars nor /sys/firmware/efi/efivars (only those are supported by ReaR)"
     fi
+
+    # Replace backslashes with slashes because uefi_bootloader_DOS_path contains path in DOS format:
+    UEFI_BOOTLOADER="/boot/efi"$( echo "$uefi_bootloader_DOS_path" | sed -e 's;\\;/;g' )
+
+    # When UEFI_BOOTLOADER is not yet specified or when it does not exist or is no regular file
+    # do further autodetection trials:
+    test -f "$UEFI_BOOTLOADER" || UEFI_BOOTLOADER=$( find /boot/efi -name 'grub*.efi' | tail -1 )
+    # In case we have an elilo bootloader then we might be lucky with next statement:
+    test -f "$UEFI_BOOTLOADER" || UEFI_BOOTLOADER=$( find /boot/efi -name 'elilo.efi' | tail -1 )
+    # In case we have a 64-bit systemd bootloader not listed in efivars then we might be lucky with next statement:
+    test -f "$UEFI_BOOTLOADER" || UEFI_BOOTLOADER=$( find /boot/EFI -name 'BOOTX64.EFI' | tail -1 )
+    # If still no valid UEFI_BOOTLOADER was autodetected, try more generic finds in whole /boot with case insensitive filename matching.
+    # The user can specify UEFI_BOOTLOADER as a single string or as an array of filename globbing patterns ("$VAR[@]" works also for a single string).
+    # On older systems where 'find' does not support '-iname' this does not make autodetection trials really worse because there 'find' just fails.
+    for uefi_bootloader_filename in 'grub*.efi' 'elilo.efi' 'BOOTX64.EFI' "${UEFI_BOOTLOADER[@]}" ; do
+        test -f "$UEFI_BOOTLOADER" && break || UEFI_BOOTLOADER=$( find /boot -iname "$uefi_bootloader_filename" | tail -1 )
+    done
+
+    # Error out when no valid UEFI_BOOTLOADER could be autodetected:
+    test -f "$UEFI_BOOTLOADER" || Error "Cannot autodetect what to use as UEFI_BOOTLOADER, you have to manually specify it in $CONFIG_DIR/local.conf"
+
+    # Show to the user what was autodetected as UEFI_BOOTLOADER:
+    LogPrint "Using UEFI_BOOTLOADER=$UEFI_BOOTLOADER"
+
 fi
 
-# in case we have an elilo bootloader then we might be lucky with next statements
-if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
-    UEFI_BOOTLOADER=$(find /boot/efi -name "elilo.efi" | tail -1)
-fi
-
-# in case we have a 64-bit systemd bootloader not listed in efivars then we might be lucky with next statements
-if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
-    UEFI_BOOTLOADER=$(find /boot/EFI -name "BOOTX64.EFI" | tail -1)
-fi
-
-# did we find a boot loader?
-if [[ ! -f ${UEFI_BOOTLOADER} ]]; then
-
-    Error "Cannot find a proper UEFI_BOOTLOADER ($UEFI_BOOTLOADER). 
-Please define it in $CONFIG_DIR/local.conf (e.g. UEFI_BOOTLOADER=/boot/efi/EFI/fedora/bootx64.efi)"
-
-else
-
-    Log "Using UEFI_BOOTLOADER=$UEFI_BOOTLOADER"
-
-fi
-
-# save the variables we need in recover mode into the rescue.conf file
+# Save the variables we need in recover mode into the rescue.conf file:
 cat - <<EOF >> "$ROOTFS_DIR/etc/rear/rescue.conf"
 USING_UEFI_BOOTLOADER=$USING_UEFI_BOOTLOADER
 UEFI_BOOTLOADER="$UEFI_BOOTLOADER"
