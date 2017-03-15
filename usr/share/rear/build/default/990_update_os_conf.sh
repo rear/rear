1a08f49c9b57101f363d069082afbeeeaacd7d7e
Johannes Meixner <jsmeix@suse.com>
2017-03-15 12:23:54 +0100

    Removed the word WARNING and the shouting (exclamation marks) from the automatically added comments in /etc/rear/os.conf in the recovery system

--- usr/share/rear/build/default/990_update_os_conf.sh
+++ usr/share/rear/build/default/990_update_os_conf.sh
@@ -1,6 +1,11 @@
-# add os/version info to os.conf in the rescue system so that we don't need to pull lsb into the rescue system
+# Add os/version info to os.conf in the rescue system
+# so that we don't need to pull 'lsb_release' into the rescue system
+# cf. the SetOSVendorAndVersion function in lib/config-functions.sh
 
-echo -e "#\n# WARNING ! This information was added automatically by the $WORKFLOW workflow !!!" >> $ROOTFS_DIR/etc/rear/os.conf
+local rescue_system_os_conf_file="$ROOTFS_DIR/etc/rear/os.conf"
+echo "# The following information was added automatically by the $WORKFLOW workflow:" >> $rescue_system_os_conf_file
 for var in ARCH OS OS_VERSION OS_VENDOR OS_VENDOR_VERSION OS_VENDOR_ARCH ; do
-	echo "$var='${!var}'"
-done >> $ROOTFS_DIR/etc/rear/os.conf
+    echo "$var='${!var}'"
+done >> $rescue_system_os_conf_file
+echo "# End of what was added automatically by the $WORKFLOW workflow." >> $rescue_system_os_conf_file
+
