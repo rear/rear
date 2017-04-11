e294ec3712d359bb9f23a3f720f35b4d0c5fb4b7
Johannes Meixner <jsmeix@suse.com>
2017-04-11 12:35:01 +0200

    actually use the NETFS_RESTORE_CAPABILITIES variable where needed (issue 1283)

--- usr/share/rear/rescue/NETFS/default/610_save_capabilities.sh
+++ usr/share/rear/rescue/NETFS/default/610_save_capabilities.sh
@@ -12,7 +12,7 @@ echo /dev/null > $VAR_DIR/recovery/capabilities
 # getcap and setcap are mandatory when NETFS_RESTORE_CAPABILITIES has a non-empty array member:
 has_binary getcap && has_binary setcap || Error "getcap and setcap are needed when NETFS_RESTORE_CAPABILITIES is non-empty"
 
-for directory in "${NETWORKING_PREPARATION_COMMANDS[@]}" ; do
+for directory in "${NETFS_RESTORE_CAPABILITIES[@]}" ; do
     getcap -r $directory 2>/dev/null | egrep -v "$TMPDIR|$ISO_DIR" >> $VAR_DIR/recovery/capabilities
 done
 
