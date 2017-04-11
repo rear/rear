d84d5709c68c345cee2a8f67b07c3f543cd7ad53
Johannes Meixner <jsmeix@suse.com>
2017-04-11 14:49:48 +0200

    Avoid a bug in rescue/NETFS/default/600_store_NETFS_variables.sh and document the current state in default.conf (issue 1283)

--- usr/share/rear/prep/GNU/Linux/310_include_cap_utils.sh
+++ usr/share/rear/prep/GNU/Linux/310_include_cap_utils.sh
@@ -4,7 +4,14 @@
 # Skip when the whole NETFS_RESTORE_CAPABILITIES array is empty.
 # For the 'test' one must have all array members as a single word i.e. "${name[*]}" because
 # the test should succeed when there is any non-empty array member, not necessarily the first one:
-test "${NETFS_RESTORE_CAPABILITIES[*]}" || return 0
+if ! test "${NETFS_RESTORE_CAPABILITIES[*]}" ; then
+    # Avoid a bug in the subsequent rescue/NETFS/default/600_store_NETFS_variables.sh script
+    # that would store a false NETFS_RESTORE_CAPABILITIES='()' into /etc/rear/rescue.conf
+    # when NETFS_RESTORE_CAPABILITIES=() but '()' is not an empty array but the string "()"
+    # cf. https://github.com/rear/rear/pull/1284#issuecomment-293246380
+    NETFS_RESTORE_CAPABILITIES=( 'No' )
+    return 0
+fi
 # Be backward compatible:
 is_false "$NETFS_RESTORE_CAPABILITIES" && return 0
 
