b98596d2e2637c8d660a156ac2e067f1cae50aba
schabrolles <s.chabrolles@fr.ibm.com>
2017-04-14 14:23:56 +0200

    TYPO in 600_store_NETFS_variables.sh

--- usr/share/rear/rescue/NETFS/default/600_store_NETFS_variables.sh
+++ usr/share/rear/rescue/NETFS/default/600_store_NETFS_variables.sh
@@ -1,6 +1,5 @@
 
 # Store all currently set NETFS* variables into /etc/rear/rescue.conf in the ReaR recovery system.
-echo "All set NETFS_* variables (cf. rescue/NETFS/default/600_store_NETFS_variables.sh):" >> $ROOTFS_DIR/etc/rear/rescue.conf
+echo "# All set NETFS_* variables (cf. rescue/NETFS/default/600_store_NETFS_variables.sh):" >> $ROOTFS_DIR/etc/rear/rescue.conf
 set | grep '^NETFS_' >>$ROOTFS_DIR/etc/rear/rescue.conf
 echo "" >> $ROOTFS_DIR/etc/rear/rescue.conf
-
