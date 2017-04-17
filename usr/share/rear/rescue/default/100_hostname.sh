1a2c45a3fbcbcec4ad4c0ced556a1f7d9884db52
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-17 17:28:42 +0200

    100_hostname changes commented (#1316)

--- usr/share/rear/rescue/default/100_hostname.sh
+++ usr/share/rear/rescue/default/100_hostname.sh
@@ -8,5 +8,11 @@
 # For Arch Linux storing the host name in /etc/hostname (lowercase)
 # will set the host name in the recovery environment without any scripting.
 
+# If /etc/hostname exists => put hostname in $ROOTFS_DIR/etc/hostsname
+# Needed by Arch Linux
 [[ -e /etc/hostname ]] && echo $HOSTNAME >$ROOTFS_DIR/etc/hostname
+
+# If /etc/HOSTNAME exists => put hostname in $ROOTFS_DIR/etc/HOSTNAME
+# Used by most of the other Linux Distro.
+# SUSE 12 has both file, but seems to use /etc/HOSTNAME to setup hostname. (see #1316)
 [[ -e /etc/HOSTNAME ]] && echo $HOSTNAME >$ROOTFS_DIR/etc/HOSTNAME
