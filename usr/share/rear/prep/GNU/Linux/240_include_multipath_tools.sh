b90eb9584e0e8a97a7f479f9248ee8260bc1bc40
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-18 13:45:15 +0200

    Modify test BOOT_OVER_SAN for better readability.

--- usr/share/rear/prep/GNU/Linux/240_include_multipath_tools.sh
+++ usr/share/rear/prep/GNU/Linux/240_include_multipath_tools.sh
@@ -1,9 +1,8 @@
 # 240_include_multipath_tools.sh
 # Boot Over SAN executables and other goodies
 
-if ! is_true "$BOOT_OVER_SAN" ; then
-    return
-fi
+# Run the following only if BOOT_OVER_SAN is true
+is_true "$BOOT_OVER_SAN" || return
 
 PROGS=( "${PROGS[@]}" multipath dmsetup kpartx multipathd scsi_id  )
 COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/multipath.conf /etc/multipath/* /lib*/multipath )
