a14a134f19b820553ff0c7546a288931f9c73df0
Sébastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-19 17:40:57 +0200

    Add comments on migration use case and issue number.

--- usr/share/rear/layout/prepare/GNU/Linux/210_load_multipath.sh
+++ usr/share/rear/layout/prepare/GNU/Linux/210_load_multipath.sh
@@ -1,6 +1,10 @@
 
-# Activating multipath if BOOT_OVER_SAN variable is true
-# or if multipath device are present in LAYOUT_FILE
+# Activating multipath if BOOT_OVER_SAN variable is true.
+# or if multipath device are present in LAYOUT_FILE.
+
+# In case of migration to a BOOT_OVER_SAN server, you need to be able 
+# to detect new multipath device even if there are no multipath device present
+# in the Layout file (original machine not multipathed). (#1309)
 if grep -q '^multipath' "$LAYOUT_FILE" || is_true "$BOOT_OVER_SAN" ; then
     Log "Activating multipath"
     modprobe dm-multipath >&2
