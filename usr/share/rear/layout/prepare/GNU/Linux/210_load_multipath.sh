92f0b60109040d40408075137bb01c7bdf4b2a88
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-21 08:13:59 +0200

    Use mpathconf to properly setup multipath in Fedora based OS. (#1309)

--- usr/share/rear/layout/prepare/GNU/Linux/210_load_multipath.sh
+++ usr/share/rear/layout/prepare/GNU/Linux/210_load_multipath.sh
@@ -3,8 +3,25 @@
 # or if multipath device are present in LAYOUT_FILE
 if grep -q '^multipath' "$LAYOUT_FILE" || is_true "$BOOT_OVER_SAN" ; then
     Log "Activating multipath"
-    modprobe dm-multipath >&2
-    [ ! -f /etc/multipath.conf ] && touch /etc/multipath.conf
+
+    # We need to create a multipath.conf if it does not exists (needed by Fedora based OS)
+    if [ ! -f /etc/multipath.conf ] ; then
+        if type mpathconf &> /dev/null ; then
+            LogPrint "Using mpathconf to configure multipath with friendly_names and find_multipath options"
+
+            # /etc/multipath dir need to be present before mpathconf runs
+            [ ! -d /etc/multipath ] && mkdir -p /etc/multipath
+
+            # create default multipath.conf with friendly_names and find_multipath options
+            # load mudules and start multipath discovery
+            mpathconf --enable --user_friendly_names y --find_multipaths y --with_module y --with_multipathd y
+        else
+            LogPrint "mpathconf not found... activating multipath with minimal options"
+            modprobe dm-multipath >&2
+            touch /etc/multipath.conf
+        fi
+    fi
+
     multipath >&2
     if [ $? -ne 0 ] ; then
         LogPrint "Failed to activate multipath. Please do this now:"
