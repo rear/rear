648df76658cc6a0569d557b14b8e8105c54fdbaa
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-21 18:33:04 +0200

    Moving is_multipath_path function to lib/layout-functions.sh

--- usr/share/rear/layout/save/GNU/Linux/200_partition_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/200_partition_layout.sh
@@ -67,7 +67,7 @@ extract_partitions() {
     done
 
     # do a numeric sort to have the partitions in numeric order (see #352)
-    # add a uniq sort "-u" to filter duplicated lines (see #1301) 
+    # add a uniq sort "-u" to filter duplicated lines (see #1301)
     sort -un  $TMP_DIR/partitions_unsorted > $TMP_DIR/partitions
 
     if [[ ! -s $TMP_DIR/partitions ]] ; then
@@ -241,10 +241,6 @@ extract_partitions() {
 }
 
 Log "Saving disk partitions."
-
-function is_multipath_path {
-    [ "$1" ] && type multipath &>/dev/null && multipath -c /dev/$1 &>/dev/null
-}
 
 (
     # Disk sizes
