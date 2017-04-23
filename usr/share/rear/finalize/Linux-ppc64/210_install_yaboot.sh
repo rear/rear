5020ae2422740d6239241516bc0b30efb014995f
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-04-23 09:29:34 +0200

    Update install_yaboot.sh

--- usr/share/rear/finalize/Linux-ppc64/210_install_yaboot.sh
+++ usr/share/rear/finalize/Linux-ppc64/210_install_yaboot.sh
@@ -8,20 +8,25 @@ test -f $TARGET_FS_ROOT/etc/yaboot.conf || return
 LogPrint "Installing PPC PReP Boot partition."
 
 # Find PPC PReP Boot partitions
-part=$( awk -F '=' '/^boot=/ {print $2}' $TARGET_FS_ROOT/etc/yaboot.conf )
+part=$( awk -F '=' '/^boot/ {print $2}' $TARGET_FS_ROOT/etc/yaboot.conf )
 
-if test "$part" ; then
-    LogPrint "Boot partion found: $part"
+# test $part is not null and is an existing partition on the current system.
+if ( test -n $part ) && ( fdisk -l 2>/dev/null | grep -q $part ) ; then
+    LogPrint "Boot partion found in yaboot.conf: $part"
     # Run mkofboot directly in chroot without a login shell in between, see https://github.com/rear/rear/issues/862
-    chroot $TARGET_FS_ROOT /sbin/mkofboot -b $part --filesystem raw -f
-    NOBOOTLOADER=
 else
-    bootparts=$( sfdisk -l 2>&1 | awk '/PPC PReP Boot/ {print $1}' )
-    LogPrint "Boot partitions found: $bootparts."
-    for part in $bootparts ; do
-        LogPrint "Initializing boot partition $part."
-        # Run mkofboot directly in chroot without a login shell in between, see https://github.com/rear/rear/issues/862
-        chroot $TARGET_FS_ROOT /sbin/mkofboot -b $part --filesystem raw -f
-    done
-    NOBOOTLOADER=
+    # If the device found in lilo.conf is not valid, find prep partition in
+    # disklayout file and use it in lilo.conf.
+    LogPrint "Can't find a valid partition in yaboot.conf"
+    LogPrint "Looking for PPC PReP partition in $DISKLAYOUT_FILE"
+    newpart=$( awk -F ' ' '/^part / {if ($6 ~ /prep/) {print $7}}' $DISKLAYOUT_FILE )
+    LogPrint "Updating boot = $newpart in lilo.conf"
+    sed -i -e "s|^boot.*|boot = $newpart|" $TARGET_FS_ROOT/etc/yaboot.conf
+    part=$newpart
 fi
+
+LogPrint "Running mkofboot ..."
+chroot $TARGET_FS_ROOT /sbin/mkofboot -b $part --filesystem raw -f
+[ $? -eq 0 ] && NOBOOTLOADER=
+
+test $NOBOOTLOADER && LogPrint "No bootloader configuration found. Install boot partition manually."
