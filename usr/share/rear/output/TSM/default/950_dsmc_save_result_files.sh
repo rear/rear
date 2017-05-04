bdc666f9b8f7e9eddc20e07a1bd462f96b5776e7
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-05-04 11:12:25 +0200

    Typo in comment

--- usr/share/rear/output/TSM/default/950_dsmc_save_result_files.sh
+++ usr/share/rear/output/TSM/default/950_dsmc_save_result_files.sh
@@ -8,7 +8,7 @@ fi
 
 # When PXE_TFTP_URL is defined, result files are directly copied on the remote
 # PXE/TFTP server, and the local files are deleted (800_copy_to_tftp.sh).
-# So, no need to backup RESULT_FILES when PXE_TFTP_UR is defined.
+# So, no need to backup RESULT_FILES when PXE_TFTP_URL is defined.
 [[ ! -z "$PXE_TFTP_URL" ]] && return
 
 [ ${#RESULT_FILES[@]} -gt 0 ]
