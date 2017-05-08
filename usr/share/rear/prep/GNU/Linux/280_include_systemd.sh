4d75a03020cd57e7693a134682108d4f07e7a25f
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-05-08 10:38:03 +0200

    Include systemd/network to preserve Predictable Network Interface Names  - Some modern distros (like ubuntu16.04) use systemd to control  inet naming. Those information are stored in /lib/systemd/network or  /usr/lib/systemd/network.

--- usr/share/rear/prep/GNU/Linux/280_include_systemd.sh
+++ usr/share/rear/prep/GNU/Linux/280_include_systemd.sh
@@ -6,7 +6,12 @@ if ps ax | grep -v grep | grep -q systemd ; then
         upstart-udev-bridge )
     # cgroup stuff - not required for ReaR
     #PROGS=( "${PROGS[@]}" cg_annotate cgclear cgcreate cgget cgrulesengd cgset cgdelete cgclassify cgexec )
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /usr/lib/systemd/systemd-* /lib/systemd/systemd-* )
+
+    # 1- Depending to the distros, systemd directory/scripts can be located in /usr/lib or /lib
+    # 2- Need to add systemd/network subdir in order to preserve rules about network device naming
+    #    (predictable naming or persitant naming / like udev).
+    #    more info here: https://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames/
+    COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/share/systemd /etc/dbus-1 /usr/lib/systemd/systemd-* /lib/systemd/systemd-* /usr/lib/systemd/network /lib/systemd/network )
     CLONE_GROUPS=( "${CLONE_GROUPS[@]}" input )
     Log "Including systemd (init replacement) tool-set to bootstrap Relax-and-Recover"
 fi
