480747be193b982cf92b14e39b4e52a4f6634f30
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2017-05-09 07:23:07 +0200

    Exit from beginning if no TSM binary.  - Code Style Update.

--- usr/share/rear/backup/TSM/default/500_make_TSM_backup.sh
+++ usr/share/rear/backup/TSM/default/500_make_TSM_backup.sh
@@ -1,6 +1,10 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
+# Exit if TSM binary cannot be found.
+has_binary dsmc || Error "Can't find TSM client dsmc; Please check your configuration."
+
+# If the TSM client is found, do an incremental backup:
 backup_tsm_log=/var/lib/rear/backup_tsm_log
 
 if [[ ! -d "$backup_tsm_log" ]]; then
@@ -9,17 +13,11 @@ fi
 
 LogPrint ""
 LogPrint "Starting Backup with TSM"
-
-# If the TSM client is found, start an incremental backup
-if has_binary dsmc ; then
-    LC_ALL=${LANG_RECOVER} dsmc incremental\
-    -verbose \
-    -subdir=yes \
-    -tapeprompt=no "${TSM_DSMC_BACKUP_OPTIONS[@]}" | tee "${TMP_DIR}/${BACKUP_PROG_ARCHIVE}.log"
-    StopIfError "Error during TSM backup... Check your configuration."
-else
-    StopIfError "Can't find TSM client dsmc; Please check your configuration."
-fi
+LC_ALL=${LANG_RECOVER} dsmc incremental\
+-verbose \
+-subdir=yes \
+-tapeprompt=no "${TSM_DSMC_BACKUP_OPTIONS[@]}" | tee "${TMP_DIR}/${BACKUP_PROG_ARCHIVE}.log"
+StopIfError "Error during TSM backup... Check your configuration."
 
 ### Copy progress log to backup media
 if cp $v "${TMP_DIR}/${BACKUP_PROG_ARCHIVE}.log" "${backup_tsm_log}/${BACKUP_PROG_ARCHIVE}.log"; then
