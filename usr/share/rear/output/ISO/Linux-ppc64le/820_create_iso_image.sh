4ef0f30156f0afea4a02d12f40c2c9d18cbe5e43
schabrolles <s.chabrolles@fr.ibm.com>
2017-06-12 14:58:43 +0200

    Add possibility for ppc64le to integrate backup files to the ISO image.  - move creation of iso in 820_create_iso_images.sh  - allow multiple iso creation when ISO_MAX_SIZE is specified

---
+++ usr/share/rear/output/ISO/Linux-ppc64le/820_create_iso_image.sh
@@ -0,0 +1,22 @@
+
+Log "Starting '$ISO_MKISOFS_BIN'"
+LogPrint "Making ISO image"
+
+pushd $TMP_DIR >&8
+
+# If isofs directory exists, add its content to ISO_FILES (when backup must be part of the ISO images)
+if [ -d isofs ] ; then
+    ISO_FILES=( ${ISO_FILES[@]} isofs )
+fi
+
+# mkisofs command for ppc64/ppc64le arch
+$ISO_MKISOFS_BIN $v -o "$ISO_DIR/$ISO_PREFIX.iso" -U -chrp-boot -R -J -volid "$ISO_VOLID" -v -graft-points "${ISO_FILES[@]}" >&8
+
+StopIfError "Could not create ISO image (with $ISO_MKISOFS_BIN)"
+popd >&8
+
+iso_image_size=( $(du -h "$ISO_DIR/$ISO_PREFIX.iso") )
+LogPrint "Wrote ISO image: $ISO_DIR/$ISO_PREFIX.iso ($iso_image_size)"
+
+# Add ISO image to result files
+RESULT_FILES=( "${RESULT_FILES[@]}" "$ISO_DIR/$ISO_PREFIX.iso" )
