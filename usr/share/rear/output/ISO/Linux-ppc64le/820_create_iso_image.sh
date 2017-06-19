7faabc2476fcccdb0f0c46f790cabb6768b93989
schabrolles <s.chabrolles@fr.ibm.com>
2017-06-19 15:03:48 +0200

    Replace >&8 by >&2

--- usr/share/rear/output/ISO/Linux-ppc64le/820_create_iso_image.sh
+++ usr/share/rear/output/ISO/Linux-ppc64le/820_create_iso_image.sh
@@ -2,7 +2,7 @@
 Log "Starting '$ISO_MKISOFS_BIN'"
 LogPrint "Making ISO image"
 
-pushd $TMP_DIR >&8
+pushd $TMP_DIR >&2
 
 # If isofs directory exists, add its content to ISO_FILES (when backup must be part of the ISO images)
 if [ -d isofs ] ; then
@@ -10,10 +10,10 @@ if [ -d isofs ] ; then
 fi
 
 # mkisofs command for ppc64/ppc64le arch
-$ISO_MKISOFS_BIN $v -o "$ISO_DIR/$ISO_PREFIX.iso" -U -chrp-boot -R -J -volid "$ISO_VOLID" -v -graft-points "${ISO_FILES[@]}" >&8
+$ISO_MKISOFS_BIN $v -o "$ISO_DIR/$ISO_PREFIX.iso" -U -chrp-boot -R -J -volid "$ISO_VOLID" -v -graft-points "${ISO_FILES[@]}" >&2
 
 StopIfError "Could not create ISO image (with $ISO_MKISOFS_BIN)"
-popd >&8
+popd >&2
 
 iso_image_size=( $(du -h "$ISO_DIR/$ISO_PREFIX.iso") )
 LogPrint "Wrote ISO image: $ISO_DIR/$ISO_PREFIX.iso ($iso_image_size)"
