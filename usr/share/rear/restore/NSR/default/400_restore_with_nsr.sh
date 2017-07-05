d6c266bb796f49aa5299c9ebbde205cd9c1c9952
Johannes Meixner <jsmeix@suse.com>
2017-07-05 12:27:58 +0200

    Make 400_restore_with_nbu.sh 400_restore_with_nsr.sh 400_restore_with_tsm.sh working with redirected STDOUT

--- usr/share/rear/restore/NSR/default/400_restore_with_nsr.sh
+++ usr/share/rear/restore/NSR/default/400_restore_with_nsr.sh
@@ -5,20 +5,27 @@ TERM=linux nsrwatch -p 1 -s $(cat $VAR_DIR/recovery/nsr_server ) </dev/tty8 >/de
 
 LogUserOutput "Restore filesystem $(cat $VAR_DIR/recovery/nsr_paths) with recover"
 
-BLANK=" "
-# Use the original STDOUT and STDERR when 'rear' was launched by the user for the 'while read ... echo' output:
-recover -s $(cat $VAR_DIR/recovery/nsr_server) -c $(hostname) -d $TARGET_FS_ROOT -a $(cat $VAR_DIR/recovery/nsr_paths) 2>&1 | \
-while read -r ; do
-    echo -ne "\r${BLANK:1-COLUMNS}\r"
-    case "$REPLY" in
-        *:*\ *)	echo "$REPLY" ;;
-        ./*)	if [ "${#REPLY}" -ge $((COLUMNS-5)) ] ; then
+blank=" "
+# Use the original STDOUT when 'rear' was launched by the user for the 'while read ... echo' output
+# (which also reads STDERR of the 'recover' command so that 'recover' errors are 'echo'ed to the user)
+# but keep STDERR of the 'while' command going to the log file so that 'rear -D' output goes to the log file:
+recover -s $(cat $VAR_DIR/recovery/nsr_server) -c $(hostname) -d $TARGET_FS_ROOT -a $(cat $VAR_DIR/recovery/nsr_paths) 2>&1 \
+  | while read -r ; do
+        echo -ne "\r${blank:1-COLUMNS}\r"
+        case "$REPLY" in
+            *:*\ *)
+                echo "$REPLY"
+                ;;
+            ./*)
+                if [ "${#REPLY}" -ge $((COLUMNS-5)) ] ; then
                     echo -n "... ${REPLY:5-COLUMNS}"
                 else
                     echo -n "$REPLY"
                 fi
                 ;;
-        *)	echo "$REPLY" ;;
-    esac
-done 1>&7 2>&8
+            *)
+                echo "$REPLY"
+                ;;
+        esac
+    done 1>&7
 
