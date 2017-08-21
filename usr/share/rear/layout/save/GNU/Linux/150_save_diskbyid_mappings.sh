6d9a00c135bfa4bb9f14a34105e405642ee697b5
schabrolles <s.chabrolles@fr.ibm.com>
2017-08-21 11:10:27 +0200

    Add comment

--- usr/share/rear/layout/save/GNU/Linux/150_save_diskbyid_mappings.sh
+++ usr/share/rear/layout/save/GNU/Linux/150_save_diskbyid_mappings.sh
@@ -19,11 +19,15 @@ type -p udevadm >/dev/null &&  UdevSymlinkName="udevadm info --root --query=syml
 ls /dev/disk/by-id | while read ID;
 do
 	# create diskbyid_mappings file:
-	# /dev/disk/by-id/XXXXX /dev/YYYYY
+	# we need to keep absolute PATH for device in order to be able to easily use 320_apply_mappings.sh
+	# and apply disk mapping during migration.
+	#
+	# example:	scsi-360060e8015268c000001268c000065c0-part4 /dev/sda4
+	#			wwn-0x600507680c82004cf8000000000000d8 /dev/mapper/maptha
 	ID_NEW=$($UdevQueryName /dev/disk/by-id/$ID)
 	if [[ $ID_NEW =~ ^dm- ]]; then
 		# If dm- device is a multipath, get its /dev/mapper/name instead of /dev/dm-X
-		# as /dev/dm-X can change across reboot
+		# as /dev/dm-X are not persistent across reboot and not used in disk mapping file.
 		SYMLINKS=$($UdevSymlinkName /dev/$ID_NEW)
 	    set -- $SYMLINKS
 	    while [ $# -gt 0 ]; do
