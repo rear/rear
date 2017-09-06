0f8906ee5d3c00cb82289c1e38329dd3751c8bb5
Johannes Meixner <jsmeix@suse.com>
2017-09-06 13:55:04 +0200

    Further enhanced and improved 900_clone_users_and_groups.sh so that it can now also copy all via getent available users and groups

--- usr/share/rear/rescue/default/900_clone_users_and_groups.sh
+++ usr/share/rear/rescue/default/900_clone_users_and_groups.sh
@@ -1,11 +1,29 @@
+#
+# 900_clone_users_and_groups.sh
+#
+# Copy users and groups into the ReaR recovery system.
 
-# Copy users and groups to the ReaR recovery system:
-
+# Copy users and groups on the current system into the ReaR recovery system.
+local cloning_all="no"
+# When CLONE_ALL_USERS_GROUPS contains a 'true' value, copy only
+# the users and groups from the /etc/passwd and /etc/group files:
 if is_true "$CLONE_ALL_USERS_GROUPS" ; then
-    # In particular exclude a possible NIS extension line '+::::::'
+    Log "Copying users and groups from /etc/passwd and /etc/group"
+    cloning_all="yes"
+    # Only keep valid user names (in particular exclude a NIS extension line '+::::::'):
     CLONE_USERS=( $( grep -o '^[[:alnum:]_][^:]*' /etc/passwd ) )
-    # In particular exclude a possible NIS extension line '+:::'
+    # Only keep valid group names (in particular exclude a NIS extension line '+:::'):
     CLONE_GROUPS=( $( grep -o '^[[:alnum:]_][^:]*' /etc/group ) )
+fi
+# When CLONE_ALL_USERS_GROUPS is 'all', copy all users and groups
+# that are available on the current system into the ReaR recovery system:
+if test 'all' = "$CLONE_ALL_USERS_GROUPS" ; then
+    Log "Copying all users and groups that are available via 'getent'"
+    cloning_all="yes"
+    # Only keep valid user names:
+    CLONE_USERS=( $( getent passwd | grep -o '^[[:alnum:]_][^:]*' ) )
+    # Only keep valid group names:
+    CLONE_GROUPS=( $( getent group | grep -o '^[[:alnum:]_][^:]*' ) )
 fi
 
 local user=""
@@ -19,10 +37,19 @@ local group=""
 test "${CLONE_USERS[*]}" && Log "Cloning users: ${CLONE_USERS[@]}"
 for user in "${CLONE_USERS[@]}" ; do
     # Skip if the user exists already in the ReaR recovery system:
-    grep "^$user:" $ROOTFS_DIR/etc/passwd 1>&2 && continue
+    grep -q "^$user:" $ROOTFS_DIR/etc/passwd && continue
     # Skip if the user does not exist in the current system:
     if ! passwd_entry="$( getent passwd $user )" ; then
         Debug "Cannot clone $user because it does not exist"
+        continue
+    fi
+    # When CLONE_ALL_USERS_GROUPS was used above, assume
+    # the users and groups in CLONE_USERS and CLONE_GROUPS
+    # are consistent so that the user's group is in CLONE_GROUPS:
+    if is_true "$cloning_all" ; then
+        # Add the user to /etc/passwd in the ReaR recovery system and
+        # proceed with the next user without further group tests:
+        echo "$passwd_entry" >>$ROOTFS_DIR/etc/passwd
         continue
     fi
     # Prepare to also add the user's group to the CLONE_GROUPS array:
@@ -48,7 +75,7 @@ done
 test "${CLONE_GROUPS[*]}" && Log "Cloning groups: ${CLONE_GROUPS[@]}"
 for group in "${CLONE_GROUPS[@]}" ; do
     # Skip if the group exists already in the ReaR recovery system:
-    grep "^$group:" $ROOTFS_DIR/etc/group 1>&2 && continue
+    grep -q "^$group:" $ROOTFS_DIR/etc/group && continue
     # Skip if the group does not exist in the current system:
     if ! group_entry="$( getent group $group )" ; then
         Debug "Cannot clone $group because it does not exist"
