6e05b6f0e51fdfdf02c67edddb117b41a364c6f4
Johannes Meixner <jsmeix@suse.com>
2017-09-11 11:18:17 +0200

    Update 310_include_uefi_tools.sh
    Simpler code in 310_include_uefi_tools.sh to fix
    https://github.com/jsmeix/rear/commit/9bb07357ac7892ad7d82c5438e76e57a44d21113#commitcomment-24177948
    and some general cleanup.

--- usr/share/rear/prep/default/310_include_uefi_tools.sh
+++ usr/share/rear/prep/default/310_include_uefi_tools.sh
@@ -1,33 +1,31 @@
-# If noefi is set, we can ignore UEFI altogether
-if grep -qw 'noefi' /proc/cmdline; then
+#
+# 310_include_uefi_tools.sh
+# Copy UEFI binaries we might need into the ReaR recovery system.
+#
+
+# If 'noefi' is set on the kernel commandline, ignore UEFI altogether:
+grep -qw 'noefi' /proc/cmdline && return
+
+# If no /boot/[eE][fF][iI] directory can be found
+# we might not be able to copy the UEFI binaries:
+if ! test -d /boot/[eE][fF][iI] ; then
+    if is_true $USING_UEFI_BOOTLOADER; then
+        Error "USING_UEFI_BOOTLOADER is set but there is no directory /boot/efi or /boot/EFI"
+    fi
     return
 fi
 
-# If no /boot/[eE][fF][iI] directory can be found we might not be able to copy the UEFI binaries.
-if [[ ! -d /boot/[eE][fF][iI] ]]; then
-    if is_true $USING_UEFI_BOOTLOADER; then
-        Error "USING_UEFI_BOOTLOADER = 1 but there is no directory at /boot/efi or /boot/EFI" # abort
-    fi
-    return # skip
- fi
-
-# We copy the UEFI binaries we might need
-REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}"
-dosfsck
-efibootmgr
-)
-
-PROGS=( "${PROGS[@]}"
-gdisk
-parted
-uefivars
-)
-
+# Copy UEFI binaries we might need:
+REQUIRED_PROGS=( "${REQUIRED_PROGS[@]}" dosfsck efibootmgr )
+PROGS=( "${PROGS[@]}" gdisk parted uefivars )
 MODULES=( "${MODULES[@]}" efivars )
-
-if [[ -f /sbin/elilo ]]; then
+if test -f /sbin/elilo ; then
     # this is probably SLES
     PROGS=( "${PROGS[@]}" elilo perl )
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/elilo.conf /usr/lib64/crt0-efi-x86_64.o /usr/lib64/elf_x86_64_efi.lds \
-    /usr/lib64/libefi.a /usr/lib64/libgnuefi.a )
+    COPY_AS_IS=( "${COPY_AS_IS[@]}"
+                 /etc/elilo.conf
+                 /usr/lib64/crt0-efi-x86_64.o
+                 /usr/lib64/elf_x86_64_efi.lds
+                 /usr/lib64/libefi.a
+                 /usr/lib64/libgnuefi.a )
 fi
