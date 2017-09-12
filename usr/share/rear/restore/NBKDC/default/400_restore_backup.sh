aadd9c7b9a2668723f5936262fc64da3ad629b88
Johannes Meixner <jsmeix@suse.com>
2017-09-12 13:28:24 +0200

    Move the static unset USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED out of the while loop

--- usr/share/rear/restore/NBKDC/default/400_restore_backup.sh
+++ usr/share/rear/restore/NBKDC/default/400_restore_backup.sh
@@ -50,12 +50,12 @@ user_input_prompt="
 Have you successfully restored the backup to $TARGET_FS_ROOT ?
 Are you ready to continue recovery? (y/n)"
 
+# Restoring the backup may take arbitrary long time so that with explicit '-t 0' it waits endlessly for user input.
+# Automated user input via a predefined USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED variable does not make sense here
+# but the UserInput function must be called with a meaningful '-I user_input_ID' option value explicitly specified.
+# To avoid that a predefined USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED variable could cause harm here it is unset:
+unset USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED
 while true ; do
-    # Restoring the backup may take arbitrary long time so that with explicit '-t 0' it waits endlessly for user input.
-    # Automated user input via a predefined USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED variable does not make sense here
-    # but the UserInput function must be called with a meaningful '-I user_input_ID' option value explicitly specified.
-    # To avoid that a predefined USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED variable could cause harm here it is unset:
-    unset USER_INPUT_NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED
     if is_true "$( UserInput -I NBKDC_WAIT_UNTIL_RESTORE_SUCCEEDED -t 0 -p "$user_input_prompt" )" ; then
         LogUserOutput "Done with restore. Continuing recovery."
         break
