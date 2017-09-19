132d2224b620796960c9f0066f5a2ea926e4291d
Oliver Okrongli <oliver.okrongli@infix.de>
2017-09-19 16:56:06 +0200

    Use ssh-agent with proper ssh version only, use AddExitTask

--- usr/share/rear/wrapup/default/970_ssh_agent_stop.sh
+++
@@ -1,9 +0,0 @@
-# ssh-agent shutdown during 'rear recover'
-#
-# This file is part of Relax-and-Recover, licensed under the GNU General
-# Public License. Refer to the included COPYING for full text of license.
-
-if [ -n "$SSH_AGENT_PID" ]; then
-    Log "Shutting down ssh-agent"
-    eval "$(ssh-agent -k)"
-fi
