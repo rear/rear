64da94e46ac976075d73d825caf6191db299da14
Johannes Meixner <jsmeix@suse.com>
2017-09-25 14:37:14 +0200

    Various enhancements for testing required libraries in recovery system (related to issue 1494)

--- usr/share/rear/build/default/980_verify_rootfs.sh
+++ usr/share/rear/build/default/980_verify_rootfs.sh
@@ -1,18 +1,18 @@
+#
 # Test that the ROOTFS_DIR contains a usable system
 # i.e. test that the ReaR recovery system will be usable
 # to avoid issues like https://github.com/rear/rear/issues/1494
 
-# In case the filesystem that contains the ROOTFS_DIR is mounted 'noexec' we must skip the 'chroot' tests.
+# In case the filesystem that contains the ROOTFS_DIR is mounted 'noexec' we cannot do the 'chroot' tests.
 # The filesystem_name function in linux-functions.sh returns the mountpoint (not a filesystem name like 'ext4'):
-rootfs_dir_fs_mountpoint=$( filesystem_name $ROOTFS_DIR )
-
+local rootfs_dir_fs_mountpoint=$( filesystem_name $ROOTFS_DIR )
 if grep -qE '^\S+ '$rootfs_dir_fs_mountpoint' \S+ \S*\bnoexec\b\S* ' /proc/mounts ; then
-    LogPrint "Cannot test if the ReaR recovery system is usable because $rootfs_dir_fs_mountpoint is mounted 'noexec'"
-    Log "We cannot run tests inside the ReaR recovery system by using 'chroot' because
-we need the 'exec' option set to the filesystem. One way to achieve this is
-by doing: mount -o remount,exec $rootfs_dir_fs_mountpoint"
-    return
+    # Intentionally failing here with Error to make it mandatory for ReaR to really check the rescue image.
+    # It is much more important to guarantee a runnable rescue image than to support noexec environments,
+    # cf. https://github.com/rear/rear/pull/1514#discussion_r140752346
+    Error "Cannot test if the ReaR recovery system is usable because $rootfs_dir_fs_mountpoint is mounted 'noexec'"
 fi
+Log "Testing that $ROOTFS_DIR contains a usable system"
 
 # The bash test ensures that we have a working bash in the ReaR recovery system:
 if ! chroot $ROOTFS_DIR /bin/bash -c true ; then
@@ -23,6 +23,7 @@ fi
 # The ldd test ensures that for dynamically linked executables the required libraries are there.
 # The ldd test runs after the bash test because /bin/ldd is a bash script.
 # First test is 'ldd /bin/bash' to ensure 'ldd' works:
+Log "Testing 'ldd /bin/bash' to ensure 'ldd' works for the subsequent 'ldd' tests"
 if ! chroot $ROOTFS_DIR /bin/ldd /bin/bash 1>&2 ; then
     KEEP_BUILD_DIR=1
     BugError "ReaR recovery system in '$ROOTFS_DIR' is broken: 'ldd /bin/bash' failed"
@@ -32,19 +33,31 @@ fi
 # When running ldd for a file that is 'not a dynamic executable' ldd returns non-zero exit code.
 local binary=""
 local broken_binaries=""
-pushd $ROOTFS_DIR 1>&2
-for binary in $( find bin/ -type f ) ; do
-    if chroot $ROOTFS_DIR /bin/ldd $binary | grep 'not found' ; then
-        echo "library missing for $binary"
-        broken_binaries="$broken_binaries $( basename $binary )"
-    fi
-done 1>&2
-popd 1>&2
+# Catch all binaries and libraries also e.g. those that are copied via COPY_AS_IS into other paths:
+for binary in $( find $ROOTFS_DIR -type f -executable -printf '/%P\n' ) ; do
+    chroot $ROOTFS_DIR /bin/ldd $binary | grep -q 'not found' && broken_binaries="$broken_binaries $binary"
+done
 if contains_visible_char "$broken_binaries" ; then
     KEEP_BUILD_DIR=1
+    LogPrint "Checking for binaries or libraries in the ReaR recovery system that require additional libraries"
+    local fatal_missing_library=""
+    local ldd_output=""
+    for binary in $broken_binaries ; do
+        if grep -q '^/bin/' <<<"$binary" ; then
+            # Only for binaries (i.e. files in /bin/ in the recovery system) treat a missing library as fatal:
+            fatal_missing_library="yes"
+            LogPrintError "$binary requires additional libraries"
+            ldd_output="$( chroot $ROOTFS_DIR /bin/ldd $binary )"
+            Log "$ldd_output"
+            PrintError "$( grep 'not found' <<<"$ldd_output" )"
+        else
+            Log "$binary requires additional libraries"
+            chroot $ROOTFS_DIR /bin/ldd $binary 1>&2
+        fi
+    done
     # Usually it should be no BugError when there are libraries missing for particular binaries because probably
-    # the reason is that the user added only the plain binaries with COPY_AS_IS (instead of using REQUIRED_PROGS)
-    # so that usually it should be a user Error when there are libraries missing for particular binaries:
-    Error "ReaR recovery system in '$ROOTFS_DIR' not usable:$broken_binaries require additional libraries, check $RUNTIME_LOGFILE for details"
+    # the reason is that the user added only the plain binaries with COPY_AS_IS (instead of using REQUIRED_PROGS):
+    is_true "$fatal_missing_library" && Error "ReaR recovery system in '$ROOTFS_DIR' not usable, check $RUNTIME_LOGFILE for details"
+    LogPrint "ReaR recovery system in '$ROOTFS_DIR' may need some additional libraries, check $RUNTIME_LOGFILE for details"
 fi
 
