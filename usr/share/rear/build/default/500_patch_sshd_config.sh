df1d9623f6202f7908e6be8787211b4787626107
Johannes Meixner <jsmeix@suse.com>
2017-10-10 15:37:38 +0200

    Empower the user to specify what ssh files get included in his recovery system (issue1512)

--- usr/share/rear/build/default/500_patch_sshd_config.sh
+++ usr/share/rear/build/default/500_patch_sshd_config.sh
@@ -1,5 +1,11 @@
 #
-# adapt some ssh configs
+# Adapt some SSH configs and as needed regenerate SSH host key:
+
+# There is nothing to do when there are no SSH binaries on the original system:
+has_binary ssh || has_binary sshd || return
+
+# Do nothing when not any SSH file should be copied into the recovery system:
+is_false "$SSH_FILES" && return
 
 # Patch sshd_config:
 # - disable password authentication because rescue system does not have PAM etc.
@@ -7,27 +13,74 @@
 # - disable PAM
 # - disable motd printing, our /etc/profile does that
 # - if SSH_ROOT_PASSWORD was defined allow root to login via ssh
-# The idea is to allow only ssh authorized_keys based access which HAS TO BE ENABLED
-# also in the original system to work here as we DO NOT ENABLE IT FOR YOU !
-
-# important for the [n] hack below because we want non-existant patterns to simply disappear
-shopt -s nullglob
-SSH_CONFIG_FILES=( $ROOTFS_DIR/etc/ssh/sshd_co[n]fig $ROOTFS_DIR/etc/sshd_co[n]fig $ROOTFS_DIR/etc/openssh/sshd_co[n]fig )
-
-if test "$SSH_CONFIG_FILES" ; then
-sed -i  -e 's/ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/ig' \
-    -e 's/UsePAM.*/UsePam no/ig' \
-    -e 's/ListenAddress.*/ListenAddress 0.0.0.0/ig' \
-    -e '1i\PrintMotd no' \
-    ${SSH_CONFIG_FILES[@]}
-    
-    if [ -n "$SSH_ROOT_PASSWORD" ] ; then 
-        sed -i -e 's/PasswordAuthentication.*/PasswordAuthentication yes/ig' ${SSH_CONFIG_FILES[@]}
-        sed -i -e 's/PermitRootLogin.*/PermitRootLogin yes/ig' ${SSH_CONFIG_FILES[@]}
+# The idea is to allow ssh authorized_keys based access in the recovery system
+# which has to be enabled in the original system to work in the recovery system.
+# The funny [] around a letter makes 'shopt -s nullglob' remove this file from the list if it does not exist.
+# Files without a [] are mandatory.
+local sshd_config_files=( $ROOTFS_DIR/etc/ssh/sshd_co[n]fig $ROOTFS_DIR/etc/sshd_co[n]fig $ROOTFS_DIR/etc/openssh/sshd_co[n]fig )
+if test "${sshd_config_files[*]}" ; then
+    sed -i -e 's/ChallengeResponseAuthentication.*/ChallengeResponseAuthentication no/ig' \
+           -e 's/UsePAM.*/UsePam no/ig' \
+           -e 's/ListenAddress.*/ListenAddress 0.0.0.0/ig' \
+           -e '1i\PrintMotd no' \
+        ${sshd_config_files[@]}
+    # Allow password authentication in the recovery system only if SSH_ROOT_PASSWORD is specified:
+    if test "$SSH_ROOT_PASSWORD" ; then
+        sed -i -e 's/PasswordAuthentication.*/PasswordAuthentication yes/ig' ${sshd_config_files[@]}
+        sed -i -e 's/PermitRootLogin.*/PermitRootLogin yes/ig' ${sshd_config_files[@]}
     else
-        sed -i  -e 's/PasswordAuthentication.*/PasswordAuthentication no/ig' ${SSH_CONFIG_FILES[@]}
+        sed -i  -e 's/PasswordAuthentication.*/PasswordAuthentication no/ig' ${sshd_config_files[@]}
     fi
 else
-    Log "WARNING: sshd configuration file missing"
+     LogPrintError "No sshd configuration files"
 fi
+
+# Generate new SSH host key in the recovery system when no SSH host key file
+# had been copied into the the recovery system in rescue/default/500_ssh.sh
+# cf. https://github.com/rear/rear/issues/1512#issuecomment-331638066
+# In SLES12 "man ssh-keygen" reads:
+#   -t dsa | ecdsa | ed25519 | rsa | rsa1
+#      Specifies the type of key to create.
+#      The possible values are "rsa1" for protocol version 1
+#      and "dsa", "ecdsa", "ed25519", or "rsa" for protocol version 2.
+# The above GitHub issue comment proposes a static
+#   ssh-keygen -t ed25519 -N '' -f "..."
+# but the key type ed25519 is not supported in older systems like SLES11.
+# On SLES10 "man ssh-keygen" reads:
+#   -t type
+#      Specifies the type of key to create.
+#      The possible values are rsa1 for protocol version 1
+#      and rsa or dsa for protocol version 2.
+# Currently (October 2017) ReaR is kept working on older systems
+# like SLES10 cf. https://github.com/rear/rear/issues/1522
+# and currently this backward compatibility should not be broken
+# (for the future see https://github.com/rear/rear/issues/1390)
+# so that we try to generate all possible types of keys.
+# This is in compliance what there is on a default SLES system, e.g.
+# on a default SLES10 there is
+#   /etc/ssh/ssh_host_rsa_key
+#   /etc/ssh/ssh_host_dsa_key
+# on a default SLES11 there is additionally
+#   /etc/ssh/ssh_host_ecdsa_key
+# on a default SLES12 there is additionally
+#   /etc/ssh/ssh_host_ed25519_key
+# only a rsa1 type key does not exists so that rsa1 is also not generated here:
+local ssh_host_key_types="rsa dsa ecdsa ed25519"
+local ssh_host_key_type=""
+local ssh_host_key_file=""
+local recovery_system_key_file=""
+local ssh_host_key_exists="no"
+for ssh_host_key_type in $ssh_host_key_types ; do
+    ssh_host_key_file="etc/ssh/ssh_host_${ssh_host_key_type}_key"
+    if test -f "$ROOTFS_DIR/$ssh_host_key_file" ; then
+        Log "Using existing SSH host key $ssh_host_key_file in recovery system"
+        ssh_host_key_exists="yes"
+        continue
+    fi
+    Log "Generating new SSH host key $ssh_host_key_file in recovery system"
+    recovery_system_key_file="$ROOTFS_DIR/$ssh_host_key_file"
+    mkdir $v -p $( dirname "$recovery_system_key_file" )
+    ssh-keygen $v -t "$ssh_host_key_type" -N '' -f "$recovery_system_key_file" && ssh_host_key_exists="yes" || Log "Cannot generate key type $ssh_host_key_type"
+done
+is_false "$ssh_host_key_exists" && LogPrintError "No SSH host key etc/ssh/ssh_host_TYPE_key of type $ssh_host_key_types in recovery system"
 
--- usr/share/rear/rescue/default/500_ssh.sh
+++ usr/share/rear/rescue/default/500_ssh.sh
@@ -3,21 +3,45 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
+# There is nothing to do when there are no SSH binaries on the original system:
 has_binary ssh || has_binary sshd || return
 
-# assume that we have openssh with configs in /etc/ssh
+# Do nothing when not any SSH file should be copied into the recovery system:
+is_false "$SSH_FILES" && return
 
-# The funny [] around a letter makes shopt -s nullglob remove this file from the list if it does not exist,
-# files without a [] are mandatory.
+# Assume that we have openssh with configs in /etc/ssh
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/ssh* /etc/openssh* /etc/centrifydc/ssh* /root/.s[s]h /root/.shos[t]s )
+# The funny [] around a letter makes 'shopt -s nullglob' remove this file from the list if it does not exist.
+# Files without a [] are mandatory.
+
+local copy_as_is_ssh_files=()
+if is_true "$SSH_FILES" ; then
+    # Copy all the "usual SSH files" (including SSH private host keys) to make things "just work"
+    # into the recovery system, cf. https://github.com/rear/rear/issues/1512
+    copy_as_is_ssh_files=( /etc/ssh* /etc/openssh* /etc/centrifydc/ssh* /root/.s[s]h /root/.shos[t]s )
+else
+    # Use a reasonably secure fallback if SSH_FILES is not set or empty:
+    test "$SSH_FILES" || SSH_FILES="avoid_sensitive_files"
+    if test "avoid_sensitive_files" = "$SSH_FILES" ; then
+        # Avoid copying sensitive SSH files:
+        # From /etc/ssh copy only moduli ssh_config sshd_config ssh_known_hosts
+        # and from /root/.ssh copy only authorized_keys known_hosts (if exists)
+        # cf. https://github.com/rear/rear/issues/1512#issuecomment-331638066
+        copy_as_is_ssh_files=( /etc/ssh/modu[l]i /etc/ssh/ssh_co[n]fig /etc/ssh/sshd_co[n]fig /etc/ssh/ssh_known_hos[t]s )
+        copy_as_is_ssh_files=( "${copy_as_is_ssh_files[@]}" /root/.ssh/authorized_ke[y]s /root/.ssh/known_hos[t]s )
+    else
+        # Copy exactly what is specified:
+        copy_as_is_ssh_files=( "${SSH_FILES[@]}" )
+    fi
+fi
+test "${copy_as_is_ssh_files[*]}" && COPY_AS_IS=( "${COPY_AS_IS[@]}" "${copy_as_is_ssh_files[@]}" )
+
 PROGS=(
     "${PROGS[@]}"
     ssh sshd scp sftp ssh-agent ssh-keygen
-    $(
-        read subsys sftp original_file junk < <( grep sftp /etc/sshd_co[n]fig /etc/ssh/sshd_co[n]fig /etc/openssh/sshd_co[n]fig /etc/centrifydc/ssh/sshd_co[n]fig 2>/dev/null )
-        echo $original_file
-    )
+    $( read subsys sftp original_file junk < <( grep sftp /etc/sshd_co[n]fig /etc/ssh/sshd_co[n]fig /etc/openssh/sshd_co[n]fig /etc/centrifydc/ssh/sshd_co[n]fig 2>/dev/null )
+       echo $original_file
+     )
 )
 
 # Part 1: SSH server (sshd) - this is for logging into the recovery system via SSH
@@ -26,23 +50,23 @@ PROGS=(
 LIBS=( "${LIBS[@]}" /usr/lib64/libfreeblpriv3.* /lib/libfreeblpriv3.* )
 Log "Adding required libfreeblpriv3.so to LIBS"
 
-# copy ssh user
+# Copy ssh user.
 # getent will return all entries that match the key(s) exactly - most systems use 'sshd', some may use 'ssh', none should use both.
 # CLONE_USERS will also automatically clone the users' primary group
-# Only the first line (first returned entry) will be used by 'read' in 'IFS=: read ... <<<"$PASSWD_SSH"', so we ask for sshd first.
-PASSWD_SSH=$(getent passwd sshd ssh)
-if test "$PASSWD_SSH" ; then
+# Only the first line (first returned entry) will be used by 'read' in 'IFS=: read ... <<<"$passswd_ssh"', so we ask for sshd first.
+local passswd_ssh=$( getent passwd sshd ssh )
+if test "$passswd_ssh" ; then
     # sshd:x:71:65:SSH daemon:/var/lib/sshd:/bin/false
-    IFS=: read user ex uid gid gecos homedir junk <<<"$PASSWD_SSH"
+    IFS=: read user ex uid gid gecos homedir junk <<<"$passswd_ssh"
     CLONE_USERS=( "${CLONE_USERS[@]}" $user )
-    DIRECTORIES_TO_CREATE=( "${DIRECTORIES_TO_CREATE[@]}" "$homedir 0700 root root")
+    DIRECTORIES_TO_CREATE=( "${DIRECTORIES_TO_CREATE[@]}" "$homedir 0700 root root" )
 fi
 
 echo "ssh:23:respawn:/bin/sshd -D" >>$ROOTFS_DIR/etc/inittab
 
-# print a warning if there is no authorized_keys file for root and no SSH_ROOT_PASSWORD set
+# Print an info if there is no authorized_keys file for root and no SSH_ROOT_PASSWORD set:
 if ! test -f "/root/.ssh/authorized_keys" -o "$SSH_ROOT_PASSWORD" ; then
-    LogPrint "TIP: To log into the recovery system as root via ssh you need to set up /root/.ssh/authorized_keys or SSH_ROOT_PASSWORD in your configuration file"
+    LogPrint "To log into the recovery system via ssh set up /root/.ssh/authorized_keys or specify SSH_ROOT_PASSWORD"
 fi
 
 # Set the SSH root password; if pw is encrypted just copy it otherwise use openssl (for backward compatibility)
@@ -55,7 +79,8 @@ if test "$SSH_ROOT_PASSWORD" ; then
             echo "root:$SSH_ROOT_PASSWORD:::::::" > $ROOTFS_DIR/etc/shadow
             ;;
         (*)
-            echo "root:$(echo $SSH_ROOT_PASSWORD | openssl passwd -1 -stdin):::::::" > $ROOTFS_DIR/etc/shadow
+            echo "root:$( echo $SSH_ROOT_PASSWORD | openssl passwd -1 -stdin ):::::::" > $ROOTFS_DIR/etc/shadow
             ;;
     esac
 fi
+
