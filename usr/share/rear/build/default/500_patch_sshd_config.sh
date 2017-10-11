69a7b3fc789dfdeda212c6c848bf397f986a4027
Johannes Meixner <jsmeix@suse.com>
2017-10-11 13:50:29 +0200

    Only generate SSH host key of a particular type when the matching /etc/ssh/ssh_host_TYPE_key exists in the original system and explain the behaviour in default.conf

--- usr/share/rear/build/default/500_patch_sshd_config.sh
+++ usr/share/rear/build/default/500_patch_sshd_config.sh
@@ -14,7 +14,7 @@ is_false "$SSH_FILES" && return
 # - disable motd printing, our /etc/profile does that
 # - if SSH_ROOT_PASSWORD was defined allow root to login via ssh
 # The idea is to allow ssh authorized_keys based access in the recovery system
-# which has to be enabled in the original system to work in the recovery system.
+# which has to be enabled on the original system to work in the recovery system.
 # The funny [] around a letter makes 'shopt -s nullglob' remove this file from the list if it does not exist.
 # Files without a [] are mandatory.
 local sshd_config_files=( $ROOTFS_DIR/etc/ssh/sshd_co[n]fig $ROOTFS_DIR/etc/sshd_co[n]fig $ROOTFS_DIR/etc/openssh/sshd_co[n]fig )
@@ -55,26 +55,37 @@ fi
 # like SLES10 cf. https://github.com/rear/rear/issues/1522
 # and currently this backward compatibility should not be broken
 # (for the future see https://github.com/rear/rear/issues/1390)
-# so that we try to generate all possible types of keys.
-# This is in compliance what there is on a default SLES system, e.g.
-# on a default SLES10 there is
+# so that we try to generate all possible types of keys provided
+# the particular type of key also exists on the original system.
+# For example what there is on a default SLES system:
+# On a default SLES10 there is
 #   /etc/ssh/ssh_host_rsa_key
 #   /etc/ssh/ssh_host_dsa_key
-# on a default SLES11 there is additionally
+# On a default SLES11 there is additionally
 #   /etc/ssh/ssh_host_ecdsa_key
-# on a default SLES12 there is additionally
+# On a default SLES12 there is additionally
 #   /etc/ssh/ssh_host_ed25519_key
-# only a rsa1 type key does not exists so that rsa1 is also not generated here:
-local ssh_host_key_types="rsa dsa ecdsa ed25519"
+local ssh_host_key_types="rsa1 rsa dsa ecdsa ed25519"
 local ssh_host_key_type=""
 local ssh_host_key_file=""
 local recovery_system_key_file=""
 local ssh_host_key_exists="no"
 for ssh_host_key_type in $ssh_host_key_types ; do
     ssh_host_key_file="etc/ssh/ssh_host_${ssh_host_key_type}_key"
+    # Do not overwrite what is already there (could have been copied via COPY_AS_IS):
     if test -f "$ROOTFS_DIR/$ssh_host_key_file" ; then
         Log "Using existing SSH host key $ssh_host_key_file in recovery system"
         ssh_host_key_exists="yes"
+        continue
+    fi
+    # Only generate the particular type of key if it also exists on the original system
+    # because it is no longer recommended to use host keys other than rsa and ed25519,
+    # see section 2.2.1 in https://bettercrypto.org/static/applied-crypto-hardening.pdf
+    # and generating old-style host keys will only help with ssh clients which are
+    # very old (OpenSSH versions < 2.9), see https://www.openssh.com/releasenotes.html
+    # cf. https://github.com/rear/rear/pull/1530#discussion_r143948453
+    if ! test -f "/$ssh_host_key_file" ; then
+        Log "Skip generating $ssh_host_key_type type key because there is no /$ssh_host_key_file on the original system"
         continue
     fi
     Log "Generating new SSH host key $ssh_host_key_file in recovery system"
@@ -82,7 +93,7 @@ for ssh_host_key_type in $ssh_host_key_types ; do
     mkdir $v -p $( dirname "$recovery_system_key_file" )
     ssh-keygen $v -t "$ssh_host_key_type" -N '' -f "$recovery_system_key_file" && ssh_host_key_exists="yes" || Log "Cannot generate key type $ssh_host_key_type"
 done
-is_false "$ssh_host_key_exists" && LogPrintError "No SSH host key etc/ssh/ssh_host_TYPE_key of type $ssh_host_key_types in recovery system"
+is_false "$ssh_host_key_exists" && LogPrintError "No SSH host key etc/ssh/ssh_host_TYPE_key of any type $ssh_host_key_types in recovery system"
 
 # Create possibly missing directories needed by sshd in the recovery system
 # cf. https://github.com/rear/rear/issues/1529
