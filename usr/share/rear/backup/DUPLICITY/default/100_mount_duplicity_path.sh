9c1dcd578d2ed3bd3202bfe04ec7293161190180
lukey <lukasstraub2@web.de>
2017-10-27 18:43:01 +0200

    Duplicity: Backup: Add Support for NETFS URLs

---
+++ usr/share/rear/backup/DUPLICITY/default/100_mount_duplicity_path.sh
@@ -0,0 +1,15 @@
+# create mount point
+if [ -n $BACKUP_URL ]; then
+	mkdir -p $v "$BUILD_DIR/outputfs" >&2
+	StopIfError "Could not mkdir '$BUILD_DIR/outputfs'"
+
+	AddExitTask "rmdir $v $BUILD_DIR/outputfs >&2"
+
+	if [[ "$BACKUP_MOUNTCMD" ]] ; then
+		BACKUP_URL="var://BACKUP_MOUNTCMD"
+	fi
+
+	mount_url $BACKUP_URL $BUILD_DIR/outputfs $BACKUP_OPTIONS
+	
+	BACKUP_DUPLICITY_URL="file://$BUILD_DIR/outputfs"
+fi
