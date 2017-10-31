59be890d3b30f49a7acfb9300eb2dff644bfbbb5
Gratien D'haese <gratien.dhaese@gmail.com>
2017-10-31 15:24:23 +0100

    Script prep/GNU/Linux/280_include_virtualbox.sh copied the whole /opt/VBoxGuestAdditions-5.1.26 directory which resulted in an issue https://github.com/gdha/rear-automated-testing/issues/35 Has to do with ReaR issue #1474 We prevented copying of the VBoxGuestAdditions directory content to the rescue image. Drawback? Unknown

--- usr/share/rear/prep/GNU/Linux/280_include_virtualbox.sh
+++ usr/share/rear/prep/GNU/Linux/280_include_virtualbox.sh
@@ -5,7 +5,10 @@ if lsmod | grep -q vbox ; then
     MODULES=( ${MODULES[@]} ${VBOX_MODULES[@]} )
     CLONE_USERS=( ${CLONE_USERS[@]}  vboxadd )
     CLONE_GROUPS=( ${CLONE_GROUPS[@]} vboxusers )
-    VBOX_COPY_AS_IS=( /etc/init.d/vboxadd* /opt/VBoxGuestAdditions-* /usr/sbin/VBoxService )
+    # As libX* seems to be required by VBoxClient - however, no idea if we really needs this at all
+    # see issue #1474 for background information
+    #VBOX_COPY_AS_IS=( /etc/init.d/vboxadd* /opt/VBoxGuestAdditions-* /usr/sbin/VBoxService )
+    VBOX_COPY_AS_IS=( /etc/init.d/vboxadd*  /usr/sbin/VBoxService )
     COPY_AS_IS=( ${COPY_AS_IS[@]} ${VBOX_COPY_AS_IS[@]} )
     Log "Adding virtualbox modules"
 fi
