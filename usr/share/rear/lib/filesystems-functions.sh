8e78c9b9d5f21265ef340308fd50fa832a89e946
Oliver Okrongli <oliver.okrongli@infix.de>
2017-11-21 18:48:19 +0100

    Fix btrfs_subvolume_exists (fix #1589)

--- usr/share/rear/lib/filesystems-functions.sh
+++ usr/share/rear/lib/filesystems-functions.sh
@@ -10,16 +10,17 @@ function btrfs_subvolume_exists() {
     # A root subvolume can be assumed to always exist
     [ "$btrfs_subvolume_path" == "/" ] && return 0
 
-    # A non-root subvolume exists if the btrfs subvolume list contains its complete path at the end of one line.
-    # This code deliberately uses a plain string comparison rather than a regexp.
+    # A non-root subvolume exists if the btrfs subvolume list contains its complete path at the end of one line,
+    # preceded by " path ". This code deliberately uses a plain string comparison rather than a regexp.
     btrfs subvolume list "$subvolume_mountpoint" |
     awk -v path="$btrfs_subvolume_path" '
         BEGIN {
-            path_length = length(path);
+            match_string = " path " path;
+            match_string_length = length(match_string);
             matching_line_count = 0;
         }
 
-        (substr($0, length($0) - path_length + 1) == path) {
+        (substr($0, length($0) - match_string_length + 1) == match_string) {
             matching_line_count++;
         }
 
