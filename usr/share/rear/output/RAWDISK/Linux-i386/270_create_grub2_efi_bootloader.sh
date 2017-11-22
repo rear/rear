3d81b341fb9965351f2cd822bf31ac92da827aa5
Oliver Okrongli <oliver.okrongli@infix.de>
2017-11-22 16:10:10 +0100

    Add new output method RAWDISK

---
+++ usr/share/rear/output/RAWDISK/Linux-i386/270_create_grub2_efi_bootloader.sh
@@ -0,0 +1,47 @@
+# Use Grub 2 to create an EFI bootloader
+#
+
+
+### Check prerequisites
+
+# Run only if no EFI bootloader has been created yet and Grub 2 is available
+([[ -n "$RAWDISK_EFI_STAGING_ROOT" ]] || ! has_binary grub-mkimage) && return 0
+
+if ! is_true ${RAWDISK_INCLUDE_GRUB2_EFI:-yes}; then
+    LogPrint "DISABLED: Using Grub 2 to create an EFI bootloader"
+    return 0
+fi
+
+
+### Copy Grub 2 files into the staging directory
+
+LogPrint "Using Grub 2 to create an EFI bootloader"
+
+if is_true $USING_UEFI_BOOTLOADER ; then
+    LogPrint "TIP: You can achieve a faster EFI boot by installing syslinux for EFI on this system"
+fi
+
+RAWDISK_EFI_STAGING_ROOT="$BUILD_DIR/EFI"
+local efi_boot_directory="$RAWDISK_EFI_STAGING_ROOT/BOOT"
+
+mkdir $v -p "$efi_boot_directory"
+StopIfError "Could not create $efi_boot_directory"
+
+# Create a Grub 2 configuration file
+cat > "$efi_boot_directory/grub.cfg" << EOF
+set timeout=0
+set default=0
+menuentry "${RAWDISK_GRUB_BOOT_MENUENTRY_TITLE:-Rescue System}" {
+    linux /$(basename "$KERNEL_FILE") $KERNEL_CMDLINE
+    initrd /$REAR_INITRD_FILENAME
+}
+EOF
+
+# Create a Grub 2 EFI core image
+local grub_modules=( part_gpt fat normal configfile linux video all_video )
+grub-mkimage -O x86_64-efi -o "$efi_boot_directory/BOOTX64.efi" -p "/EFI/BOOT" "${grub_modules[@]}"
+StopIfError "Error occurred during grub-mkimage of $efi_boot_directory/BOOTX64.efi"
+
+
+# Note: Since the PBA should boot as quietly as possible (and errors are not an option ;-)),
+# this installation sticks to the bare minimum configuration, omitting fonts and locale files.
