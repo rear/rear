a6f7951beb862a6b176f8cfa6192ffb5d5d4e482
Vladimir Gozora <c@gozora.sk>
2017-11-22 20:20:50 +0100

    Borg backup as back end now displays progress, when ReaR is launched in verbose mode.

--- usr/share/rear/backup/BORG/default/500_make_backup.sh
+++ usr/share/rear/backup/BORG/default/500_make_backup.sh
@@ -19,11 +19,16 @@ done
 Log "Creating archive ${BORGBACKUP_ARCHIVE_PREFIX}_$BORGBACKUP_SUFFIX \
 in repository $BORGBACKUP_REPO on host $BORGBACKUP_HOST"
 
+# Only in ReaR verbose mode also show borg progress output and stats
+local borg_progress=''
+test "$verbose" && borg_progress='--progress --stats'
+
 # Start actual Borg backup.
-borg create --one-file-system --verbose --stats $BORGBACKUP_OPT_COMPRESSION \
-$BORGBACKUP_OPT_REMOTE_PATH --exclude-from $TMP_DIR/backup-exclude.txt \
+borg create --one-file-system $borg_progress $verbose \
+$BORGBACKUP_OPT_COMPRESSION $BORGBACKUP_OPT_REMOTE_PATH \
+--exclude-from $TMP_DIR/backup-exclude.txt \
 $BORGBACKUP_USERNAME@$BORGBACKUP_HOST:$BORGBACKUP_REPO::\
 ${BORGBACKUP_ARCHIVE_PREFIX}_$BORGBACKUP_SUFFIX \
-${include_list[@]}
+${include_list[@]} 0<&6 1>&7 2>&8
 
 StopIfError "Failed to create backup"
