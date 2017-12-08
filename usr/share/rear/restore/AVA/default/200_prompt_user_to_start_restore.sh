6913919aa63441b6c103d13a2fb134d7cd9b49ba
kai7p <kai.scharwies@7p-group.com>
2017-12-08 12:09:34 +0100

    Update 200_prompt_user_to_start_restore.sh

--- usr/share/rear/restore/AVA/default/200_prompt_user_to_start_restore.sh
+++ usr/share/rear/restore/AVA/default/200_prompt_user_to_start_restore.sh
@@ -1,7 +1,7 @@
 # the user has to do the main part here :-)
 #
 #
-/opt/avamar/bin/avagent.bin --bindir="/opt/avamar/bin" --vardir="/opt/avamar/var" --sysdir="/opt/avamar/etc" --logfile="/tmp/avagent.log"
+$AVA_ROOT_DIR/bin/avagent.bin --bindir="$AVA_ROOT_DIR/bin" --vardir="$AVA_ROOT_DIR/var" --sysdir="$AVA_ROOT_DIR/etc" --logfile="/tmp/avagent.log" 0<&6 1>&7 2>&8
 LogPrint "Please restore your backup in the Avamar WebGui (to path '/mnt/local/').
 When finished enter 'touch /mnt/local/.autorelabel' to ensure that SELinux relabels the files on the next boot.
 Afterwards type 'exit' in the shell to continue recovery, and then 'reboot'"
