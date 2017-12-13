d7d9b3c91bb626f18bd3fdfd9a551d864dc0bad1
Johannes Meixner <jsmeix@suse.com>
2017-12-13 14:34:59 +0100

    Report 'dsmc query session' non-zero exit code to the user but do not abort 'rear recover' (issue 1534)

--- usr/share/rear/verify/TSM/default/389_check_TSM_connection.sh
+++ usr/share/rear/verify/TSM/default/389_check_TSM_connection.sh
@@ -14,6 +14,16 @@
 # Test with "dsmc query session" (it is faster than e.g. "dsmc query mgmt")
 # and it is also enough for the prompt of the general TSM password, cf.
 # https://github.com/rear/rear/issues/1534#issuecomment-351067465
-dsmc query session 0<&6 1>&7 2>&8 && return
-Error "No connection to TSM server ('dsmc query session' returns '$?')"
+LogUserOutput "Testing connection to TSM server"
+dsmc query session 0<&6 1>&7 2>&8
+local dsmc_exit_code=$?
+# When 'dsmc query session' results a non-zero exit code inform the user but do not abort the whole "rear recover" here
+# because it could be an unimportant reason why 'dsmc query session' finished with a non-zero exit code.
+# What usual exit codes mean see http://publib.boulder.ibm.com/tividd/td/TSMC/GC32-0787-04/en_US/HTML/ans10000117.htm
+# and see the code in usr/share/rear/restore/TSM/default/400_restore_with_tsm.sh
+if test $dsmc_exit_code -eq 0 ; then
+    LogUserOutput "Testing connection to TSM server completed successfully"
+else
+    LogUserOutput "Testing connection to TSM server completed with 'dsmc query session' exit code $dsmc_exit_code"
+fi
 
