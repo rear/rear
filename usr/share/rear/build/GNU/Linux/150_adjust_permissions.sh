da08aaed0f02b1f3657bc729b230b13fc5d8714c
Oliver Okrongli <oliver.okrongli@infix.de>
2018-01-03 22:38:41 +0100

    Fix chown typo

--- usr/share/rear/build/GNU/Linux/150_adjust_permissions.sh
+++ usr/share/rear/build/GNU/Linux/150_adjust_permissions.sh
@@ -4,6 +4,6 @@ pushd $ROOTFS_DIR >/dev/null
 # SSH requires this
 chmod $v 0700 root >&2
 chmod $v 0755 var/empty var/lib/empty >&2
-chown $v -R root.root root var/empty var/lib/empty >&2
+chown $v -R root:root root var/empty var/lib/empty >&2
 
 popd >/dev/null
