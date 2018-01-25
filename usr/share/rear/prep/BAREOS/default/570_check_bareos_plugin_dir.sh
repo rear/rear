e40ba18f024a504531de3b579d1c1f4af6a2fb90
Gratien D'haese <gratien.dhaese@gmail.com>
2018-01-25 11:07:49 +0100

    fix typo - I -> If

--- usr/share/rear/prep/BAREOS/default/570_check_bareos_plugin_dir.sh
+++ usr/share/rear/prep/BAREOS/default/570_check_bareos_plugin_dir.sh
@@ -1,5 +1,5 @@
 # prep/BAREOS/default/570_check_bareos_plugin_dir.sh
-# Purpose: check if the Bareos 'plugin' directory exists? I so, then add it to the COPY_AS_IS array
+# Purpose: check if the Bareos 'plugin' directory exists? If so, then add it to the COPY_AS_IS array
 
 if [[ -d /usr/lib/bareos/plugins ]] ; then
     COPY_AS_IS=( "${COPY_AS_IS[@]}" /usr/lib/bareos/plugins )
