6b2cc2687982714b2e033c4cdbc05162b591d185
Vladimir Gozora <c@gozora.sk>
2018-02-10 19:51:37 +0100

    Removed AddExitTask from 250_mount_usb.sh as whole $BUILD_DIR gets removed when ReaR ends.

--- usr/share/rear/prep/BORG/default/250_mount_usb.sh
+++ usr/share/rear/prep/BORG/default/250_mount_usb.sh
@@ -7,7 +7,5 @@ if [[ -z $BORGBACKUP_HOST ]]; then
     mkdir -p $v "$BUILD_DIR/borg_backup" >&2
     StopIfError "Could not mkdir '$BUILD_DIR/borg_backup'"
 
-    AddExitTask "rm -Rf $v $BUILD_DIR/borg_backup >&2"
-
     mount_url usb://$USB_DEVICE $BUILD_DIR/borg_backup
 fi
