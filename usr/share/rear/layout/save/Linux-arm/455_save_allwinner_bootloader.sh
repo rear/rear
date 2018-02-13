0b71ee6296cce4a4c1f465789aba5d7f16a76c53
Johannes Meixner <jsmeix@suse.com>
2018-02-13 10:07:39 +0100

    Update 455_save_allwinner_bootloader.sh
    Fixed spelling in comment and Log message.

--- usr/share/rear/layout/save/Linux-arm/455_save_allwinner_bootloader.sh
+++ usr/share/rear/layout/save/Linux-arm/455_save_allwinner_bootloader.sh
@@ -1,7 +1,7 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-# Save the 2nd-Stage Bootloader on Allwinner Devices
+# Save the 2nd-stage bootloader on Allwinner devices
 
 [ "$BOOTLOADER" = "ARM-ALLWINNER" ] || return 0
 
@@ -14,6 +14,6 @@ for block_device in /sys/block/* ; do
     # Continue with the next block device when the current block device is not a disk that can be used for booting:
     [[ $blockd = hd* || $blockd = sd* || $blockd = cciss* || $blockd = vd* || $blockd = xvd* || $blockd = nvme* || $blockd = mmcblk* || $blockd = dasd*  ]] || continue
     disk_device=$( get_device_name $block_device )
-    Log "Saving Allwinner 2nd Stage Bootloader for Device $disk_device"
+    Log "Saving Allwinner 2nd stage bootloader for device $disk_device"
     dd if=$disk_device bs=1024 skip=8 count=$((1024-8)) of=$base_dir/$blockd
 done
