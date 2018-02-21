7fc06af1d32827aa1d1839b9b27fd51762c93645
Rich Alloway <richard.alloway@roguewave.com>
2018-02-21 17:12:11 -0500

    Clean up backup/YUM/default/600_capture_selinux_contexts.sh

--- usr/share/rear/backup/YUM/default/600_capture_selinux_contexts.sh
+++ usr/share/rear/backup/YUM/default/600_capture_selinux_contexts.sh
@@ -2,20 +2,8 @@
 # backup/YUM/default/600_capture_selinux_contexts.sh
 #
 
-# For BACKUP=YUM the RPM data got stored into the
-# ReaR recovery system via prep/YUM/default/400_prep_rpm.sh
-# When backup/YUM/default/500_make_backup.sh runs
-# the ReaR recovery system is already made
-# (its recovery/rescue system initramfs/initrd is already created)
-# so that at this state nothing can be stored into the recovery system.
-# At this state an additional normal file based backup can be made
-# in particular to backup all those files that do not belong to an installed RPM package
-# (e.g. files in /home/ directories or third-party software in /opt/) or files
-# that belong to a RPM package but are changed (i.e. where "rpm -V" reports differences)
-# (e.g. config files like /etc/default/grub).
-
 # Since SELinux security contexts could be set differently on the source system than
-# what the RPM-provided or non-RPM-provided files will have, we need to capture the
+# what the RPM-provided or non-RPM-provided files will have, we will capture the
 # security contexts for every file on the filesystem that will be in the backup archive
 
 if ! is_true "$YUM_BACKUP_FILES" ; then
