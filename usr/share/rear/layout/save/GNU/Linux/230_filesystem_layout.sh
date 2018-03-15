85157adeee26dec6ad9f30934196353f24cb2c71
Gratien D'haese <gratien.dhaese@gmail.com>
2018-03-15 17:58:26 +0100

    improving the docker exception (advise from jsmeix) #1749

--- usr/share/rear/layout/save/GNU/Linux/230_filesystem_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/230_filesystem_layout.sh
@@ -51,6 +51,14 @@ fi
 read_filesystems_command="$read_filesystems_command | sort -t ' ' -k 1,1 -u"
 # Begin writing output to DISKLAYOUT_FILE:
 (
+    # docker daemon mounts file systems for its docker containers
+    # see also https://docs.docker.com/storage/storagedriver/device-mapper-driver/#configure-direct-lvm-mode-for-production
+    # As it is for container usage only we do not to backup these up or recreate as this disk device is completely under
+    # control by docker itself (even the recreation of it incl, the creation of the volume group). Usually this is
+    # done via a kind of cookbook (Chef, puppet or ansible)
+    docker_is_running=""
+    service docker status >/dev/null 2>&1 && docker_is_running="yes"
+    #
     echo "# Filesystems (only $supported_filesystems are supported)."
     echo "# Format: fs <device> <mountpoint> <fstype> [uuid=<uuid>] [label=<label>] [<attributes>]"
     # Read the output of the read_filesystems_command:
@@ -80,11 +88,13 @@ read_filesystems_command="$read_filesystems_command | sort -t ' ' -k 1,1 -u"
             continue
         fi
         # docker specific exclude part
-        if service docker status >/dev/null 2>&1  ; then
+        if is_true $docker_is_running ; then
             # docker daemon/service is running
             docker_root_dir=$( docker info 2>/dev/null | grep 'Docker Root Dir' | awk '{print $4}' )
-            # if $docker_root_dir is the begining of the $mountpoint then FS is under docker control
-            # and we better exclude from saving the layout
+            # If $docker_root_dir is in the beginning of the $mountpoint string then FS is under docker control
+            # and we better exclude from saving the layout,
+            # see https://github.com/rear/rear/issues/1749
+            Log "$device is mounted below $docker_root_dir (mount point $mountpoint is under docker control), skipping."
             echo "$mountpoint" | grep -q "^$docker_root_dir" && continue
         fi
         # Replace a symbolic link /dev/disk/by-uuid/a1b2c3 -> ../../sdXn
