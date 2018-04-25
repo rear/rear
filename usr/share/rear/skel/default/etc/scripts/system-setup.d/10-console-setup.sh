cb63f51c0e155a7b7654991ee38bb5bf7d1cf529
Johannes Meixner <jsmeix@suse.de>
2018-04-25 15:11:30 +0200

    Better code in 10-console-setup.sh

--- usr/share/rear/skel/default/etc/scripts/system-setup.d/10-console-setup.sh
+++ usr/share/rear/skel/default/etc/scripts/system-setup.d/10-console-setup.sh
@@ -1,17 +1,20 @@
-# Console keyboad mapping setup
+
+# Console keyboad mapping setup.
+#
+# With topmost priority it tries to set what the user specified as KEYMAP
+# which may fail when needed keymap files are missing (e.g. include files).
+# With second priority it tries to set the keymap of the original system.
+# As fallback it tries to set at least the default (US keyboad) keymap.
 
 default_keymap="defkeymap"
 original_system_dumpkeys_file="/etc/dumpkeys.out"
 
-keymap=""
-# Use KEYMAP if it is explicitly specified by the user:
-test $KEYMAP && keymap="$KEYMAP"
-
-# Load keymap:
-if test $keymap ; then
-    echo "Using $keymap keymap"
-    if ! loadkeys $keymap ; then
-        echo "Failed to set $keymap keymap" 1>&2 
+# Set KEYMAP if it is explicitly specified by the user:
+if test $KEYMAP ; then
+    echo "Using $KEYMAP keymap"
+    if ! loadkeys $KEYMAP ; then
+        # If setting KEYMAP failed try to set the keymap of the original system:
+        echo "Failed to set $KEYMAP keymap" 1>&2 
         if test -s $original_system_dumpkeys_file ; then
             echo "Using keymap of the original system" 1>&2
             if ! loadkeys $original_system_dumpkeys_file ; then
@@ -20,11 +23,13 @@ if test $keymap ; then
                 loadkeys $default_keymap || echo "Even failed to set $default_keymap" 1>&2
             fi
         else
+            # When setting KEYMAP failed and there is no original_system_dumpkeys_file set the default keymap:
             echo "Using $default_keymap (US keyboad)" 1>&2
             loadkeys $default_keymap || echo "Also failed to set $default_keymap" 1>&2
         fi
     fi
 else
+    # When there no KEYMAP specified try to set the keymap of the original system:
     if test -s $original_system_dumpkeys_file ; then
         echo "Using keymap of the original system"
         if ! loadkeys $original_system_dumpkeys_file ; then
@@ -32,6 +37,10 @@ else
             echo "Failed to set original system keymap, using $default_keymap (US keyboad)" 1>&2
             loadkeys $default_keymap || echo "Also failed to set $default_keymap" 1>&2
         fi
+    else
+        # When there is neither KEYMAP nor original_system_dumpkeys_file set the default keymap:
+        echo "Using $default_keymap (US keyboad)"
+        loadkeys $default_keymap || echo "Failed to set $default_keymap" 1>&2
     fi
 fi
 
