ef5aea6efb96233eaebe30ffc6fbbdf0baf2dd22
Johannes Meixner <jsmeix@suse.de>
2018-04-26 12:39:54 +0200

    Implement KEYMAPS_DEFAULT_DIRECTORY autodetection for various known default directories for keymaps which also simplified the code that finds the defkeymap keymap file

--- usr/share/rear/rescue/GNU/Linux/500_clone_keyboard_mappings.sh
+++ usr/share/rear/rescue/GNU/Linux/500_clone_keyboard_mappings.sh
@@ -4,25 +4,42 @@
 local original_system_dumpkeys_file="/etc/dumpkeys.out"
 dumpkeys -f >$ROOTFS_DIR$original_system_dumpkeys_file
 
-# At least on SUSE systems /usr/share/kbd/keymaps is the default directory for keymaps:
-local keymaps_default_directory="/usr/share/kbd/keymaps"
-
+# Determine the default directory for keymaps:
+# Different Linux distributions use a different default directory for keymaps,
+# see https://github.com/rear/rear/pull/1781#issuecomment-384322051
+# and https://github.com/rear/rear/pull/1781#issuecomment-384331316
+# and https://github.com/rear/rear/pull/1781#issuecomment-384560856
+# that read (excerpts):
+#   SUSE systems: /usr/share/kbd/keymaps
+#   Debian 9.2 (stretch): /usr/share/keymaps
+#   Centos 7.3.1611 (Core): /lib/kbd/keymaps
+#   Fedora release 26 (Twenty Six): /lib/kbd/keymaps
+#   Arch Linux: /usr/share/kbd/keymaps
+#   Ubuntu 12.04.5 LTS (Precise): /usr/share/keymaps
+#   RHEL 6/7: /lib/kbd/keymaps
+# so that we have this summary:
+#   /usr/share/kbd/keymaps is used by SUSE and Arch Linux
+#   /usr/share/keymaps is used by Debian and Ubuntu
+#   /lib/kbd/keymaps is used by Centos and Fedora and Red Hat
+# We do not test and distinguish by Linux distribution identifier strings
+# because such tests result an endless nightmare to keep them up-to-date.
+# We prefer (whenever possible) to generically test "some real thing".
+# Accordingly we test known directories and use the first one that exist.
+# The last '' is there to keep keymaps_default_directory empty if none of the directories exist
+# which is (currently) not strictly requied by the code below but it is cleaner code here:
+local keymaps_default_directory=""
+for keymaps_default_directory in /usr/share/kbd/keymaps /usr/share/keymaps /lib/kbd/keymaps '' ; do
+    test -d $keymaps_default_directory && break
+done
 # Use KEYMAPS_DEFAULT_DIRECTORY if it is explicitly specified by the user:
 test $KEYMAPS_DEFAULT_DIRECTORY && keymaps_default_directory="$KEYMAPS_DEFAULT_DIRECTORY"
-
 # Report when there is no keymaps default directory because other keyboard mappings (at least 'defkeymap') should get included
 # but that is not a severe error because the current keyboard mapping is dumped and gets used by default and as fallback:
 test -d $keymaps_default_directory || LogPrintError "Cannot include keyboard mappings (no keymaps default directory $keymaps_default_directory)"
 
-# Try to include at least the default US keyboard mapping:
+# Try to find and include at least the default US keyboard mapping:
 local defkeymap_file="$( find $keymaps_default_directory -name 'defkeymap.*' | head -n1 )"
-if test $defkeymap_file ; then
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" $defkeymap_file )
-else
-    # If no defkeymap file was found in $keymaps_default_directory try some RHEL, SLES and Ubuntu qwerty flavours.
-    # The funny ? makes 'shopt -s nullglob' remove such a file from the list if it does not exist:
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" /lib/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/k?d/keymaps/i386/qwerty/defkeymap.map.gz /usr/share/ke?maps/i386/qwerty/defkeymap.map.gz )
-fi
+test $defkeymap_file && COPY_AS_IS=( "${COPY_AS_IS[@]}" $defkeymap_file )
 
 # Additionally include other keyboard mappings to also support users with a non-US keyboard
 # who can then manually switch to their keyboard mapping (e.g. via a command like "loadkeys de-latin1")
