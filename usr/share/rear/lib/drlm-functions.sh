8a435ed80b7d4ef3cf49601a9f855f710caf2669
didacog <didac@brainupdaters.net>
2018-05-03 16:19:34 +0200

    Solve problem with some variables loading config from DRLM drlm issue 68

--- usr/share/rear/lib/drlm-functions.sh
+++ usr/share/rear/lib/drlm-functions.sh
@@ -26,7 +26,7 @@ function drlm_import_runtime_config() {
                 local DRLM_CFG="/tmp/$config_append_file"
                 local http_response_code=$(curl $verbose -f -s -S -w '%{http_code}' $DRLM_REST_OPTS -o $DRLM_CFG https://$DRLM_SERVER/clients/$DRLM_ID/config/$config_append_file)
                 test "200" = "$http_response_code" || Error "DRLM_MANAGED: curl failed with HTTP response code '$http_response_code' trying to load '$config_append_file' from DRLM."
-                eval $(cat $DRLM_CFG)
+                source $DRLM_CFG
                 rm $DRLM_CFG
             done
         else
@@ -34,7 +34,7 @@ function drlm_import_runtime_config() {
             local DRLM_CFG="/tmp/drlm_config"
             local http_response_code=$(curl $verbose -f -s -S -w '%{http_code}' $DRLM_REST_OPTS -o $DRLM_CFG https://$DRLM_SERVER/clients/$DRLM_ID/config)
             test "200" = "$http_response_code" || Error "DRLM_MANAGED: curl failed with HTTP response code '$http_response_code' trying to load configuration from DRLM."
-            eval $(cat $DRLM_CFG)
+            source $DRLM_CFG
             rm $DRLM_CFG
         fi
     else
