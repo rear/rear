123ba14960a96cd0f0428b642775283a65920e47
Johannes Meixner <jsmeix@suse.com>
2018-06-12 09:04:15 +0200

    Update 502_include_mdadm_conf.sh
    Added a comment with a URL to an explanation about the reason behind
    cf. https://github.com/rear/rear/pull/1819#discussion_r193341333

--- usr/share/rear/build/default/502_include_mdadm_conf.sh
+++ usr/share/rear/build/default/502_include_mdadm_conf.sh
@@ -1,6 +1,8 @@
 grep -q blocks /proc/mdstat 2>/dev/null || return 0
 
 # Include /etc/mdadm.conf without building arrays automatically
+# for the reason behind see
+# https://github.com/rear/rear/issues/1722#issuecomment-394746478
 if [ -e "/etc/mdadm.conf" ] ; then
 	(
 		echo "AUTO -all"
