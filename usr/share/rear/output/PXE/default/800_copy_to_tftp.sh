18ed9af79fbc2d0943a7f48455e7d329abb35ca0
Gratien D'haese <gratien.dhaese@gmail.com>
2018-07-17 10:59:51 +0200

    fedora28: syslinux needs libcom32.c32 to boot from HD

--- usr/share/rear/output/PXE/default/800_copy_to_tftp.sh
+++ usr/share/rear/output/PXE/default/800_copy_to_tftp.sh
@@ -55,6 +55,7 @@ if [[ ! -z "$PXE_TFTP_URL" ]] && [[ "$PXE_RECOVER_MODE" = "unattended" ]] ; then
     syslinux_modules_dir=$( find_syslinux_modules_dir menu.c32 )
     [[ -z "$syslinux_modules_dir" ]] && syslinux_modules_dir=$(dirname $PXELINUX_BIN)
     cp $v $syslinux_modules_dir/ldlinux.c32 $BUILD_DIR/tftpbootfs >&2
+    cp $v $syslinux_modules_dir/libcom32.c32 $BUILD_DIR/tftpbootfs >&2
     cp $v $syslinux_modules_dir/menu.c32 $BUILD_DIR/tftpbootfs >&2
     cp $v $syslinux_modules_dir/chain.c32 $BUILD_DIR/tftpbootfs >&2
     cp $v $syslinux_modules_dir/hdt.c32 $BUILD_DIR/tftpbootfs >&2
