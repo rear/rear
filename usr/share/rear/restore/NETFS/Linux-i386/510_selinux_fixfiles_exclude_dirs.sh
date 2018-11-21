ad335ffb8c1a2c0906e55c0343b46037f9a31f3b
Vladimir Gozora <c@gozora.sk>
2018-11-21 18:16:57 +0100

    Check for existence of /boot/efi directory in such late stage of rear recover process and ending with error if directory does not exist is pointless.

--- usr/share/rear/restore/NETFS/Linux-i386/510_selinux_fixfiles_exclude_dirs.sh
+++ usr/share/rear/restore/NETFS/Linux-i386/510_selinux_fixfiles_exclude_dirs.sh
@@ -4,11 +4,6 @@
 # empty or 0 means using BIOS method
 is_true $USING_UEFI_BOOTLOADER || return 0
 
-# check if $TARGET_FS_ROOT/boot/efi is mounted
-if ! test -d "$TARGET_FS_ROOT/boot/efi" ; then
-    Error "Could not find directory $TARGET_FS_ROOT/boot/efi"
-fi
-
 cat > $TARGET_FS_ROOT/etc/selinux/fixfiles_exclude_dirs <<EOF
 /boot/efi
 /boot/efi(/.*)?
