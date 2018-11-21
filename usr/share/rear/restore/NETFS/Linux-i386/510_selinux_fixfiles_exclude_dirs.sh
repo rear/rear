f280441cd3a6dac53b292bc0efab6a69ae83e91d
Vladimir Gozora <c@gozora.sk>
2018-11-21 18:53:21 +0100

    Only add /boot/efi to SElinux relabeling, if directory exists.

--- usr/share/rear/restore/NETFS/Linux-i386/510_selinux_fixfiles_exclude_dirs.sh
+++ usr/share/rear/restore/NETFS/Linux-i386/510_selinux_fixfiles_exclude_dirs.sh
@@ -4,8 +4,9 @@
 # empty or 0 means using BIOS method
 is_true $USING_UEFI_BOOTLOADER || return 0
 
-cat > $TARGET_FS_ROOT/etc/selinux/fixfiles_exclude_dirs <<EOF
+if test -d "$TARGET_FS_ROOT/boot/efi" ; then
+    cat > $TARGET_FS_ROOT/etc/selinux/fixfiles_exclude_dirs <<EOF
 /boot/efi
 /boot/efi(/.*)?
 EOF
-
+fi
