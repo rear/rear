f9e53a331c226fa305145866492bb978a1e66a2a
Gratien D'haese <gratien.dhaese@gmail.com>
2018-12-17 15:07:01 +0100

    simplify the code as suggested #1996
    Signed-off-by: Gratien D'haese <gratien.dhaese@gmail.com>

--- usr/share/rear/lib/uefi-functions.sh
+++ usr/share/rear/lib/uefi-functions.sh
@@ -46,11 +46,11 @@ function build_bootx86_efi {
         Log "Did not find grub-mkimage (cannot build bootx86.efi)"
         return
     fi
-    if [[ -f /etc/slackware-version ]] || [[ -f /etc/fedora-release ]] ; then
-        # slackware grub doesn't have linuxefi module
-        $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" part_gpt part_msdos fat ext2 normal chain boot configfile linux multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs
-    else
-        $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" part_gpt part_msdos fat ext2 normal chain boot configfile linux linuxefi multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs
-    fi
+    # as not all Linux distro's have the same kernel modules present we verify what we have (see also https://github.com/rear/rear/pull/2001)
+    grub_modules=""
+    for grub_module in part_gpt part_msdos fat ext2 normal chain boot configfile linux linuxefi multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs ; do
+        test "$( find /boot -type f -name "$grub_module.mod" 2>/dev/null )" && grub_modules="$grub_modules $grub_module"
+    done
+    $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" $grub_modules
     StopIfError "Error occurred during $gmkimage of BOOTX64.efi"
 }
