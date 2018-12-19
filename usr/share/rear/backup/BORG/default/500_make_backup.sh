0280fff2c287cdc5c8db0aa91c9f640ee1e654f4
Vladimir Gozora <c@gozora.sk>
2018-12-19 18:47:11 +0100

    Add possibility for user to specify whether or not show Borg stats at the end of backup session. Add possibility for user to specify whether or not show Borg backup session progress. Corrected URL to Borg backup environment variables documentation.

--- usr/share/rear/backup/BORG/default/500_make_backup.sh
+++ usr/share/rear/backup/BORG/default/500_make_backup.sh
@@ -17,14 +17,16 @@ for i in $(cat $TMP_DIR/backup-include.txt); do
 done
 
 # Only in ReaR verbose mode also show borg progress output and stats
-local borg_progress=''
-test "$verbose" && borg_progress='--progress --stats'
+local borg_additional_options=''
+
+is_true $BORGBACKUP_SHOW_PROGRESS && borg_additional_options+='--progress '
+is_true $BORGBACKUP_SHOW_STATS && borg_additional_options+='--stats '
 
 # Start actual Borg backup.
 Log "Creating archive ${BORGBACKUP_ARCHIVE_PREFIX}_$BORGBACKUP_SUFFIX \
 in repository $BORGBACKUP_REPO"
 
-borg create --one-file-system $borg_progress $verbose \
+borg create --one-file-system $borg_additional_options $verbose \
 $BORGBACKUP_OPT_COMPRESSION $BORGBACKUP_OPT_REMOTE_PATH \
 $BORGBACKUP_OPT_UMASK --exclude-from $TMP_DIR/backup-exclude.txt \
 ${borg_dst_dev}${BORGBACKUP_REPO}::\
