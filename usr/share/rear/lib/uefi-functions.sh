3548430302cef3a8445de970eb763da33d9f6e8e
Ivan Aragonés Muniesa <iaragone@redhat.com>
2019-01-09 12:17:21 +0100

    I've added the correct variables to allow non-interactive authentication using rsync protocol. Now, one can use BACKUP_RSYNC_OPTIONS to authenticate using the "--password-file=/full/path/to/file" rsync's option.
    I already fixed the missing username at checking stage.
    
    I already fixed uefi-funcions.sh (I'm on Fedora29) as described at https://github.com/rear/rear/issues/1996
    
    I already changed the behaviour of --fake-root on rsync protocol versions < 29, as it may not return an error it the option is not used at all.
    
     Changes to be committed:
    	modified:   usr/share/rear/lib/uefi-functions.sh
    	modified:   usr/share/rear/output/RSYNC/default/200_make_prefix_dir.sh
    	modified:   usr/share/rear/output/RSYNC/default/900_copy_result_files.sh
    	modified:   usr/share/rear/prep/RSYNC/default/100_check_rsync.sh
    	modified:   usr/share/rear/prep/RSYNC/default/150_check_rsync_protocol_version.sh

--- usr/share/rear/lib/uefi-functions.sh
+++ usr/share/rear/lib/uefi-functions.sh
@@ -48,7 +48,7 @@ function build_bootx86_efi {
     fi
     # as not all Linux distro's have the same grub modules present we verify what we have (see also https://github.com/rear/rear/pull/2001)
     grub_modules=""
-    for grub_module in part_gpt part_msdos fat ext2 normal chain boot configfile linux linuxefi multiboot jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs ; do
+    for grub_module in part_gpt part_msdos fat ext2 normal chain boot configfile linux jfs iso9660 usb usbms usb_keyboard video udf ntfs all_video gzio efi_gop reboot search test echo btrfs ; do
         test "$( find /boot -type f -name "$grub_module.mod" 2>/dev/null )" && grub_modules="$grub_modules $grub_module"
     done
     $gmkimage $v -O x86_64-efi -c $TMP_DIR/mnt/EFI/BOOT/embedded_grub.cfg -o $TMP_DIR/mnt/EFI/BOOT/BOOTX64.efi -p "/EFI/BOOT" $grub_modules
