1bdeebe8a4e610e1f7773dc3b1f17c6966578b75
Johannes Meixner <jsmeix@suse.de>
2019-01-10 17:02:37 +0100

    Improve rear dump output to clearly distinguish array elements

--- usr/share/rear/lib/dump-workflow.sh
+++ usr/share/rear/lib/dump-workflow.sh
@@ -10,86 +10,137 @@ WORKFLOW_dump_DESCRIPTION="dump configuration and system information"
 WORKFLOWS=( ${WORKFLOWS[@]} dump )
 WORKFLOW_dump () {
 
-        # Do nothing in simulation mode, cf. https://github.com/rear/rear/issues/1939
-        if is_true "$SIMULATE" ; then
-            LogPrint "${BASH_SOURCE[0]} dumps configuration and system information"
-            return 0
+    # The dump workflow is always verbose (see usr/sbin/rear).
+
+    # Do nothing in simulation mode, cf. https://github.com/rear/rear/issues/1939
+    if is_true "$SIMULATE" ; then
+        LogPrint "${BASH_SOURCE[0]} dumps configuration and system information"
+        return 0
+    fi
+
+    LogPrint "Dumping out configuration and system information"
+
+    if [ "$ARCH" != "$REAL_ARCH" ] ; then
+        LogPrint "This is a '$REAL_ARCH' system, compatible with '$ARCH'."
+    fi
+
+    LogPrint "System definition:"
+    for var in "ARCH" "OS" \
+               "OS_MASTER_VENDOR" "OS_MASTER_VERSION" "OS_MASTER_VENDOR_ARCH" "OS_MASTER_VENDOR_VERSION" "OS_MASTER_VENDOR_VERSION_ARCH" \
+               "OS_VENDOR" "OS_VERSION" "OS_VENDOR_ARCH" "OS_VENDOR_VERSION" "OS_VENDOR_VERSION_ARCH" ; do
+        LogPrint "$( printf "%40s = %s" "$var" "${!var}" )"
+    done
+
+    LogPrint "Configuration tree:"
+    for config in "$ARCH" "$OS" \
+                  "$OS_MASTER_VENDOR" "$OS_MASTER_VENDOR_ARCH" "$OS_MASTER_VENDOR_VERSION" "$OS_MASTER_VENDOR_VERSION_ARCH" \
+                  "$OS_VENDOR" "$OS_VENDOR_ARCH" "$OS_VENDOR_VERSION" "$OS_VENDOR_VERSION_ARCH" ; do
+        if [ "$config" ] ; then
+            LogPrint "$( printf "%40s : %s" "$config".conf "$( test -s $SHARE_DIR/conf/"$config".conf && echo OK || echo missing/empty )" )"
         fi
+    done
+    for config in site local ; do
+        LogPrint "$( printf "%40s : %s" "$config".conf "$( test -s $CONFIG_DIR/"$config".conf && echo OK || echo missing/empty )" )"
+    done
 
-	LogPrint "Dumping out configuration and system information"
+    LogPrint "Backup with $BACKUP:"
+    # Output all $BACKUP_* config variable values e.g. for BACKUP=NETFS as something like
+    #   NETFS_CONFIG_STRING = 'string of words'
+    # or when it is an array variable than as
+    #   NETFS_CONFIG_ARRAY = 'first element' 'second element' ...
+    for variable_name in $( eval echo '${!'"$BACKUP"'_*}' ) ; do
+        # The command substitution for the list of items in the above 'for' loop evaluates
+        # to all $BACKUP_* config variable names e.g. for BACKUP=NETFS to something like:
+        #   ++ eval echo '${!NETFS_*}'
+        #   +++ echo NETFS_CONFIG_STRING NETFS_CONFIG_ARRAY ...
+        variable_values="$( eval 'for array_element in "${'"$variable_name"'[@]:-}" ; do echo -n "'"'"'$array_element'"'"' " ; done' )"
+        # Using an empty default value ( via "${ARRAY[@]:-}" ) in the above 'for' loop is needed for empty array variables because
+        # otherwise the 'for' loop would not be run at all for empty arrays like ARRAY=( ) which would result variable_values=
+        # instead of the intended variable_values="'' " that is output as ARRAY = '' to explicitly show an empty '' value.
+        # Welcome to the quoting hell in the command substitution for the variable_values assignment above:
+        # cf. "How to escape single quotes within single quoted strings?" at
+        # https://stackoverflow.com/questions/1250079/how-to-escape-single-quotes-within-single-quoted-strings
+        # that reads (excerpts and a bit changed here):
+        #   To use single quotes in the outermost layer ... you can glue both kinds of quotation.
+        #   Example:
+        #     eval ' ... '"'"' ... '"'"' ... '
+        #   Explanation of how '"'"' is interpreted as just ' :
+        #   1. ' end first quotation which uses single quotes
+        #   2. " start second quotation using double-quotes
+        #   3. ' quoted character
+        #   4. " end second quotation using double-quotes
+        #   5. ' start third quotation using single quotes
+        # If you do not place any whitespaces between (1) and (2) or between (4) and (5)
+        # the shell will interpret that string as a one long word.
+        LogPrint "$( printf "%40s = %s" "$variable_name" "$variable_values" )"
+    done
+    # Output all BACKUP_* config variable values e.g. as something like
+    #   BACKUP_CONFIG_STRING = 'string of words'
+    # or when it is an array variable than as
+    #   BACKUP_CONFIG_ARRAY = 'first element' 'second element' ...
+    for variable_name in $( eval echo '${!BACKUP_*}' ) ; do
+        case $variable_name in
+	    (BACKUP_PROG*)
+                ;;
+            (*)
+                # LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
+                variable_values="$( eval 'for array_element in "${'"$variable_name"'[@]:-}" ; do echo -n "'"'"'$array_element'"'"' " ; done' )"
+                LogPrint "$( printf "%40s = %s" "$variable_name" "$variable_values" )"
+                ;;
+        esac
+    done
+    case "$BACKUP" in
+        (NETFS)
+            LogPrint "Backup program is '$BACKUP_PROG':"
+            # Output all BACKUP_PROG_* config variable values e.g. as something like
+            #   BACKUP_PROG_STRING = 'string of words'
+            # or when it is an array variable than as
+            #   BACKUP_PROG_ARRAY = 'first element' 'second element' ...
+            for variable_name in $( eval echo '${!BACKUP_PROG_*}' ) ; do
+                # LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
+                variable_values="$( eval 'for array_element in "${'"$variable_name"'[@]:-}" ; do echo -n "'"'"'$array_element'"'"' " ; done' )"
+                LogPrint "$( printf "%40s = %s" "$variable_name" "$variable_values" )"
+            done
+        ;;
+    esac
 
-	if [ "$ARCH" != "$REAL_ARCH" ] ; then
-		LogPrint "This is a '$REAL_ARCH' system, compatible with '$ARCH'."
-	fi
+    LogPrint "Output to $OUTPUT:"
+    # Output all $OUTPUT_* config variable values e.g. for OUTPUT=ISO as something like
+    #   ISO_CONFIG_STRING = 'string of words'
+    # or when it is an array variable than as
+    #   ISO_CONFIG_ARRAY = 'first element' 'second element' ...
+    # and output all OUTPUT_* config variable values e.g. as something like
+    #   OUTPUT_CONFIG_STRING = 'string of words'
+    # or when it is an array variable than as
+    #   OUTPUT_CONFIG_ARRAY = 'first element' 'second element' ...
+    # and finally output the RESULT_MAILTO config variable value:
+    for variable_name in $( eval echo '${!'"$OUTPUT"'_*}' '${!OUTPUT_*}' ) RESULT_MAILTO ; do
+        # LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
+        variable_values="$( eval 'for array_element in "${'"$variable_name"'[@]:-}" ; do echo -n "'"'"'$array_element'"'"' " ; done' )"
+        LogPrint "$( printf "%40s = %s" "$variable_name" "$variable_values" )"
+    done
 
-	LogPrint "System definition:"
-	for var in "ARCH" "OS" \
-		"OS_MASTER_VENDOR" "OS_MASTER_VERSION" "OS_MASTER_VENDOR_ARCH" "OS_MASTER_VENDOR_VERSION" "OS_MASTER_VENDOR_VERSION_ARCH" \
-		"OS_VENDOR" "OS_VERSION" "OS_VENDOR_ARCH" "OS_VENDOR_VERSION" "OS_VENDOR_VERSION_ARCH"; do
-		LogPrint "$( printf "%40s = %s" "$var" "${!var}" )"
-	done
+    Print ""
 
-	LogPrint "Configuration tree:"
-	for config in "$ARCH" "$OS" \
-		"$OS_MASTER_VENDOR" "$OS_MASTER_VENDOR_ARCH" "$OS_MASTER_VENDOR_VERSION" "$OS_MASTER_VENDOR_VERSION_ARCH" \
-		"$OS_VENDOR" "$OS_VENDOR_ARCH" "$OS_VENDOR_VERSION" "$OS_VENDOR_VERSION_ARCH"; do
-		if [ "$config" ] ; then
-			LogPrint "$( printf "%40s : %s" "$config".conf "$(
-									test -s $SHARE_DIR/conf/"$config".conf && echo OK || echo missing/empty
-									)" )"
-		fi
-	done
-	for config in site local ; do
-		LogPrint "$( printf "%40s : %s" "$config".conf "$(
-								test -s $CONFIG_DIR/"$config".conf && echo OK || echo missing/empty
-								)" )"
-	done
-
-	LogPrint "Backup with $BACKUP"
-	for opt in $(eval echo '${!'"$BACKUP"'_*}') ; do
-		LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
-	done
-	for opt in $(eval echo '${!BACKUP_*}') ; do
-		case $opt in
-			BACKUP_PROG*) ;;
-			*) LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )" ;;
-		esac
-	done
-
-	case "$BACKUP" in
-		NETFS)
-		LogPrint "Backup program is '$BACKUP_PROG':"
-		for opt in $(eval echo '${!BACKUP_PROG*}') ; do
-			LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
-		done
-		;;
-	esac
-
-	LogPrint "Output to $OUTPUT"
-	for opt in $(eval echo '${!'"$OUTPUT"'_*}' '${!OUTPUT_*}') RESULT_MAILTO ; do
-		LogPrint "$( printf "%40s = %s" "$opt" "$(eval 'echo "${'"$opt"'[@]}"')" )"
-	done
-
-	Print ""
-
-	UserOutput "$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt"
-	if test -s "$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt" ; then
-		LogPrint "Your system is validated with the following details:"
-		while read -r ; do
-			LogPrint "$REPLY"
-		done <"$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt"
-	else
-		LogPrint "Your system is not yet validated. Please carefully check all functions"
-		LogPrint "and create a validation record with '$PROGRAM validate'. This will help others"
-		LogPrint "to know about the validation status of $PRODUCT on this system."
-		# if the master OS is validated print out a suitable hint
-		if test -s "$SHARE_DIR/lib/validated/$OS_MASTER_VENDOR_VERSION_ARCH.txt" ; then
-			LogPrint ""
-			LogPrint "Your system is derived from $OS_MASTER_VENDOR_VERSION which is validated:"
-			while read -r ; do
-				LogPrint "$REPLY"
-			done <"$SHARE_DIR/lib/validated/$OS_MASTER_VENDOR_VERSION_ARCH.txt"
-		fi
-	fi
+    UserOutput "$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt"
+    if test -s "$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt" ; then
+        LogPrint "Your system is validated with the following details:"
+        while read -r ; do
+            LogPrint "$REPLY"
+        done <"$SHARE_DIR/lib/validated/$OS_VENDOR_VERSION_ARCH.txt"
+    else
+        LogPrint "Your system is not yet validated. Please carefully check all functions"
+        LogPrint "and create a validation record with '$PROGRAM validate'. This will help others"
+        LogPrint "to know about the validation status of $PRODUCT on this system."
+        # if the master OS is validated print out a suitable hint
+        if test -s "$SHARE_DIR/lib/validated/$OS_MASTER_VENDOR_VERSION_ARCH.txt" ; then
+            LogPrint ""
+            LogPrint "Your system is derived from $OS_MASTER_VENDOR_VERSION which is validated:"
+            while read -r ; do
+                LogPrint "$REPLY"
+            done <"$SHARE_DIR/lib/validated/$OS_MASTER_VENDOR_VERSION_ARCH.txt"
+        fi
+    fi
 
 }
+
