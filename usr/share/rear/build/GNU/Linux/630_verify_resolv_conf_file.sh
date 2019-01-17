f7120cea293ba7712be7560ad38cb6f25f79fb3e
Johannes Meixner <jsmeix@suse.de>
2019-01-17 12:08:28 +0100

    Simpler and more straightforward implementation

--- usr/share/rear/build/GNU/Linux/630_verify_resolv_conf_file.sh
+++ usr/share/rear/build/GNU/Linux/630_verify_resolv_conf_file.sh
@@ -4,18 +4,32 @@
 #
 # We do not want to replicate in the recovery system
 # whatever complicated DNS setup there is on the original system
-# (like systemd-resolved).
+# (e.g. a local name server or a stub resolver like systemd-resolved).
 # In the recovery system a plain traditional /etc/resolv.conf file
-# with some actually usable content should be sufficient, cf.
+# with a remote name server that the resolver should query should be sufficient, cf.
 # https://github.com/rear/rear/issues/2015#issuecomment-454749972
+# By default things cannot work when only loopback nameservers are specified
+# or when there is no nameserver entry in /etc/resolv.conf in the recovery system
+# so that we error out in this case.
+# For non-default cases the user must specify what he wants via USE_RESOLV_CONF
+# (e.g. to use a local name server that he manually starts in the recovery system):
 
-# Use what the user specified as /etc/resolv.conf in the recovery system:
+# Use what the user specified for /etc/resolv.conf in the recovery system:
 if test "$USE_RESOLV_CONF" ; then
     rm -f $ROOTFS_DIR/etc/resolv.conf
+    # No /etc/resolv.conf in the recovery system when USE_RESOLV_CONF is false:
+    is_false "$USE_RESOLV_CONF" && return
+    # Copy a file if USE_RESOLV_CONF specifies an existing non-empty file:
+    if test -s "$USE_RESOLV_CONF" ; then
+        cp $v "$USE_RESOLV_CONF" $ROOTFS_DIR/etc/resolv.conf
+        return
+    fi
+    # Otherwise USE_RESOLV_CONF specifies the lines for /etc/resolv.conf in the recovery system:
     local resolv_conf_line
     for resolv_conf_line in "${USE_RESOLV_CONF[@]}" ; do
         echo "$resolv_conf_line" >>$ROOTFS_DIR/etc/resolv.conf
     done
+    return
 fi
 
 # Ensure /etc/resolv.conf in the recovery system contains actual content.
@@ -26,53 +40,41 @@ fi
 # where on Ubuntu /etc/resol.conf is linked to /run/resolvconf/resolv.conf
 # and since Ubuntu 18.x /etc/resol.conf is linked to /lib/systemd/resolv.conf
 # so that we need to remove the link and have the actual content in /etc/resolv.conf
-# (in case of USE_RESOLV_CONF /etc/resolv.conf in the recovery system is no symbolic link):
 if test -h $ROOTFS_DIR/etc/resolv.conf ; then
     rm -f $ROOTFS_DIR/etc/resolv.conf
     cp $v /etc/resolv.conf $ROOTFS_DIR/etc/resolv.conf
 fi
 
 # Check that the content in /etc/resolv.conf in the recovery system
-# seems to be actually usable within the recovery system:
+# seems to be actually usable within the recovery system
+# (i.e. that there is at least one remote nameserver IP address).
 # On Ubuntu 18.x versions /etc/resol.conf is linked to /lib/systemd/resolv.conf
 # where its actual content is only the following single line
 #   nameserver 127.0.0.53
 # cf. https://github.com/rear/rear/issues/2015#issuecomment-454082087
-# but a loopback IP address for the DNS nameserver cannot work within
-# the recovery system because there is no DNS server listening at 127.0.0.53
-# because systemd-resolved is not running within the recovery system.
+# but a loopback IP address for the nameserver cannot work
+# because neither a local name server nor systemd-resolved
+# is running within the recovery system.
 # According to "man resolv.conf"
 #   ... the keyword (e.g., nameserver) must start the line.
 #   The value follows the keyword, separated by white space.
-local only_loopback_nameservers="yes"
+local valid_nameserver="no"
 local nameserver_keyword nameserver_value junk
 while read nameserver_keyword nameserver_value junk ; do
     test "$nameserver_value" || continue
-    # One non-empty and non-loopback nameserver IP address is considered to be valid
-    # (i.e. we do not verify here if a nameserver does actually work):
+    # One non-empty/non-loopback nameserver IP address is considered to be valid
+    # (i.e. we do not verify here if a nameserver does actually work).
+    # TODO: Do we also have to check for IPv6 loopback addresses like '::1' ?
     if grep -q '^127\.' <<<"$nameserver_value" ; then
         Log "Useless loopback nameserver '$nameserver_value' in $ROOTFS_DIR/etc/resolv.conf"
     else
-        only_loopback_nameservers="no"
+        valid_nameserver="yes"
         Log "Supposedly valid nameserver '$nameserver_value' in $ROOTFS_DIR/etc/resolv.conf"
         # We may no 'break' here if we like to 'Log' all supposedly valid nameserver values:
         break
     fi
-    # The 'echo "nameserver none"' is needed to allow an empty /etc/resolv.conf in the recovery system
-    # that could be enforced via USE_RESOLV_CONF=" " which must contain at least one space so that
-    # the above 'test "$USE_RESOLV_CONF"' gets true to overwrite the copied (original) resolv.conf
-    # and to set only_loopback_nameservers="no" because 'none' is a "Supposedly valid nameserver":
-done < <( grep '^nameserver[[:space:]]' $ROOTFS_DIR/etc/resolv.conf || echo "nameserver none" )
-# It is o.k. to have an empty /etc/resolv.conf in the recovery system
-# (perhaps no DNS should be used within the recovery system)
-# but when /etc/resolv.conf in the recovery system contains nameserver values
-# it means DNS should be used within the recovery system and then things cannot work
-# when only loopback nameservers are specified so that we error out in this case:
-if is_true "$only_loopback_nameservers" ; then
-    Error "Recovery system etc/resolv.conf contains no real nameserver (e.g. only loopback addresses 127.*), specify a real nameserver via USE_RESOLV_CONF"
-else
-    # The 'true' avoids that this script results a non-zero exit code which would cause a Debug message (in debug mode)
-    #   Source function: 'source /usr/share/rear/build/GNU/Linux/630_verify_resolv_conf_file.sh' returns 1
-    true
-fi
+done < <( grep '^nameserver[[:space:]]' $ROOTFS_DIR/etc/resolv.conf )
+# When there is no '^nameserver ' line in /etc/resolv.conf in the recovery system
+# the 'while' loop is not run so that valid_nameserver="no" is still set.
+is_true "$valid_nameserver" || Error "No nameserver or only loopback addresses in $ROOTFS_DIR/etc/resolv.conf, specify a real nameserver via USE_RESOLV_CONF"
 
