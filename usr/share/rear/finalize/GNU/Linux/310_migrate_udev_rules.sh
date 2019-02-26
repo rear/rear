d4ee9de5dfd2d4ebdac74b6d496f38518cf63491
Johannes Meixner <jsmeix@suse.de>
2019-02-26 16:17:34 +0100

    some cleanup in 310_migrate_udev_rules.sh and tried to implement symlink handling in 320_migrate_network_configuration_files.sh but that code it too obsure for me

--- usr/share/rear/finalize/GNU/Linux/310_migrate_udev_rules.sh
+++ usr/share/rear/finalize/GNU/Linux/310_migrate_udev_rules.sh
@@ -25,13 +25,20 @@ RULE_FILES=( /etc/udev/rules.d/*persistent*{names,net,cd}.rules /etc/udev/rules.
 # and, if they differ, copy the version from the rescue system into the recovered system, of course
 # preserving a backup in /root/rear-*.old
 for rule in "${RULE_FILES[@]}" ; do
-    rulefile="$(basename "$rule")"
-    if test -s "$rule" && ! diff -q "$rule" $TARGET_FS_ROOT/"$rule" >/dev/null ; then
-        LogPrint "Updating udev configuration ($rulefile)"
-        # test for file $TARGET_FS_ROOT/"$rule" as BACKUP_RESTORE_MOVE_AWAY_FILES variable
-        # may have prevented the restore of one of these files
-        [[ -f $TARGET_FS_ROOT/"$rule" ]] && cp $v $TARGET_FS_ROOT/"$rule" $TARGET_FS_ROOT/root/rear-"$rulefile".old >&2
-        # copy the $rule from the rescue image to $TARGET_FS_ROOT/
-        cp $v "$rule" $TARGET_FS_ROOT/"$rule" || LogPrintError "Failed to copy '$rule' -> '$TARGET_FS_ROOT/$rule'"
-    fi
+    rulefile="$( basename "$rule" )"
+    # Skip if the one in the rescue system does not exists or is empty:
+    test -s "$rule" || continue
+    # Skip if the one in the rescue system is the same as the one from the restores backup:
+    cmp -s "$rule" $TARGET_FS_ROOT/"$rule" && continue
+    # Test for file $TARGET_FS_ROOT/$rule as BACKUP_RESTORE_MOVE_AWAY_FILES variable
+    # may have prevented the restore of one of these files:
+    test -f "$TARGET_FS_ROOT/$rule" && cp $v "$TARGET_FS_ROOT/$rule" $TARGET_FS_ROOT/root/rear-"$rulefile".old
+    # Copy the rule from the rescue system to TARGET_FS_ROOT even if it did not exist in TARGET_FS_ROOT.
+    # FIXME: It does not look right to test for $TARGET_FS_ROOT/$rule before making a backup in /root/rear-*.old
+    # but to copy it from the rescue system into TARGET_FS_ROOT in any case even if it did not exist before
+    # (why do we put files into the target system that were not restored from the backup?)
+    # or when BACKUP_RESTORE_MOVE_AWAY_FILES has explicitly moved it away:
+    LogPrint "Updating udev rule '$rulefile' with the one from the Relax-and-Recover rescue system"
+    cp $v "$rule" "$TARGET_FS_ROOT/$rule" || LogPrintError "Failed to copy '$rule' to '$TARGET_FS_ROOT/$rule'"
 done
+
