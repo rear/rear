c3da7ab4cda74e093f63977b09478e6852fb5359
Holger Pannenbäcker <hpannenb@users.noreply.github.com>
2019-02-28 10:39:09 +0100

    Update 470_safe_filesystems.sh
    It is of additional use to retrieve the filesystems in NSR_CLIENT_MODE; it does not harm since it is "read-only" on EMC Networker server. Due to saving the save sets filesystem information in $VAR_DIR/recovery/nsr_paths within the recovery image it is also possible to retrieve/read them during a recovery process.

--- usr/share/rear/rescue/NSR/default/470_safe_filesystems.sh
+++ usr/share/rear/rescue/NSR/default/470_safe_filesystems.sh
@@ -1,10 +1,5 @@
 # 470_safe_filesystems.sh
 #
-# In case NSR_CLIENT_MODE is enabled return else continue ...
-if is_true "$NSR_CLIENT_MODE"; then
-    return
-fi
-
 savefs -p -s $NSRSERVER 2>&1 | awk -F '(=|,)' '/path/ { printf ("%s ", $2) }' > $VAR_DIR/recovery/nsr_paths
 [[ ! -s $VAR_DIR/recovery/nsr_paths ]] && Error "The savefs command could not retrieve the \"save sets\" from this client"
 
