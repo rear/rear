12932ea907196723cd6e7a0169ed099ebf484665
Oliver Okrongli <oliver.okrongli@infix.de>
2019-03-06 16:33:39 +0100

    RAWDISK output: improve device partition detection (e.g. Ubuntu 18.04)

--- usr/share/rear/output/RAWDISK/Linux-i386/280_create_bootable_disk_image.sh
+++ usr/share/rear/output/RAWDISK/Linux-i386/280_create_bootable_disk_image.sh
@@ -63,7 +63,8 @@ gdisk -l "$disk_image" >&2
 ### Create block devices representing the raw disk image
 
 local disk_device  # separate 'local' statement to avoid losing $(...) exit status - cf. https://stackoverflow.com/a/10397996
-disk_device="$(losetup --show --find "$disk_image")"
+# Set up the loop device, trying the '--partscan' option first (introduced in util-linux v2.21)
+disk_device="$(losetup --show --find "$disk_image" --partscan || losetup --show --find "$disk_image")"
 StopIfError "Could not create loop device on $disk_image"
 AddExitTask "losetup -d $disk_device >&2"
 
