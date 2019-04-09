36dc1dfa7cbc56fa56b581ddaecd7717a3b2247f
Johannes Meixner <jsmeix@suse.de>
2019-04-09 11:24:05 +0200

    Better comments and removed obsolete known_broken_symlink variable

--- usr/share/rear/build/default/985_fix_broken_links.sh
+++ usr/share/rear/build/default/985_fix_broken_links.sh
@@ -29,15 +29,14 @@ local broken_symlinks=$( chroot $ROOTFS_DIR find . -xdev -path './proc' -prune -
 # cf. what is done when '$lib is a symbolic link' in build/GNU/Linux/390_copy_binaries_libraries.sh
 pushd $ROOTFS_DIR
     local broken_symlink=''
-    local known_broken_symlink=''
     local link_target=''
     for broken_symlink in $broken_symlinks ; do
-        link_target=$( readlink $v -e $broken_symlink )
         # If in the original system there was a chain of symbolic links like
         #   /some/path/to/file1 -> /another/path/to/file2 -> /final/path/to/file3
         # where $broken_symlink='/some/path/to/file1' and $link_target='/final/path/to/file3'
         # the chain of symbolic links gets simplified in the recovery system to $broken_symlink -> $link_target like
         #   /some/path/to/file1 -> /final/path/to/file3
+        link_target=$( readlink $v -e $broken_symlink )
         if test "$link_target" ; then
             # Never copy symlink targets in /proc/ /sys/ /dev/ or /run/ into the ReaR recovery system
             # for example the symlink ./etc/mtab that points to /proc/self/mounts
@@ -54,6 +53,8 @@ pushd $ROOTFS_DIR
             # The leading './' is crucial to create the parent directories inside the current working directory ROOTFS_DIR
             # and to copy the symlink target into the current working directory ROOTFS_DIR:
             mkdir $v -p ./$( dirname $link_target ) || LogPrintError "Failed to make parent directories for symlink target '$link_target'"
+            # Continue even after "Failed to make parent directories for symlink target ..." and let 'cp' also fail
+            # to also show the explicit "Failed to copy symlink target ..." message to the user:
             cp $v --preserve=all $link_target ./$link_target || LogPrintError "Failed to copy symlink target '$link_target'"
         else
             LogPrintError "Broken symlink '$broken_symlink' in recovery system because 'readlink' cannot determine its link target"
