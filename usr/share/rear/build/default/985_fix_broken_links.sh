b4e70e067dbb90f080d725cc991536f852f62000
Johannes Meixner <jsmeix@suse.com>
2019-05-03 15:00:03 +0200

    Update 985_fix_broken_links.sh
    Added comments with examples how broken symlinks and their targets look like
    so it is hopefully easier to imagine what goes on here when looking at that code.

--- usr/share/rear/build/default/985_fix_broken_links.sh
+++ usr/share/rear/build/default/985_fix_broken_links.sh
@@ -2,7 +2,8 @@
 # build/default/985_fix_broken_links.sh
 #
 # Check for broken symbolic links inside the recovery system in ROOTFS_DIR
-# and add missing symlink targets to the recovery system,
+# and add missing symlink targets to the recovery system
+# when the symlink target is a file (and not a directory)
 # see https://github.com/rear/rear/issues/1638
 # and https://github.com/rear/rear/pull/1734
 
@@ -19,6 +20,12 @@
 # for example $ROOTFS_DIR/etc/localtime -> /usr/share/zoneinfo/Europe/Berlin is broken
 # and should be $ROOTFS_DIR/etc/localtime -> $ROOTFS_DIR/usr/share/zoneinfo/Europe/Berlin
 # cf. https://github.com/rear/rear/pull/1734#issuecomment-434635175
+# Eveny broken symlink has the form of an absolute path inside ROOTFS_DIR, for example:
+#   /etc/localtime                       (e.g. with link target /usr/share/zoneinfo/Europe/Berlin)
+#   /etc/mtab                            (e.g. with link target /proc/1234/mounts)
+#   /etc/termcap                         (e.g. with link target /usr/share/misc/termcap)
+#   /dir1/dir2/relative_symlink_to_file  (e.g. with link target ../../path/to/file)
+#   /dir3/relative_symlink_to_directory  (e.g. with link target ../path/to/directory)
 local broken_symlinks=$( chroot $ROOTFS_DIR find / -xdev -path '/proc' -prune -o -path '/sys' -prune -o -path '/dev' -prune -o -xtype l -print )
 
 # Copy missing symlink targets into the recovery system if the symlink target exists
@@ -31,12 +38,22 @@ pushd $ROOTFS_DIR
     local broken_symlink=''
     local link_target=''
     for broken_symlink in $broken_symlinks ; do
+        # For each broken symlink absolute path inside ROOTFS_DIR
+        # we call "readlink -e" in the original system to get its link target there.
         # If in the original system there was a chain of symbolic links like
         #   /some/path/to/file1 -> /another/path/to/file2 -> /final/path/to/file3
         # where $broken_symlink='/some/path/to/file1' and $link_target='/final/path/to/file3'
         # the chain of symbolic links gets simplified in the recovery system to $broken_symlink -> $link_target like
         #   /some/path/to/file1 -> /final/path/to/file3
         link_target=$( readlink $v -e $broken_symlink )
+        # The '-e' option results that every link_target has the form of an absolute path,
+        # in particular also link targets of relative symlinks,
+        # for example for the above broken symlink examples each link_target could be like:
+        #   /usr/share/zoneinfo/Europe/Berlin  (e.g. the link target of /etc/localtime)
+        #   /proc/1234/mounts                  (e.g. the link target of /etc/mtab)
+        #   /usr/share/misc/termcap            (e.g. the link target of /etc/termcap)
+        #   /path/to/file                      (e.g. the link target of /dir1h/dir2/relative_symlink_to_file)
+        #   /path/to/directory                 (e.g. the link target of /dir3/relative_symlink_to_directory)
         if [[ -d "$link_target" ]]; then
             LogPrintError "Symlink '$broken_symlink' -> '$link_target' refers to a non-existing directory on the recovery system."
             LogPrintError "It will not be copied by default. You can include '$link_target' via the 'COPY_AS_IS' configuration variable."
@@ -64,4 +81,3 @@ pushd $ROOTFS_DIR
         fi
     done
 popd
-
