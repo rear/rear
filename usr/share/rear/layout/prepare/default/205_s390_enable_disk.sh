e80389037cc3aad19d8a9711980368e3eae87fae
mutable-dan <gdan@fdr>
2019-06-28 18:31:27 -0400

    network_devcies writes startup file etc/scripts/system-setup.d/50-prepare-network-devices.sh for s390 network device enabling pull #2142 cf. https://github.com/rear/rear/pull/2142#issuecomment-505818214 todo    change to case stmt
    cf. https://github.com/rear/rear/pull/2142#issuecomment-506900480

--- usr/share/rear/layout/prepare/default/205_s390_enable_disk.sh
+++ usr/share/rear/layout/prepare/default/205_s390_enable_disk.sh
@@ -1,4 +1,42 @@
+enable_s390_disk() {
 
+    dch=$(cat $LAYOUT_FILE | grep -v "^#" | grep "^dasd_channel" )
+
+    echo "run chccwdev"
+    while read line
+    do
+        echo 'dasd channel:' "$line"
+        device=$( echo $line | awk '{ print $4 }' ) 
+        bus=$( echo $line | awk '{ print $2 }' ) 
+        channel=$( echo $line | awk '{ print $5 }' ) 
+        echo 'chccwdev:' $device ', bus:' $bus ', channel:' $channel
+        chccwdev -e $bus 
+    done < <( echo "$dch" )
+}
+
+
+
+if [[ "$ARCH" == "Linux-s390"  ]]
+then
+    case $OS_MASTER_VENDOR in
+        (SUSE)
+            # no cases for suse
+        ;;
+        (Fedora)
+            # handles RH
+            enable_s390_disk
+        ;;
+        (Debian)
+            # no cases for debian
+            # handles ubuntu also, may need to look at $OS_VENDOR also as dasd disk layout is distro specific
+        ;;
+        (Arch)
+            # no cases for debian
+        ;;
+    esac
+fi
+
+cat >>$LAYOUT_CODE << 'EOF'
 
 #
 # for s390 (zLinux) systems, the dasd device must be enabled 
@@ -11,6 +49,7 @@ enable_s390_disk() {
     echo "run chccwdev"
     while read line
     do
+        echo 'dasd channel:' "$line"
         device=$( echo $line | awk '{ print $4 }' ) 
         bus=$( echo $line | awk '{ print $2 }' ) 
         channel=$( echo $line | awk '{ print $5 }' ) 
@@ -20,7 +59,24 @@ enable_s390_disk() {
 }
 
 
-if [[ "$ARCH" == "Linux-s390" && "$OS_VENDOR" != "SUSE_LINUX" ]]
+
+if [[ "$ARCH" == "Linux-s390"  ]]
 then
-    enable_s390_disk
+    case $OS_MASTER_VENDOR in
+        (SUSE)
+            # no cases for suse
+        ;;
+        (Fedora)
+            # handles RH
+            enable_s390_disk
+        ;;
+        (Debian)
+            # no cases for debian
+            # handles ubuntu also, may need to look at $OS_VENDOR also as dasd disk layout is distro specific
+        ;;
+        (Arch)
+            # no cases for debian
+        ;;
+    esac
 fi
+EOF
