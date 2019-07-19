b2be35bbb3a3764abb1fc5897ab55df4459fce2a
Johannes Meixner <jsmeix@suse.de>
2019-07-19 14:54:03 +0200

    Re-read the BTRFS_SUBVOLUME_SLES_SETUP variable from /etc/rear/rescue.conf (issue #2181)

--- usr/share/rear/layout/prepare/default/320_apply_mappings.sh
+++ usr/share/rear/layout/prepare/default/320_apply_mappings.sh
@@ -21,6 +21,18 @@ for file_to_migrate in "$LAYOUT_FILE" "$original_disk_space_usage_file" "$rescue
         applied_mappings_to_all_files="no"
     fi
 done
-
 is_true $applied_mappings_to_all_files || Error "Failed to apply disk layout mappings"
+
+# The rescue_config_file '/etc/rear/rescue.conf' may have contained a line like
+# (cf. usr/share/rear/layout/save/GNU/Linux/230_filesystem_layout.sh):
+#   BTRFS_SUBVOLUME_SLES_SETUP=( /dev/sda2 )
+# that is now changed (e.g. because of a mapping from /dev/sda to /dev/sdb) to
+#   BTRFS_SUBVOLUME_SLES_SETUP=( /dev/sdb2 )
+# but /etc/rear/rescue.conf was already read by /etc/scripts/system-setup
+# so that this changed variable must now be read again from /etc/rear/rescue.conf
+# but we do not source the whole /etc/rear/rescue.conf again to be on the safe side
+# perhaps some is not idempotent like "array+=( new elements )"
+# or other variables in /etc/rear/rescue.conf may have been changed
+# so we only re-read the BTRFS_SUBVOLUME_SLES_SETUP variable:
+eval $( grep '^BTRFS_SUBVOLUME_SLES_SETUP=' "$rescue_config_file" )
 
