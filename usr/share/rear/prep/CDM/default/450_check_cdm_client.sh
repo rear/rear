58fa93e6593f4a0c7e8ee33884a4657883a02c8b
Damani <damani@rubrik.com>
2019-10-11 12:58:40 -0700

    Updated stderr handling per Coding-Style

--- usr/share/rear/prep/CDM/default/450_check_cdm_client.sh
+++ usr/share/rear/prep/CDM/default/450_check_cdm_client.sh
@@ -8,6 +8,6 @@ if [ ! -x /usr/bin/rubrik/backup_agent_main ]; then
     Error "Please install Rubrik (CDM) client software."
 fi
 
-ps ax | grep backup_agent_main | grep -v grep  1>/dev/null
+ps ax | grep -v grep | grep backup_agent_main
 StopIfError $? "Rubrik (CDM) RBS backup_agent_main was not running on this client."
 
