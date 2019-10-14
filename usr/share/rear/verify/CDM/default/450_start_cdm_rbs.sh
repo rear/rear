2475dbe9b07c0a3c4f5d99ac1decdf3403e1c96d
Damani <damani@rubrik.com>
2019-10-14 12:52:02 -0700

    Updated to comply with Coding-Style

--- usr/share/rear/verify/CDM/default/450_start_cdm_rbs.sh
+++ usr/share/rear/verify/CDM/default/450_start_cdm_rbs.sh
@@ -4,14 +4,14 @@
 RBA_DIR=/etc/rubrik
 RBA_BIN_DIR=/usr/bin/rubrik
 
-BOOTSTRAP_DAEMON_OPTS="`cat ${RBA_DIR}/conf/bootstrap_flags.conf`"
-AGENT_DAEMON_OPTS="`cat ${RBA_DIR}/conf/agent_flags.conf`"
+BOOTSTRAP_DAEMON_OPTS="$( < ${RBA_DIR}/conf/bootstrap_flags.conf )"
+AGENT_DAEMON_OPTS="$( < ${RBA_DIR}/conf/agent_flags.conf )"
 BOOTSTRAP_DAEMON=$RBA_BIN_DIR/bootstrap_agent_main
 AGENT_DAEMON=$RBA_BIN_DIR/backup_agent_main
 
-$BOOTSTRAP_DAEMON $BOOTSTRAP_DAEMON_OPTS >&2
+$BOOTSTRAP_DAEMON $BOOTSTRAP_DAEMON_OPTS
 StopIfError "Unable to start RBS Bootstrap service"
-$AGENT_DAEMON $AGENT_DAEMON_OPTS >&2
+$AGENT_DAEMON $AGENT_DAEMON_OPTS
 StopIfError "Unable to start RBS Agent service"
 
 LogPrint "Rubrik (CDM) RBS agent started."
