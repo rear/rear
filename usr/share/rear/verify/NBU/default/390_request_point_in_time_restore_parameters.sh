2a060f6513427bde62c52862afa44a913af3e949
Johannes Meixner <jsmeix@suse.de>
2019-10-18 14:58:02 +0200

    Do not abort in case of invalid user input but retry

--- usr/share/rear/verify/NBU/default/390_request_point_in_time_restore_parameters.sh
+++ usr/share/rear/verify/NBU/default/390_request_point_in_time_restore_parameters.sh
@@ -6,33 +6,41 @@
 
 NBU_ENDTIME=()
 
-LogPrint ""
-LogPrint "NetBackup restores by default the latest backup data."
-LogPrint "Press ENTER to restore the most recent available backup."
-LogPrint "Alternatively specify date (and time) for Point-In-Time Restore."
+UserOutput ""
+UserOutput "NetBackup restores by default the latest backup data."
+UserOutput "Press only ENTER to restore the most recent available backup."
+UserOutput "Alternatively specify date (and time) for Point-In-Time Restore."
 
 local answer=""
-answer=$( UserInput -I NBU_RESTORE_PIT -t $WAIT_SECS -r -p "Enter date (mm/dd/yyyy) or date and time (mm/dd/yyyy HH:MM:SS) or press ENTER" )
-
-if test -z "$answer"; then
-    LogPrint "Skipping NetBackup Point-In-Time Restore, will restore most recent backup."
-    return
-fi
-
-# Validate user date and time input:
-local valid_date_and_time_input="yes"
+local valid_date_and_time_input=""
 local nbu_endtime_date=""
 local nbu_endtime_time=""
-# Validate date:
-nbu_endtime_date=$( date -d "$answer" +%m/%d/%Y ) || valid_date_and_time_input="no"
-# Validate time:
-nbu_endtime_time=$( date -d "$answer" +%T ) || valid_date_and_time_input="no"
-# It is an Error (not a BugError) when user input is invalid:
-is_true $valid_date_and_time_input || Error "Invalid date and/or time '$answer' specified. Ending NetBackup Restore attempt..."
 
+# Let the user enter date and time again and again until the input is valid
+# or the user pressed only ENTER to restore the most recent available backup:
+while true ; do
+    answer=$( UserInput -I NBU_RESTORE_PIT -r -p "Enter date (mm/dd/yyyy) or date and time (mm/dd/yyyy HH:MM:SS) or press ENTER" )
+    # When the user pressed only ENTER leave this script to restore the most recent available backup:
+    if test -z "$answer" ; then
+        UserOutput "Skipping NetBackup Point-In-Time Restore, will restore most recent backup."
+        return
+    fi
+    # Try to do NetBackup Point-In-Time Restore provided the user input is valid date and time:
+    valid_date_and_time_input="yes"
+    # Validate date:
+    nbu_endtime_date=$( date -d "$answer" +%m/%d/%Y ) || valid_date_and_time_input="no"
+    # Validate time:
+    nbu_endtime_time=$( date -d "$answer" +%T ) || valid_date_and_time_input="no"
+    # Exit the while loop when the user input is valid date and time:
+    is_true $valid_date_and_time_input && break
+    # Show the user that his input is invalid and do the the while loop again:
+    LogPrintError "Invalid date and/or time '$answer' specified."
+done
+
+# Do NetBackup Point-In-Time Restore:
 NBU_ENDTIME=( "$nbu_endtime_date" )
 # When also an actual time was specified (i.e. when it is not "00:00:00") add it:
 test "$nbu_endtime_time" != "00:00:00" && NBU_ENDTIME+=( "$nbu_endtime_time" )
 
-LogPrint "Doing NetBackup Point-In-Time Restore of all filespaces at or before ${NBU_ENDTIME[@]}"
+UserOutput "Doing NetBackup Point-In-Time Restore of all filespaces at or before ${NBU_ENDTIME[@]}"
 
--- usr/share/rear/verify/TSM/default/390_request_point_in_time_restore_parameters.sh
+++ usr/share/rear/verify/TSM/default/390_request_point_in_time_restore_parameters.sh
@@ -2,35 +2,44 @@
 # Ask for point in time to recover with TSM.
 # One point in time is used for all filespaces.
 
-LogPrint ""
-LogPrint "TSM restores by default the latest backup data."
-LogPrint "Press ENTER to restore the most recent available backup."
-LogPrint "Alternatively specify date and time for Point-In-Time restore."
+UserOutput ""
+UserOutput "TSM restores by default the latest backup data."
+UserOutput "Press only ENTER to restore the most recent available backup."
+UserOutput "Alternatively specify date and time for Point-In-Time restore."
 
 local answer=""
-
-answer=$( UserInput -I TSM_RESTORE_PIT -t $WAIT_SECS -r -p "Enter date/time (YYYY-MM-DD HH:mm:ss) or press ENTER" )
-
-if test -z "$answer"; then
-    LogPrint "Skipping TSM Point-In-Time restore, will restore most recent backup."
-    return
-fi
-
 local tsm_restore_pit_date=""
 local tsm_restore_pit_time=""
 
-# Validate date:
-tsm_restore_pit_date=$( date -d "$answer" +%Y.%m.%d ) || Error "Invalid date specified for TSM recovery: '$answer'"
+# Let the user enter date and time again and again until the input is valid
+# or the user pressed only ENTER to restore the most recent available backup:
+while true ; do
+    answer=$( UserInput -I TSM_RESTORE_PIT -r -p "Enter date/time (YYYY-MM-DD HH:mm:ss) or press ENTER" )
+    # When the user pressed only ENTER leave this script to restore the most recent available backup:
+    if test -z "$answer"; then
+        UserOutput "Skipping TSM Point-In-Time restore, will restore most recent backup."
+        return
+    fi
+    # Try to do TSM Point-In-Time restore provided the user input is valid date and time:
+    valid_date_and_time_input="yes"
+    # Validate date:
+    tsm_restore_pit_date=$( date -d "$answer" +%Y.%m.%d ) || valid_date_and_time_input="no"
+    # Validate time:
+    tsm_restore_pit_time=$( date -d "$answer" +%T ) || valid_date_and_time_input="no"
+    # Exit the while loop when the user input is valid date and time:
+    is_true $valid_date_and_time_input && break
+    # Show the user that his input is invalid and do the the while loop again:
+    LogPrintError "Invalid date and/or time '$answer' specified."
+done
+
+# Do TSM Point-In-Time restore:
 # Add valid date to dsmc options:
 TSM_DSMC_RESTORE_OPTIONS+=( -date=5 -pitd="$tsm_restore_pit_date" )
-
-# Validate time:
-tsm_restore_pit_time=$( date -d "$answer" +%T ) || Error "Invalid time specified for TSM recovery: '$answer'"
 # Add valid actual time (i.e. when it is not "00:00:00") to dsmc options:
 # FIXME: Is it right to add '-date=5' here a second time to the TSM_DSMC_RESTORE_OPTIONS array
 # because it was already added above in the "Add valid date to dsmc options" step?
 test "$tsm_restore_pit_time" != "00:00:00" && TSM_DSMC_RESTORE_OPTIONS+=( -date=5 -pitt="$tsm_restore_pit_time" )
 
-LogPrint "Restoring all TSM filespaces from backup before $tsm_restore_pit_date $tsm_restore_pit_time (MM/DD/YYYY HH:mm:ss)"
-LogPrint "Note: The following list of file spaces always shows the latest backup and not the date/time you specified here."
+UserOutput "Restoring all TSM filespaces from backup before $tsm_restore_pit_date $tsm_restore_pit_time (YYYY.MM.DD HH:mm:ss)"
+UserOutput "Note: The following list of file spaces always shows the latest backup and not the date/time you specified here."
 
