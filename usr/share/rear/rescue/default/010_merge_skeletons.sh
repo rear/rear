4ac799deadd6ac232b65e9bab4fb5689ec214ada
Johannes Meixner <jsmeix@suse.com>
2020-01-20 14:05:33 +0100

    Update 010_merge_skeletons.sh
    Simplified rescue/default/010_merge_skeletons.sh
    and made its behaviour more fail safe.
    Now is is also supported that both a skel_dir directory
    plus a skel_dir.tar.gz exist and then both get copied
    into the recovery system, first the directory and then
    the tar.gz so that via the tar.gz existing files could be
    overwritten if needed.

--- usr/share/rear/rescue/default/010_merge_skeletons.sh
+++ usr/share/rear/rescue/default/010_merge_skeletons.sh
@@ -15,26 +15,23 @@ for skel_dir in default "$ARCH" "$OS" \
         "$OS_MASTER_VENDOR/default" "$OS_MASTER_VENDOR_ARCH" "$OS_MASTER_VENDOR_VERSION" \
         "$OS_VENDOR/default" "$OS_VENDOR_ARCH" "$OS_VENDOR_VERSION" \
         "$BACKUP" "$OUTPUT" ; do
-    # Skip empty $skel_dir (e.g. if $OS_MASTER_* is empty):
+    # Silently skip when $skel_dir is empty (e.g. when $OS_MASTER_* is empty):
     test "$skel_dir" || continue
-    # If a $skel_dir.tar.gz exists (e.g. usr/share/rear/skel/Debian/default.tar.gz)
-    # use only that (i.e. there cannot be also usr/share/rear/skel/Debian/default/).
-    # FIXME: Explain what the reason behind is why there cannot be both.
+    # Skip if there is neither a $skel_dir directory nor a $skel_dir.tar.gz:
+    if ! test -d "$skel_dir" -o -s "$skel_dir.tar.gz" ; then
+        Debug "Neither a '$skel_path/$skel_dir' directory nor a '$skel_path/$skel_dir.tar.gz'"
+        continue   
+    fi
+    # When $skel_dir is a directory (e.g. usr/share/rear/skel/default/) copy all its contents:
+    if test -d "$skel_dir" ; then
+        Log "Copying '$skel_path/$skel_dir' contents to $ROOTFS_DIR"
+        tar -C "$skel_dir" -c . | tar -C $ROOTFS_DIR -x || Error "Failed to copy '$skel_path/$skel_dir' contents to $ROOTFS_DIR"
+    fi
+    # If $skel_dir.tar.gz exists (e.g. usr/share/rear/skel/Debian/default.tar.gz) extract it:
     if test -s "$skel_dir.tar.gz" ; then
         Log "Extracting '$skel_path/$skel_dir.tar.gz' into $ROOTFS_DIR"
         tar -C $ROOTFS_DIR -xzf "$skel_dir.tar.gz" || Error "Failed to extract '$skel_path/$skel_dir.tar.gz' into $ROOTFS_DIR"
-        test -d "$skel_dir" && Log "Skip copying '$skel_path/$skel_dir' contents to $ROOTFS_DIR"
-        continue
     fi
-    # When $skel_dir is a directory copy it with all its contents:
-	if test -d "$skel_dir" ; then
-		Log "Copying '$skel_path/$skel_dir' contents to $ROOTFS_DIR"
-		tar -C "$skel_dir" -c . | tar -C $ROOTFS_DIR -x || Error "Failed to copy '$skel_path/$skel_dir' contents to $ROOTFS_DIR"
-        continue
-	fi
-    # It is no error when there is neither a $skel_dir directory nor a $skel_dir.tar.gz
-    # FIXME: Explain what the reason behind is why that is no error.
-    Debug "Neither a '$skel_path/$skel_dir' directory nor a '$skel_path/$skel_dir.tar.gz'"
 done
 popd >/dev/null
 
