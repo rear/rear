26cde864bec923312f681beefab0ce17882931d3
Sebastian Koehler <sebastian.koehler@microfocus.com>
2020-02-24 15:48:57 +0100

    Remove specific Data Protector version

--- usr/share/rear/prep/DP/default/450_check_dp_client_configured.sh
+++ usr/share/rear/prep/DP/default/450_check_dp_client_configured.sh
@@ -3,9 +3,9 @@
 # defined on the DP cell server and that a backup specification has been
 # made for this client - no more no less
 
-Log "Backup method is DP: check Data Protector 6.* requirements"
+Log "Backup method is DP: check Data Protector requirements"
 [ -x /opt/omni/bin/omnir ]
-StopIfError "Please install Data Protector 6 User Interface component."
+StopIfError "Please install Data Protector User Interface (cc component) on the client."
 
 /opt/omni/bin/omnidb -filesystem | grep $(hostname) >/dev/null
 StopIfError "Data Protector check failed with error code $? (no filesystem backup found).
