7aad002323dac3f213556356b50754efefbd60b9
Johannes Meixner <jsmeix@suse.com>
2020-03-13 09:20:48 +0100

    Update 500_selinux_autorelabel.sh
    Use 'is_true' for BACKUP_SELINUX_DISABLE everywhere,
    cf. "Relax-and-Recover functions" in
    https://github.com/rear/rear/wiki/Coding-Style
    and tell the user when SELinux relabeling of all files
    will be done at reboot to have the user better informed, cf.
    https://github.com/rear/rear/issues/2340#issuecomment-598237555

--- usr/share/rear/restore/default/500_selinux_autorelabel.sh
+++ usr/share/rear/restore/default/500_selinux_autorelabel.sh
@@ -1,9 +1,9 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-# when the variable BACKUP_SELINUX_DISABLE is unset then silently return
-[[ ! "$BACKUP_SELINUX_DISABLE"  =~ ^[yY1] ]] && return
+# Skip when disabling SELinux policy during backup/restore with NETFS or RSYNC is not wanted:
+is_true "$BACKUP_SELINUX_DISABLE" || return 0
 
-# force relabeling after reboot of the recovered system
+# Force SELinux relabeling of all files after reboot of the recovered system:
 touch $TARGET_FS_ROOT/.autorelabel
-Log "Created /.autorelabel file : after reboot SELinux will relabel all files"
+LogPrint "Created SELinux $TARGET_FS_ROOT/.autorelabel file : after reboot SELinux will relabel all files"
