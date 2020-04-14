36eab239004b9c3016450bebafe914bef036aaa7
Johannes Meixner <jsmeix@suse.com>
2020-04-14 17:01:01 +0200

    Update 300_create_yaboot.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/output/ISO/Linux-ppc64/300_create_yaboot.sh
+++ usr/share/rear/output/ISO/Linux-ppc64/300_create_yaboot.sh
@@ -6,26 +6,24 @@
 # Public License. Refer to the included COPYING for full text of license.
 
 ## other bootloader distro case
-if [[ ! -r /etc/yaboot.conf ]] && [[ ! -r /etc/lilo.conf ]]
-then
+if [[ ! -r /etc/yaboot.conf ]] && [[ ! -r /etc/lilo.conf ]] ; then
     return
 fi
 
 SUSE_STYLE=
 
 # create yaboot directory structure
-mkdir -p $v $TMP_DIR/ppc/chrp >&2
+mkdir -p $v $TMP_DIR/ppc/chrp
 ISO_YABOOT_BIN=$(find_yaboot_file yaboot)
 
-if [[ $ISO_YABOOT_BIN == *"/lib/lilo/pmac"* ]]
-then
+if [[ $ISO_YABOOT_BIN == *"/lib/lilo/pmac"* ]] ; then
    ISO_YABOOT_BIN="/lib/lilo/chrp/yaboot.chrp"
    SUSE_STYLE=1
 fi
 
-if [[ "$SUSE_STYLE" ]]; then
+if [[ "$SUSE_STYLE" ]] ; then
   #SUSE type distos
-  cp $v $ISO_YABOOT_BIN $TMP_DIR/yaboot >&2
+  cp $v $ISO_YABOOT_BIN $TMP_DIR/yaboot
   # SUSE ppc64 use /yaboot, need to add it to ISO_FILES see #1407
   ISO_FILES=( ${ISO_FILES[@]} yaboot )
 
@@ -39,7 +37,7 @@ EOF
 
 else
   #Red Hat type distros
-  cp $v $ISO_YABOOT_BIN $TMP_DIR/ppc/chrp >&2
+  cp $v $ISO_YABOOT_BIN $TMP_DIR/ppc/chrp
 
 cat >"$TMP_DIR/ppc/bootinfo.txt" <<EOF
 <chrp-boot>
@@ -51,7 +49,7 @@ EOF
 
 fi
 
-mkdir -p $v $TMP_DIR/etc >&2
+mkdir -p $v $TMP_DIR/etc
 cat >"$TMP_DIR/etc/yaboot.conf" <<EOF
 init-message = "\nRelax-and-Recover boot\n\n"
 timeout=100
@@ -64,4 +62,6 @@ image=kernel
 
 EOF
 
-ISO_FILES=( "${ISO_FILES[@]}" etc=etc ppc=ppc )
+# FIXME: Those additional array elements are no ISO files
+# so what is that special code meant to do?
+ISO_FILES+=( etc=etc ppc=ppc )
