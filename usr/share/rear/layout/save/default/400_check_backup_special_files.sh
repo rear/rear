1640de1d92c9b3148c353177de6021767800b2ac
Johannes Meixner <jsmeix@suse.com>
2020-04-15 14:49:50 +0200

    Update 400_check_backup_special_files.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/layout/save/default/400_check_backup_special_files.sh
+++ usr/share/rear/layout/save/default/400_check_backup_special_files.sh
@@ -1,6 +1,6 @@
 # 400_check_backup_special_files.sh
 # tell "rear checklayout" to watch over special backup related files
 case $BACKUP in
-    TSM         ) CHECK_CONFIG_FILES=( ${CHECK_CONFIG_FILES[@]}  /etc/adsm/* ) ;;
-    FDRUPSTREAM ) CHECK_CONFIG_FILES=( "${CHECK_CONFIG_FILES[@]}" "${CHECK_CONFIG_FILES_FDRUPSTREAM[@]}" ) ;;
+    TSM         ) CHECK_CONFIG_FILES+=( /etc/adsm/* ) ;;
+    FDRUPSTREAM ) CHECK_CONFIG_FILES+=( "${CHECK_CONFIG_FILES_FDRUPSTREAM[@]}" ) ;;
 esac
