8a152b2e270b9d372fd4effe825dd3010b09a821
Johannes Meixner <jsmeix@suse.com>
2020-04-16 10:17:09 +0200

    Update 450_prepare_omni_xinetd.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/rescue/DP/default/450_prepare_omni_xinetd.sh
+++ usr/share/rear/rescue/DP/default/450_prepare_omni_xinetd.sh
@@ -1,8 +1,8 @@
-PROGS=( "${PROGS[@]}" xinetd )
-COPY_AS_IS=( "${COPY_AS_IS[@]}" /etc/xinetd.conf /etc/xinetd.d/omni )
+PROGS+=( xinetd )
+COPY_AS_IS+=( /etc/xinetd.conf /etc/xinetd.d/omni )
 cat >$ROOTFS_DIR/etc/scripts/system-setup.d/90-xinetd.sh <<-EOF
 # Data Protector needs omni service to be started from xinetd
 echo "Starting a minimal xinetd daemon ..."
 xinetd
 EOF
-chmod $v +x $ROOTFS_DIR/etc/scripts/system-setup.d/90-xinetd.sh >&2
+chmod $v +x $ROOTFS_DIR/etc/scripts/system-setup.d/90-xinetd.sh
