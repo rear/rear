84402b8b50b80b315fa9777f0d029f3cb03963f3
Johannes Meixner <jsmeix@suse.com>
2020-04-17 16:18:43 +0200

    Update 400_prep_bareos.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/BAREOS/default/400_prep_bareos.sh
+++ usr/share/rear/prep/BAREOS/default/400_prep_bareos.sh
@@ -1,10 +1,10 @@
 ### prepare stuff for BAREOS
-CLONE_GROUPS=( "${CLONE_GROUPS[@]}" bareos )
-COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_BAREOS[@]}" )
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_BAREOS[@]}" )
-PROGS=( "${PROGS[@]}" "${PROGS_BAREOS[@]}" )
+CLONE_GROUPS+=( bareos )
+COPY_AS_IS+=( "${COPY_AS_IS_BAREOS[@]}" )
+COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_BAREOS[@]}" )
+PROGS+=( "${PROGS_BAREOS[@]}" )
 
 ### Include mt when we are restoring from Bareos tape (for troubleshooting)
-if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]]; then
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" mt )
+if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]] ; then
+    COPY_AS_IS+=( mt )
 fi
