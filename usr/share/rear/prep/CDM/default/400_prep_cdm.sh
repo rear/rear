5e16718ca152de924d83a4b547c29bb63f0ef6a5
Johannes Meixner <jsmeix@suse.com>
2020-04-17 15:55:11 +0200

    Update 400_prep_cdm.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/CDM/default/400_prep_cdm.sh
+++ usr/share/rear/prep/CDM/default/400_prep_cdm.sh
@@ -2,6 +2,6 @@
 # prepare stuff for CDM
 #
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_CDM[@]}" )
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_CDM[@]}" )
-PROGS=( "${PROGS[@]}" "${PROGS_CDM[@]}" fmt )
+COPY_AS_IS+=( "${COPY_AS_IS_CDM[@]}" )
+COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_CDM[@]}" )
+PROGS+=( "${PROGS_CDM[@]}" fmt )
