340b3418b233d8a2169f0dc0727bc7b7a6de266c
Johannes Meixner <jsmeix@suse.com>
2020-04-17 16:35:34 +0200

    Update 400_prep_nsr.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/NSR/default/400_prep_nsr.sh
+++ usr/share/rear/prep/NSR/default/400_prep_nsr.sh
@@ -2,6 +2,6 @@
 # prepare stuff for NSR
 #
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_NSR[@]}" )
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_NSR[@]}" )
-PROGS=( "${PROGS[@]}" "${PROGS_NSR[@]}" fmt )
+COPY_AS_IS+=( "${COPY_AS_IS_NSR[@]}" )
+COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_NSR[@]}" )
+PROGS+=( "${PROGS_NSR[@]}" fmt )
