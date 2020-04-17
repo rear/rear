28fbd1e85c8d0b4439feb4cc15b77774df3e6757
Johannes Meixner <jsmeix@suse.com>
2020-04-17 15:37:55 +0200

    Update 400_prep_galaxy.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/GALAXY10/default/400_prep_galaxy.sh
+++ usr/share/rear/prep/GALAXY10/default/400_prep_galaxy.sh
@@ -1,13 +1,13 @@
 #
-# prepare stuff for Galaxy
+# prepare stuff for Galaxy 10
 #
 
-COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_GALAXY10[@]}" )
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_GALAXY10[@]}" )
+COPY_AS_IS+=( "${COPY_AS_IS_GALAXY10[@]}" )
+COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_GALAXY10[@]}" )
 
-PROGS=( "${PROGS[@]}" chgrp touch )
+PROGS+=( chgrp touch )
 
 # include argument file if specified
 if test "$GALAXY10_Q_ARGUMENTFILE" ; then
-	COPY_AS_IS=( "${COPY_AS_IS[@]}" "$GALAXY10_Q_ARGUMENTFILE" )
+	COPY_AS_IS+=( "$GALAXY10_Q_ARGUMENTFILE" )
 fi
