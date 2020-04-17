f80cd3fa5def85ff34fcc97ec20037655c04c20a
Johannes Meixner <jsmeix@suse.com>
2020-04-17 15:25:12 +0200

    Update 400_prep_bacula.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/BACULA/default/400_prep_bacula.sh
+++ usr/share/rear/prep/BACULA/default/400_prep_bacula.sh
@@ -1,10 +1,10 @@
 ### prepare stuff for BACULA
-CLONE_GROUPS=( "${CLONE_GROUPS[@]}" bacula )
-COPY_AS_IS=( "${COPY_AS_IS[@]}" "${COPY_AS_IS_BACULA[@]}" )
-COPY_AS_IS_EXCLUDE=( "${COPY_AS_IS_EXCLUDE[@]}" "${COPY_AS_IS_EXCLUDE_BACULA[@]}" )
-PROGS=( "${PROGS[@]}" "${PROGS_BACULA[@]}" )
+CLONE_GROUPS+=( bacula )
+COPY_AS_IS+=( "${COPY_AS_IS_BACULA[@]}" )
+COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_BACULA[@]}" )
+PROGS+=( "${PROGS_BACULA[@]}" )
 
 ### Include mt when we are restoring from Bacula tape (for troubleshooting)
-if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]]; then
-    COPY_AS_IS=( "${COPY_AS_IS[@]}" mt )
+if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]] ; then
+    COPY_AS_IS+=( mt )
 fi
