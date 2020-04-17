c77ee64f69a508d07a2b382bdeb121ddf0a0673b
Johannes Meixner <jsmeix@suse.com>
2020-04-17 15:50:39 +0200

    Update 380_copy_usb_fs_module.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/USB/default/380_copy_usb_fs_module.sh
+++ usr/share/rear/prep/USB/default/380_copy_usb_fs_module.sh
@@ -2,9 +2,9 @@
 # we better copy the kernel module, if modular, to out MODULES array so it
 # is available in our recovery image
 local usb_fs_type
-usb_fs_type=$(fsck -N  $USB_DEVICE | tail -1 | awk '{print $1}' | cut -d. -f2)
+usb_fs_type=$( fsck -N $USB_DEVICE | tail -1 | awk '{print $1}' | cut -d. -f2 )
 [[ -z "$usb_fs_type" ]] && usb_fs_type="ext3"
 
-MODULES=( "${MODULES[@]}" "$usb_fs_type" )
-MODULES_LOAD=( "${MODULES_LOAD[@]}" "$usb_fs_type" )
+MODULES+=( "$usb_fs_type" )
+MODULES_LOAD+=( "$usb_fs_type" )
 Log "Added USB Device $USB_DEVICE file system type $usb_fs_type to MODULES/MODULES_LOAD"
