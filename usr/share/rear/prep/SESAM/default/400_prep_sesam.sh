6c2f857162c6631fbef7e3501232442e513c0588
Johannes Meixner <jsmeix@suse.com>
2020-04-20 09:44:53 +0200

    Update 400_prep_sesam.sh
    Use simpler and more fail-safe syntax
    ARRAY+=( additional elements )
    see https://github.com/rear/rear/issues/2364

--- usr/share/rear/prep/SESAM/default/400_prep_sesam.sh
+++ usr/share/rear/prep/SESAM/default/400_prep_sesam.sh
@@ -11,34 +11,27 @@ if [ -e /etc/sesam2000.ini ]; then
         source $SHARE_DIR/lib/sesam-functions.sh
 
         # include sesam executables and configuration files 
-        COPY_AS_IS=( 
-                "${COPY_AS_IS[@]}" 
-                "${COPY_AS_IS_SESAM[@]}" 
-                $SHARE_DIR 
-                $VAR_DIR 
-                $SESAM_BIN_DIR 
-                $SESAM_VAR_DIR 
-                /etc/sesam2000.ini
+        COPY_AS_IS+=(
+            "${COPY_AS_IS_SESAM[@]}" 
+            $SHARE_DIR 
+            $VAR_DIR 
+            $SESAM_BIN_DIR 
+            $SESAM_VAR_DIR 
+            /etc/sesam2000.ini
         )
 
         # do not include certain sesam folders as generated boot
         # image will grow too big if sesam listing and temporary
         # files are included
-        COPY_AS_IS_EXCLUDE=( 
-                "${COPY_AS_IS_EXCLUDE[@]}" 
-                "${COPY_AS_IS_EXCLUDE_SESAM[@]}" 
-                $SESAM_WORK_DIR 
-                $SESAM_TMP_DIR 
-                $SESAM_LIS_DIR 
-                $SESAM_LGC_DIR 
-                $SESAM_PROT_DIR 
+        COPY_AS_IS_EXCLUDE+=(
+            "${COPY_AS_IS_EXCLUDE_SESAM[@]}" 
+            $SESAM_WORK_DIR 
+            $SESAM_TMP_DIR 
+            $SESAM_LIS_DIR 
+            $SESAM_LGC_DIR 
+            $SESAM_PROT_DIR 
         )
-
 
         # include libssl as it is needed to run sesam sm_sshd if included
-        LIBS=( 
-            "${LIBS[@]}"
-            /usr/lib*/libssl.so.* 
-            /usr/lib*/libcrypto.so.* 
-        )
+        LIBS+=( /usr/lib*/libssl.so.* /usr/lib*/libcrypto.so.* )
 fi
