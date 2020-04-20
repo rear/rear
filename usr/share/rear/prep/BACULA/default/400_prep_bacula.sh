d03952f6b8135d4e859cd14db338e80f0cb42c07
Johannes Meixner <jsmeix@suse.com>
2020-04-20 17:58:59 +0200

    Update 400_prep_bacula.sh
    Fixed COPY_AS_IS+=( mt )
    to PROGS+=( mt )
    because mt is a program, cf. what there is in
    prep/TAPE/default/400_check_tape_drive.sh
    REQUIRED_PROGS+=( mt )

--- usr/share/rear/prep/BACULA/default/400_prep_bacula.sh
+++ usr/share/rear/prep/BACULA/default/400_prep_bacula.sh
@@ -6,5 +6,5 @@ PROGS+=( "${PROGS_BACULA[@]}" )
 
 ### Include mt when we are restoring from Bacula tape (for troubleshooting)
 if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]] ; then
-    COPY_AS_IS+=( mt )
+    PROGS+=( mt )
 fi
