3034acda78dffee579eb4c8299e2da7d4d9cb169
Johannes Meixner <jsmeix@suse.com>
2020-04-27 10:23:07 +0200

    Error out when LUKS version 2 is used (issue #2204)
    Error out during "rear mkrescue/mkbackup" when LUKS version 2 is used.
    LUKS version 2 is not suppported, see https://github.com/rear/rear/issues/2204
    When LUKS version 2 is used it fails at least to determine the hash value
    so we use an empty hash value as a simple test if gathering crypt information
    was successful and error out if not.

--- usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
@@ -42,6 +42,11 @@ while read target_name junk ; do
     uuid=$(cryptsetup luksDump $source_device | grep "UUID" | sed -r 's/^.+:\s*(.+)$/\1/')
     keyfile_option=$([ -f /etc/crypttab ] && awk '$1 == "'"$target_name"'" && $3 != "none" && $3 != "-" && $3 != "" { print "keyfile=" $3; }' /etc/crypttab)
 
+    # LUKS version 2 is not yet suppported, see https://github.com/rear/rear/issues/2204
+    # When LUKS version 2 is used the above code fails at least to determine the hash value
+    # so we use an empty hash value as a simple test if gathering crypt information was successful:
+    test "$hash" || Error "No hash value for LUKS device /dev/mapper/$target_name at $source_device (only LUKS version 1 is supported)"
+
     echo "crypt /dev/mapper/$target_name $source_device cipher=$cipher-$mode key_size=$key_size hash=$hash uuid=$uuid $keyfile_option" >> $DISKLAYOUT_FILE
 done < <( dmsetup ls --target crypt )
 
