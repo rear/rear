dd2994fdc98dd0c23d7178473b2f3c35b72e4486
Peter Hoffmann <hoffmann@b1-systems.de>
2020-04-29 22:17:47 +0200

    Add some options to borg_additional_options for borg prune.

--- usr/share/rear/backup/BORG/default/800_prune_old_backups.sh
+++ usr/share/rear/backup/BORG/default/800_prune_old_backups.sh
@@ -3,10 +3,18 @@
 #
 # 800_prune_old_backups.sh
 
+# User might specify some additional output options in Borg.
+local borg_additional_options=''
+
+is_true $BORGBACKUP_SHOW_PROGRESS && borg_additional_options+='--progress '
+is_true $BORGBACKUP_SHOW_STATS && borg_additional_options+='--stats '
+is_true $BORGBACKUP_SHOW_LIST && borg_additional_options+='--list '
+is_true $BORGBACKUP_SHOW_RC && borg_additional_options+='--show-rc '
+
 if [ ! -z $BORGBACKUP_OPT_PRUNE ]; then
     # Purge old archives according user settings.
     LogPrint "Purging old Borg archives in repository $BORGBACKUP_REPO"
-    borg prune --list ${BORGBACKUP_OPT_PRUNE[@]} \
+    borg prune $verbose $borg_additional_options ${BORGBACKUP_OPT_PRUNE[@]} \
     $BORGBACKUP_OPT_REMOTE_PATH $BORGBACKUP_OPT_UMASK \
     --prefix ${BORGBACKUP_ARCHIVE_PREFIX}_ \
     ${borg_dst_dev}${BORGBACKUP_REPO}
