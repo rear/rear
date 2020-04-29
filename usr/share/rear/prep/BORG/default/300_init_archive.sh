7985c585ad848e7353f171c51f446b91516937ef
Peter Hoffmann <hoffmann@b1-systems.de>
2020-04-29 22:44:26 +0200

    Improve error handling in prep/BORG/default/300_init_archive.sh.

--- usr/share/rear/prep/BORG/default/300_init_archive.sh
+++ usr/share/rear/prep/BORG/default/300_init_archive.sh
@@ -16,30 +16,45 @@ is_true $BORGBACKUP_INIT_MAKE_PARENT_DIRS && borg_additional_options+='--make-pa
 # and store it to BORGBACKUP_ARCHIVE_CACHE.
 # This should avoid repeatingly quering Borg server, which could be slow.
 borg_archive_cache_create
-
-# If $rc == 0 we have repository present, but it is empty.
-# In my tests, Borg returned RC = 2, when repository was non-existent or
-# due connection problems (bad hostname, etc ...).
-# If repository is present but empty, rc will be set to 0, and initialization
-# will be skipped.
 rc=$?
 
-# This might be a Borg connection / mount error, or missing repository.
-# If initialization succeeds, we can rule out connection problems.
-# `borg init` has to be triggered in "prep" stage if user decides to include
+# Check Borg return code of `borg list`.
+# See https://borgbackup.readthedocs.io/en/stable/usage/general.html#return-codes
+#
+# If $rc == 0 we have a repository present, maybe it is empty.
+# Borg returns $rc == 2 in case of fatal error like local or remote exceptions.
+# This might be a Borg connection / mount error or a missing repository.
+#
+# If Borg reports `Repository doesn't exist`, we try to create it.
+# `borg init` has to be triggered in "prep" stage, if user decides to include
 # keyfiles to Relax-and-Recover rescue/recovery system using COPY_AS_IS_BORG.
 if [ $rc -ne 0 ]; then
-    LogPrint "Failed to list $BORGBACKUP_REPO"
-    LogPrint "Creating new Borg repository $BORGBACKUP_REPO"
+    LogPrint "Couldn't list Borg repository '$BORGBACKUP_REPO' on ${BORGBACKUP_HOST:-USB}"
 
-    borg init $verbose $borg_additional_options \
-    $BORGBACKUP_OPT_ENCRYPTION $BORGBACKUP_OPT_REMOTE_PATH \
-    $BORGBACKUP_OPT_UMASK ${borg_dst_dev}${BORGBACKUP_REPO}
-    rc=$?
+    if [ -e "$BORGBACKUP_STDERR_FILE" ]; then
+        if grep --quiet 'Failed to create/acquire the lock' "$BORGBACKUP_STDERR_FILE"; then
+            LogPrint "Borg: $( cat "$BORGBACKUP_STDERR_FILE" )"
+            Error "Borg failed to create/acquire the lock, borg rc $rc!"
+        fi
+
+        if grep --quiet 'Repository' "$BORGBACKUP_STDERR_FILE" \
+            && grep --quiet 'does not exist' "$BORGBACKUP_STDERR_FILE"; then
+            LogPrint "Borg: $( cat "$BORGBACKUP_STDERR_FILE" )"
+            LogPrint "Hence initializing new Borg repository '$BORGBACKUP_REPO' on ${BORGBACKUP_HOST:-USB}"
+
+            borg init $verbose $borg_additional_options \
+            $BORGBACKUP_OPT_ENCRYPTION $BORGBACKUP_OPT_REMOTE_PATH \
+            $BORGBACKUP_OPT_UMASK ${borg_dst_dev}${BORGBACKUP_REPO} \
+            2> $BORGBACKUP_STDERR_FILE
+            rc=$?
+        fi
+    fi
 fi
 
-# Borg repository initialization failed in previous step,
-# backup abort is inevitable.
+# Borg repository initialization failed in previous step
+# or there was still another error from `borg list` before.
+# Nevertheless backup abort is inevitable.
 if [ $rc -ne 0 ]; then
-    Error "Could not initialize Borg repository"
+    LogPrint "Borg: $( cat "$BORGBACKUP_STDERR_FILE" )"
+    Error "Failed to initialize Borg repository, borg rc $rc!"
 fi
