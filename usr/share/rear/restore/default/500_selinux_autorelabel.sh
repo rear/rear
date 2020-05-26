cd668fe7392d4ba2c83f7d2c0d192d975c5cca09
Johannes Meixner <jsmeix@suse.de>
2020-05-26 14:44:25 +0200

    Update 500_selinux_autorelabel.sh
    Typo fix "in de ... file" => "in the ... file"

--- usr/share/rear/restore/default/500_selinux_autorelabel.sh
+++ usr/share/rear/restore/default/500_selinux_autorelabel.sh
@@ -1,7 +1,7 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-# In de /usr/share/rear/conf/default.conf file the variable to temporary disasble SELinux during backup is
+# In the /usr/share/rear/conf/default.conf file the variable to temporary disasble SELinux during backup is
 # defined as BACKUP_SELINUX_DISABLE=1 (so, by default disable SELinux during backup/archive of our system disks).
 # In most case this is honored with BACKUP=NETFS setting using tar or rsync backup programs.
 
