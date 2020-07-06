9defe430130eaccd952fa717fe766d69a0be8e83
Johannes Meixner <jsmeix@suse.de>
2020-07-06 12:32:34 +0200

    Update 400_verify_dp.sh
    better test for /etc/opt/omni/client/cell_server content according to
    https://github.com/rear/rear/commit/61da9495944da0b86b85f7bec979041486115eab

--- usr/share/rear/verify/DP/default/400_verify_dp.sh
+++ usr/share/rear/verify/DP/default/400_verify_dp.sh
@@ -5,8 +5,8 @@ CELL_SERVER="$( cat /etc/opt/omni/client/cell_server )"
 OMNICHECK=/opt/omni/bin/omnicheck
 
 # the Cell Manager must be configured on the client
-# TODO: the above comment and the error message do not match the test (does not actually test TCPSERVERADDRESS)
-[ "${CELL_SERVER}" ] || Error "Data Protector Cell Manager TCPSERVERADDRESS not set in /etc/opt/omni/client/cell_server"
+# if /etc/opt/omni/client/cell_server exists and contains something, we're good
+test -s "${CELL_SERVER}" || Error "Data Protector Cell Manager not configured in /etc/opt/omni/client/cell_server"
 
 # check that the Cell Manager is responding on the INET port
 ${OMNICHECK} -patches -host ${CELL_SERVER} || Error "Data Protector Cell Manager is not responding, error code $?.
