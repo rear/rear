a3cd469e69b3fc071066b8e57fb100e561dac089
Johannes Meixner <jsmeix@suse.de>
2020-07-06 10:49:56 +0200

    Update 450_check_dp_client_configured.sh
    No longer use the StopIfError function but plain bash "COMMAND || Error"
    cf. https://github.com/rear/rear/commit/daf35e235d0770c663ff8dba866dddec76586a27

--- usr/share/rear/prep/DP/default/450_check_dp_client_configured.sh
+++ usr/share/rear/prep/DP/default/450_check_dp_client_configured.sh
@@ -7,13 +7,12 @@ OMNIR=/opt/omni/bin/omnir
 VBDA=/opt/omni/lbin/vbda
 
 Log "Backup method is DP: check Data Protector requirements"
-[ -x ${VBDA} ]
-StopIfError "Please install Data Protector Disk Agent (DA component) on the client."
+test -x $VBDA || Error "Cannot execute $VBDA
+Install Data Protector Disk Agent (DA component) on the client."
 
-[ -x ${OMNIR} ]
-StopIfError "Please install Data Protector User Interface (CC component) on the client."
+test -x $OMNIR || Error "Cannot execute $OMNIR
+Install Data Protector User Interface (CC component) on the client."
 
-${OMNIDB} -filesystem | grep $(hostname)
-StopIfError "Data Protector check failed, error code $?.
+$OMNIDB -filesystem | grep $( hostname ) || Error "Data Protector check failed, error code $?.
 Check if the user root is configured in Data Protector UserList and if backups for this client exist in the IDB.
 See $RUNTIME_LOGFILE for more details."
