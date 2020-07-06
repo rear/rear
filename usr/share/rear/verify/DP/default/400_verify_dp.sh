12a94ae2bd8eff2f55ffb3a3b9cde1188897fc33
Johannes Meixner <jsmeix@suse.de>
2020-07-06 12:40:37 +0200

    Update 400_verify_dp.sh
    Fixed https://github.com/rear/rear/commit/9defe430130eaccd952fa717fe766d69a0be8e83
    because CELL_SERVER contains the content of /etc/opt/omni/client/cell_server

--- usr/share/rear/verify/DP/default/400_verify_dp.sh
+++ usr/share/rear/verify/DP/default/400_verify_dp.sh
@@ -1,13 +1,12 @@
 # 400_verify_dp.sh
 # read Data Protector vars from config file
 
-CELL_SERVER="$( cat /etc/opt/omni/client/cell_server )"
-OMNICHECK=/opt/omni/bin/omnicheck
-
 # the Cell Manager must be configured on the client
 # if /etc/opt/omni/client/cell_server exists and contains something, we're good
-test -s "${CELL_SERVER}" || Error "Data Protector Cell Manager not configured in /etc/opt/omni/client/cell_server"
+test -s /etc/opt/omni/client/cell_server || Error "Data Protector Cell Manager not configured in /etc/opt/omni/client/cell_server"
+CELL_SERVER="$( cat /etc/opt/omni/client/cell_server )"
 
+OMNICHECK=/opt/omni/bin/omnicheck
 # check that the Cell Manager is responding on the INET port
 ${OMNICHECK} -patches -host ${CELL_SERVER} || Error "Data Protector Cell Manager is not responding, error code $?.
 See $RUNTIME_LOGFILE for more details."
