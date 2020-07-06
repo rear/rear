61da9495944da0b86b85f7bec979041486115eab
Johannes Meixner <jsmeix@suse.de>
2020-07-06 11:00:31 +0200

    Update 400_verify_dp.sh
    No longer use the StopIfError function but plain bash "COMMAND || Error"
    cf. https://github.com/rear/rear/commit/daf35e235d0770c663ff8dba866dddec76586a27

--- usr/share/rear/verify/DP/default/400_verify_dp.sh
+++ usr/share/rear/verify/DP/default/400_verify_dp.sh
@@ -1,14 +1,13 @@
 # 400_verify_dp.sh
 # read Data Protector vars from config file
 
-CELL_SERVER="`cat /etc/opt/omni/client/cell_server`"
+CELL_SERVER="$( cat /etc/opt/omni/client/cell_server )"
 OMNICHECK=/opt/omni/bin/omnicheck
 
 # the Cell Manager must be configured on the client
-[ "${CELL_SERVER}" ]
-StopIfError "Data Protector Cell Manager TCPSERVERADDRESS not set in /etc/opt/omni/client/cell_server"
+# TODO: the above comment and the error message do not match the test (does not actually test TCPSERVERADDRESS)
+[ "${CELL_SERVER}" ] || Error "Data Protector Cell Manager TCPSERVERADDRESS not set in /etc/opt/omni/client/cell_server"
 
 # check that the Cell Manager is responding on the INET port
-${OMNICHECK} -patches -host ${CELL_SERVER}
-StopIfError "Data Protector Cell Manager is not responding, error code $?.
+${OMNICHECK} -patches -host ${CELL_SERVER} || Error "Data Protector Cell Manager is not responding, error code $?.
 See $RUNTIME_LOGFILE for more details."
