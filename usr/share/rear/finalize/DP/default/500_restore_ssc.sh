e6ac686d811c13350b26b776733852ca814862d1
sebastian-koehler <sebastian.koehler@microfocus.com>
2020-07-07 15:44:33 +0200

    Fixed problem found during testing

--- usr/share/rear/finalize/DP/default/500_restore_ssc.sh
+++ usr/share/rear/finalize/DP/default/500_restore_ssc.sh
@@ -9,18 +9,20 @@ local certfile=$sscpath/localhost_cert.pem
 local keyfile=$sscpath/localhost_key.pem
 
 # Nothing to do when the certificate files already exist in the recreated system:
-test -s $TARGET_FS_ROOT/$certfile -a -s $TARGET_FS_ROOT/$keyfile && return
+test -s $TARGET_FS_ROOT/$certfile -a -s $TARGET_FS_ROOT/$keyfile && return 0
 
 # Tell what will be done so that subsequent (error) messages make sense for the user:
-LogPrint "Restoring Data Protector client certificate ($certfile $keyfile)"
+LogPrint "Restoring Data Protector client certificate:
+LogPrint "- $certfile"
+LogPrint "- $keyfile"
 
 # Inform the user but do not error out here at this late state of "rear recover"
 # when it failed to copy specific files into the recreated system:
 cp $v $certfile $TARGET_FS_ROOT/$certfile || LogPrintError "Failed to copy $certfile"
-cp $v $keyfileS $TARGET_FS_ROOT/$keyfile || LogPrintError "Failed to copy $keyfile"
+cp $v $keyfile  $TARGET_FS_ROOT/$keyfile  || LogPrintError "Failed to copy $keyfile"
 
 # All is done when the certificate files exist now in the recreated system:
-test -s $TARGET_FS_ROOT/$certfile -a -s $TARGET_FS_ROOT/$keyfile && return
+test -s $TARGET_FS_ROOT/$certfile -a -s $TARGET_FS_ROOT/$keyfile && return 0
 
 LogPrint "Client certificate not properly restored. A new certificate will be generated now"
 local omnicc=/opt/omni/bin/omnicc
@@ -35,4 +37,4 @@ if ! chroot $TARGET_FS_ROOT $omnicc -secure_comm -get_fingerprint ; then
     return 1
 fi
 LogPrint "Generated new Data Protector client certificate"
-LogPrint "Run 'omnicc -secure_comm -configure_peer <Client>' on the Cell Manager after rebooting the client system"
+LogPrint "Run 'omnicc -secure_comm -configure_peer <Client>' on the Cell Manager after rebooting the client"
