7d2fd6afdf77b3c1ff01e092ef6fd000aeef11bb
Johannes Meixner <jsmeix@suse.com>
2020-09-15 09:52:13 +0200

    Update 310_include_exclude.sh
    Don't have a trailing dot in "LogPrint "Excluding ... $something." messages
    to show the $something value literally without a trailing dot appended.

--- usr/share/rear/layout/save/default/310_include_exclude.sh
+++ usr/share/rear/layout/save/default/310_include_exclude.sh
@@ -15,7 +15,7 @@
 # If you somehow need this functionality, it's advised to exclude the
 # device or volume group
 #for mountpoint in "${EXCLUDE_MOUNTPOINTS[@]}" ; do
-#    LogPrint "Excluding mountpoint $mountpoint."
+#    LogPrint "Excluding mountpoint $mountpoint"
 #    mark_as_done "fs:$mountpoint"
 #    mark_tree_as_done "fs:$mountpoint"
 #done
@@ -25,7 +25,7 @@
 if [ "${MANUAL_INCLUDE:-NO}" == "YES" ] ; then
     while read fs device mountpoint junk ; do
         if ! IsInArray "$mountpoint" "${BACKUP_PROG_INCLUDE[@]}" ; then
-            LogPrint "Excluding mountpoint $mountpoint. (MANUAL_INCLUDE mode)"
+            LogPrint "Excluding mountpoint $mountpoint (MANUAL_INCLUDE mode)"
             mark_as_done "fs:$mountpoint"
             mark_tree_as_done "fs:$mountpoint"
         fi
@@ -33,13 +33,13 @@ if [ "${MANUAL_INCLUDE:-NO}" == "YES" ] ; then
 fi
 
 for md in "${EXCLUDE_MD[@]}" ; do
-    LogPrint "Excluding RAID $md."
+    LogPrint "Excluding RAID $md"
     mark_as_done "/dev/$md"
     mark_tree_as_done "/dev/$md"
 done
 
 for vg in "${EXCLUDE_VG[@]}" ; do
-    LogPrint "Excluding Volume Group $vg."
+    LogPrint "Excluding Volume Group $vg"
     mark_as_done "/dev/$vg"
     mark_tree_as_done "/dev/$vg"
 done
@@ -55,7 +55,7 @@ if [ ${#ONLY_INCLUDE_VG[@]} -gt 0 ] ; then
 fi
 
 for component in "${EXCLUDE_COMPONENTS[@]}" ; do
-    LogPrint "Excluding component $component."
+    LogPrint "Excluding component $component"
     mark_as_done "$component"
     mark_tree_as_done "$component"
 done
