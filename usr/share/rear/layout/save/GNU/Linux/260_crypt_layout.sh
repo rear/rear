5057f8bdc00196c08cd9ff33b0b4c2cc0c96cda5
Johannes Meixner <jsmeix@suse.com>
2020-09-17 12:17:28 +0200

    Update 260_crypt_layout.sh
    Some general code cleanup and improvements in layout/save/GNU/Linux/260_crypt_layout.sh
    in particular no longer multiple 'cryptsetup luksDump ... | grep' calls but only one
    'cryptsetup luksDump ... >TMP_FILE' call and then multiple 'grep TMP_FILE' calls.

--- usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
@@ -1,46 +1,62 @@
 # Describe LUKS devices
 # We use /etc/crypttab and cryptsetup for information
 
-if ! has_binary cryptsetup; then
-    return
-fi
+# Skip when not needed:
+has_binary cryptsetup || return 0
 
-Log "Saving Encrypted volumes."
+Log "Saving Encrypted volumes"
+
+# cryptsetup is required in the recovery system if disklayout.conf contains at least one 'crypt' entry
+# but also in case of an incomplete commented '#crypt' entry from a LUKS2 volume
+# so we include things in any case so that the user could do a manual setup if needed
+# cf. https://github.com/rear/rear/issues/2491
+# See the create_crypt function in layout/prepare/GNU/Linux/160_include_luks_code.sh
+# what program calls are written to diskrestore.sh abd
+# see also https://github.com/rear/rear/issues/1963
 REQUIRED_PROGS+=( cryptsetup dmsetup )
 COPY_AS_IS+=( /usr/share/cracklib/\* /etc/security/pwquality.conf )
 
 while read target_name junk ; do
-    # find the target device we're mapping
-    if ! [ -e /dev/mapper/$target_name ] ; then
-        Log "Device Mapper name $target_name not found in /dev/mapper."
+
+    if ! test -e /dev/mapper/$target_name ; then
+        Log "Skipping $target_name (there is no /dev/mapper/$target_name)"
         continue
     fi
 
-    sysfs_device=$(get_sysfs_name /dev/mapper/$target_name)
-    if [ -z "$sysfs_device" ] ; then
-        Log "Could not find device $target_name in sysfs."
+    sysfs_device=$( get_sysfs_name /dev/mapper/$target_name )
+    if ! test "$sysfs_device" ; then
+        Log "Skipping $target_name (could not find device for $target_name in /sys/block/)"
         continue
     fi
 
     source_device=""
     for slave in /sys/block/$sysfs_device/slaves/* ; do
-        if ! [ -z "$source_device" ] ; then
-            BugError "Multiple Device Mapper slaves for crypt $target_name detected."
+        if test "$source_device" ; then
+            BugError "Crypt $target_name has multiple device mapper slaves in /sys/block/$sysfs_device/slaves/"
         fi
-        source_device="$(get_device_name ${slave##*/})"
+        source_device="$( get_device_name ${slave##*/} )"
     done
-
-    if ! cryptsetup isLuks $source_device >/dev/null 2>&1; then
+    if ! test "$source_device" ; then
+        Log "Skipping $target_name (could not get its device in /sys/block/$sysfs_device/slaves/)"
+        continue
+    fi
+    
+    if ! cryptsetup isLuks $source_device ; then
+        Log "Skipping $target_name (its $source_device is not a LUKS device)"
         continue
     fi
 
-    # gather crypt information
-    cipher=$(cryptsetup luksDump $source_device | grep "Cipher name" | sed -r 's/^.+:\s*(.+)$/\1/')
-    mode=$(cryptsetup luksDump $source_device | grep "Cipher mode" | cut -d: -f2- | awk '{printf("%s",$1)};')
-    key_size=$(cryptsetup luksDump $source_device | grep "MK bits" | sed -r 's/^.+:\s*(.+)$/\1/')
-    hash=$(cryptsetup luksDump $source_device | grep "Hash spec" | sed -r 's/^.+:\s*(.+)$/\1/')
-    uuid=$(cryptsetup luksDump $source_device | grep "UUID" | sed -r 's/^.+:\s*(.+)$/\1/')
-    keyfile_option=$([ -f /etc/crypttab ] && awk '$1 == "'"$target_name"'" && $3 != "none" && $3 != "-" && $3 != "" { print "keyfile=" $3; }' /etc/crypttab)
+    # Gather crypt information:
+    if ! cryptsetup luksDump $source_device >$TMP_DIR/cryptsetup.luksDump ; then
+        LogPrintError "Error: Cannot get LUKS values for $target_name ('cryptsetup luksDump $source_device' failed)"
+        continue
+    fi
+    cipher=$( grep "Cipher name" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    mode=$( grep "Cipher mode" $TMP_DIR/cryptsetup.luksDump | cut -d: -f2- | awk '{printf("%s",$1)};' )
+    key_size=$( grep "MK bits" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    hash=$( grep "Hash spec" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    uuid=$( grep "UUID" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    keyfile_option=$( [ -f /etc/crypttab ] && awk '$1 == "'"$target_name"'" && $3 != "none" && $3 != "-" && $3 != "" { print "keyfile=" $3; }' /etc/crypttab )
 
     # LUKS version 2 is not yet suppported, see https://github.com/rear/rear/issues/2204
     # When LUKS version 2 is used the above code fails at least to determine the hash value
@@ -62,11 +78,4 @@ while read target_name junk ; do
     echo "crypt /dev/mapper/$target_name $source_device cipher=$cipher-$mode key_size=$key_size hash=$hash uuid=$uuid $keyfile_option" >> $DISKLAYOUT_FILE
     
 done < <( dmsetup ls --target crypt )
-
-# cryptsetup is required in the recovery system if disklayout.conf contains at least one 'crypt' entry
-# (also in case of an incomplete commented '#crypt' entry from a LUKS2 volume for manual setup by the user)
-# see the create_crypt function in layout/prepare/GNU/Linux/160_include_luks_code.sh
-# what program calls are written to diskrestore.sh
-# cf. https://github.com/rear/rear/issues/1963
-grep -q '^#*crypt ' $DISKLAYOUT_FILE && REQUIRED_PROGS+=( cryptsetup ) || true
 
