53951898a70ab049e62f17e8f3ec8131660662df
Johannes Meixner <jsmeix@suse.com>
2020-11-02 14:35:09 +0100

    Make recreating LUKS volumes work with optional cryptsetup options
    The "cryptseup luksFormat" command does not require
    any of the cipher, key-size, hash option values
    because if omitted a cryptseup default value is used, cf.
    https://github.com/rear/rear/pull/2504#issuecomment-720341023

--- usr/share/rear/layout/prepare/GNU/Linux/160_include_luks_code.sh
+++ usr/share/rear/layout/prepare/GNU/Linux/160_include_luks_code.sh
@@ -1,38 +1,51 @@
 # Code to recreate and/or open LUKS volumes.
 
 create_crypt() {
+    # See the create_device() function in lib/layout-functions.sh what "device type" means:
+    local device_type="$1"
+    if ! grep -q "^crypt $device_type " "$LAYOUT_FILE" ; then
+        LogPrintError "Skip recreating LUKS device $device_type (no 'crypt $device_type' entry in $LAYOUT_FILE)"
+        # FIXME: The return code is ignored in the create_device() function in lib/layout-functions.sh:
+        return 1
+    fi
+    
     local crypt target_device source_device options
-    read crypt target_device source_device options < <(grep "^crypt $1 " "$LAYOUT_FILE")
-
+    read crypt target_device source_device options < <( grep "^crypt $device_type " "$LAYOUT_FILE" )
     local target_name=${target_device#/dev/mapper/}
-
     local cryptsetup_options="" keyfile="" password=""
     local option key value
     for option in $options ; do
         key=${option%=*}
         value=${option#*=}
-
+        # The "cryptseup luksFormat" command does not require any of the cipher, key-size, hash option values
+        # because if omitted a cryptseup default value is used so treat those values as optional.
+        # Using plain test to ensure the value is a single non empty and non blank word
+        # without quoting because test " " would return zero exit code
+        # cf. "Beware of the emptiness" in https://github.com/rear/rear/wiki/Coding-Style
         case "$key" in
-            type)
+            (type)
                 cryptsetup_options+=" --type $value"
                 ;;
-            cipher)
-                cryptsetup_options+=" --cipher $value"
+            (cipher)
+                test $value && cryptsetup_options+=" --cipher $value"
                 ;;
-            key_size)
-                cryptsetup_options+=" --key-size $value"
+            (key_size)
+                test $value && cryptsetup_options+=" --key-size $value"
                 ;;
-            hash)
-                cryptsetup_options+=" --hash $value"
+            (hash)
+                test $value && cryptsetup_options+=" --hash $value"
                 ;;
-            uuid)
+            (uuid)
                 cryptsetup_options+=" --uuid $value"
                 ;;
-            keyfile)
+            (keyfile)
                 keyfile=$value
                 ;;
-            password)
+            (password)
                 password=$value
+                ;;
+            (*)
+                LogPrintError "Skipping unsupported LUKS cryptsetup option '$key' in 'crypt $target_device $source_device' entry in $LAYOUT_FILE"
                 ;;
         esac
     done
@@ -49,16 +62,15 @@ create_crypt() {
         keyfile="$TMP_DIR/LUKS-keyfile-$target_name"
         dd bs=512 count=4 if=/dev/urandom of="$keyfile"
         chmod u=rw,go=- "$keyfile"
-
         echo "cryptsetup luksFormat --batch-mode $cryptsetup_options $source_device $keyfile"
         echo "cryptsetup luksOpen --key-file $keyfile $source_device $target_name"
     elif [ -n "$password" ] ; then
         echo "echo \"$password\" | cryptsetup luksFormat --batch-mode $cryptsetup_options $source_device"
         echo "echo \"$password\" | cryptsetup luksOpen $source_device $target_name"
     else
-        echo "LogPrint \"Please enter the password for LUKS device $target_name ($source_device):\""
+        echo "LogPrint \"Enter the password for LUKS device $target_name (for 'cryptsetup luksFormat' on $source_device):\""
         echo "cryptsetup luksFormat --batch-mode $cryptsetup_options $source_device"
-        echo "LogPrint \"Please re-enter the password for LUKS device $target_name ($source_device):\""
+        echo "LogPrint \"Again enter the password for LUKS device $target_name (for 'cryptsetup luksOpen' on $source_device):\""
         echo "cryptsetup luksOpen $source_device $target_name"
     fi
     echo ""
@@ -77,12 +89,11 @@ open_crypt() {
     for option in $options ; do
         key=${option%=*}
         value=${option#*=}
-
         case "$key" in
-            keyfile)
+            (keyfile)
                 keyfile=$value
                 ;;
-            password)
+            (password)
                 password=$value
                 ;;
         esac
@@ -97,7 +108,7 @@ open_crypt() {
     elif [ -n "$password" ] ; then
         echo "echo \"$password\" | cryptsetup luksOpen $source_device $target_name"
     else
-        echo "LogPrint \"Please enter the password for LUKS device $target_name ($source_device):\""
+        echo "LogPrint \"Enter the password for LUKS device $target_name (for 'cryptsetup luksOpen' on $source_device):\""
         echo "cryptsetup luksOpen $source_device $target_name"
     fi
     echo ""
