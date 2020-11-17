ae48d3ca8d8c251c71c6322bc99ee46ac7b61fa5
Johannes Meixner <jsmeix@suse.com>
2020-11-17 14:53:40 +0100

    Update 150_wipe_disks.sh
    Overhauled removing LVM storage objects:
    Only remove PVs and need to do that forcefully.
    Do not remove VGs and LVs because they can be spread over disks
    that contain disks that should not be wiped.
    Additionally added support to remove storage objects that are
    nested in LUKS volumes by temporarily opening the LUKS volumes.

--- usr/share/rear/layout/recreate/default/150_wipe_disks.sh
+++ usr/share/rear/layout/recreate/default/150_wipe_disks.sh
@@ -12,6 +12,36 @@
 Log "Block devices structure on the unchanged replacement hardware before the disks $DISKS_TO_BE_WIPED will be wiped (lsblk):"
 Log "$( lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,SIZE,MOUNTPOINT || lsblk -io NAME,KNAME,FSTYPE,SIZE,MOUNTPOINT || lsblk -i || lsblk )"
 
+# Open LUKS volumes so that nested storage objects inside LUKS volumes become visible.
+# For example in SLES the YaST installer provides a default LUKS encrypted LVM storage structure
+# where LVM storage objects are nested storage objects inside a LUKS encrypted volume as follows (lsblk output):
+#   NAME                                               KNAME     PKNAME    TRAN TYPE  FSTYPE       SIZE MOUNTPOINT
+#   /dev/sda                                           /dev/sda            ata  disk                20G
+#   |-/dev/sda1                                        /dev/sda1 /dev/sda       part                 8M
+#   `-/dev/sda2                                        /dev/sda2 /dev/sda       part  crypto_LUKS   20G
+#     `-/dev/mapper/cr_ata-QEMU_HARDDISK_QM00001-part2 /dev/dm-0 /dev/sda2      crypt LVM2_member   20G
+#       |-/dev/mapper/system-swap                      /dev/dm-1 /dev/dm-0      lvm   swap           2G [SWAP]
+#       |-/dev/mapper/system-root                      /dev/dm-2 /dev/dm-0      lvm   btrfs       12.6G /
+#       `-/dev/mapper/system-home                      /dev/dm-3 /dev/dm-0      lvm   xfs          5.4G /home
+# where /dev/mapper/cr_ata-QEMU_HARDDISK_QM00001-part2 is the (only) PV of a VG 'system'
+# that contains the LVs /dev/system/swap /dev/system/home /dev/system/root
+# To make such LVM storage objects visible for LVM tools like pvscan vgscan lvscan (cf. the LVM related code below)
+# so that those LVM storage objects can be properly removed their parent LUKS volume needs to be opened:
+local crypto_LUKS_kernel_devices luks_device luks_mapping_name
+# The luks_mapping_names where 'cryptsetup luksOpen' succeeded need to be later closed again (see below):
+local luks_mapping_names=""
+crypto_LUKS_kernel_devices="$( lsblk -nlpo KNAME,FSTYPE | grep ' crypto_LUKS$' | cut -s -d ' ' -f1 | tr -s '[:space:]' ' ' )"
+for luks_device in $crypto_LUKS_kernel_devices ; do
+    luks_mapping_name=luks-$( basename $luks_device )
+    LogUserOutput "Enter the password to open LUKS device $luks_device temporarily as $luks_mapping_name (or skip with plain 'enter')"
+    if ! cryptsetup luksOpen $luks_device $luks_mapping_name 0<&6 1>&7 2>&8 ; then
+        DebugPrint "LUKS device $luks_device not opened"
+        continue
+    fi
+    DebugPrint "LUKS device $luks_device temporarily opened as $luks_mapping_name"
+    luks_mapping_names+="$luks_mapping_name "
+done
+
 # Plain wiping a disk with LVM storage objects leads to errors later when "parted -s $disk mklabel $label" is called
 # in the diskrestore.sh script because then 'parted' fails (which lets diskrestore.sh fail which lets "rear recover" fail)
 # with the following error message:
@@ -19,11 +49,11 @@ Log "$( lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,SIZE,MOUNTPOINT || lsblk -
 #   probably because it/they are in use. As a result, the old partition(s) will remain in use.
 #   You should probably reboot now before making further changes.
 # In this case it works to reboot the recovery system and then a second "rear recover" usually succeeds
-# because after plain wiping a disk with LVM storage objects the LVM metadata/signatures is no longer there
+# because after plain wiping a disk with LVM storage objects the LVM metadata/signatures are no longer there
 # so that LVM cannot be in use in the rebooted recovery system and "parted -s $disk mklabel $label" can succeed.
 # But we like to do all in one single "rear recover" run so we need to clean up LVM storage objects properly.
 # LVM storage objects must be properly detected and removed with LVM tools step by step from LVs to VGs to PVs:
-local detected_LVs detected_VGs detected_PVs detected_LV detected_VG detected_PV
+local detected_LVs detected_VGs detected_PVs
 # Detect LVs:
 # The 'lvscan' output in the recovery system looks like:
 #   inactive          '/dev/system/swap' [2.00 GiB] inherit
@@ -43,29 +73,101 @@ detected_VGs="$( vgscan -y | grep 'Found volume group' |cut -s -d '"' -f2 | tr -
 #   Total: 1 [19.98 GiB] / in use: 1 [19.98 GiB] / in no VG: 0 [0   ]
 # cf. layout/recreate/default/README.wipe_disks
 detected_PVs="$( pvscan -y | grep -o 'PV /dev/[^ ]*' | cut -s -d ' ' -f2 | tr -s '[:space:]' ' ' )"
+# From the detected LVs VGs PVs remove only those that belong to one of the disks that will be wiped
+# i.e. keep LVs VGs PVs on disks that will not be wiped (e.g. a separated storage disk with LVM on it).
+local belongs_to_a_disk_to_be_wiped disk_to_be_wiped devices_belonging_to_disk detected_LV detected_VG detected_PV
 # Remove detected LVs ('for' does nothing when $detected_LVs is empty):
-for detected_LV in $detected_LVs ; do
-    if lvremove -f -y $detected_LV ; then
-        DebugPrint "Removed LVM logical volume $detected_LV"
-    else
-        LogPrintError "Failed to remove LVM logical volume $detected_LV ('lvremove $detected_LV' failed)"
-    fi
-done
+#for detected_LV in $detected_LVs ; do
+#    belongs_to_a_disk_to_be_wiped="no"
+#    for disk_to_be_wiped in $DISKS_TO_BE_WIPED ; do
+#        # Get NAME (which could be a symlink like /dev/system/home for the LV 'home' in the 'system' VG)
+#        # and KNAME (which is the kernel device like /dev/dm-2 where the /dev/system/home symlink points to)
+#        # to be on the safe side with which name a detected LV is reported (usually the symlink is reported):
+#        devices_belonging_to_disk=( $( lsblk -nlpo NAME,KNAME $disk_to_be_wiped ) )
+#        # When NAME and KNAME are same it results duplicates in the devices_belonging_to_disk array
+#        # but duplicates do not matter for IsInArray (see the IsInArray implementation in lib/array-functions.sh):
+#        IsInArray $detected_LV "${devices_belonging_to_disk[@]}" && belongs_to_a_disk_to_be_wiped="yes"
+#    done
+#    if ! is_true $belongs_to_a_disk_to_be_wiped ; then
+#        DebugPrint "Skip removing LVM logical volume $detected_LV (not recognized as belonging to a disk to be wiped)"
+#        continue
+#    fi
+#    if lvremove -f -y $detected_LV ; then
+#        DebugPrint "Removed LVM logical volume $detected_LV"
+#    else
+#        LogPrintError "Failed to remove LVM logical volume $detected_LV ('lvremove $detected_LV' failed)"
+#    fi
+#done
 # Remove detected VGs ('for' does nothing when $detected_VGs is empty):
-for detected_VG in $detected_VGs ; do
-    if vgremove -ff -y $detected_VG ; then
-        DebugPrint "Removed LVM volume group $detected_VG"
-    else
-        LogPrintError "Failed to remove LVM volume group $detected_VG ('vgremove $detected_VG' failed)"
-    fi
-done
+#for detected_VG in $detected_VGs ; do
+#    if vgremove -ff -y $detected_VG ; then
+#        DebugPrint "Removed LVM volume group $detected_VG"
+#    else
+#        LogPrintError "Failed to remove LVM volume group $detected_VG ('vgremove $detected_VG' failed)"
+#    fi
+#done
 # Remove detected PVs ('for' does nothing when $detected_PVs is empty):
 for detected_PV in $detected_PVs ; do
-    if pvremove -f -y $detected_PV ; then
+    belongs_to_a_disk_to_be_wiped="no"
+    for disk_to_be_wiped in $DISKS_TO_BE_WIPED ; do
+        # Get NAME (which could be a symlink e.g. /dev/mapper/luks1sda2 for a PV that is a LUKS volume)
+        # and KNAME (which is the kernel device like /dev/dm-0 where /dev/mapper/luks1sda2 points to)
+        # to be on the safe side with which name a detected PV is reported
+        # (usually the symlink is reported when such a symlink exists):
+        devices_belonging_to_disk=( $( lsblk -nlpo NAME,KNAME $disk_to_be_wiped ) )
+        # When NAME and KNAME are same it results duplicates in the devices_belonging_to_disk array
+        # but duplicates do not matter for IsInArray (see the IsInArray implementation in lib/array-functions.sh):
+        IsInArray $detected_PV "${devices_belonging_to_disk[@]}" && belongs_to_a_disk_to_be_wiped="yes"
+    done
+    if ! is_true $belongs_to_a_disk_to_be_wiped ; then
+        DebugPrint "Skip removing LVM physical volume $detected_PV (not recognized as belonging to a disk to be wiped)"
+        continue
+    fi
+    # Example how things fail when we try to cleanly remove a PV that is a LUKS volume
+    # in the ReaR recovery system and what worked in the end (at least on SLES 15 SP1):
+    #   RESCUE # pvscan         
+    #     No matching physical volumes found
+    #   RESCUE # cryptsetup luksOpen /dev/sda2 luks1sda2
+    #     Enter passphrase for /dev/sda2: ...
+    #   RESCUE # pvscan
+    #     PV /dev/mapper/luks1sda2   VG system          lvm2 [19.98 GiB / 0    free]
+    #     Total: 1 [19.98 GiB] / in use: 1 [19.98 GiB] / in no VG: 0 [0   ]
+    #   RESCUE # pvremove -f -y /dev/mapper/luks1sda2
+    #     PV /dev/mapper/luks1sda2 is used by VG system so please use vgreduce first.
+    #     (If you are certain you need pvremove, then confirm by using --force twice.)
+    #     /dev/mapper/luks1sda2: physical volume label not removed.
+    #   RESCUE linux-uxxi:~ # vgreduce -f system /dev/mapper/luks1sda2
+    #     Physical volume "/dev/mapper/luks1sda2" still in use
+    #   RESCUE linux-uxxi:~ # pvremove -f -f -y /dev/mapper/luks1sda2
+    #     WARNING: PV /dev/mapper/luks1sda2 is used by VG system.
+    #     WARNING: Wiping physical volume label from /dev/mapper/luks1sda2 of volume group "system".
+    #     Labels on physical volume "/dev/mapper/luks1sda2" successfully wiped.
+    #   RESCUE linux-uxxi:~ # pvscan
+    #     No matching physical volumes found
+    # So all we can do is using force twice because only that works (with warnings):
+    if pvremove --force --force -y $detected_PV ; then
         DebugPrint "Removed LVM physical volume $detected_PV"
     else
         LogPrintError "Failed to remove LVM physical volume $detected_PV ('pvremove $detected_PV' failed)"
     fi
+done
+
+# The luks_mapping_names where 'cryptsetup luksOpen' had succeeded above need to be closed again.
+# Leaving LUKS volumes open leads to errors later when "parted -s $disk mklabel $label" is called
+# in the diskrestore.sh script because then 'parted' fails with the following error message:
+#   Partitions ... have been written, but we have been unable to inform the kernel of the change,
+#   probably because it/they are in use. As a result, the old partition(s) will remain in use.
+#   You should probably reboot now before making further changes.
+# In this case it works to reboot the recovery system and then a second "rear recover" usually succeeds
+# because after plain wiping a disk with LUKS volumes the LUKS metadata/signatures are no longer there
+# so that LUKS cannot be in use in the rebooted recovery system and "parted -s $disk mklabel $label" can succeed.
+# But we like to do all in one single "rear recover" run so we need to clean up LUKS storage objects properly:
+for luks_mapping_name in $luks_mapping_names ; do
+    if ! cryptsetup luksClose $luks_mapping_name ; then
+        LogPrintError "Failed to close LUKS device $luks_device (still opened as $luks_mapping_name)"
+        continue
+    fi
+    DebugPrint "Closed LUKS device $luks_device (was temporarily opened as $luks_mapping_name)"
 done
 
 # Wipe RAID plus LVM plus LUKS metadata.
@@ -77,7 +179,7 @@ done
 # wipe 4 MiB + 1 MiB = 5 MiB at the end of the device.
 # To be future proof (perhaps LUKS may add a backup header at the end of the device)
 # wiping 16 MiB at the beginning and at the end of the device should be sufficiently safe.
-local disk_to_be_wiped children_to_be_wiped device_to_be_wiped device_to_be_wiped_size_bytes bytes_to_be_wiped dd_seek_byte
+local children_to_be_wiped device_to_be_wiped device_to_be_wiped_size_bytes bytes_to_be_wiped dd_seek_byte
 # 16 * 1024 * 1024 = 16777216
 local bytes_of_16_MiB=16777216
 for disk_to_be_wiped in $DISKS_TO_BE_WIPED ; do
