26366b6b39646cb8bd9fe85d2be9b166e33a9003
sebastian-koehler <sebastian.koehler@microfocus.com>
2020-11-30 23:53:48 +0100

    Minor cleanup

--- usr/share/rear/verify/DP/default/400_verify_dp.sh
+++ usr/share/rear/verify/DP/default/400_verify_dp.sh
@@ -6,9 +6,9 @@
 test -s /etc/opt/omni/client/cell_server || Error "Data Protector Cell Manager not configured in /etc/opt/omni/client/cell_server"
 CELL_SERVER="$( cat /etc/opt/omni/client/cell_server )"
 
-if [ $ARCH == "Linux-i386" ] || [ $ARCH == "Linux-x86_64" ] || [ $ARCH == "Linux-ia64" ]; then
-    OMNICHECK=/opt/omni/bin/omnicheck
+OMNICHECK=/opt/omni/bin/omnicheck
 
+if [ $ARCH == "Linux-i386" ] || [ $ARCH == "Linux-x86_64" ] || [ $ARCH == "Linux-ia64" ]; then
     # check that the Cell Manager is responding on the INET port
     ${OMNICHECK} -patches -host ${CELL_SERVER} || Error "Data Protector Cell Manager is not responding, error code $?.
     See $RUNTIME_LOGFILE for more details."
