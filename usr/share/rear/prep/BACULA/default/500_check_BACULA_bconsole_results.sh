6922f729b3cfa1c937824ea139ce9d415939f6a2
Gratien Dhaese <gratien.dhaese@gmail.com>
2020-12-09 11:26:26 +0100

    #2152 replaced StopIfError by Error
    Signed-off-by: Gratien Dhaese <gratien.dhaese@gmail.com>

--- usr/share/rear/prep/BACULA/default/500_check_BACULA_bconsole_results.sh
+++ usr/share/rear/prep/BACULA/default/500_check_BACULA_bconsole_results.sh
@@ -13,12 +13,10 @@ fi
 #
 # is the director server present? Fetch from $BACULA_CONF_DIR/bconsole.conf file
 BACULA_DIRECTOR=$(grep -i address $BACULA_CONF_DIR/bconsole.conf | awk '{ print $3 }')
-[ "${BACULA_DIRECTOR}" ]
-StopIfError "Director not defined in $BACULA_CONF_DIR/bconsole.conf"
+[ "${BACULA_DIRECTOR}" ] || Error "Director not defined in $BACULA_CONF_DIR/bconsole.conf"
 
 if test "$PING"; then
-	ping -c 2 -q  $BACULA_DIRECTOR >/dev/null
-	StopIfError "Backup host [$BACULA_DIRECTOR] not reachable."
+	ping -c 2 -q  $BACULA_DIRECTOR >/dev/null || Error "Backup host [$BACULA_DIRECTOR] not reachable."
 else
 	Log "Skipping ping test"
 fi
@@ -30,18 +28,15 @@ fi
 # "Connecting to Director 'director_name-fd:9101'"
 # "Connecting to Client 'bacula_client_name-fd at FQDN:9102"
 BACULA_CLIENT=$(grep $(hostname -s) $BACULA_CONF_DIR/bacula-fd.conf | grep "\-fd" | awk '{print $3}' | sed -e "s/-fd//g")
-[ "${BACULA_CLIENT}" ]
-StopIfError "Client $(hostname -s) not defined in $BACULA_CONF_DIR/bacula-fd.conf"
+[ "${BACULA_CLIENT}" ] || Error "Client $(hostname -s) not defined in $BACULA_CONF_DIR/bacula-fd.conf"
 
 BACULA_RESULT=( $(echo -e " status client=${BACULA_CLIENT}-fd" | bconsole | grep Connect) )
 
 director=${BACULA_RESULT[3]}
 client=${BACULA_RESULT[9]}
 
-[ "$director" ]
-StopIfError "Bacula director not reachable."
+[ "$director" ] || Error "Bacula director not reachable."
 
-[ "$client" ]
-StopIfError "Bacula client status unknown on director."
+[ "$client" ] || Error "Bacula client status unknown on director."
 
 Log "Bacula director = $director, client = $client"
