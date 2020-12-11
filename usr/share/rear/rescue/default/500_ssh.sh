a47b63b31bf04298b6f5601b6b29f1f43d66d4b2
Johannes Meixner <jsmeix@suse.com>
2020-12-11 12:05:21 +0100

    Update 500_ssh.sh
    Another place to avoid that the SSH_ROOT_PASSWORD value is shown in debugscript mode

--- usr/share/rear/rescue/default/500_ssh.sh
+++ usr/share/rear/rescue/default/500_ssh.sh
@@ -89,19 +89,21 @@ fi
 echo "ssh:23:respawn:/etc/scripts/run-sshd" >>$ROOTFS_DIR/etc/inittab
 
 # Print an info if there is no authorized_keys file for root and no SSH_ROOT_PASSWORD set:
-if ! test -f "$ROOT_HOME_DIR/.ssh/authorized_keys" -o "$SSH_ROOT_PASSWORD" ; then
+# Avoid that the SSH_ROOT_PASSWORD value is shown in debugscript mode
+# cf. the comment of the UserInput function in lib/_input-output-functions.sh
+# how to keep things confidential when usr/sbin/rear is run in debugscript mode
+# ('2>/dev/null' should be sufficient here because 'test' does not output on stdout):
+{ if ! test -f "$ROOT_HOME_DIR/.ssh/authorized_keys" -o "$SSH_ROOT_PASSWORD" ; then
     LogPrintError "To log into the recovery system via ssh set up $ROOT_HOME_DIR/.ssh/authorized_keys or specify SSH_ROOT_PASSWORD"
-fi
+  fi
+} 2>/dev/null
 
 # Set the SSH root password in the recovery system:
 # If the password is encrypted just copy it otherwise use openssl (for backward compatibility).
 # Encryption syntax is detected as a '$D$' or '$Dx$' prefix in the password, where D is a single digit and x is one lowercase character.
 # For more information on encryption IDs, check out the NOTES section of the man page for crypt(3).
 # The extglob shell option is required for this to work.
-# Avoid that the SSH_ROOT_PASSWORD value is shown in debugscript mode
-# cf. the comment of the UserInput function in lib/_input-output-functions.sh
-# how to keep things confidential when usr/sbin/rear is run in debugscript mode
-# ('2>/dev/null' should be sufficient here because 'test' does not output on stdout):
+# Avoid that the SSH_ROOT_PASSWORD value is shown in debugscript mode (same as above):
 { if test "$SSH_ROOT_PASSWORD" ; then
     case "$SSH_ROOT_PASSWORD" in
         (\$[0-9]?([a-z])\$*)
