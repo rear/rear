8bc2b1cccccbd4806feb0bfa5fdcc41d0ca47942
Oliver Okrongli <oliver.okrongli@infix.de>
2020-12-11 13:25:02 +0100

    Avoid exposing TTY_ROOT_PASSWORD when ReaR runs in debugscript mode

--- usr/share/rear/build/default/503_store_tty_root_password.sh
+++ usr/share/rear/build/default/503_store_tty_root_password.sh
@@ -1,3 +1,4 @@
 # Store TTY_ROOT_PASSWORD for terminal logins
 
-[[ -n "$TTY_ROOT_PASSWORD" ]] && echo "$TTY_ROOT_PASSWORD" > "$ROOTFS_DIR/.TTY_ROOT_PASSWORD"
+# stderr is redirected here to avoid exposing the password hash in the log file when ReaR runs in debugscript mode.
+{ [[ -n "$TTY_ROOT_PASSWORD" ]] && echo "$TTY_ROOT_PASSWORD" > "$ROOTFS_DIR/.TTY_ROOT_PASSWORD"; } 2>/dev/null
