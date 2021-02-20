ec067b27bcd0d11cb8ae04f91052a89679c206d9
John Grigutis <john@lingo.technology>
2021-02-19 22:13:10 -0500

    Fixes docker_root_dir not being set when `docker info` writes to stderr.
    On Ubuntu Server 20.04.2 LTS with Docker 19.03.13, rear "Cannot determine Docker Root Dir" even though 'Docker Root Dir' is found in `docker info`. This seems to be because docker also sends to stderr which interferes with the variable assignment:
    
    `$ timeout -k 2s 5s docker info | grep 'Docker Root Dir' | awk '{print $4}'
    WARNING: No swap limit support
    /var/lib/docker`
    
    redirecting stderr to null allows the 'Docker Root Dir' value to be assigned to the `docker_root_dir` variable correctly.

--- usr/share/rear/layout/save/GNU/Linux/230_filesystem_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/230_filesystem_layout.sh
@@ -62,7 +62,7 @@ if service docker status ; then
     # When the Docker daemon/service is running, try to get its 'Docker Root Dir':
     # Kill 'docker info' with SIGTERM after 5 seconds and with SIGKILL after additional 2 seconds
     # because there are too many crippled Docker installations, cf. https://github.com/rear/rear/pull/2021
-    docker_root_dir=$( timeout -k 2s 5s docker info | grep 'Docker Root Dir' | awk '{print $4}' )
+    docker_root_dir=$( timeout -k 2s 5s docker info 2>/dev/null | grep 'Docker Root Dir' | awk '{print $4}' )
     # Things may go wrong in the 'Docker specific exclude part' below
     # when Docker is used but its 'Docker Root Dir' cannot be determined
     # cf. https://github.com/rear/rear/issues/1989
