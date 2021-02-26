8a477faffa8e654c1aa970bf9c4cccb63de84fcd
Johannes Meixner <jsmeix@suse.com>
2021-02-26 14:00:02 +0100

    Update 200_mount_bootimg.sh
    For 'mkfs.vfat' do not use a '-F 16' or '-F 32' option and hope for the best,
    see https://github.com/rear/rear/issues/2575#issuecomment-786602931

--- usr/share/rear/output/ISO/Linux-ia64/200_mount_bootimg.sh
+++ usr/share/rear/output/ISO/Linux-ia64/200_mount_bootimg.sh
@@ -1,9 +1,11 @@
 # 200_mount_bootimg.sh
 dd if=/dev/zero of=$TMP_DIR/boot.img count=64000 bs=1024
-# make sure we select FAT16 instead of FAT12 as size >30MB
-mkfs.vfat $v -F 16 $TMP_DIR/boot.img
-mkdir -p $v $TMP_DIR/mnt
-# Do not specify '-o fat=16' when loop mounting boot.img file
-# but rely on the automatic FAT type detection when mounting
+# Make a FAT filesystem on the boot.img file and loop mount it
 # cf. https://github.com/rear/rear/issues/2575
+# and output/ISO/Linux-i386/700_create_efibootimg.sh
+# and output/RAWDISK/Linux-i386/280_create_bootable_disk_image.sh
+# Let mkfs.vfat automatically select the FAT type based on the size.
+# I.e. do not use a '-F 16' or '-F 32' option and hope for the best:
+mkfs.vfat $v $TMP_DIR/boot.img
+mkdir -p $v $TMP_DIR/mnt
 mount $v -o loop -t vfat $TMP_DIR/boot.img $TMP_DIR/mnt || Error "Failed to loop mount boot.img"
