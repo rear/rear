32a3b96a922a353fc81159594d5ac1a2837a5119
Pavel Cahyna <pcahyna@redhat.com>
2021-04-26 19:17:32 +0200

    Use dependencies when determining partitions for UEFI
    The ESP may be located on a RAID device. In this case, we need to
    determine the physical RAID components and call efibootmgr on them,
    because efibootmgr does not know how to tell the firmware to boot from
    RAID. Moreover, the current code tries to parse a device like /dev/md125
    into a disk called /dev/md and partition 125.
    
    Fix by using storage dependencies: RAID components (the actual ESP
    copies) can be found as dependencies of the ESP block device. For
    consistency, use dependencies also to locate the ESP block device itself
    (the old method is preserved as a fallback).  Similar strategy is used
    by the GRUB2 installation code
    (finalize/Linux-i386/660_install_grub2.sh), which is able to install
    GRUB2 on the RAID components correctly.
    
    Remove a comment that has been obsolete since revision
    bf6742bec5181ad7987a4a705668cc1a3834bef4 (partition numbers can have
    more digits now).
    
    TODO: finalize/Linux-i386/610_EFISTUB_run_efibootmgr.sh needs a similar
    fix.
    
    Fixes #2595

--- usr/share/rear/finalize/Linux-i386/670_run_efibootmgr.sh
+++ usr/share/rear/finalize/Linux-i386/670_run_efibootmgr.sh
@@ -24,27 +24,56 @@ esp_mountpoint=$( filesystem_name "$TARGET_FS_ROOT/$UEFI_BOOTLOADER" )
 # if mountpoint not found otherwise):
 if [ "$esp_mountpoint" = "/" ] ; then
     esp_mountpoint="$TARGET_FS_ROOT/boot/efi"
-    Log "Mountpoint of $TARGET_FS_ROOT/$UEFI_BOOTLOADER not found, trying $esp_mountpoint"
+    LogPrint "Mountpoint of $TARGET_FS_ROOT/$UEFI_BOOTLOADER not found, trying $esp_mountpoint"
 fi
 
 # Skip if there is no esp_mountpoint directory (e.g. the fallback ESP mountpoint may not exist).
 # Double quotes are mandatory here because 'test -d' without any (possibly empty) argument results true:
 test -d "$esp_mountpoint" || return 0
 
-BootEfiDev="$( mount | grep "$esp_mountpoint" | awk '{print $1}' )"
-# /dev/sda1 or /dev/mapper/vol34_part2 or /dev/mapper/mpath99p4
-Dev=$( get_device_name $BootEfiDev )
-# 1 (must anyway be a low nr <9)
-ParNr=$( get_partition_number $Dev )
-Disk=$( get_device_from_partition $Dev $ParNr )
-# EFI\fedora\shim.efi
-BootLoader=$( echo $UEFI_BOOTLOADER | cut -d"/" -f4- | sed -e 's;/;\\;g' )
-LogPrint "Creating  EFI Boot Manager entry '$OS_VENDOR $OS_VERSION' for '$BootLoader' (UEFI_BOOTLOADER='$UEFI_BOOTLOADER')"
-Log efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label \"${OS_VENDOR} ${OS_VERSION}\" --loader \"\\${BootLoader}\"
-if efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label "${OS_VENDOR} ${OS_VERSION}" --loader "\\${BootLoader}" ; then
-    # ok, boot loader has been set-up - tell rear we are done using following var.
-    NOBOOTLOADER=''
-    return
+# Mount point inside the target system,
+# accounting for possible trailing slashes in TARGET_FS_ROOT
+esp_mountpoint_inside="${esp_mountpoint#${TARGET_FS_ROOT%%*(/)}}"
+
+boot_efi_parts=$( find_partition "fs:$esp_mountpoint_inside" fs )
+if ! test "$boot_efi_parts" ; then
+    LogPrint "Unable to find ESP $esp_mountpoint_inside in layout"
+    LogPrint "Trying to determine device currently mounted at $esp_mountpoint as fallback"
+    boot_efi_dev="$( mount | grep "$esp_mountpoint" | awk '{print $1}' )"
+    if ! test "$boot_efi_dev" ; then
+        LogPrintError "Cannot create EFI Boot Manager entry (unable to find ESP $esp_mountpoint among mounted devices)"
+        return 1
+    fi
+    if test $(get_component_type "$boot_efi_dev") = part ; then
+        boot_efi_parts="$boot_efi_dev"
+    else
+        boot_efi_parts=$( find_partition "$boot_efi_dev" )
+    fi
+    if ! test "$boot_efi_parts" ; then
+        LogPrintError "Cannot create EFI Boot Manager entry (unable to find partition for $boot_efi_dev)"
+        return 1
+    fi
+    LogPrint "Using fallback EFI boot partition(s) $boot_efi_parts (unable to find ESP $esp_mountpoint_inside in layout)"
 fi
 
+# EFI\fedora\shim.efi
+BootLoader=$( echo $UEFI_BOOTLOADER | cut -d"/" -f4- | sed -e 's;/;\\;g' )
+
+for efipart in $boot_efi_parts ; do
+    # /dev/sda1 or /dev/mapper/vol34_part2 or /dev/mapper/mpath99p4
+    Dev=$( get_device_name $efipart )
+    # 1 or 2 or 4 for the examples above
+    ParNr=$( get_partition_number $Dev )
+    Disk=$( get_device_from_partition $Dev $ParNr )
+    LogPrint "Creating  EFI Boot Manager entry '$OS_VENDOR $OS_VERSION' for '$BootLoader' (UEFI_BOOTLOADER='$UEFI_BOOTLOADER') "
+    Log efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label \"${OS_VENDOR} ${OS_VERSION}\" --loader \"\\${BootLoader}\"
+    if efibootmgr --create --gpt --disk ${Disk} --part ${ParNr} --write-signature --label "${OS_VENDOR} ${OS_VERSION}" --loader "\\${BootLoader}" ; then
+        # ok, boot loader has been set-up - continue with other disks (ESP can be on RAID)
+        NOBOOTLOADER=''
+    else
+        LogPrintError "efibootmgr failed to create EFI Boot Manager entry on $Disk partition $ParNr (ESP $Dev)"
+    fi
+done
+
+is_true $NOBOOTLOADER || return 0
 LogPrintError "efibootmgr failed to create EFI Boot Manager entry for '$BootLoader' (UEFI_BOOTLOADER='$UEFI_BOOTLOADER')"
