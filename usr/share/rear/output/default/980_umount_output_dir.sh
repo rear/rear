cdfe8d981fef23f36de46e8d5e5ebc8c86bf3c91
Pavel Cahyna <pcahyna@redhat.com>
2021-05-20 14:30:34 +0200

    Remove the removal of outputfs/$RSYNC_PREFIX
    $BUILD_DIR/outputfs/$RSYNC_PREFIX has never been used by rsync. Removing
    it is even dangerous, because usually $RSYNC_PREFIX == $NETFS_PREFIX ==
    $OUTPUT_PREFIX, so this would lead to a loss of data if the unmount
    fails. It could even remove all the backups if $RSYNC_PREFIX is empty.

--- usr/share/rear/output/default/980_umount_output_dir.sh
+++ usr/share/rear/output/default/980_umount_output_dir.sh
@@ -11,7 +11,6 @@ fi
 umount_url $OUTPUT_URL $BUILD_DIR/outputfs
 
 [[ -d $BUILD_DIR/outputfs/$NETFS_PREFIX ]] && rm -rf $v $BUILD_DIR/outputfs/$NETFS_PREFIX
-[[ -d $BUILD_DIR/outputfs/$RSYNC_PREFIX ]] && rm -rf $v $BUILD_DIR/outputfs/$RSYNC_PREFIX
 
 rmdir $v $BUILD_DIR/outputfs >&2
 if [[ $? -eq 0 ]] ; then
