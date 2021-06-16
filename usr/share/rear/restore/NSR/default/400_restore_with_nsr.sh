6e470da26903a54d72575456d3834f947d6a7ba5
hpannenb <hpannenb@users.noreply.github.com>
2021-06-16 08:54:11 +0200

    Added NSR_CLIENT_CAN_RECOVER handling for EMC Networker

--- usr/share/rear/restore/NSR/default/400_restore_with_nsr.sh
+++ usr/share/rear/restore/NSR/default/400_restore_with_nsr.sh
@@ -1,10 +1,10 @@
 # 400_restore_with_nsr.sh
 #
-# In case NSR_CLIENT_MODE is enabled we need to prompt and wait
-# until the restore rpocess executed at the NSRSERVER has finished.
+# In case the NSR_CLIENT_MODE = YES and NSR_CLIENT_CAN_RECOVER = NO then
+# prompt the user to request the recovery action elsewhere on the NSRSERVER
+# and wait until the restore process has finished.
 #
-
-if is_true "$NSR_CLIENT_MODE"; then
+if is_true "$NSR_CLIENT_MODE" && is_false "$NSR_CLIENT_CAN_RECOVER"; then
     LogPrint "Please let the restore process start on Your backup server i.e. $(cat $VAR_DIR/recovery/nsr_server)."
     LogPrint "Make sure all required data is restored to $TARGET_FS_ROOT ."
     LogPrint ""
@@ -15,15 +15,26 @@ if is_true "$NSR_CLIENT_MODE"; then
     rear_shell "Has the restore been completed and are You ready to continue the recovery?"
 else
     LogUserOutput "Starting nsrwatch on console 8"
-    TERM=linux nsrwatch -p 1 -s $(cat $VAR_DIR/recovery/nsr_server ) </dev/tty8 >/dev/tty8 &
+    TERM=linux nsrwatch -p 1 -s $(cat $VAR_DIR/recovery/nsr_server) </dev/tty8 >/dev/tty8 &
 
     LogUserOutput "Restore filesystem $(cat $VAR_DIR/recovery/nsr_paths) with recover"
+
+    # If a point-in-time recovery requested use this date/time as recover argument
+    # else leave it empty
+    if [ ${#NSR_ENDTIME[@]} -gt 0 ] ; then
+        recover_date="${NSR_ENDTIME[@]}"
+        recover_args="-t ${recover_date}"
+        LogUserOutput "The recovery date/time is to/until ${recover_date} ."
+    else
+        recover_args=""
+        LogUserOutput "The most recent recovery date/time will be used."
+    fi
 
     blank=" "
     # Use the original STDOUT when 'rear' was launched by the user for the 'while read ... echo' output
     # (which also reads STDERR of the 'recover' command so that 'recover' errors are 'echo'ed to the user)
     # but keep STDERR of the 'while' command going to the log file so that 'rear -D' output goes to the log file:
-    recover -s $(cat $VAR_DIR/recovery/nsr_server) -c $(hostname) -d $TARGET_FS_ROOT -a $(cat $VAR_DIR/recovery/nsr_paths) 2>&1 \
+    recover -s $(cat $VAR_DIR/recovery/nsr_server) -c $(hostname) -d $TARGET_FS_ROOT -a $(cat $VAR_DIR/recovery/nsr_paths) $recover_args 2>&1 \
       | while read -r ; do
             echo -ne "\r${blank:1-COLUMNS}\r"
             case "$REPLY" in
