bd7403e9eee947e8913ad4fa7283517f0423fc65
Johannes Meixner <jsmeix@suse.com>
2021-07-20 12:07:11 +0200

    Update 300_create_grub.sh
    Simpler and more straightforward code in output/USB/Linux-i386/300_create_grub.sh

--- usr/share/rear/output/USB/Linux-i386/300_create_grub.sh
+++ usr/share/rear/output/USB/Linux-i386/300_create_grub.sh
@@ -1,64 +1,49 @@
-# only run for grub
-if [ -z $USB_BOOTLOADER ] && [[ ! $USB_BOOTLOADER =~ grub ]]; then
-    return 0
-fi
 
-# we assume that REAL_USB_DEVICE and RAW_USB_DEVICE are both set from the script
-# in prep/USB/Linux-i386/350_check_usb_disk.sh
+# Only run when GRUB2 is specified to be used as USB bootloader:
+test "$USB_BOOTLOADER" = "grub" || return 0
 
-[ "$RAW_USB_DEVICE" -a "$REAL_USB_DEVICE" ] || Error "RAW_USB_DEVICE and REAL_USB_DEVICE should be already set"
+DebugPrint "Installing GRUB2 as USB bootloader (USB_BOOTLOADER='$USB_BOOTLOADER')"
+
+# Choose right GRUB2 install binary
+# cf. https://github.com/rear/rear/issues/849
+# and error out if there is neither grub-install nor grub2-install:
+GRUB_INSTALL="false"
+has_binary grub-install && GRUB_INSTALL="grub-install"
+has_binary grub2-install && GRUB_INSTALL="grub2-install"
+is_false $GRUB_INSTALL && Error "Cannot install GRUB2 as USB bootloader (neither grub-install nor grub2-install found)"
+
+# Verify the GRUB version because only GRUB2 is supported.
+# Because substr() for awk did not work as expected for this case here
+# 'cut' is used (awk '{print $NF}' prints the last column which is the version).
+# Only the first character of the version should be enough (at least for now).
+# Example output (on openSUSE Leap 15.2)
+# # grub2-install --version
+# grub2-install (GRUB2) 2.04
+# # grub2-install --version | awk '{print $NF}' | cut -c1
+# 2
+grub_version=$( $GRUB_INSTALL --version | awk '{print $NF}' | cut -c1 )
+test "$grub_version" = "2" || Error "Cannot install GRUB as USB bootloader (only GRUB2 is supported, '$GRUB_INSTALL --version' shows '$grub_version')"
+
+# We assume REAL_USB_DEVICE and RAW_USB_DEVICE are both set by prep/USB/Linux-i386/350_check_usb_disk.sh
+[ "$RAW_USB_DEVICE" -a "$REAL_USB_DEVICE" ] || BugError "RAW_USB_DEVICE and REAL_USB_DEVICE are not both set"
 
 USB_REAR_DIR="$BUILD_DIR/outputfs/$USB_PREFIX"
-if [ ! -d "$USB_REAR_DIR" ]; then
-    mkdir -p $v "$USB_REAR_DIR" >/dev/null || Error "Could not create USB ReaR dir [$USB_REAR_DIR] !"
+if [ ! -d "$USB_REAR_DIR" ] ; then
+    mkdir -p $v "$USB_REAR_DIR" || Error "Could not create USB ReaR dir '$USB_REAR_DIR'"
 fi
 
 USB_BOOT_DIR="$BUILD_DIR/outputfs/boot"
-if [ ! -d "$USB_BOOT_DIR" ]; then
-    mkdir -p $v "$USB_BOOT_DIR" >/dev/null || Error "Could not create USB boot dir [$USB_BOOT_DIR] !"
+if [ ! -d "$USB_BOOT_DIR" ] ; then
+    mkdir -p $v "$USB_BOOT_DIR" || Error "Could not create USB boot dir '$USB_BOOT_DIR'"
 fi
 
-# Hope this assumption is not wrong ...
-if has_binary grub-install grub2-install; then
-
-    # Choose right grub binary
-    # Issue #849
-    if has_binary grub2-install; then
-        Log "using grub2 binary"
-        NUM=2
-    fi
-
-    GRUB_INSTALL=grub${NUM}-install
-
-    # install
-    Log "installing grub..."
-    $GRUB_INSTALL --boot-directory=${USB_BOOT_DIR} --recheck $RAW_USB_DEVICE || Error "Could not install grub on $RAW_USB_DEVICE !"
-
-    # What version of grub are we using
-    # substr() for awk did not work as expected for this reason cut was used
-    # First charecter should be enough to identify grub version
-    grub_version=$(( $GRUB_INSTALL --version | awk '{print $NF}' | cut -c1-1 ))
-
-    case ${grub_version} in
-        0)
-            BugError "grub 0.97 not supported"
-        ;;
-        2)
-            Log "Configuring grub 2.0 for legacy boot"
-            # We need to explicitly set $root variable to boot label
-            # (currently "REAR-BOOT") in Grub because default $root would
-            # point to memdisk, where kernel and initrd are NOT present.
-            # Variable grub2_set_usb_root will be used in later call of
-            # create_grub2_cfg().
-            grub2_set_usb_root="search --no-floppy --set=root --label REAR-BOOT"
-
-            # Create config for grub 2.0
-            Log "creating new grub config..."
-            create_grub2_cfg /$USB_PREFIX/kernel /$USB_PREFIX/$REAR_INITRD_FILENAME > ${USB_BOOT_DIR}/grub/grub.cfg
-        ;;
-        *)
-            BugError "Neither grub 0.97 nor 2.0"
-        ;;
-    esac
-
-fi
+# Install and configure GRUB2 as USB bootloader:
+$GRUB_INSTALL --boot-directory=$USB_BOOT_DIR --recheck $RAW_USB_DEVICE || Error "Failed to install GRUB2 on $RAW_USB_DEVICE"
+Log "Configuring GRUB2 as USB bootloader for legacy boot"
+# We need to explicitly set $root variable to boot label (currently "REAR-BOOT") in GRUB2
+# because default $root would point to ramdisk, where kernel and initrd are NOT present.
+# Variable grub2_set_usb_root will be used in later call of create_grub2_cfg():
+grub2_set_usb_root="search --no-floppy --set=root --label REAR-BOOT"
+# Create config for GRUB2:
+Log "Creating GRUB2 config as USB bootloader"
+create_grub2_cfg /$USB_PREFIX/kernel /$USB_PREFIX/$REAR_INITRD_FILENAME > $USB_BOOT_DIR/grub/grub.cfg
