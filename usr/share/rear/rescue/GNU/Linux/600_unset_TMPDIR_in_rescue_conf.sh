2721c2743ea959631be0d30eac92984a708c1e66
Pavel Cahyna <pcahyna@redhat.com>
2021-08-02 17:38:36 +0200

    Remove code that unset TMPDIR in rescue.conf
    This code probably has not worked since commit 0022063 (PR #2633),
    because rescue.conf is sourced only when the build area has been
    already created by invoking mktemp. It is also unneeded, because since
    that commit, setting TMPDIR in local.conf (which was the initial concern
    that motivated this code) does not work anyway, as discussed in #2654.

--- usr/share/rear/rescue/GNU/Linux/600_unset_TMPDIR_in_rescue_conf.sh
+++
@@ -1,8 +0,0 @@
-cat - <<EOF >> "$ROOTFS_DIR/etc/rear/rescue.conf"
-# TMPDIR variable may be defined in local.conf file as prefix dir for mktemp command
-# e.g. by defining TMPDIR=/var we would get our BUILD_DIR=/var/tmp/rear.XXXXXXXXXXXX
-# However, in rescue we want our BUILD_DIR=/tmp/rear.XXXXXXX as we are not sure that
-# the user defined TMPDIR would exist in our rescue image
-# by 'unset TMPDIR' we achieve above goal (as rescue.conf is read after local.conf)!
-unset TMPDIR
-EOF
