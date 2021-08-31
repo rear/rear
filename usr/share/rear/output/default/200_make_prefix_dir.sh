53ec3f39dcb50cd10ea752556dc8b273f78c45ef
Johannes Meixner <jsmeix@suse.com>
2021-08-31 16:31:54 +0200

    Update 200_make_prefix_dir.sh
    Comment why the OUTPUT_PREFIX directory
    should be neither world-readable nor world-writable.

--- usr/share/rear/output/default/200_make_prefix_dir.sh
+++ usr/share/rear/output/default/200_make_prefix_dir.sh
@@ -5,7 +5,7 @@
 # This happens usually under a mounted network filesystem share
 # e.g. in case of OUTPUT_URL=nfs://NFS.server.IP.address/remote/nfs/share
 # but it is also happens for local stuff like OUTPUT_URL=usb:///dev/disk/by-label/REAR-000
-#
+
 # Do not do this for tapes and special attention for file:///path
 local scheme=$( url_scheme $OUTPUT_URL )
 local path=$( url_path $OUTPUT_URL )
@@ -15,7 +15,10 @@ scheme_supports_filesystem $scheme || return 0
 
 local opath=$( output_path $scheme $path )
 
-# Create $OUTPUT_PREFIX sub-directory:
+# Create $OUTPUT_PREFIX sub-directory.
+# That directory should be neither world-readable nor world-writable
+# because it contains confidential data. In particular it may contain
+# the backup of (almost) all files of the system:
 mkdir -p $v -m0750 "$opath" && return
 
 # A failure to create the $OUTPUT_PREFIX sub-directory is fatal:
