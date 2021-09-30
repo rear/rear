bd91d2b7ea9e955b7d33634dfa51674c870583ca
Johannes Meixner <jsmeix@suse.com>
2021-09-30 09:30:20 +0200

    Update 100_create_efiboot.sh
    Typo fixes in comments and more explanatory LogPrintError messages.

--- usr/share/rear/output/USB/Linux-i386/100_create_efiboot.sh
+++ usr/share/rear/output/USB/Linux-i386/100_create_efiboot.sh
@@ -14,11 +14,11 @@ DebugPrint "Configuring EFI partition '$efi_part' for EFI boot with '$uefi_bootl
 # Fail if EFI partition is not present
 test -b "$efi_part" || Error "EFI partition '$efi_part' is no block device (did you use 'rear format -- --efi ...' for correct format?)"
 
-# $BUILD_DIR is not present at this stage so TMPDIR (by default /var/tmp see default.conf)" will be used instead.
+# $BUILD_DIR is not present at this stage so TMPDIR (by default /var/tmp see default.conf) will be used instead.
 # Slackware version of mktemp requires 6 Xs in template and
 # plain 'mktemp' uses XXXXXXXXXX by default (at least on SLES11 and openSUSE Leap 15.0)
 # so that we comply with the 'mktemp' default to avoid 'mktemp' errors "too few X's in template".
-# We use "local var ; var=$( COMMAND)" because "local var="$( COMMAND )" || Error "COMMAND failed"
+# We use local var ; var=$( COMMAND ) because local var=$( COMMAND ) || Error "COMMAND failed"
 # will not error out when COMMAND failed, see https://github.com/rear/rear/wiki/Coding-Style
 # and https://github.com/koalaman/shellcheck/wiki/SC2155
 local efi_mpt
@@ -34,7 +34,7 @@ mount $efi_part $efi_mpt || Error "Failed to mount EFI partition '$efi_part' at 
 mkdir -p $efi_dst || Error "Failed to create directory '$efi_dst'"
 
 # Follow symbolic links to ensure the real content gets copied
-# but do not preserve mode,ownership,timestamps because that may fail like
+# but do not preserve mode,ownership,timestamps (i.e. no -p option) because that may fail like
 # "cp: failed to preserve ownership for '/tmp/rear-efi.XXXXXXXXXX/EFI/BOOT/kernel': Operation not permitted"
 # because it copies to a VFAT filesystem on the EFI partition (see format/USB/default/300_format_usb_disk.sh)
 # cf. https://github.com/rear/rear/issues/2683
@@ -97,7 +97,7 @@ fi
 
 # Cleanup of EFI temporary mount point:
 if umount $efi_mpt ; then
-    rmdir $efi_mpt || LogPrintError "Could not remove temporary directory '$efi_mpt'"
+    rmdir $efi_mpt || LogPrintError "Could not remove temporary directory '$efi_mpt' (you should do it manually)"
 else
-    LogPrintError "Could not umount '$efi_mpt'"
+    LogPrintError "Could not umount EFI partition '$efi_part' at '$efi_mpt' (you should do it manually)"
 fi
