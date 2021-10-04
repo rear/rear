rear/rear

commit b184194f37dd22a7e55655ff388579297239e73c

Issue #1380 - ReaR recovery fails when the OS contains a Thin Pool/Volume
Using 'vgcfgrestore' to restore Thin LVs doesn't work, because there
isn't the metadata in the configuration file.
To be able to use Thin LVs, upon failure of 'vgcfgrestore' (or migration),
traditional 'vgcreate/lvcreate' are used instead.

This approach is sub-optimal because it requires to know all the
possible options to 'lvcreate', which is hard/impossible to do.
Nonetheless, this is better than nothing since the issue with 'lvcreate'
options was already there in case of migration.

The code using 'lvcreate' only runs upon 'vgcfgrestore' failure, which
typically happens only with Thin LVs (IMHO). Hence, on most setups, the
optimized code will still run, preventing any regression to happen.

Signed-off-by: Renaud Métrich <rmetrich@redhat.com>

usr/share/rear/layout/prepare/GNU/Linux/110_include_lvm_code.sh
@@ -52,17 +52,34 @@ create_lvmdev() {
 
 # Create a new VG.
 create_lvmgrp() {
-    if ! is_true "$MIGRATION_MODE" ; then
-        restore_lvmgrp "$1"
-        cat >> "$LAYOUT_CODE" <<EOF
-LogPrint "Sleeping 3 seconds to let udev or systemd-udevd create their devices..."
-sleep 3 >&2
-EOF
-        return
-    fi
-
     local lvmgrp vgrp extentsize junk
     read lvmgrp vgrp extentsize junk < <(grep "^lvmgrp $1 " "$LAYOUT_FILE")
+
+    # If we are not migrating, then try using "vgcfgrestore", but this can
+    # fail, typically if Thin Pools are used.
+    #
+    # In such case, we need to rely on vgcreate/lvcreate commands which is not
+    # recommended because we are not able to collect all required options yet.
+    # For example, we do not take the '--stripes' option into account, nor
+    # '--mirrorlog', etc.
+    # Also, we likely do not support every layout yet (e.g. 'cachepool').
+
+    if ! is_true "$MIGRATION_MODE" ; then
+        cat >> "$LAYOUT_CODE" <<EOF
+LogPrint "Restoring LVM VG ${vgrp#/dev/}"
+if [ -e "$vgrp" ] ; then
+    rm -rf "$vgrp"
+fi
+if lvm vgcfgrestore -f "$VAR_DIR/layout/lvm/${vgrp#/dev/}.cfg" ${vgrp#/dev/} >&2 ; then
+    lvm vgchange --available y ${vgrp#/dev/} >&2
+
+    LogPrint "Sleeping 3 seconds to let udev or systemd-udevd create their devices..."
+    sleep 3 >&2
+    create_logical_volumes=0
+else
+    LogPrint "Warning: could not restore LVM configuration using 'vgcfgrestore'. Using traditional 'vgcreate/lvcreate' commands instead ..."
+EOF
+    fi
 
     local -a devices=($(grep "^lvmdev $vgrp " "$LAYOUT_FILE" | cut -d " " -f 3))
 
@@ -73,29 +90,19 @@ if [ -e "$vgrp" ] ; then
 fi
 lvm vgcreate --physicalextentsize ${extentsize}k ${vgrp#/dev/} ${devices[@]} >&2
 lvm vgchange --available y ${vgrp#/dev/} >&2
-EOF
-}
 
-# Restore a VG from a backup.
-restore_lvmgrp() {
-    local lvmgrp vgrp extentsize junk
-    read lvmgrp vgrp extentsize junk < <(grep "^lvmgrp $1 " "$LAYOUT_FILE")
-cat >> "$LAYOUT_CODE" <<EOF
-LogPrint "Restoring LVM VG ${vgrp#/dev/}"
-if [ -e "$vgrp" ] ; then
-    rm -rf "$vgrp"
-fi
-lvm vgcfgrestore -f "$VAR_DIR/layout/lvm/${vgrp#/dev/}.cfg" ${vgrp#/dev/} >&2
-lvm vgchange --available y ${vgrp#/dev/} >&2
+create_logical_volumes=1
 EOF
+
+    if ! is_true "$MIGRATION_MODE" ; then
+        cat >> "$LAYOUT_CODE" <<EOF
+fi
+EOF
+    fi
 }
 
 # Create a LV.
 create_lvmvol() {
-    if ! is_true "$MIGRATION_MODE" ; then
-        return
-    fi
-
     local name vg lv
     name=${1#/dev/mapper/}
     ### split between vg and lv is single dash
@@ -103,13 +110,73 @@ create_lvmvol() {
     vg=$(sed "s/\([^-]\)-[^-].*/\1/;s/--/-/g" <<< "$name")
     lv=$(sed "s/.*[^-]-\([^-]\)/\1/;s/--/-/g" <<< "$name")
 
-    local lvmvol vgrp lvname nrextents junk
-    read lvmvol vgrp lvname nrextents junk < <(grep "^lvmvol /dev/$vg $lv " "$LAYOUT_FILE")
+    # kval: "key:value" pairs, separated by spaces
+    local lvmvol vgrp lvname size layout kval
+    read lvmvol vgrp lvname size layout kval < <(grep "^lvmvol /dev/$vg $lv " "$LAYOUT_FILE")
 
-    (
-    echo "LogPrint \"Creating LVM volume ${vgrp#/dev/}/$lvname\""
-    #echo "lvm lvcreate -l $nrextents -n ${lvname} ${vgrp#/dev/} >&2"
-    # remove the '>&2' from lvcreate as sometimes we get an question like "Wipe it? [y/n]"
-    echo "lvm lvcreate -l $nrextents -n ${lvname} ${vgrp#/dev/} <<<y"
-    ) >> "$LAYOUT_CODE"
+    local lvopts=""
+
+    # Handle 'key:value' pairs
+    for kv in $kval ; do
+        local key=$(awk -F ':' '{ print $1 }' <<< "$kv")
+        local value=$(awk -F ':' '{ print $2 }' <<< "$kv")
+        lvopts="${lvopts:+$lvopts }--$key $value"
+    done
+
+    if [[ ,$layout, == *,thin,* ]] ; then
+
+        if [[ ,$layout, == *,pool,* ]] ; then
+            # Thin Pool
+
+            lvopts="${lvopts:+$lvopts }--type thin-pool -L $size"
+
+        else
+            # Thin Volume within Thin Pool
+
+            if [[ ,$layout, == *,sparse,* ]] ; then
+                lvopts="${lvopts:+$lvopts }-V $size"
+            else
+                BugError "Unsupported Thin LV layout '$layout' for LV '$lv'"
+            fi
+
+        fi
+
+    elif [[ ,$layout, == *,linear,* ]] ; then
+
+        lvopts="${lvopts:+$lvopts }-L $size"
+
+    elif [[ ,$layout, == *,mirror,* ]] ; then
+
+        lvopts="${lvopts:+$lvopts }--type mirror -L $size"
+
+    elif [[ ,$layout, == *,raid,* ]] ; then
+
+        local found=0
+        local lvl
+        for lvl in raid0 raid1 raid4 raid5 raid6 raid10 ; do
+            if [[ ,$layout, == *,$lvl,* ]] ; then
+                lvopts="${lvopts:+$lvopts }--type $lvl"
+                found=1
+                break
+            fi
+        done
+
+        [ $found -ne 0 ] || BugError "Unsupported LV layout '$layout' found for LV '$lv'"
+
+        lvopts="${lvopts:+$lvopts }-L $size"
+
+    else
+
+        BugError "Unsupported LV layout '$layout' found for LV '$lv'"
+
+    fi
+
+    cat >> "$LAYOUT_CODE" <<EOF
+if [ "\$create_logical_volumes" -eq 1 ]; then
+    LogPrint "Creating LVM volume ${vgrp#/dev/}/$lvname"
+    lvm lvcreate $lvopts -n ${lvname} ${vgrp#/dev/} <<<y
+fi
+EOF
 }
+
+# vim: set et ts=4 sw=4:

usr/share/rear/layout/save/GNU/Linux/220_lvm_layout.sh
@@ -37,13 +37,28 @@ Log "Saving LVM layout."
     done
 
     ## Get all logical volumes
-    # format: lvmvol <volume_group> <name> <size(extents)> [<size(bytes)>]
-    lvm 8>&- 7>&- lvdisplay -c | while read line ; do
-        lvol=$(echo $line | cut -d ":" -f "1" | cut -d "/" -f "4")
-        vgrp=$(echo $line | cut -d ":" -f "2")
-        size=$(echo $line | cut -d ":" -f "7")
-        extents=$(echo $line | cut -d ":" -f "8")
+    # format: lvmvol <volume_group> <name> <size(bytes)> <layout> [key:value ...]
 
-        echo "lvmvol /dev/$vgrp $lvol $extents $size "
+    lvm 8>&- 7>&- lvs --separator=":" --noheadings --units b --nosuffix -o origin,lv_name,vg_name,lv_size,lv_layout,pool_lv,chunk_size,stripes | while read line ; do
+        origin=$(echo $line | awk -F ':' '{ print $1 }')
+        # Skip snapshots
+        [ -z "$origin" ] || continue
+
+        lv=$(echo $line | awk -F ':' '{ print $2 }')
+        vg=$(echo $line | awk -F ':' '{ print $3 }')
+        size=$(echo $line | awk -F ':' '{ print $4 }')
+        layout=$(echo $line | awk -F ':' '{ print $5 }')
+        thinpool=$(echo $line | awk -F ':' '{ print $6 }')
+        chunksize=$(echo $line | awk -F ':' '{ print $7 }')
+        stripes=$(echo $line | awk -F ':' '{ print $8 }')
+
+        kval=""
+        [ -z "$thinpool" ] || kval="${kval:+$kval }thinpool:$thinpool"
+        [ $chunksize -eq 0 ] || kval="${kval:+$kval }chunksize:${chunksize}b"
+        [[ ,$layout, != ,mirror, ]] || kval="${kval:+$kval }mirrors:$(($stripes - 1))"
+
+        echo "lvmvol /dev/$vg $lv ${size}b $layout $kval"
     done
 ) >> $DISKLAYOUT_FILE
+
+# vim: set et ts=4 sw=4:

usr/share/rear/lib/layout-functions.sh
@@ -126,13 +126,18 @@ generate_layout_dependencies() {
             lvmvol)
                 vgrp=$(echo "$remainder" | cut -d " " -f "1")
                 lvol=$(echo "$remainder" | cut -d " " -f "2")
+                # When a LV is a Thin, then we need to create the Thin Pool first
+                pool=$(echo "$remainder" | egrep -ow "thinpool:\\S+" | cut -d ":" -f 2)
 
                 # Vgs and Lvs containing - in their name have a double dash in DM
                 dm_vgrp=${vgrp//-/--}
                 dm_lvol=${lvol//-/--}
+                dm_pool=${pool//-/--}
 
-                add_dependency "/dev/mapper/${dm_vgrp#/dev/}-$dm_lvol" "$vgrp"
-                add_component "/dev/mapper/${dm_vgrp#/dev/}-$dm_lvol" "lvmvol"
+                dm_prefix="/dev/mapper/${dm_vgrp#/dev/}"
+                add_dependency "$dm_prefix-$dm_lvol" "$vgrp"
+                [ -z "$pool" ] || add_dependency "$dm_prefix-$dm_lvol" "$dm_prefix-$dm_pool"
+                add_component "$dm_prefix-$dm_lvol" "lvmvol"
                 ;;
             raid)
                 name=$(echo "$remainder" | cut -d " " -f "1")
@@ -862,3 +867,5 @@ function apply_layout_mappings() {
         sed -i -r "\|$replacement|s|$replacement([0-9]+)|$target\1|g" "$file_to_migrate"
     done < "$MAPPING_FILE"
 }
+
+# vim: set et ts=4 sw=4:

usr/share/rear/prep/GNU/Linux/220_include_lvm_tools.sh
@@ -13,3 +13,9 @@ fsadm
 COPY_AS_IS=( "${COPY_AS_IS[@]}"
 /etc/lvm
 )
+
+if lvs --noheadings -o thin_count | grep -q -v "^\s*$" ; then
+    # There are Thin Pools on the system, include required binaries
+    PROGS=( "${PROGS[@]}" /usr/sbin/thin_* )
+    LIBS=( "${LIBS[@]}" /usr/lib64/*lvm2* )
+fi

commit 1cfe44dfdcc8c63f4a29c12260f03c8a7a2f60f8

Do not use global variables in diskrestore.sh
Fix a problem introduced in commits
b184194f37dd22a7e55655ff388579297239e73c and
311bfb3da1d5e47a2ff144123a2457e634f67893 (PR #1806) that shows up when
there are multiple VGs to restore.

Using variables create_thin_volumes_only and create_logical_volumes to
propagate information from VG creation to LV creation does not work well
in the case of multiple VGs, because the variables are global and if
there are multiple VGs, their values will leak from one VG to another.
The generated diskrestore.sh script does not guarantee that the LVs of a
given VG are created immediately after their VG and before creating
another VG. Currently, the script first creates all VGs and then all LVs,
so all the LVs in all VGs will see the value of create_logical_volumes
and create_thin_volumes_only from the last VG, not from their own. This
matters when different VGs behave differently (typically if one has a
thin pool and the other does not).

Fix by replacing the scalar values by arrays of VG names. If a given VG
is in the array, it is the equivalent of the former scalar value being 1
for the given VG, if it is not in the array, it is an equivalent of a
former value of 0.

For the create_volume_group variable the change is not needed, but do it
nevertheless for symmetry with other variables.

usr/share/rear/layout/prepare/GNU/Linux/110_include_lvm_code.sh
@@ -68,9 +68,9 @@ create_lvmgrp() {
     local vg=${vgrp#/dev/}
 
     cat >> "$LAYOUT_CODE" <<EOF
-create_volume_group=1
-create_logical_volumes=1
-create_thin_volumes_only=0
+create_volume_group+=( "$vg" )
+create_logical_volumes+=( "$vg" )
+create_thin_volumes_only=( \$( RmInArray "$vg" "\${create_thin_volumes_only[@]}" ) )
 
 EOF
 
@@ -97,8 +97,8 @@ if lvm vgcfgrestore -f "$VAR_DIR/layout/lvm/${vg}.cfg" $vg >&2 ; then
 
     LogPrint "Sleeping 3 seconds to let udev or systemd-udevd create their devices..."
     sleep 3 >&2
-    create_volume_group=0
-    create_logical_volumes=0
+    create_volume_group=( \$( RmInArray "$vg" "\${create_volume_group[@]}" ) )
+    create_logical_volumes=( \$( RmInArray "$vg" "\${create_logical_volumes[@]}" ) )
 
 #
 # It failed ... restore layout using 'vgcfgrestore --force', but then remove Thin volumes, they are broken
@@ -121,8 +121,8 @@ elif lvm vgcfgrestore --force -f "$VAR_DIR/layout/lvm/${vg}.cfg" $vg >&2 ; then
     sleep 3 >&2
 
     # All logical volumes have been created, except Thin volumes and pools
-    create_volume_group=0
-    create_thin_volumes_only=1
+    create_volume_group=( \$( RmInArray "$vg" "\${create_volume_group[@]}" ) )
+    create_thin_volumes_only+=( "$vg" )
  
 #
 # It failed also ... restore using 'vgcreate/lvcreate' commands
@@ -138,7 +138,7 @@ EOF
     local -a devices=($(awk "\$1 == \"lvmdev\" && \$2 == \"$vgrp\" { print \$3 }" "$LAYOUT_FILE"))
 
 cat >> "$LAYOUT_CODE" <<EOF
-if [ \$create_volume_group -eq 1 ] ; then
+if IsInArray $vg "\${create_volume_group[@]}" ; then
     LogPrint "Creating LVM VG '$vg' (some properties may not be preserved)"
     lvm vgremove --force --force --yes $vg >&2 || true
     if [ -e "$vgrp" ] ; then
@@ -241,9 +241,9 @@ create_lvmvol() {
     local warnraidline
 
     if [ $is_thin -eq 0 ] ; then
-        ifline="if [ \"\$create_logical_volumes\" -eq 1 ] && [ \"\$create_thin_volumes_only\" -eq 0 ] ; then"
+        ifline="if IsInArray $vg \"\${create_logical_volumes[@]}\" && ! IsInArray $vg \"\${create_thin_volumes_only[@]}\" ; then"
     else
-        ifline="if [ \"\$create_logical_volumes\" -eq 1 ] ; then"
+        ifline="if IsInArray $vg \"\${create_logical_volumes[@]}\" ; then"
     fi
 
     if [ $is_raidunknown -eq 1 ]; then