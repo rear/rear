093bd1bc58bf68819f731ce374469147e06c2f15
Johannes Meixner <jsmeix@suse.com>
2021-10-19 13:07:49 +0200

    Update bootloader-functions.sh

--- usr/share/rear/lib/bootloader-functions.sh
+++ usr/share/rear/lib/bootloader-functions.sh
@@ -543,7 +543,7 @@ function create_grub2_cfg {
                     # Add GRUB serial console config for real serial devices:
                     if speed=$( get_serial_device_speed $devnode ) ; then
                         # FIXME: ${devnode##/dev/ttyS} only works when $devnode is of the form /dev/ttyS<number>
-                       # but get_serial_console_devices() results both /dev/ttyS[0-9]* and /dev/hvsi[0-9]*
+                        # but get_serial_console_devices() results both /dev/ttyS[0-9]* and /dev/hvsi[0-9]*
                         # and the other options "--word=8 --parity=no --stop=1" are hardcoded:
                         echo "serial --unit=${devnode##/dev/ttyS} --speed=$speed --word=8 --parity=no --stop=1"
                         # Use the first one and skip the rest to avoid that the last 'serial' line wins in GRUB:
