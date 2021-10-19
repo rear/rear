5d2449bd0d89a2fd8f0f203e3bd87efad6a97a43
Johannes Meixner <jsmeix@suse.com>
2021-10-19 13:00:49 +0200

    Update 300_create_extlinux.sh

--- usr/share/rear/output/USB/Linux-i386/300_create_extlinux.sh
+++ usr/share/rear/output/USB/Linux-i386/300_create_extlinux.sh
@@ -262,17 +262,17 @@ echo "$VERSION_INFO" >$BUILD_DIR/outputfs/$SYSLINUX_PREFIX/message
 # default functionality
 Log "Creating $SYSLINUX_PREFIX/extlinux.conf"
 {
-    # Enable serial console, unless explicitly disabled
-    if [[ "$USE_SERIAL_CONSOLE" =~ ^[yY1] ]]; then
-        for devnode in $(get_serial_console_devices); do
-            # Not sure if using all serial devices do screw up syslinux in general
+    # Enable serial console:
+    # For the SERIAL directive to work properly, it must be the first directive in the configuration file,
+    # see "SERIAL port" at https://wiki.syslinux.org/wiki/index.php?title=SYSLINUX
+    if is_true "$USE_SERIAL_CONSOLE" ; then
+        for devnode in $( get_serial_console_devices ) ; do
+            # Not sure if using all serial devices do screw up SYSLINUX in general
             # for me listing more then one serial line in the config screwed it
             # see https://github.com/rear/rear/pull/2650
-            if [ -z $SERIAL_CONSOLE_DEVICE_SYSLINUX ] || [[ $SERIAL_CONSOLE_DEVICE_SYSLINUX == $devnode ]]; then
-                speed=$(stty -F $devnode 2>/dev/null | awk '/^speed / { print $2 }')
-                if [ "$speed" ]; then
-                    syslinux_write "serial ${devnode##/dev/ttyS} $speed"
-                fi
+            if [ -z $SERIAL_CONSOLE_DEVICE_SYSLINUX ] || [[ $SERIAL_CONSOLE_DEVICE_SYSLINUX == $devnode ]] ; then
+                # Add SYSLINUX serial console config for real serial devices:
+                speed=$( get_serial_device_speed $devnode ) && syslinux_write "serial ${devnode##/dev/ttyS} $speed"
             fi
         done
     fi
