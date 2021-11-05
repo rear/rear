d7a121c3689be2bf7386e59c7b1c961dde7fa356
Johannes Meixner <jsmeix@suse.com>
2021-11-05 12:46:37 +0100

    Update 480_initialize_write_protect_settings.sh
    Use IDs instead of UUIDs

--- usr/share/rear/prep/USB/default/480_initialize_write_protect_settings.sh
+++ usr/share/rear/prep/USB/default/480_initialize_write_protect_settings.sh
@@ -3,17 +3,19 @@
 # cf. https://github.com/rear/rear/issues/1271
 # This code registers the USB output device as write protected.
 
-# All available UUIDs of the ReaR recovery system disk (i.e. USB_DEVICE)
-# are added to the WRITE_PROTECTED_UUIDS array.
-# UUIDs are those that 'lsblk' reports (which depends on the lsblk version):
+# All available IDs of the ReaR recovery system disk (i.e. USB_DEVICE)
+# are added to the WRITE_PROTECTED_IDS array.
+# IDs are those that 'lsblk' reports (which depends on the lsblk version):
 #       UUID filesystem UUID
 #     PTUUID partition table identifier (usually UUID)
 #   PARTUUID partition UUID
-local uuids
-uuids="$( write_protection_uuids "$USB_DEVICE" )"
-# uuids is a string of UUIDs separated by newline characters so quoting for 'test' is required
-# but no quoting to add them to the array to get each UUID as a separated array element:
-test "$uuids" && WRITE_PROTECTED_UUIDS+=( $uuids ) || LogPrintError "Cannot write protect USB disk '$USB_DEVICE' via UUID (no UUID found)"
+#     SERIAL disk serial number
+#        WWN unique storage identifier
+local ids
+ids="$( write_protection_ids "$USB_DEVICE" )"
+# ids is a string of IDs separated by newline characters so quoting for 'test' is required
+# but no quoting to add them to the array to get each ID as a separated array element:
+test "$ids" && WRITE_PROTECTED_IDS+=( $ids ) || LogPrintError "Cannot write protect USB disk '$USB_DEVICE' via ID (no ID found)"
 
 # All available file system labels of the ReaR recovery system disk (i.e. USB_DEVICE)
 # are added to the WRITE_PROTECTED_FS_LABEL_PATTERNS array.
