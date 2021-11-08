8d1e912766832bdfff0f9a2287871f612a87f527
Johannes Meixner <jsmeix@suse.com>
2021-11-08 17:17:47 +0100

    Update 480_initialize_write_protect_settings.sh
    Corrected comments in prep/USB/default/480_initialize_write_protect_settings.sh
    how things actually work plus DebugPrint when USB disks get write protected
    so "rear -D mkrescue/mkbackup" output looks now like (excerpt):
    --------
    USB disk IDs of '/dev/disk/by-label/MY-DATA' added to WRITE_PROTECTED_IDS
    File system label of '/dev/disk/by-label/MY-DATA' added to WRITE_PROTECTED_FS_LABEL_PATTERNS
    --------

--- usr/share/rear/prep/USB/default/480_initialize_write_protect_settings.sh
+++ usr/share/rear/prep/USB/default/480_initialize_write_protect_settings.sh
@@ -3,21 +3,27 @@
 # cf. https://github.com/rear/rear/issues/1271
 # This code registers the USB output device as write protected.
 
-# All available IDs of the ReaR recovery system disk (i.e. USB_DEVICE)
-# are added to the WRITE_PROTECTED_IDS array.
-# IDs are those that 'lsblk' reports (which depends on the lsblk version):
-#       UUID filesystem UUID
-#     PTUUID partition table identifier (usually UUID)
-#   PARTUUID partition UUID
-#        WWN unique storage identifier
+# The values of the 'lsblk' output columns in WRITE_PROTECTED_ID_TYPES
+# of the ReaR recovery system disk (parent of USB_DEVICE) are automatically added
+# to the WRITE_PROTECTED_IDS array during "rear mkrescue/mkbackup".
+# The default WRITE_PROTECTED_ID_TYPES are UUID PTUUID PARTUUID WWN.
 local ids
 ids="$( write_protection_ids "$USB_DEVICE" )"
 # ids is a string of IDs separated by newline characters so quoting for 'test' is required
 # but no quoting to add them to the array to get each ID as a separated array element:
-test "$ids" && WRITE_PROTECTED_IDS+=( $ids ) || LogPrintError "Cannot write protect USB disk '$USB_DEVICE' via ID (no ID found)"
+if test "$ids" ; then
+    WRITE_PROTECTED_IDS+=( $ids )
+    DebugPrint "USB disk IDs of '$USB_DEVICE' added to WRITE_PROTECTED_IDS"
+else
+    LogPrintError "Cannot write protect USB disk of '$USB_DEVICE' via ID (no ID found)"
+fi
 
-# All available file system labels of the ReaR recovery system disk (i.e. USB_DEVICE)
-# are added to the WRITE_PROTECTED_FS_LABEL_PATTERNS array.
+# The file system label of the ReaR data partition (i.e. USB_DEVICE) on the ReaR recovery system disk
+# is automatically added to WRITE_PROTECTED_FS_LABEL_PATTERNS during "rear mkrescue/mkbackup".
 # Empty lines in the lsblk output get automatically ignored (i.e. no empty array elements get added)
 # and we do not alert the user via LogPrintError because file system labels are optional:
-WRITE_PROTECTED_FS_LABEL_PATTERNS+=( $( lsblk -ino LABEL "$USB_DEVICE" ) ) || DebugPrint "Cannot write protect USB disk '$USB_DEVICE' via file system label (none found)"
+if WRITE_PROTECTED_FS_LABEL_PATTERNS+=( $( lsblk -ino LABEL "$USB_DEVICE" ) ) ; then
+    DebugPrint "File system label of '$USB_DEVICE' added to WRITE_PROTECTED_FS_LABEL_PATTERNS"
+else
+    DebugPrint "Cannot write protect USB disk of '$USB_DEVICE' via file system label (none found)"
+fi
