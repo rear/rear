d5505e6d4a404b70db6f7552c92bdfd1cdb66b04
bwelterl <bwelterl@redhat.com>
2021-11-19 15:09:24 +0100

    Add a prerelabel with setfiles before the first reboot, because autorelabel is not always reliable

--- usr/share/rear/restore/default/500_selinux_autorelabel.sh
+++ usr/share/rear/restore/default/500_selinux_autorelabel.sh
@@ -12,3 +12,22 @@ is_true "$BACKUP_SELINUX_DISABLE" || return 0
 # Force SELinux relabeling of all files after reboot of the recovered system:
 touch $TARGET_FS_ROOT/.autorelabel
 LogPrint "Created SELinux $TARGET_FS_ROOT/.autorelabel file : after reboot SELinux will relabel all files"
+
+# If we are in enforcing, we should try to relabel before the reboot, because if some files
+# are not correctly labelled, system may not be able to start the autorelabel service (ie: /etc/localtime)
+
+test $( grep "SELINUX=enforcing" $TARGET_FS_ROOT/etc/selinux/config ) || return 0
+
+local policy=$( grep "^SELINUXTYPE" $TARGET_FS_ROOT/etc/selinux/config | sed 's/SELINUXTYPE=//' )
+
+Logprint "We try to restore the selinux labels before the first reboot because the configuration is enforcing and autorelabel may fail. \n
+        This can take a several minutes."
+
+if [[ -d "$TARGET_FS_ROOT/etc/selinux/${policy}/" ]] ; then
+     # setfiles -c $TARGET_FS_ROOT/etc/selinux/${policy}/policy/policy.*  $TARGET_FS_ROOT/etc/selinux/${policy}/contexts/files/file_contexts
+     setfiles -r $TARGET_FS_ROOT $TARGET_FS_ROOT/etc/selinux/${policy}/contexts/files/file_contexts $TARGET_FS_ROOT
+else
+     LogPrint "The configured selinux policy $policy is not accessbible in default path $TARGET_FS_ROOT/etc/selinux/${policy}/. \n
+     If the first boot fails, please add 'enforcing=0' on kernel command line, and an autorelabel should fix the labels."
+fi
+
