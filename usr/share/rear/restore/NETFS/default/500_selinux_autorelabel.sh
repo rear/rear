bde2da1ba7d5f54373ecd218b48dc50e7c9c817b
bwelterl <32841830+bwelterl@users.noreply.github.com>
2021-11-22 09:53:54 +0100

    Update 500_selinux_autorelabel.sh
    - Switch the setfiles command into the chroot to avoid the inclusion of the binary in the initramfs

--- usr/share/rear/restore/NETFS/default/500_selinux_autorelabel.sh
+++ usr/share/rear/restore/NETFS/default/500_selinux_autorelabel.sh
@@ -28,7 +28,7 @@ if test -f "$opath/selinux.autorelabel" ; then
 
     if [[ -d "$TARGET_FS_ROOT/etc/selinux/${policy}/" ]] ; then
          #setfiles -c $TARGET_FS_ROOT/etc/selinux/${policy}/policy/policy.*  $TARGET_FS_ROOT/etc/selinux/${policy}/contexts/files/file_contexts
-         setfiles -r $TARGET_FS_ROOT $TARGET_FS_ROOT/etc/selinux/${policy}/contexts/files/file_contexts $TARGET_FS_ROOT
+         chroot $TARGET_FS_ROOT /usr/sbin/setfiles /etc/selinux/${policy}/contexts/files/file_contexts /
     else
         LogPrint "The configured selinux policy $policy is not accessbible in default path $TARGET_FS_ROOT/etc/selinux/${policy}/. \n
         If the first boot fails, please add 'enforcing=0' on kernel command line, and an autorelabel should fix the labels."
