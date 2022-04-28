0d76d6b4da44a7ec21f4b8e47a6cc831af1ac5f5
Pavel Cahyna <pcahyna@redhat.com>
2022-04-28 15:37:11 +0200

    Improve the visibility of checksum errors
    Prefix the message about mismatch between layout and restored files
    by "Error:", as it is probably a serious problem that may require manual
    fixup.
    
    Show the list of changed files displayed by md5sum and also save it in
    the log.

--- usr/share/rear/finalize/default/060_compare_files.sh
+++ usr/share/rear/finalize/default/060_compare_files.sh
@@ -1,6 +1,6 @@
 if [ -e $VAR_DIR/layout/config/files.md5sum ] ; then
-    if ! chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum ; then
-        LogPrintError "Some configuration files in the restored system do not match the saved layout!"
+    if ! chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 ) ; then
+        LogPrintError "Error: Restored files do not match the recreated system in $TARGET_FS_ROOT"
         return 1
     fi
 fi
