8c847f7b17d905d769acdf44dcb6d914cbe49c4a
Johannes Meixner <jsmeix@suse.com>
2022-05-04 13:12:50 +0200

    Update 450_check_network_files.sh
    In layout/save/default/450_check_network_files.sh
    added bash globbing characters [] around the first letter to ensure
    that with 'shopt -s nullglob' files that do not exist will not appear
    so nonexistent files are not appended to CHECK_CONFIG_FILES
    cf. https://github.com/rear/rear/pull/2796#issuecomment-1117171070

--- usr/share/rear/layout/save/default/450_check_network_files.sh
+++ usr/share/rear/layout/save/default/450_check_network_files.sh
@@ -1,6 +1,12 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
+
+# No quoting of the elements that are appended to the CHECK_CONFIG_FILES array together with
+# the bash globbing characters like '*' or the [] around the first letter make sure
+# that with 'shopt -s nullglob' files that do not exist will not appear
+# so nonexistent files are not appended to CHECK_CONFIG_FILES
+# cf. https://github.com/rear/rear/pull/2796#issuecomment-1117171070
 if [[ -d /etc/sysconfig/network ]] ; then
     # SUSE
     CHECK_CONFIG_FILES+=( /etc/sysconfig/network/ifcfg-* )
@@ -9,6 +15,6 @@ elif [[ -d /etc/sysconfig/network-scripts ]] ; then
     CHECK_CONFIG_FILES+=( /etc/sysconfig/network-scripts/ifcfg-* )
 elif [[ -d /etc/network ]] ; then
     # Debian
-    CHECK_CONFIG_FILES+=( /etc/network/interfaces )
+    CHECK_CONFIG_FILES+=( /[e]tc/network/interfaces )
 fi
 
