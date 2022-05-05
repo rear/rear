b4d537c53fbee16f0328fdace52f75ea95165a2a
Gratien D'haese <gratien.dhaese@gmail.com>
2022-05-05 11:46:22 +0200

    Comment out remote space check with rsync #2760
    Signed-off-by: Gratien D'haese <gratien.dhaese@gmail.com>

--- usr/share/rear/backup/RSYNC/default/450_calculate_req_space.sh
+++ usr/share/rear/backup/RSYNC/default/450_calculate_req_space.sh
@@ -13,21 +13,22 @@ while read -r ; do
 done < $TMP_DIR/backup-include.txt
 LogPrint "Estimated size of local file systems is $(( _local_size / 1024 )) MB"
 
-case $RSYNC_PROTO in
-    (ssh)
-        LogPrint "Calculating size of $RSYNC_HOST:$RSYNC_PATH"
-        ssh -l $RSYNC_USER $RSYNC_HOST "df -P $RSYNC_PATH" >$TMP_DIR/rs_size
-        StopIfError "Failed to determine size of $RSYNC_PATH"
-        _div=1 # 1024-blocks
-        grep -q "512-blocks" $TMP_DIR/rs_size && _div=2 # HPUX: divide with 2 to get kB size
-        _remote_size=$( tail -n 1 $TMP_DIR/rs_size | awk '{print $2}' )
-        _remote_size=$(( _remote_size / _div ))
-        [[ $_remote_size -gt $_local_size ]]
-        StopIfError "Not enough disk space available on $RSYNC_HOST:$RSYNC_PATH ($_remote_size < $_local_size)"
-        ;;
-    (rsync)
-        # TODO: how can we calculate the free size on remote system via rsync protocol??
-        :
-        ;;
-esac
+# Commenting out next block according decision of issue #2760
+#case $RSYNC_PROTO in
+#    (ssh)
+#        LogPrint "Calculating size of $RSYNC_HOST:$RSYNC_PATH"
+#        ssh -l $RSYNC_USER $RSYNC_HOST "df -P $RSYNC_PATH" >$TMP_DIR/rs_size
+#        StopIfError "Failed to determine size of $RSYNC_PATH"
+#        _div=1 # 1024-blocks
+#        grep -q "512-blocks" $TMP_DIR/rs_size && _div=2 # HPUX: divide with 2 to get kB size
+#        _remote_size=$( tail -n 1 $TMP_DIR/rs_size | awk '{print $2}' )
+#        _remote_size=$(( _remote_size / _div ))
+#        [[ $_remote_size -gt $_local_size ]]
+#        StopIfError "Not enough disk space available on $RSYNC_HOST:$RSYNC_PATH ($_remote_size < $_local_size)"
+#        ;;
+#    (rsync)
+#        # TODO: how can we calculate the free size on remote system via rsync protocol??
+#        :
+#        ;;
+#esac
 
