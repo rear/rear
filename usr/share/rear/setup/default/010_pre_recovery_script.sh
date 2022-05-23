f3700b94d8d2186dd181a5d68b4de6c785cf4722
Johannes Meixner <jsmeix@suse.com>
2022-05-23 15:21:41 +0200

    Update 010_pre_recovery_script.sh
    In usr/share/rear/setup/default/010_pre_recovery_script.sh
    call PRE_RECOVERY_COMMANDS before PRE_RECOVERY_SCRIPT
    so PRE_RECOVERY_COMMANDS can also be used
    to prepare things for PRE_RECOVERY_SCRIPT

--- usr/share/rear/setup/default/010_pre_recovery_script.sh
+++ usr/share/rear/setup/default/010_pre_recovery_script.sh
@@ -1,4 +1,14 @@
+
+# The PRE_RECOVERY_COMMANDS are called directly before the PRE_RECOVERY_SCRIPT
+# so PRE_RECOVERY_COMMANDS can also be used to prepare things for the PRE_RECOVERY_SCRIPT:
+
+local command
+for command in "${PRE_RECOVERY_COMMANDS[@]}" ; do
+    Log "Running PRE_RECOVERY_COMMANDS '$command'"
+    eval "$command"
+done
+
 if test "$PRE_RECOVERY_SCRIPT" ; then
-	Log "Running PRE_RECOVERY_SCRIPT '${PRE_RECOVERY_SCRIPT[@]}'"
-	eval "${PRE_RECOVERY_SCRIPT[@]}"
+    Log "Running PRE_RECOVERY_SCRIPT '${PRE_RECOVERY_SCRIPT[@]}'"
+    eval "${PRE_RECOVERY_SCRIPT[@]}"
 fi
