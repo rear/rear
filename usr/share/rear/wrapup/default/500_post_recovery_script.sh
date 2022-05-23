ae0ab2c343cf9572cc15ffcde4147379016c3972
Johannes Meixner <jsmeix@suse.com>
2022-05-23 15:25:14 +0200

    Update 500_post_recovery_script.sh
    In usr/share/rear/wrapup/default/500_post_recovery_script.sh
    call POST_RECOVERY_COMMANDS directly after POST_RECOVERY_SCRIPT
    so POST_RECOVERY_COMMANDS can also be used
    to clean up things after POST_RECOVERY_SCRIPT

--- usr/share/rear/wrapup/default/500_post_recovery_script.sh
+++ usr/share/rear/wrapup/default/500_post_recovery_script.sh
@@ -1,4 +1,14 @@
+
+# The POST_RECOVERY_COMMANDS are called directly after the POST_RECOVERY_SCRIPT
+# so POST_RECOVERY_COMMANDS can also be used to clean up things after the POST_RECOVERY_SCRIPT:
+
 if test "$POST_RECOVERY_SCRIPT" ; then
-	Log "Running POST_RECOVERY_SCRIPT '${POST_RECOVERY_SCRIPT[@]}'"
-	eval "${POST_RECOVERY_SCRIPT[@]}"
+    Log "Running POST_RECOVERY_SCRIPT '${POST_RECOVERY_SCRIPT[@]}'"
+    eval "${POST_RECOVERY_SCRIPT[@]}"
 fi
+
+local command
+for command in "${POST_RECOVERY_COMMANDS[@]}" ; do
+    Log "Running POST_RECOVERY_COMMANDS '$command'"
+    eval "$command"
+done
