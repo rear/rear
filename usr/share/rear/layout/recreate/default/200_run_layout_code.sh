f4adfc0cc399bba7cdbe5c734e4ef6a3086424d2
Johannes Meixner <jsmeix@suse.com>
2022-10-04 11:54:42 +0200

    Implement basic 'barrel' support
    In layout/recreate/default/200_run_layout_code.sh
    add "Recreating storage layout with 'barrel' devicegraph"
    as optional alternative to recreating with diskrestore.sh
    when the config variable BARREL_DEVICEGRAPH is 'true'
    plus an option to switch from 'barrel' back to diskrestore.sh.
    Additionally there are the new options
    "Show what is currently on the disks ('lsblk' block devices list)"
    and "Again wipe those disks ..." in DISKS_TO_BE_WIPED
    to check after a failed attempt what was recreated and
    to redo the recreation from scratch on clean disks.

--- usr/share/rear/layout/recreate/default/200_run_layout_code.sh
+++ usr/share/rear/layout/recreate/default/200_run_layout_code.sh
@@ -29,10 +29,23 @@ unset choices
 choices[0]="Rerun disk recreation script ($LAYOUT_CODE)"
 choices[1]="View '$rear_workflow' log file ($RUNTIME_LOGFILE)"
 choices[2]="Edit disk recreation script ($LAYOUT_CODE)"
-choices[3]="View original disk space usage ($original_disk_space_usage_file)"
-choices[4]="Use Relax-and-Recover shell and return back to here"
-choices[5]="Abort '$rear_workflow'"
+choices[3]="Again wipe those disks: $DISKS_TO_BE_WIPED"
+choices[4]="Show what is currently on the disks ('lsblk' block devices list)"
+choices[5]="View original disk space usage ($original_disk_space_usage_file)"
+choices[6]="Use Relax-and-Recover shell and return back to here"
+choices[7]="Abort '$rear_workflow'"
 prompt="The disk layout recreation script failed"
+# Do not show choice to wipe disks when whiping disks is switched off:
+is_false "$DISKS_TO_BE_WIPED" && choices[3]="n/a"
+# When 'barrel' is used to recreate the storage layout different choices must be shown:
+BARREL_DEVICEGRAPH_DIR="$VAR_DIR/layout/barrel"
+BARREL_DEVICEGRAPH_FILE=$BARREL_DEVICEGRAPH_DIR/devicegraph.xml
+if is_true "$BARREL_DEVICEGRAPH" ; then
+    DebugPrint "Recreating storage layout with 'barrel' devicegraph ($BARREL_DEVICEGRAPH_FILE)"
+    choices[0]="Rerun 'barrel load devicegraph' ($BARREL_DEVICEGRAPH_FILE)"
+    choices[2]="Switch to recreating with disk recreation script ($LAYOUT_CODE)"
+fi
+
 choice=""
 # When USER_INPUT_LAYOUT_CODE_RUN has any 'true' value be liberal in what you accept and
 # assume choices[0] 'Rerun disk recreation script' was actually meant:
@@ -50,12 +63,34 @@ confirm_choice=""
 is_true "$USER_INPUT_LAYOUT_MIGRATED_CONFIRMATION" && USER_INPUT_LAYOUT_MIGRATED_CONFIRMATION="${confirm_choices[0]}"
 
 # Run the disk layout recreation code (diskrestore.sh)
+# or recreate storage layout with 'barrel' devicegraph
 # again and again until it succeeds or the user aborts:
 while true ; do
-    # Run LAYOUT_CODE in a sub-shell because it sets 'set -e'
-    # so that it exits the running shell in case of an error
-    # but that exit must not exit this running bash here:
-    ( source $LAYOUT_CODE )
+    if is_true "$BARREL_DEVICEGRAPH" ; then
+        # See https://github.com/rear/rear/pull/2382#discussion_r417852393
+        # and https://github.com/rear/rear/pull/2382#discussion_r417998820
+        # and https://github.com/rear/rear/pull/2382#discussion_r418018571
+        # that explains the reasoning behind the stdin stdout stderr redirection plus process substitution in
+        #   COMMAND 0<&6 1>> >( tee -a $RUNTIME_LOGFILE 1>&7 ) 2>> >( tee -a $RUNTIME_LOGFILE 1>&8 )
+        # which is used to show 'barrel' messges on the user's terminal and also have them in the log file.
+        # It is also used to be on the save side if 'barrel' behaves interactively (regardless of '--yes').
+        # Quoting "$BARREL_MAPPING_FILE" is needed because 'test -s' would falsely succeed with an empty argument:
+        if test -s "$BARREL_MAPPING_FILE" ; then
+            # barrel --verbose --yes --prefix /mnt/local load devicegraph --name /etc/barrel-devicegraph.xml --mapping /etc/barrel-mapping.json
+            barrel --verbose --yes --prefix $TARGET_FS_ROOT load devicegraph --name $BARREL_DEVICEGRAPH_FILE --mapping $BARREL_MAPPING_FILE 0<&6 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
+        else
+            # barrel --verbose --yes --prefix /mnt/local load devicegraph --name /etc/barrel-devicegraph.xml
+            barrel --verbose --yes --prefix $TARGET_FS_ROOT load devicegraph --name $BARREL_DEVICEGRAPH_FILE 0<&6 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
+        fi
+    else
+        # After switching to recreating with disk recreation script
+        # change choices[0] from "Run ..." to "Rerun ...":
+        choices[0]="Rerun disk recreation script ($LAYOUT_CODE)"
+        # Run LAYOUT_CODE in a sub-shell because it sets 'set -e'
+        # so that it exits the running shell in case of an error
+        # but that exit must not exit this running bash here:
+        ( source $LAYOUT_CODE )
+    fi
     # One must explicitly test whether or not $? is zero in a separated bash command
     # because with bash 3.x and bash 4.x code like
     #   # ( set -e ; cat qqq ; echo "hello" ) && echo ok || echo failed
@@ -91,6 +126,19 @@ while true ; do
         # When LAYOUT_CODE succeeded in migration mode
         # let the user explicitly confirm the recreated (and usually migrated) disk layout
         # before continuing the "rear recover" workflow with restoring the backup.
+        # Show the recreated disk layout to the user on his terminal (and also in the log file):
+        LogPrint "Recreated storage layout:"
+        # First try the command (which works on SLES12 and SLES15)
+        #   lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,LABEL,SIZE,MOUNTPOINT
+        # but on older systems (like SLES11) that do not support all that lsblk things
+        # cf. https://github.com/rear/rear/pull/2626#issuecomment-856700823
+        # try the simpler command
+        #   lsblk -io NAME,KNAME,FSTYPE,LABEL,SIZE,MOUNTPOINT
+        # and as fallback try 'lsblk -i' and finally try plain 'lsblk'.
+        # In contrast to the lsblk commands in layout/save/GNU/Linux/100_create_layout_file.sh
+        # skip UUID and WWN because they are not scrictly needed to check if things look OK
+        # but those columns make the output lines too long for a normal terminal:
+        { lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,LABEL,SIZE,MOUNTPOINT || lsblk -io NAME,KNAME,FSTYPE,LABEL,SIZE,MOUNTPOINT || lsblk -i || lsblk ; } 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
         # Run an inner while loop with a user dialog so that the user can inspect the recreated disk layout
         # and perhaps even manually fix the recreated disk layout if it is not what the user wants
         # (e.g. by using the Relax-and-Recover shell and returning back to this user dialog):
@@ -127,8 +175,13 @@ while true ; do
         choice="$( UserInput -I LAYOUT_CODE_RUN -p "$prompt" -D "${choices[0]}" "${choices[@]}" )" && wilful_input="yes" || wilful_input="no"
         case "$choice" in
             (${choices[0]})
-                # Rerun disk recreation script:
-                is_true "$wilful_input" && LogPrint "User reruns disk recreation script" || LogPrint "Rerunning disk recreation script by default"
+                if is_true "$BARREL_DEVICEGRAPH" ; then
+                    # Rerun 'barrel load devicegraph':
+                    is_true "$wilful_input" && LogPrint "User runs 'barrel load devicegraph'" || LogPrint "Running 'barrel load devicegraph' by default"
+                else
+                    # Rerun or run (after switching to recreating with disk recreation script) disk recreation script:
+                    is_true "$wilful_input" && LogPrint "User runs disk recreation script" || LogPrint "Running disk recreation script by default"
+                fi
                 # Only break the inner while loop (i.e. the user dialog loop):
                 break
                 ;;
@@ -137,18 +190,52 @@ while true ; do
                 less $RUNTIME_LOGFILE 0<&6 1>&7 2>&8
                 ;;
             (${choices[2]})
-                # Run 'vi' with the original STDIN STDOUT and STDERR when 'rear' was launched by the user:
-                vi $LAYOUT_CODE 0<&6 1>&7 2>&8
+                if is_true "$BARREL_DEVICEGRAPH" ; then
+                    BARREL_DEVICEGRAPH="false"
+                    choices[0]="Run disk recreation script ($LAYOUT_CODE)"
+                    choices[2]="Edit disk recreation script ($LAYOUT_CODE)"
+                    LogPrint "Switched to recreating with disk recreation script ($LAYOUT_CODE)"
+                    is_false "$DISKS_TO_BE_WIPED" || LogPrint "You may need to wipe disks again before running the disk recreation script"
+                else
+                    # Run 'vi' with the original STDIN STDOUT and STDERR when 'rear' was launched by the user:
+                    vi $LAYOUT_CODE 0<&6 1>&7 2>&8
+                fi
                 ;;
             (${choices[3]})
+                if is_false "$DISKS_TO_BE_WIPED" ; then
+                    LogPrint "Not applicable (DISKS_TO_BE_WIPED is 'false')" 
+                else
+                    # Again wipe disks:
+                    # Before wiping disks umount all and turn off all swap
+                    # because after 'barrel' recreated storage layout things are mounted and swap is on:
+                    umount -v -a 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
+                    swapoff -v -a 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
+                    Source $SHARE_DIR/layout/recreate/default/150_wipe_disks.sh
+                fi
+                ;;
+            (${choices[4]})
+                LogPrint "This is currently on the disks:"
+                # First try the command (which works on SLES12 and SLES15)
+                #   lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,LABEL,SIZE,MOUNTPOINT
+                # but on older systems (like SLES11) that do not support all that lsblk things
+                # cf. https://github.com/rear/rear/pull/2626#issuecomment-856700823
+                # try the simpler command
+                #   lsblk -io NAME,KNAME,FSTYPE,LABEL,SIZE,MOUNTPOINT
+                # and as fallback try 'lsblk -i' and finally try plain 'lsblk'.
+                # In contrast to the lsblk commands in layout/save/GNU/Linux/100_create_layout_file.sh
+                # skip UUID and WWN because they are not scrictly needed to check if things look OK
+                # but those columns make the output lines too long for a normal terminal:
+                { lsblk -ipo NAME,KNAME,PKNAME,TRAN,TYPE,FSTYPE,LABEL,SIZE,MOUNTPOINT || lsblk -io NAME,KNAME,FSTYPE,LABEL,SIZE,MOUNTPOINT || lsblk -i || lsblk ; } 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 )
+                ;;
+            (${choices[5]})
                 # Run 'less' with the original STDIN STDOUT and STDERR when 'rear' was launched by the user:
                 less $original_disk_space_usage_file 0<&6 1>&7 2>&8
                 ;;
-            (${choices[4]})
+            (${choices[6]})
                 # rear_shell runs 'bash' with the original STDIN STDOUT and STDERR when 'rear' was launched by the user:
                 rear_shell "" "$rear_shell_history"
                 ;;
-            (${choices[5]})
+            (${choices[7]})
                 abort_recreate
                 Error "User chose to abort '$rear_workflow' in ${BASH_SOURCE[0]}"
                 ;;
