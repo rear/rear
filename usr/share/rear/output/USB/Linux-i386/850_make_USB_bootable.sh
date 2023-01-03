986c1b5ccea5e882d6555494d5d49f86b1ce2df3
Johannes Meixner <jsmeix@suse.com>
2023-01-03 10:59:57 +0100

    Install extlinux for OUTPUT=USB for a vfat boot partition
    Installing extlinux should also be working for a mounted vfat boot partition
    see https://github.com/rear/rear/issues/2884

--- usr/share/rear/output/USB/Linux-i386/850_make_USB_bootable.sh
+++ usr/share/rear/output/USB/Linux-i386/850_make_USB_bootable.sh
@@ -30,7 +30,13 @@ LogPrint "Making $RAW_USB_DEVICE bootable with syslinux/extlinux"
 # so we search for " $BUILD_DIR/outputfs " in /proc/mounts to get the filesystem (third field) where the booting related files are:
 usb_filesystem=$( grep " $BUILD_DIR/outputfs " /proc/mounts | cut -d' ' -f3 | tail -1 )
 case "$usb_filesystem" in
-    (ext?)
+    # Installing extlinux should also be working for a mounted vfat boot partition
+    # see https://github.com/rear/rear/issues/2884
+    # because https://wiki.archlinux.org/title/syslinux reads (excerpts):
+    #   For FAT, ext2/3/4, or btrfs boot partition use extlinux, where the device has been mounted:
+    #     # extlinux --install ...
+    #   Alternatively, for a FAT boot partition use syslinux, where the device is unmounted
+    (ext?|vfat)
         if [[ "$FEATURE_SYSLINUX_EXTLINUX_INSTALL" ]] ; then
             extlinux -i "$BUILD_DIR/outputfs/$SYSLINUX_PREFIX" || Error "'extlinux -i $BUILD_DIR/outputfs/$SYSLINUX_PREFIX' failed"
         else
