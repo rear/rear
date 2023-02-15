4195b948c7cdc7183c617f46f8750cb63ee93fe3
Schlomo Schapiro <schlomo@schapiro.org>
2023-02-15 01:01:28 +0100

    check for wrong array assignment in user configs

---
+++ usr/share/rear/init/default/001_verify_config_arrays.sh
@@ -0,0 +1,22 @@
+# ensure that all array variables defined in default.conf are still array variables after
+# reading the user configuration, fixes common user config error: https://github.com/rear/rear/issues/2930
+
+array_variables=($(
+    sed -n -e '/^[A-Z_]\+=(/s/=.*//p' "$SHARE_DIR"/conf/default.conf
+))
+
+
+for config in "$CONFIG_DIR"/{site,local,rescue}.conf; do
+    test -r "$config" || continue
+    for var in "${array_variables[@]}"; do
+        mapfile -t var_assignments < <(
+            sed -n -E -e "/$var\+?=/p" "$config"
+            )
+        for line in "${var_assignments[@]}"; do
+            [[ "$line" == *\(* ]] || Error "Missing array assignment like +=(...) for $var in $config:$LF$line$LF"
+        done
+    done
+done
+
+
+unset array_variables
