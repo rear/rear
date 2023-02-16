b9e21a4c1cb0494f1b172507b9dbdde5df8da2db
Schlomo Schapiro <schlomo@schapiro.org>
2023-02-16 19:54:15 +0100

    harden pattern matching to avoid false positives and catch more potential errors

--- usr/share/rear/init/default/001_verify_config_arrays.sh
+++ usr/share/rear/init/default/001_verify_config_arrays.sh
@@ -6,16 +6,19 @@
 #       Therefore it is enough to simply take all the currently defined array variables and check
 #       for any wrong assignment in the user configuration
 
-local var_assignments array_variables=($(
-    declare -p | sed -n -E -e '/^declare -a/s/declare [-arxlu]+ ([A-Za-z0-9_-]+)=.*/\1/p'
-))
+local var_assignments array_variables
 
+mapfile -t array_variables < <(
+    declare -p | sed -n -E -e '/^declare -a/s/declare [-arxlu]+ ([A-Za-z0-9_-]+)=.*/\1/p'
+)
+
+declare -p array_variables
 
 for config in "$CONFIG_DIR"/{site,local,rescue}.conf "${CONFIG_APPEND_FILES_PATHS[@]}"; do
     test -r "$config" || continue
     for var in "${array_variables[@]}"; do
         mapfile -t var_assignments < <(
-            sed -n -E -e "/$var\+?=/p" "$config"
+            sed -n -E -e "/(^|\W+)$var\+?=/p" "$config"
             )
         for line in "${var_assignments[@]}"; do
             [[ "$line" == *$var?(+)=\(* ]] || Error "Syntax error: Variable $var not assigned as Bash array in $config:$LF$line$LF"
