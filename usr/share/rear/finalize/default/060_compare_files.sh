50885c1963d0787f1e45f14ad983141e61d12654
Johannes Meixner <jsmeix@suse.com>
2023-03-07 13:45:21 +0100

    Update 060_compare_files.sh
    In finalize/default/060_compare_files.sh show what will be done
    so the user understands what possibly subsequent error messages mean,
    cf. https://github.com/rear/rear/issues/2952#issuecomment-1458094671

--- usr/share/rear/finalize/default/060_compare_files.sh
+++ usr/share/rear/finalize/default/060_compare_files.sh
@@ -1,6 +1,9 @@
-if [ -e $VAR_DIR/layout/config/files.md5sum ] ; then
-    if ! chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 ) ; then
-        LogPrintError "Error: Restored files do not match the recreated system in $TARGET_FS_ROOT"
-        return 1
-    fi
+
+# Skip when there are no checksums:
+test -s $VAR_DIR/layout/config/files.md5sum || return 0
+
+LogPrint "Checking if restored files may not match the recreated system"
+if ! chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 ) ; then
+    LogPrintError "Error: Restored files do not match the recreated system in $TARGET_FS_ROOT"
+    return 1
 fi
