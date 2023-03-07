2507822e9841bae97fd4408f9dfc0087fe495fca
Johannes Meixner <jsmeix@suse.com>
2023-03-07 14:24:16 +0100

    Update 060_compare_files.sh
    Completely overhauled finalize/default/060_compare_files.sh
    that now only shows something when there is an issue
    but now with the comprehensible error message first so
    the user understands what subsequent md5sum messages mean.

--- usr/share/rear/finalize/default/060_compare_files.sh
+++ usr/share/rear/finalize/default/060_compare_files.sh
@@ -2,8 +2,15 @@
 # Skip when there are no checksums:
 test -s $VAR_DIR/layout/config/files.md5sum || return 0
 
-LogPrint "Checking if restored files may not match the recreated system"
-if ! chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum 1>> >( tee -a "$RUNTIME_LOGFILE" 1>&7 ) 2>> >( tee -a "$RUNTIME_LOGFILE" 1>&8 ) ; then
+local md5sum_output
+# The exit status of the assignment is the exit status of the command substitution
+# which is usually the exit status of 'md5sum' that is forwared by 'chroot'
+# except 'chroot' fails (e.g. with 127 if 'md5sum' cannot be found):
+if ! md5sum_output="$( chroot $TARGET_FS_ROOT md5sum -c --quiet 2>&1 )" ; then
     LogPrintError "Error: Restored files do not match the recreated system in $TARGET_FS_ROOT"
+    # Add two spaces indentation for better readability what the md5sum output lines are.
+    # This 'sed' call must not be done in the above command substitution as a pipe
+    # because then the command substitution exit status would be the one of 'sed'.
+    LogPrintError "$( sed -e 's/^/  /' <<< "$md5sum_out" )"
     return 1
 fi
