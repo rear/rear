76214060eb3cb6c56a17ec56894ac0fc3b1a10a0
Johannes Meixner <jsmeix@suse.com>
2023-03-08 15:45:13 +0100

    Update 060_compare_files.sh
    Make it clear where the 'md5sum' stdin comes from
    (VAR_DIR/layout/config/files.md5sum in the recovery system).

--- usr/share/rear/finalize/default/060_compare_files.sh
+++ usr/share/rear/finalize/default/060_compare_files.sh
@@ -5,10 +5,13 @@ test -s $VAR_DIR/layout/config/files.md5sum || return 0
 local md5sum_output
 # The exit status of the assignment is the exit status of the command substitution
 # which is usually the exit status of 'md5sum' that is forwared by 'chroot'
-# except 'chroot' fails (e.g. with 127 if 'md5sum' cannot be found):
+# except 'chroot' fails (e.g. with 127 if 'md5sum' cannot be found).
+# Because the redirections are done before chroot is run
+# (in chroot plain 'md5sum -c --quiet' is run with redirected stdin and stderr)
+# the 'md5sum' stdin comes from VAR_DIR/layout/config/files.md5sum in the recovery system:
 if ! md5sum_output="$( chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum 2>&1 )" ; then
     LogPrintError "Error: Restored files do not match the recreated system in $TARGET_FS_ROOT"
-    # Add two spaces indentation for better readability what the md5sum output lines are.
+    # Add two spaces indentation for better readability what the 'md5sum' output lines are.
     # This 'sed' call must not be done in the above command substitution as a pipe
     # because then the command substitution exit status would be the one of 'sed'.
     LogPrintError "$( sed -e 's/^/  /' <<< "$md5sum_output" )"
