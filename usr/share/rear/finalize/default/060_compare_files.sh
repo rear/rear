d2139d2355026029b4bc75fdabe29c91f3db1587
Johannes Meixner <jsmeix@suse.com>
2023-03-14 10:44:55 +0100

    Update 060_compare_files.sh
    Prefix the reported files in the 'md5sum' output lines with '/mnt/local'.
    See https://github.com/rear/rear/pull/2954#issuecomment-1467645338
    and subsequent comments.

--- usr/share/rear/finalize/default/060_compare_files.sh
+++ usr/share/rear/finalize/default/060_compare_files.sh
@@ -12,21 +12,25 @@ local md5sum_stdout
 # (in chroot plain 'md5sum -c --quiet' is run with redirected stdin)
 # the 'md5sum' stdin comes from VAR_DIR/layout/config/files.md5sum in the recovery system.
 # The 'md5sum' stdout messages like
-#   testy/bar: FAILED
-#   testy/blub: FAILED open or read
+#   /path/to/this: FAILED
+#   /path/to/that: FAILED open or read
 # provide sufficient information so we do not output 'md5sum' stderr messages like
-#   md5sum: testy/blub: No such file or directory
+#   md5sum: /path/to/that: No such file or directory
 #   md5sum: WARNING: 1 line is improperly formatted
 #   md5sum: WARNING: 1 listed file could not be read
 #   md5sum: WARNING: 1 computed checksum did NOT match
-# on the user's terminal but have them only in the log file as usual for stderr:
+# on the user's terminal but have them only in the log file as usual via stderr:
 if ! md5sum_stdout="$( chroot $TARGET_FS_ROOT md5sum -c --quiet < $VAR_DIR/layout/config/files.md5sum )" ; then
     LogPrintError "Restored files do not fully match the recreated system in $TARGET_FS_ROOT"
     LogPrintError "(files in the backup are not same as when the ReaR rescue/recovery system was made)"
     # Add two spaces indentation for better readability what the 'md5sum' output lines are.
     # This 'sed' call must not be done in the above command substitution as a pipe
     # because then the command substitution exit status would be the one of 'sed'.
-    LogPrintError "$( sed -e 's/^/  /' <<< "$md5sum_stdout" )"
+    # Also prefix the reported files in the 'md5sum' output lines with '/mnt/local'
+    # because this is the right path for the user in the currently running ReaR recovery system
+    # for the restored files that do not match the md5sums that were saved at "rear mkrescue" time,
+    # cf. https://github.com/rear/rear/pull/2954#issuecomment-1467645338 and subsequent comments:
+    LogPrintError "$( sed -e "s|^|  $TARGET_FS_ROOT|" <<< "$md5sum_stdout" )"
     LogPrintError "Manually check if those changed files cause issues in your recreated system"
     return 1
 fi
