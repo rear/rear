be6366e7ba135e5d00bc350c96bae593d30959c0
Johannes Meixner <jsmeix@suse.com>
2023-03-28 14:14:47 +0200

    Copy the console= kernel arguments from the original system
    Overhauled the whole automated serial console setup
    for the recovery system in particular
    prep/GNU/Linux/200_include_serial_console.sh
    and lib/serial-functions.sh which results that
    rescue/GNU/Linux/400_use_serial_console.sh is obsolete,
    see https://github.com/rear/rear/pull/2749#issuecomment-1196650631

--- usr/share/rear/prep/GNU/Linux/200_include_serial_console.sh
+++ usr/share/rear/prep/GNU/Linux/200_include_serial_console.sh
@@ -1,16 +1,4 @@
 
-# If possible auto-enable serial console when not specified:
-if [[ -z "$USE_SERIAL_CONSOLE" ]] ; then
-    local devnode speed=""
-    for devnode in $( get_serial_console_devices ) ; do
-        # Enable serial console when there is at least one real serial device:
-        if speed=$( get_serial_device_speed $devnode ) ; then
-            USE_SERIAL_CONSOLE="yes"
-            break
-        fi
-    done
-fi
-
 # Always include getty or agetty as we don't know in advance whether they are needed
 # (the user may boot the recovery system with manually specified kernel options
 # to get serial console support in his recovery system).
@@ -27,5 +15,24 @@ else
     # The user must have the programs in REQUIRED_PROGS installed on his system:
     Error "Failed to find 'getty' or 'agetty' for serial console"
 fi
-
 REQUIRED_PROGS+=( "$getty_binary" stty )
+
+# Auto-enable serial console support for the recovery system
+# unless the user specified to not have serial console support:
+is_false "$USE_SERIAL_CONSOLE" && return 0
+
+# Scan the kernel command line of the currently running original system
+# and auto-enable serial console support for the recovery system
+# only if there is at least one 'console=...' option:
+local kernel_option
+for kernel_option in $( cat /proc/cmdline ) ; do
+    # Get the kernel option name (part before leftmost "="):
+    if test "${kernel_option%%=*}" = "console" ; then
+        USE_SERIAL_CONSOLE="yes"
+        # Get all 'console=...' kernel command line options
+        # copied from the currently running original system
+        # via rescue/GNU/Linux/290_kernel_cmdline.sh that runs later:
+        COPY_KERNEL_PARAMETERS+=( console )
+        break
+    fi
+done
