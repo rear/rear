369f8b886f628c9db75a63cdbb0fc6bd07294029
Johannes Meixner <jsmeix@suse.com>
2023-04-05 12:22:54 +0200

    Update 998_dump_variables.sh
    Removed duplicate space '  ' -> ' '

--- usr/share/rear/init/default/998_dump_variables.sh
+++ usr/share/rear/init/default/998_dump_variables.sh
@@ -6,6 +6,6 @@
 test "$DEBUGSCRIPTS" || return 0
 
 # Suppress output that contains 'pass' or 'key' or 'crypt' (ignore case) to skip output
-# e.g. for BACKUP_PROG_CRYPT_KEY or SSH_ROOT_PASSWORD or  LUKS_CRYPTSETUP_OPTIONS
+# e.g. for BACKUP_PROG_CRYPT_KEY or SSH_ROOT_PASSWORD or LUKS_CRYPTSETUP_OPTIONS
 # cf. https://github.com/rear/rear/issues/2967
 Debug "Runtime Configuration:$LF$( declare -p | egrep -vi 'pass|key|crypt' )"
