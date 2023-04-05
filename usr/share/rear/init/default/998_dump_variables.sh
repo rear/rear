95f30827c3b51992c9b41aef9b3cf718c82eb58c
Johannes Meixner <jsmeix@suse.com>
2023-04-05 12:17:03 +0200

    Update 998_dump_variables.sh
    Run init/default/998_dump_variables.sh
    only in debugscripts mode where the 'set -x' output in general
    already reveals possibly confidential information and additionally
    suppress output that contains 'pass' or 'key' or 'crypt' (ignore case)
    to skip output e.g. for BACKUP_PROG_CRYPT_KEY
    or SSH_ROOT_PASSWORD or  LUKS_CRYPTSETUP_OPTIONS
    cf. https://github.com/rear/rear/issues/2967

--- usr/share/rear/init/default/998_dump_variables.sh
+++ usr/share/rear/init/default/998_dump_variables.sh
@@ -1,1 +1,11 @@
-Debug "Runtime Configuration:$LF$(declare -p)"
+
+# Output all variable values into the log file
+# which may also output possibly confidential values 
+# so do this only in debugscripts mode where the 'set -x' output
+# in general already reveals possibly confidential information:
+test "$DEBUGSCRIPTS" || return 0
+
+# Suppress output that contains 'pass' or 'key' or 'crypt' (ignore case) to skip output
+# e.g. for BACKUP_PROG_CRYPT_KEY or SSH_ROOT_PASSWORD or  LUKS_CRYPTSETUP_OPTIONS
+# cf. https://github.com/rear/rear/issues/2967
+Debug "Runtime Configuration:$LF$( declare -p | egrep -vi 'pass|key|crypt' )"
