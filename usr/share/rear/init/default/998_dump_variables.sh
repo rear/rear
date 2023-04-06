65f0ad5729ce32eff0d07f4da44e93504a46d277
Johannes Meixner <jsmeix@suse.com>
2023-04-06 09:51:25 +0200

    Update 998_dump_variables.sh
    Disable init/default/998_dump_variables.sh
    because of legal liability worries until
    https://github.com/rear/rear/issues/2967 is solved

--- usr/share/rear/init/default/998_dump_variables.sh
+++ usr/share/rear/init/default/998_dump_variables.sh
@@ -5,6 +5,11 @@
 # in general already reveals possibly confidential information:
 test "$DEBUGSCRIPTS" || return 0
 
+# For now disable this script until https://github.com/rear/rear/issues/2967 is solved.
+# Users who need this script can disable the next two lines:
+DebugPrint "Skipped init/default/998_dump_variables.sh for legal liability worries, see https://github.com/rear/rear/issues/2967"
+return 1
+
 # Suppress output that contains 'pass' or 'key' or 'crypt' (ignore case) to skip output
 # e.g. for BACKUP_PROG_CRYPT_KEY or SSH_ROOT_PASSWORD or LUKS_CRYPTSETUP_OPTIONS
 # cf. https://github.com/rear/rear/issues/2967
