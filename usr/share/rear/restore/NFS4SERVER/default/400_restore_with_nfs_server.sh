f061cb23591847ed4f5a858c00e05d3779792e90
Pascal Pfluegner <pascal-pf@hotmail.de>
2023-05-03 13:31:47 +0200

    improvements; cleanup; rename NFS_SERVER to NFS4SERVER

---
+++ usr/share/rear/restore/NFS4SERVER/default/400_restore_with_nfs_server.sh
@@ -0,0 +1,19 @@
+# 400_restore_with_nfs_server.sh
+
+local check_file="$TARGET_FS_ROOT/$NFS4SERVER_RESTORE_FINISHED_FILE"
+
+LogPrint "Mount the nfs share: 'mount -t nfs <ip>:/ <destination>' and restore all files to mounted destination."
+LogPrint "Create the $check_file file when the restore is completed and umount the share."
+
+rm -f $check_file || Error "Couldn't delete restore finished file $check_file"
+
+LogPrint "Wait until $check_file was created and there is no connection on the NFS-Port 2049."
+
+# or look at /var/lib/nfs/rmtab
+local nfs_connections=1
+while [ ! -f "$check_file" ] || [ "$nfs_connections" -gt 0 ]; do
+    sleep 5
+    nfs_connections=$(ss -tanpH state established "( sport = 2049 )" | wc -l)
+done
+
+rm -f $check_file || Error "Couldn't delete restore finished file $check_file"
