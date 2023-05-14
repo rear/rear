9050c66d3d4aba286eb8004135b520b89d9a13e5
Schlomo Schapiro <schlomo+git@schapiro.org>
2023-05-14 17:30:34 +0200

    Add ability to abort NFS4SERVER restore

--- usr/share/rear/restore/NFS4SERVER/default/400_restore_with_nfs_server.sh
+++ usr/share/rear/restore/NFS4SERVER/default/400_restore_with_nfs_server.sh
@@ -1,14 +1,21 @@
-local check_file="$TARGET_FS_ROOT/$NFS4SERVER_RESTORE_FINISHED_FILE" nfs_connections used_space
+local check_file abort_file nfs_connections used_space
+
+check_file="$TARGET_FS_ROOT/$NFS4SERVER_RESTORE_FINISHED_FILE"
+abort_file="$TARGET_FS_ROOT/$NFS4SERVER_RESTORE_ABORT_FILE"
 
 LogPrint "Mount the nfs share: 'mount -t nfs <ip>:/ <destination>' and restore all files to mounted destination."
 LogPrint "Create the <destination>/$NFS4SERVER_RESTORE_FINISHED_FILE file when the restore is completed and umount the share."
 
-rm -f "$check_file" || Error "Couldn't delete restore finished file $check_file"
+rm -f "$check_file" "$abort_file" || Error "Couldn't delete restore finished file $check_file or abort file $abort_file"
 
-LogPrint "Waiting until $check_file was created and there is no connection on the NFS-Port 2049."
+LogPrint "Waiting until $check_file was created and there is no active connection on the NFS port 2049."
 
 nfs_connections=1
 while [ ! -f "$check_file" ] || [ "$nfs_connections" -gt 0 ]; do
+    if [ -f "$abort_file" ]; then
+        echo ""
+        Error "Restore aborted by abort file $abort_file. Reason given:$LF$(< "$abort_file"))"
+    fi
     used_space=$(
         df --total --local -h --exclude-type=tmpfs --exclude-type=devtmpfs | awk 'END{print $3}'
         ) && \
