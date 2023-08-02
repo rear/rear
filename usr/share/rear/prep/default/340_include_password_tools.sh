e0cbc3d1487f06009beb6bb2cbb073c9e4431fc0
Johannes Meixner <jsmeix@suse.com>
2023-08-02 12:00:48 +0200

    Update 340_include_password_tools.sh
    In prep/default/340_include_password_tools.sh
    use '2>>/dev/$SECRET_OUTPUT_DEV'

--- usr/share/rear/prep/default/340_include_password_tools.sh
+++ usr/share/rear/prep/default/340_include_password_tools.sh
@@ -1,4 +1,4 @@
 # Include tools to check TTY_ROOT_PASSWORD for terminal logins
 
 # stderr is redirected here to avoid exposing the password hash in the log file when ReaR runs in debugscript mode.
-{ [[ -n "$TTY_ROOT_PASSWORD" ]] && REQUIRED_PROGS+=( openssl ); } 2>/dev/null
+{ test "$TTY_ROOT_PASSWORD" && REQUIRED_PROGS+=( openssl ) ; } 2>>/dev/$SECRET_OUTPUT_DEV
