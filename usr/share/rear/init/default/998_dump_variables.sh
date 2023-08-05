1e171478d76cb13c87948c4b13276710ef856f61
Johannes Meixner <jsmeix@suse.com>
2023-08-05 08:36:42 +0200

    Update 998_dump_variables.sh
    Every LogSecret function call must be within '{ ... ; } 2>>/dev/$SECRET_OUTPUT_DEV'
    in particular also when there is no directly visible secret argument
    but command substitution may result confidential information like
    { LogSecret "The value is: $( COMMAND )" ; } 2>>/dev/$SECRET_OUTPUT_DEV
    because 'set -x' prints the result of COMMAND as part of the LogSecret argument.

--- usr/share/rear/init/default/998_dump_variables.sh
+++ usr/share/rear/init/default/998_dump_variables.sh
@@ -8,7 +8,10 @@
 # (like passwords, encryption keys, authentication tokens, ...)
 # this happens only if rear is run by the user with the 'expose-secrets' option
 # see https://github.com/rear/rear/issues/2967
-if LogSecret "Runtime Configuration:$LF$( declare -p )" ; then
+
+# 'declare -p' is a secret command so '$( declare -p )' is also a secret command
+# because 'set -x' prints the result of $( declare -p ) as part of the LogSecret argument:
+if { LogSecret "Runtime Configuration:$LF$( declare -p )" ; } 2>>/dev/$SECRET_OUTPUT_DEV ; then
     # Show the log file name here in any case because sbin/rear shows "Using log file ..." only in verbose mode:
     LogUserOutput "Dumped all variable values (including possibly confidential values) into $RUNTIME_LOGFILE"
 else
