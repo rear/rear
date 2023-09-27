daf1d3ebeffdcb4fceed5468cc6fd9040a19d273
Johannes Meixner <jsmeix@suse.com>
2023-09-27 12:55:51 +0200

    Update serial-functions.sh
    Removed function cmdline_add_console()

--- usr/share/rear/lib/serial-functions.sh
+++ usr/share/rear/lib/serial-functions.sh
@@ -17,7 +17,7 @@ function get_serial_console_devices () {
         console_option_value="${kernel_option#*=}"
         # Get the console option device (part before leftmost optional ',' separator) e.g. 'ttyS1' 'ttyS3' 'tty0'
         console_option_device="${console_option_value%%,*}"
-        # Continue with next kernel option when the current console option device is no serial device.
+        # Continue with next kernel option when the current console option device is no serial device (exclude 'tty0').
         # The special /dev/hvsi* devices should exist only on systems that have the HVSI driver loaded
         # (a console driver for IBM's p5 servers) cf. https://lwn.net/Articles/98442/
         [[ $console_option_device == ttyS* ]] || [[ $console_option_device == hvsi* ]] || continue
@@ -46,67 +46,15 @@ function get_serial_device_speed () {
     # Run it in a subshell so that 'set -o pipefail' does not affect the current shell and
     # it can run in a subshell because the caller of this function only needs its stdout
     # cf. the function get_root_disk_UUID in lib/bootloader-functions.sh
-    # so when stty fails the get_serial_device_speed return code is the stty exit code and not the awk exit code
-    # therefore one can call get_serial_device_speed with error checking for example like
+    # so when stty fails the get_serial_device_speed return code is the stty exit code and not the awk exit code.
+    # Therefore one can call get_serial_device_speed with error checking for example like
     # speed=$( get_serial_device_speed $serial_device ) && COMMAND_WITH_speed || COMMAND_WITHOUT_speed
-    # because the return code of variable=$( PIPE ) is the return code of the pipe,
-    # cf. how get_serial_device_speed is called in cmdline_add_console below.
+    # because the return code of variable=$( PIPE ) is the return code of the pipe
+    # cf. how get_serial_device_speed is called in lib/bootloader-functions.sh
+    # and output/USB/Linux-i386/300_create_extlinux.sh
     # Suppress stty stderr output because for most /dev/ttyS* device nodes the result is
     #   stty: /dev/ttyS...: Input/output error
     # when the device node does not belong to an actual serial device (i.e. to real serial hardware)
     # so get_serial_device_speed is also used to get those device nodes that belong to real serial devices:
     ( set -o pipefail ; stty -F $devnode 2>/dev/null | awk '/^speed / { print $2 }' )
-}
-
-# Add serial console to kernel cmdline:
-function cmdline_add_console {
-
-    BugError "function cmdline_add_console is obsoleted"
-
-    # Nothing to do when using serial console is not wanted:
-    is_true "$USE_SERIAL_CONSOLE" || return 0
-
-    # Strip existing 'console=...' kernel cmd parameters:
-    local param cmdline=""
-    for param in $KERNEL_CMDLINE ; do
-        case "$param" in
-            (console=*) ;;
-            (*) cmdline+=" $param";;
-        esac
-    done
-
-    # Add serial console config to kernel cmd line:
-    local devnode speed=""
-    if test "$SERIAL_CONSOLE_DEVICES_KERNEL" ; then
-        # When the user has specified SERIAL_CONSOLE_DEVICES_KERNEL use only that (no automatisms):
-        for devnode in $SERIAL_CONSOLE_DEVICES_KERNEL ; do
-            # devnode can be a character device node like "/dev/ttyS0" or "/dev/lp0" or "/dev/ttyUSB0"
-            # cf. https://www.kernel.org/doc/html/latest/admin-guide/serial-console.html
-            # or devnode can be a 'console=...' kernel cmd parameter like "console=ttyS1,9600"
-            if test -c "$devnode" ; then
-                if speed=$( get_serial_device_speed $devnode ) ; then
-                    cmdline+=" console=${devnode##/dev/},$speed"
-                else
-                    cmdline+=" console=${devnode##/dev/}"
-                fi
-            else
-                # When devnode is a 'console=...' kernel cmd parameter use it as specified:
-                cmdline+=" $devnode"
-            fi
-        done
-    else
-        local real_consoles=""
-        for devnode in $( get_serial_console_devices ) ; do
-            # Only add for those device nodes that belong to actual serial devices:
-            speed=$( get_serial_device_speed $devnode ) && real_consoles+=" console=${devnode##/dev/},$speed"
-        done
-        cmdline+=" $real_consoles"
-
-        # Add fallback console if no real serial device was found:
-        test "$real_consoles" || cmdline+=" console=tty0"
-    fi
-
-    # Have a trailing space to be on the safe side
-    # so that more kernel cmd parameters could be "just appended" by other scripts:
-    echo "$cmdline "
 }
