f950930e254197c3d86cd3698fc8d70fc866aa4e
Johannes Meixner <jsmeix@suse.com>
2023-09-27 13:27:00 +0200

    Update 400_use_serial_console.sh
    rescue/GNU/Linux/400_use_serial_console.sh is obsolete

--- usr/share/rear/rescue/GNU/Linux/400_use_serial_console.sh
+++ usr/share/rear/rescue/GNU/Linux/400_use_serial_console.sh
@@ -1,9 +1,1 @@
 
-DebugPrint "skipping usr/share/rear/rescue/GNU/Linux/400_use_serial_console.sh"
-return 0
-
-# Nothing to do when using serial console is not wanted:
-is_true "$USE_SERIAL_CONSOLE" || return 0
-
-KERNEL_CMDLINE=$( cmdline_add_console )
-Log "Modified kernel commandline to: '$KERNEL_CMDLINE'"
