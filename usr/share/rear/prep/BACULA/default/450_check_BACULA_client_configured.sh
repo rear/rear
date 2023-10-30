3c9398bb3a6062e0f907656a5ef6286870ff8ace
Johannes Meixner <jsmeix@suse.com>
2023-10-30 14:12:19 +0100

    Update 450_check_BACULA_client_configured.sh
    In prep/BACULA/default/450_check_BACULA_client_configured.sh
    show the command or file name to the user if one is missing
    to make it clear to the user what exactly is missing.
    This enhancement was triggered by
    https://github.com/rear/rear/issues/3060

--- usr/share/rear/prep/BACULA/default/450_check_BACULA_client_configured.sh
+++ usr/share/rear/prep/BACULA/default/450_check_BACULA_client_configured.sh
@@ -1,22 +1,22 @@
 #
-# Check that bacula is installed and configuration files exist
+# Check that Bacula is installed and that its configuration files exist
 
 if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
 
    ### Bacula support using bextract
-   has_binary bextract || Error "Bacula bextract is missing"
+   has_binary bextract || Error "Bacula 'bextract' is missing"
 
-   [ -s $BACULA_CONF_DIR/bacula-sd.conf ] || Error "Bacula configuration file (bacula-sd.conf) missing"
+   [ -s $BACULA_CONF_DIR/bacula-sd.conf ] || Error "Bacula configuration file '$BACULA_CONF_DIR/bacula-sd.conf' missing"
 
 else
 
    ### Bacula support using bconsole
-   has_binary bacula-fd || Error "Bacula File Daemon is missing"
+   has_binary bacula-fd || Error "Bacula File Daemon 'bacula-fd' is missing"
 
-   [ -s $BACULA_CONF_DIR/bacula-fd.conf ] || Error "Bacula configuration file (bacula-fd.conf) missing"
+   [ -s $BACULA_CONF_DIR/bacula-fd.conf ] || Error "Bacula configuration file '$BACULA_CONF_DIR/bacula-fd.conf' missing"
 
-   has_binary bconsole || Error "Bacula console executable is missing"
+   has_binary bconsole || Error "Bacula console executable 'bconsole' is missing"
 
-   [ -s $BACULA_CONF_DIR/bconsole.conf ] || Error "Bacula configuration file (bconsole.conf) missing"
+   [ -s $BACULA_CONF_DIR/bconsole.conf ] || Error "Bacula configuration file '$BACULA_CONF_DIR/bconsole.conf' missing"
 
 fi
