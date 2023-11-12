05d6de385722e4dd00a9febe0cc4329acad70eda
Schlomo Schapiro <schlomo@schapiro.org>
2023-11-12 22:19:26 +0100

    Initial VEEAM Bare Metal Recovery integration - Register the Veeam Linux Agent to Veeam VBRServer with Role of Veeam Restore Operator - Query latest Fullbackup, mount and copy it to the original restored operating system disks
    Sponsored by ANEO Solutions GmbH und erstellt von Andreas Schaller

---
+++ usr/share/rear/verify/VEEAM/default/400_verify_veeam.sh
@@ -0,0 +1,35 @@
+# 400_verify_veeam.sh
+# check that veeam vbrserver is actually reachable on port 10006
+test "${VEEAM_SERVER_HOSTNAME}" || Error "Define VEEAM_SERVER_HOSTNAME (hostname or IP address)"
+
+if nc -w 3 -z "$VEEAM_SERVER_HOSTNAME" 10006; then
+    LogPrint "Veeam VBR Server: '$VEEAM_SERVER_HOSTNAME' seems to be up and running."
+else
+    Error "Unable to contact Veeam VBR Server: '$VEEAM_SERVER_HOSTNAME' on port 10006"
+fi
+
+# Veeam linux client agent registration
+
+Log "Deleting Veeam SQLite Linux Agent Database"
+rm -rf $v /var/lib/veeam/*
+
+LogPrint "Starting veeamservice agent for linux"
+systemctl start veeamservice || Error "Failed to start veeamservice Agent for Linux"
+
+Log "Accept Veeam EULA agreement"
+# create directories for EULA agreement
+mkdir -p $v /usr/share/doc/veeam/
+touch $v /usr/share/doc/veeam/EULA
+touch $v /usr/share/doc/veeam/3rdPartyNotices.txt
+
+Log "Query available Veeam VBR Server to trigger license agreement"
+yes yes | veeamconfig vbrServer list 1>/dev/null || Error "Unable to query a Veeam VBR server"
+
+LogPrint "Registering Veeam linux client agent to Veeam VBR backup server"
+if test "$VEEAM_USER" && { test "$VEEAM_PASSWORD"; } 2>>/dev/$SECRET_OUTPUT_DEV; then
+    if { veeamconfig vbrServer add --name "$VEEAM_SERVER_HOSTNAME" --address "$VEEAM_IPADDR" --domain "$VEEAM_DOMAIN" --login "$VEEAM_USER" --password "$VEEAM_PASSWORD"; } 2>>/dev/$SECRET_OUTPUT_DEV; then
+        LogPrint "Veeam linux client agent registered successfully to veeam VBR server: '$VEEAM_SERVER_HOSTNAME'"
+    else
+        Error "Veeam linux client agent registration failed as '$VEEAM_DOMAIN\\$VEEAM_USER' at '$VEEAM_SERVER_HOSTNAME' ($VEEAM_IPADDR) failed"
+    fi
+fi
