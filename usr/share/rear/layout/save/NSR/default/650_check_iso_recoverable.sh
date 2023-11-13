a5b589f75222bcafd209fb92eea62a54c5f0bbce
Johannes Meixner <jsmeix@suse.com>
2023-11-13 09:40:30 +0100

    Update 650_check_iso_recoverable.sh
    In layout/save/NSR/default/650_check_iso_recoverable.sh
    alsos read NSR server name from var/lib/rear/recovery/nsr_server
    by using the same code snippet as in verify/NSR/default/400_verify_nsr.sh
    see https://github.com/rear/rear/issues/2162#issuecomment-541343374
    and https://github.com/rear/rear/issues/3069

--- usr/share/rear/layout/save/NSR/default/650_check_iso_recoverable.sh
+++ usr/share/rear/layout/save/NSR/default/650_check_iso_recoverable.sh
@@ -4,8 +4,20 @@
 if is_true "$NSR_CLIENT_MODE"; then
     return
 fi
-    
+
 CLIENTNAME=$(hostname)
+
+# read NSR server name from /nsr/res/servers file of /var/lib/rear/recovery/nsr_server
+# code snippet taken from verify/NSR/default/400_verify_nsr.sh
+# see https://github.com/rear/rear/issues/2162#issuecomment-541343374
+# and https://github.com/rear/rear/issues/3069
+if [[ ! -z "$NSRSERVER" ]]; then
+    Log "NSRSERVER ($NSRSERVER) was defined in $CONFIG_DIR/local.conf"
+elif [[ -f $VAR_DIR/recovery/nsr_server ]]; then
+    NSRSERVER=$( cat $VAR_DIR/recovery/nsr_server )
+else
+    Error "Could not retrieve the EMC NetWorker Server name. Define NSRSERVER in $CONFIG_DIR/local.conf file"
+fi
 
 OBJECTS=$( nsrinfo -s ${NSRSERVER} -N ${ISO_DIR}/${ISO_PREFIX}.iso ${CLIENTNAME} | \
            awk '/objects found/ { print $1; }' )
