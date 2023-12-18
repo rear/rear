30c880740e026d71143f29ce9d6d683ae0dc826b
Johannes Meixner <jsmeix@suse.com>
2023-12-18 15:10:14 +0100

    Update 800_enforce_usb_output.sh
    Additional LogPrintError infos before BugError
    in particular to link to the matching GitHub issue
    https://github.com/rear/rear/pull/3110

--- usr/share/rear/build/USB/default/800_enforce_usb_output.sh
+++ usr/share/rear/build/USB/default/800_enforce_usb_output.sh
@@ -5,8 +5,8 @@
 # and https://github.com/rear/rear/issues/1571#issuecomment-343461088
 # and https://github.com/rear/rear/issues/1571#issuecomment-343516020
 
-real_output=$( source $ROOTFS_DIR/etc/rear/local.conf; echo $OUTPUT )
-test "USB" = "$real_output" && return
+local_conf_output=$( source $ROOTFS_DIR/etc/rear/local.conf ; echo $OUTPUT )
+test "USB" = "$local_conf_output" && return
 
 # At this point we run this script build/USB/default/800_enforce_usb_output.sh
 # which means there is OUTPUT=USB in etc/rear/local.conf
@@ -17,4 +17,6 @@ test "USB" = "$real_output" && return
 # so OUTPUT got somehow modified in $ROOTFS_DIR/etc/rear/local.conf 
 # after etc/rear/local.conf got copied via build/GNU/Linux/100_copy_as_is.sh
 # which is a bug in ReaR:
-BugError "OUTPUT=USB is used but that is missing in $ROOTFS_DIR/etc/rear/local.conf"
+LogPrintError "OUTPUT=USB is used but that is missing in $ROOTFS_DIR/etc/rear/local.conf"
+LogPrintError "See https://github.com/rear/rear/pull/3110 and follow the links therein"
+BugError "rear $WORKFLOW uses OUTPUT=USB which will not be used for rear recover"
