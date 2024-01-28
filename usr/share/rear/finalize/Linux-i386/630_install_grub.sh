636158131782448476ef0ea4ba8cd716d9b6dbc7
Pavel Cahyna <pcahyna@redhat.com>
2024-01-28 16:03:21 +0100

    Support BOOTLOADER=GRUB with UEFI
    This will happen with hybrid boot: BOOTLOADER=GRUB indicates that there
    is the BIOS version of GRUB installed and at the same time,
    USING_UEFI_BOOTLOADER=1 indicates theat there is also an EFI bootloader.

--- usr/share/rear/finalize/Linux-i386/630_install_grub.sh
+++ usr/share/rear/finalize/Linux-i386/630_install_grub.sh
@@ -15,8 +15,10 @@
 # (then $NOBOOTLOADER is not a true value cf. finalize/default/010_prepare_checks.sh):
 is_true $NOBOOTLOADER || return 0
 
-# For UEFI systems with grub legacy with should use efibootmgr instead:
-is_true $USING_UEFI_BOOTLOADER && return
+# For UEFI systems with grub legacy with should use efibootmgr instead,
+# but if BOOTLOADER is explicitly set to GRUB, we are on a hybrid (BIOS/UEFI)
+# boot system and we need to install GRUB to MBR as well.
+# Therefore, we don't test $USING_UEFI_BOOTLOADER.
 
 # If the BOOTLOADER variable (read by finalize/default/010_prepare_checks.sh)
 # is not "GRUB" (which means GRUB Legacy) skip this script (which is only for GRUB Legacy)
