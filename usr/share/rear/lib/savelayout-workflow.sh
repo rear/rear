c1d4e552ba7c2139c1c46e123fac07196f7a13b9
Pavel Cahyna <pcahyna@redhat.com>
2024-01-28 17:50:50 +0100

    Make USING_UEFI_BOOTLOADER known during savelayout
    layout/save/default/445_guess_bootloader.sh needs to know whether we are
    using UEFI. Source prep/default/320_include_uefi_env.sh (which sets
    USING_UEFI_BOOTLOADER) in the savelayout workflow.
    
    This is a minimally invasive change; there may be other variables set
    during prep stage that are needed during savelayout (see
    https://github.com/rear/rear/pull/1673 ) so a more general, but more
    invasive solution would be to source the entire prep stage in the
    savelayout workflow.

--- usr/share/rear/lib/savelayout-workflow.sh
+++ usr/share/rear/lib/savelayout-workflow.sh
@@ -10,6 +10,10 @@ if [[ "$VERBOSE" ]]; then
 fi
 WORKFLOWS+=( savelayout )
 WORKFLOW_savelayout () {
+    # layout code needs to know whether we are using UEFI (USING_UEFI_BOOTLOADER)
+    # as it also detects the bootloader in use ( layout/save/default/445_guess_bootloader.sh )
+    Source $SHARE_DIR/prep/default/320_include_uefi_env.sh
+
     #DISKLAYOUT_FILE=$VAR_DIR/layout/disklayout.conf # defined in default.conf now (issue #678)
     SourceStage "layout/save"
 }
