01d5fb5226bdb750a2ea09c76ceb646ffcc3a511
Pavel Cahyna <pcahyna@redhat.com>
2024-02-05 15:11:39 +0100

    Migrate NM keyfiles during network conf migration
    See
    https://fedoramagazine.org/converting-networkmanager-from-ifcfg-to-keyfiles/
    for more details on NetworkManager keyfiles.
    
    Only MAC addresses and interface names are migrated for now.
    
    TODO: migrate also IP addresses and routes.

--- usr/share/rear/finalize/GNU/Linux/320_migrate_network_configuration_files.sh
+++ usr/share/rear/finalize/GNU/Linux/320_migrate_network_configuration_files.sh
@@ -102,7 +102,7 @@ function valid_restored_file_for_patching () {
 # FIXME: The following code fails if file names contain characters from IFS (e.g. blanks),
 # see https://github.com/rear/rear/pull/1514#discussion_r141031975
 # and for the general issue see https://github.com/rear/rear/issues/1372
-for restored_file in $TARGET_FS_ROOT/etc/sysconfig/*/ifcfg-* $TARGET_FS_ROOT/etc/network/inter[f]aces $TARGET_FS_ROOT/etc/network/interfaces.d/* ; do
+for restored_file in $TARGET_FS_ROOT/etc/sysconfig/*/ifcfg-* $TARGET_FS_ROOT/etc/network/inter[f]aces $TARGET_FS_ROOT/etc/network/interfaces.d/* $TARGET_FS_ROOT/etc/NetworkManager/system-connections/*.nmconnection ; do
     network_config_file="$( valid_restored_file_for_patching "$restored_file" )" || continue
     network_config_files+=( $network_config_file )
 done
