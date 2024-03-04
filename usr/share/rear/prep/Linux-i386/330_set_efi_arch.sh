3488a9760ff90efbf157881366f5eb4923943a43
Pavel Cahyna <pcahyna@redhat.com>
2024-03-04 16:33:56 +0100

    fixup! Introduce variables EFI_ARCH{_UPPER}

--- usr/share/rear/prep/Linux-i386/330_set_efi_arch.sh
+++ usr/share/rear/prep/Linux-i386/330_set_efi_arch.sh
@@ -5,8 +5,11 @@
 # Se the variables even if USING_UEFI_BOOTLOADER empty or no explicit 'true' value
 
 case "$REAL_MACHINE" in
+    # cf. the seting of REAL_MACHINE and MACHINE in default.conf
     (i686|i586|i386)
         # all these behave exactly like i386.
+        # ia32 is another name for i386, used by EFI
+        # (but ia64 is not x86_64 aka amd64, it is the architecture of Itanium)
         EFI_ARCH=ia32
         # argument for grub2-mkstandalone -O ...
         GRUB2_IMAGE_FORMAT=x86_64-efi
