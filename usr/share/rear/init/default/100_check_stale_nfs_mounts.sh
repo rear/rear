778257528fe2cd669ca5f61729d7efb99a691839
gdha <gratien.dhaese@gmail.com>
2024-03-18 17:37:26 +0100

    Stale NFS detection in a very early stage #3109
    Signed-off-by: gdha <gratien.dhaese@gmail.com>

---
+++ usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
@@ -0,0 +1,11 @@
+# 100_check_stale_nfs_mounts.sh
+# Purpose is to have a simple test if we have stale NFS mount points which
+# could lead to a hangind ReaR session.
+# In case there is a stale NFS present we bail out with an error to get it fixed before
+# running ReaR again.
+# See issue https://github.com/rear/rear/issues/3109
+cat /proc/mounts | grep -i "nfs" | \
+    while read exported_fs mount_point junk ;
+    do
+       timeout 5 df $mount_point >/dev/null || Error "Stale NFS mount point $mount_point detected - please fix it first!"
+    done
