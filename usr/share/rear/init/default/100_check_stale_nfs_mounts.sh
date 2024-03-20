61fb83875960a8f82b91a591f8d2c3c411a2f557
gdha <gratien.dhaese@gmail.com>
2024-03-20 11:51:51 +0100

    Make code robust for obscure mount point naming #3109
    Signed-off-by: gdha <gratien.dhaese@gmail.com>

--- usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
+++ usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
@@ -4,7 +4,17 @@
 # In case there is a stale NFS present we bail out with an error to get it fixed before
 # running ReaR again.
 # See issue https://github.com/rear/rear/issues/3109
-while read exported_fs mount_point junk ;
+# To make the code more robust we take care of spaces and \ in the mount point naming.
+### example mount point /mnt/!@#$%^&**()_-=+{}|\
+# + read -r exported_fs mount_point junk
+#++ grep -i '[[:blank:]]nfs' /proc/mounts
+#++ printf '%s\n' '/mnt/!@#$%^&**()_-=+{}|\134'
+#++ sed -e 's/\\040/ /g' -e 's/\\134/\\/g'
+#+ mntpt='/mnt/!@#$%^&**()_-=+{}|\'
+#+ timeout 5 df '/mnt/!@#$%^&**()_-=+{}|\'
+
+while read -r exported_fs mount_point junk ;
 do
-    timeout 5 df $mount_point >/dev/null || Error "Stale NFS mount point $mount_point detected - please fix it first!"
-done < <(cat /proc/mounts | grep -i "[[:blank:]]nfs")
+    mntpt="$(printf "%s\n" "$mount_point" | sed -e 's/\\040/ /g' -e 's/\\134/\\/g')"
+    timeout 5 df "$mntpt" >/dev/null || Error "Stale NFS mount point $mount_point detected - please fix it first!"
+done < <(grep -i "[[:blank:]]nfs" /proc/mounts)
