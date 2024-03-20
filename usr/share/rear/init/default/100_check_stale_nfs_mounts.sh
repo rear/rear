5ed9c08ed81a6e76038acba436b6bc57e7b33041
gdha <gratien.dhaese@gmail.com>
2024-03-20 08:58:57 +0100

    #3109 improve the nfs grep to avoid false postives
    Signed-off-by: gdha <gratien.dhaese@gmail.com>

--- usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
+++ usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
@@ -4,7 +4,7 @@
 # In case there is a stale NFS present we bail out with an error to get it fixed before
 # running ReaR again.
 # See issue https://github.com/rear/rear/issues/3109
-cat /proc/mounts | grep -i "nfs" | \
+cat /proc/mounts | grep -i "[[:blank:]]nfs" | \
     while read exported_fs mount_point junk ;
     do
        timeout 5 df $mount_point >/dev/null || Error "Stale NFS mount point $mount_point detected - please fix it first!"
