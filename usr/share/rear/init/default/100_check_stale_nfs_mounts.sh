31e76af75d5df2def218279033e26e25605562f2
gdha <gratien.dhaese@gmail.com>
2024-03-21 08:43:55 +0100

    Add additional comments + code cleanup #3109
    Signed-off-by: gdha <gratien.dhaese@gmail.com>

--- usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
+++ usr/share/rear/init/default/100_check_stale_nfs_mounts.sh
@@ -8,13 +8,20 @@
 ### example mount point /mnt/!@#$%^&**()_-=+{}|\
 # + read -r exported_fs mount_point junk
 #++ grep -i '[[:blank:]]nfs' /proc/mounts
-#++ printf '%s\n' '/mnt/!@#$%^&**()_-=+{}|\134'
 #++ sed -e 's/\\040/ /g' -e 's/\\134/\\/g'
 #+ mntpt='/mnt/!@#$%^&**()_-=+{}|\'
 #+ timeout 5 df '/mnt/!@#$%^&**()_-=+{}|\'
+#
+# or
+#
+#+ read -r exported_fs mount_point junk
+#++ grep -i '[[:blank:]]nfs' /proc/mounts
+#++ sed -e 's/\\040/ /g' -e 's/\\134/\\/g'
+#+ mntpt='/mnt/something else with xXx going on!'
+#+ timeout 5 df '/mnt/something else with xXx is going on!'
 
 while read -r exported_fs mount_point junk ;
 do
-    mntpt="$(printf "%s\n" "$mount_point" | sed -e 's/\\040/ /g' -e 's/\\134/\\/g')"
+    mntpt="$(sed -e 's/\\040/ /g' -e 's/\\134/\\/g' <<< "$mount_point")"
     timeout 5 df "$mntpt" >/dev/null || Error "Stale NFS mount point $mount_point detected - please fix it first!"
 done < <(grep -i "[[:blank:]]nfs" /proc/mounts)
