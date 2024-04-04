a6e635f81f6e537d31d598e9cd6831bd7aa93bcc
Johannes Meixner <jsmeix@suse.com>
2024-04-04 11:38:16 +0200

    Update 330_set_efi_arch.sh

--- usr/share/rear/prep/Linux-ia64/330_set_efi_arch.sh
+++ usr/share/rear/prep/Linux-ia64/330_set_efi_arch.sh
@@ -2,7 +2,8 @@
 # Set EFI architecture, used as suffix for various files in the ESP
 # See https://github.com/rhboot/shim/blob/main/Make.defaults
 
-# Se the variables even if USING_UEFI_BOOTLOADER empty or no explicit 'true' value
+# Set the variables even if USING_UEFI_BOOTLOADER empty or no explicit 'true' value
+# cf. prep/Linux-i386/330_set_efi_arch.sh
 
 EFI_ARCH=ia64
 # argument for grub2-mkstandalone -O ...
