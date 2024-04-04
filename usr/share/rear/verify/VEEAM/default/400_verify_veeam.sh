de7b240f2fa129307a7ca9b14691e3b02a74cb93
SchallerA <150189387+idna38@users.noreply.github.com>
2024-04-04 21:51:23 +0200

    Update 400_verify_veeam.sh

--- usr/share/rear/verify/VEEAM/default/400_verify_veeam.sh
+++ usr/share/rear/verify/VEEAM/default/400_verify_veeam.sh
@@ -14,8 +14,8 @@ Log "Deleting Veeam SQLite Linux Agent Database"
 rm -rf $v /var/lib/veeam/*
 
 # check linux distribution and change the veeamservice systemd unit file for correct startup
-if ( egrep -q "Debian|Ubuntu" /etc/os-release ) ;
-    then sed -i 's/\/var//g' /usr/lib/systemd/system/veeamservice.service
+if ( egrep -q "Debian|Ubuntu" /etc/os-release ) ; then 
+    sed -i 's/\/var//g' /usr/lib/systemd/system/veeamservice.service
     else Error
 fi
 
