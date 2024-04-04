937bcd0af95bff585862a2e4a648e0e21dfc4b4e
SchallerA <150189387+idna38@users.noreply.github.com>
2024-04-04 21:53:25 +0200

    Update 450_find_backup.sh

--- usr/share/rear/restore/VEEAM/default/450_find_backup.sh
+++ usr/share/rear/restore/VEEAM/default/450_find_backup.sh
@@ -8,7 +8,6 @@ local backuplist
 LogPrint "Query the latest full backup for Veeam client: $(hostname)"
 # output of veeamconfig backup list looks like this:
 # Job name Backup ID Repository Created at
-# Linux_FS01_nosnap_filelevel - 192.168.107.211 {7eb175f2-b006-4440-9b4f-c4be407c8dd5} [qlveeam11] Default Backup Repository 2023-12-23 07:33
 # Linux_FS_nosnap_01 - rhel8-veeam02.lab.quorum.at {62749ef7-970a-4ff9-89b9-134bc276a1e6} [qlveeam11] Default Backup Repository 2024-01-15 18:07
 # Linux_FS_nosnap_02 - rhel8-veeam02.lab.quorum.at {27c3120b-ae5f-4086-b508-10c49489c06a} [qlveeam11] Default Backup Repository 2024-01-15 20:59
 # Linux_FS_nosnap_02 - rhel8-veeam02.lab.quorum.at {a28896fb-61bc-4ed9-9b30-5801a6eb5698} [qlveeam11] Default Backup Repository 2024-01-15 22:05
