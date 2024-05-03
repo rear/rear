40fd9a24353e163615c85477519664ba3f40616b
Schaller Andreas <150189387+idna38@users.noreply.github.com>
2024-05-03 15:53:31 +0200

    search for short HOSTNAME, also to match the fqdn and return all backups

--- usr/share/rear/restore/VEEAM/default/450_find_backup.sh
+++ usr/share/rear/restore/VEEAM/default/450_find_backup.sh
@@ -12,9 +12,9 @@ LogPrint "Query the latest full backup for Veeam client: $(hostname)"
 # Linux_FS_nosnap_02 - rhel8-veeam02.lab.quorum.at {27c3120b-ae5f-4086-b508-10c49489c06a} [qlveeam11] Default Backup Repository 2024-01-15 20:59
 # Linux_FS_nosnap_02 - rhel8-veeam02.lab.quorum.at {a28896fb-61bc-4ed9-9b30-5801a6eb5698} [qlveeam11] Default Backup Repository 2024-01-15 22:05
 
-backuplist=$(veeamconfig backup list --all | grep $(hostname)) || Error "Failed to query backup list"
+backuplist=$(veeamconfig backup list --all | grep " - $HOSTNAME") || Error "Failed to query backup list" # search for short HOSTNAME, also to match the fqdn
 [[ "$backuplist" == *{* ]] || Error "Backup list doesn't contain any backups:$LF$backuplist"
 
-VEEAM_BACKUPID=$(sed -n -e '$s/^.*\({.*}\).*$/\1/p' <<<"$backuplist") # pick backup ID {...} from last line
+VEEAM_BACKUPID=$(sed -n -e '$s/^.*\({.*}\).*$/\1/p' <<<"$backuplist") # pick backup ID {...} from last line. We assume that the last line is the most recent backup
 
 test "$VEEAM_BACKUPID" || Error "Could not determine backup ID from backup list:$LF$backuplist"
