1740617020c02c6e9c1404bafc6d1162902e40bd
Schaller Andreas <150189387+idna38@users.noreply.github.com>
2024-05-03 15:44:17 +0200

    enable default ramdisk if not set to satisfy Veeam 12.1ff requirements
    Closes #3213

--- usr/share/rear/prep/VEEAM/default/400_prep_veeam.sh
+++ usr/share/rear/prep/VEEAM/default/400_prep_veeam.sh
@@ -4,3 +4,5 @@
 
 COPY_AS_IS+=("${COPY_AS_IS_VEEAM[@]}")
 REQUIRED_PROGS+=("${REQUIRED_PROGS_VEEAM[@]}")
+# enable default ramdisk if not set to satisfy Veeam 12.1ff requirements 
+is_true "$USE_RAMDISK" || USE_RAMDISK=yes
