911051ea0854ba78118704658bc7d28879052bd0
Johannes Meixner <jsmeix@suse.com>
2024-05-15 17:39:18 +0200

    Overhauled 400_create_include_exclude_files.sh (#3221)
    Overhauled backup/NETFS/default/400_create_include_exclude_files.sh
    * Now do first backup the mounted filesystems to backup '/' first
    so the basic system files get stored first in the backup and
    then backup what is specified in BACKUP_PROG_INCLUDE
    see https://github.com/rear/rear/pull/3177#issuecomment-1985926458
    and https://github.com/rear/rear/issues/3217#issue-2277985295
    * Report suspicious cases as LogPrintError to have the user at least informed.
    * Remove duplicates in backup-[in/ex]clude.txt but keep the ordering
    to avoid possibly unwanted and unexpected subtle consequences
    see https://github.com/rear/rear/pull/3175#issuecomment-1985382738
    * Verify that at least '/' is in backup-include.txt
    see https://github.com/rear/rear/issues/3217
    * Redirect stdout into files exactly at the command where needed
    instead of more global redirections, cf. "horrible coding style"
    in https://github.com/rear/rear/pull/3175#issuecomment-2109554865
    
    Update backup/NETFS/default/500_make_backup.sh
    * In backup/NETFS/default/500_make_backup.sh
    unique_unsorted is no longer needed because
    backup-include.txt is already without duplicates
    because unique_unsorted is now called in
    backup/NETFS/default/400_create_include_exclude_files.sh

--- usr/share/rear/backup/NETFS/default/400_create_include_exclude_files.sh
+++ usr/share/rear/backup/NETFS/default/400_create_include_exclude_files.sh
@@ -1,33 +1,86 @@
 
-# Backup all that is explicitly specified in BACKUP_PROG_INCLUDE:
-for backup_include_item in "${BACKUP_PROG_INCLUDE[@]}" ; do
-    test "$backup_include_item" && echo "$backup_include_item"
-done > $TMP_DIR/backup-include.txt
+# What to include in the backup:
+cat /dev/null > "$TMP_DIR/backup-includes.txt"
 
-# Implicitly also backup all local filesystems as defined in mountpoint_device
-# except BACKUP_ONLY_INCLUDE or MANUAL_INCLUDE is set:
+# First by default backup the mounted filesystems in disklayout.conf
+# (except BACKUP_ONLY_INCLUDE or MANUAL_INCLUDE is set)
+# as defined in var/lib/rear/recovery/mountpoint_device which contains at least '/'
+# where usually '/' is first because "fs ... /" is first in disklayout.conf
+# see usr/share/rear/layout/save/default/340_generate_mountpoint_device.sh
+# so the basic system files and directories get stored first in the backup
+# see https://github.com/rear/rear/pull/3177#issuecomment-1985926458
+# and https://github.com/rear/rear/issues/3217#issue-2277985295
 if ! is_true "$BACKUP_ONLY_INCLUDE" ; then
     if [ "${MANUAL_INCLUDE:-NO}" != "YES" ] ; then
-        # Add the mountpoints that will be recovered to the backup include list
-        # unless a mountpoint is excluded:
+        # Add the mountpoints that will be recovered to the backup include list unless a mountpoint is excluded.
+        # This looks contradicting because "mountpoints that will be recovered" should not be excluded via EXCLUDE_MOUNTPOINTS
+        # which excludes filesystems from being recreated by specifying their mountpoints (see EXCLUDE_MOUNTPOINTS in default.conf)
+        # so we report suspicious cases as LogPrintError to have the user at least informed:
         while read mountpoint device junk ; do
-            if ! IsInArray "$mountpoint" "${EXCLUDE_MOUNTPOINTS[@]}" ; then
-                echo "$mountpoint"
+            if IsInArray "$mountpoint" "${EXCLUDE_MOUNTPOINTS[@]}" ; then
+                LogPrintError "Mountpoint '$mountpoint' in $VAR_DIR/recovery/mountpoint_device is excluded in EXCLUDE_MOUNTPOINTS"
+                continue
             fi
-        done <"$VAR_DIR/recovery/mountpoint_device" >> $TMP_DIR/backup-include.txt
+            if ! mountpoint "$mountpoint" ; then
+                LogPrintError "Mountpoint '$mountpoint' in $VAR_DIR/recovery/mountpoint_device is not a mountpoint"
+                continue
+            fi
+            echo "$mountpoint" >> "$TMP_DIR/backup-includes.txt"
+        done < "$VAR_DIR/recovery/mountpoint_device"
     fi
 fi
 
-# Exclude all that is explicitly specified in BACKUP_PROG_EXCLUDE:
-for backup_exclude_item in "${BACKUP_PROG_EXCLUDE[@]}" ; do
-    test "$backup_exclude_item" && echo "$backup_exclude_item"
-done > $TMP_DIR/backup-exclude.txt
+# Then also backup all that is explicitly specified in BACKUP_PROG_INCLUDE:
+for backup_include_item in "${BACKUP_PROG_INCLUDE[@]}" ; do
+    test "$backup_include_item" || continue
+    echo "$backup_include_item" >> "$TMP_DIR/backup-includes.txt"
+done
 
-# Implicitly also add excluded mountpoints to the backup exclude list
+# backup-include.txt is backup-includes.txt without duplicates but keeps the ordering
+# in particular when the user may have specified something in BACKUP_PROG_INCLUDE
+# which also gets backed up by the default backup of the mounted local filesystems
+# to avoid possibly unwanted and unexpected subtle consequences
+# see https://github.com/rear/rear/pull/3175#issuecomment-1985382738
+unique_unsorted "$TMP_DIR/backup-includes.txt" > "$TMP_DIR/backup-include.txt"
+
+# Verify that at least '/' is in backup-include.txt
+# because ReaR is meant to recreate a system so at least
+# the basic system files in the root filesystem must be backed up.
+# Examples how '/' could be missing in backup-include.txt:
+# - When BACKUP_ONLY_INCLUDE or MANUAL_INCLUDE is set
+#   the user may not have added '/' to BACKUP_PROG_INCLUDE
+#   so the '/' mountpoint will not be written into backup-include.txt
+# - When '/' is on a multipath device the default AUTOEXCLUDE_MULTIPATH=y
+#   will automatically exclude '/' and dependent components
+#   so '/' is not in var/lib/rear/recovery/mountpoint_device
+#   see https://github.com/rear/rear/issues/3189#issuecomment-2082747052
+#   and https://github.com/rear/rear/issues/3189#issuecomment-2082981807
+#   so that the above default backup of the mounted local filesystems
+#   will not write the '/' mountpoint into backup-include.txt
+# See https://github.com/rear/rear/issues/3217
+grep -q '^/$' "$TMP_DIR/backup-include.txt" || Error "At least the root filesystem must be backed up (no '/' in $TMP_DIR/backup-include.txt)"
+
+# What to exclude from the backup:
+cat /dev/null > "$TMP_DIR/backup-excludes.txt"
+
+# First exclude all that is specified to be excluded from the backup via BACKUP_PROG_EXCLUDE:
+for backup_exclude_item in "${BACKUP_PROG_EXCLUDE[@]}" ; do
+    test "$backup_exclude_item" || continue
+    echo "$backup_exclude_item" >> "$TMP_DIR/backup-excludes.txt"
+done
+
+# Then also add filesystems that are specified to be excluded from being recreated via their mountpoints
+# to the backup exclude list (see EXCLUDE_MOUNTPOINTS in default.conf)
 # except BACKUP_ONLY_EXCLUDE is set:
 if ! is_true "$BACKUP_ONLY_EXCLUDE" ; then
     for excluded_mountpoint in "${EXCLUDE_MOUNTPOINTS[@]}" ; do
-        test "$excluded_mountpoint" && echo "$excluded_mountpoint/"
-    done >> $TMP_DIR/backup-exclude.txt
+        if ! mountpoint "$excluded_mountpoint" ; then
+            LogPrintError "Mountpoint '$excluded_mountpoint' in EXCLUDE_MOUNTPOINTS is no mountpoint"
+            continue
+        fi
+        echo "$excluded_mountpoint/" >> "$TMP_DIR/backup-excludes.txt"
+    done
 fi
 
+# backup-exclude.txt is backup-excludes.txt without duplicates but keeps the ordering:
+unique_unsorted "$TMP_DIR/backup-excludes.txt" > "$TMP_DIR/backup-exclude.txt"
