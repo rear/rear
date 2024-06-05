f9a933c4373ce4ee277bbd73200fab9fc6b86412
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-05 14:04:07 +0200

    bareos: start bareos-fd in restore-only mode
    Starting the bareos-fd is enough for the rear environment
    and prevents problems if a backup jobs gets started
    that have been indended for the productive system.
    Before, this jobs would have created a backup of the rescue system,
    now it will just fail.

--- usr/share/rear/verify/BAREOS/default/100_start_bareos-fd.sh
+++ usr/share/rear/verify/BAREOS/default/100_start_bareos-fd.sh
@@ -13,7 +13,10 @@ if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
 else
 
    ### Bareos support using bconsole
-   bareos-fd -u root -g bareos
+   # -u <user>: Run as given user (requires starting as root).
+   # -g <group>: Run as given group (requires starting as root).
+   # -r: Restore only mode. (Backup jobs will fail).
+   bareos-fd -u root -g bareos -r
    StopIfError "Cannot start bareos-fd file daemon"
 
 fi
