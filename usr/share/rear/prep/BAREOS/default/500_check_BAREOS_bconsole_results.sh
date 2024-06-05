f5bf16e0d834ca2acaefe451c1a19fa942751a41
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-05 14:03:56 +0200

    bareos: cleanup prep, ping handling
    Extracting data from the Bareos configuration
    is only required for the ping test.
    Therefore we extract these only then.
    Get the version by `bconsole --version` does only work for Bareos >= 22.
    As this is also the relevant information,
    the handling is modified accordingly.

--- usr/share/rear/prep/BAREOS/default/500_check_BAREOS_bconsole_results.sh
+++ usr/share/rear/prep/BAREOS/default/500_check_BAREOS_bconsole_results.sh
@@ -3,39 +3,41 @@
 # are all  the files/directories present?
 
 if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
-
-   ### Bacule restore using bextract, no need for a director
+   ### restore using bextract, no need for a director
    return
 fi
 
-# Check bconsole version and use appropriate CLI switch:
-# Bareos 22 introduced a breaking change how its CLI tools (such as bconsole) parse the arguments.
-# Before it was "bconsole -xc" but since Bareos 22 it must be "bconsole --xc"
-# (otherwise it terminates with exit code 113 so "rear mkrescue" will fail).
-# Before "bconsole ...xc" is called, it now checks the bconsole version
-# and uses the new argument format '--xc' when version >= 22.
-# The backward compatible fallback is "bconsole -xc".
-# See https://github.com/rear/rear/issues/2900
-BCONSOLE_VERSION=$(bconsole --version | awk -F '.' '{print $1}')
-if [ "$BCONSOLE_VERSION" -ge 22 ]; then
-	BCONSOLE_XC="--xc"
-else
-	BCONSOLE_XC="-xc"
-fi
+if [ "$PING" ]; then
 
-#
-# See if we can ping the director
-#
-# is the director server present? Fetch from /etc/bareos/bconsole.conf file
-BAREOS_DIRECTOR=$(bconsole "$BCONSOLE_XC" | grep -i address | awk '{ print $3 }')
-[ "${BAREOS_DIRECTOR}" ]
-StopIfError "Director not configured in bconsole"
+    if [ -z $BAREOS_DIRECTOR_ADDRESS ]; then
+        # Check bconsole version and use appropriate CLI switch:
+        # Bareos 22 introduced a breaking change how its CLI tools (such as bconsole) parse the arguments.
+        # Before it was "bconsole -xc" but since Bareos 22 it must be "bconsole --xc"
+        # (otherwise it terminates with exit code 113 so "rear mkrescue" will fail).
+        # "bconsole --version" has also been introduced in Bareos 22,
+        # therefore if this command succeeds, we use "--xc".
+        # The backward compatible fallback is "bconsole -xc".
+        # See https://github.com/rear/rear/issues/2900
+        BCONSOLE_XC="-xc"
+        if bconsole --version >/dev/null 2>&1; then
+            BCONSOLE_XC="--xc"
+        fi
 
-if test "$PING"; then
-	ping -c 2 -q  $BAREOS_DIRECTOR >/dev/null
-	StopIfError "Backup host [$BAREOS_DIRECTOR] not reachable."
+        #
+        # See if we can ping the director
+        #
+        # is the director server present? Fetch address from configuration.
+        BAREOS_DIRECTOR_ADDRESS=$(bconsole "$BCONSOLE_XC" | sed -n -r  's/ *address *= *["](.*)["]/\1/ip')
+        [ "${BAREOS_DIRECTOR_ADDRESS}" ]
+        StopIfError "Failed to get Bareos Director address via bconsole configuration. Please configure BAREOS_DIRECTOR_ADDRESS."
+    fi
+
+    ping -c 2 -q  $BAREOS_DIRECTOR_ADDRESS >/dev/null
+    StopIfError "Backup host [$BAREOS_DIRECTOR_ADDRESS] not reachable."
+    LogPrint "Backup host [$BAREOS_DIRECTOR_ADDRESS] is reachable."
+
 else
-	Log "Skipping ping test"
+    Log "Skipping ping test"
 fi
 
 # does the director allow connections from this client? bconsole knows!
@@ -44,13 +46,11 @@ fi
 # and that the director can connect to the file daemon on this system.
 # "Connecting to Director 'director_name-fd:9101'"
 # "Connecting to Client 'bareos_client_name-fd at FQDN:9102"
-if [ -z "$BAREOS_CLIENT" ]
-then
+if [ -z "$BAREOS_CLIENT" ]; then
    BAREOS_CLIENT="$HOSTNAME-fd"
 fi
 
-BAREOS_RESULT=( `echo -e " status client=${BAREOS_CLIENT}" | bconsole |grep Connect ` )
-
+BAREOS_RESULT=( $(echo -e "status client=${BAREOS_CLIENT}" | bconsole | grep Connect) )
 director=${BAREOS_RESULT[3]}
 client=${BAREOS_RESULT[9]}
 
