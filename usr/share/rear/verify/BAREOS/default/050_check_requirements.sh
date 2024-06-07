100daa9f12e5581c090f6bdd8009cc54a9dd6904
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-07 19:25:01 +0200

    bareos: also check bconsole connections

--- usr/share/rear/verify/BAREOS/default/050_check_requirements.sh
+++ usr/share/rear/verify/BAREOS/default/050_check_requirements.sh
@@ -1,6 +1,8 @@
 #
 # Check that bareos is installed and configuration files exist
 
+LogPrint "Bareos: checking requirements ..."
+
 # First determine whether we need to restore using bconsole or bextract.
 
 if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
@@ -18,17 +20,46 @@ if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
 
 else
 
-   ### Bareos support using bconsole
-   [ -x /usr/sbin/bareos-fd ]
-   StopIfError "Bareos executable (bareos-fd) missing or not executable"
+    ### Bareos support using bconsole
 
-   bareos-fd -t
-   StopIfError "Bareos-fd configuration invalid"
+    if ! has_binary bareos-fd; then
+        Error "Bareos executable (bareos-fd) missing or not executable"
+    fi
 
-   [ -x /usr/sbin/bconsole ]
-   StopIfError "Bareos executable (bconsole) missing or not executable"
+    if ! bareos-fd -t; then
+        Error "bareos-fd: configuration invalid"
+    fi
 
-   bconsole -t
-   StopIfError "Bareos bconsole configuration invalid"
+    if ! systemctl --quiet is-active bareos-fd.service; then
+        Log "$(systemctl status bareos-fd.service)"
+        Error "bareos-fd.service is not running"
+    fi
+
+    if ! has_binary bconsole; then
+        Error "Bareos executable (bconsole) missing or not executable"
+    fi
+
+    if ! bconsole -t; then
+        Error "Bareos bconsole configuration invalid"
+    fi
+
+    LogPrint "Connecting to the Bareos Director ..."
+    local bconsole_client_status=$(bconsole <<< "status client=$BAREOS_CLIENT")
+    local rc=$?
+    Log "${bconsole_client_status}"
+    if [ $rc -ne 0 ]; then
+        Error "Failed to connect to Bareos Director."
+    fi
+    LogPrint "Connecting to the Bareos Director: OK"
+
+    if ! grep "Connecting to Client $BAREOS_CLIENT" <<< "${bconsole_client_status}"; then
+        Error "Failure: The Bareos Director cannot connect to the local filedaemon ($BAREOS_CLIENT)."
+    fi
+
+    if ! grep "Running Jobs:" <<< "${bconsole_client_status}"; then
+        Error "Failure: The Bareos Director cannot connect to the local filedaemon ($BAREOS_CLIENT)."
+    fi
+
+    LogPrint "Bareos Director: can connect to the local filedaemon ($BAREOS_CLIENT)."
 
 fi
