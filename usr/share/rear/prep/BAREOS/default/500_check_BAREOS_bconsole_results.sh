6e03ec7240eca560556379db8a2e1abd2c39c697
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-13 17:48:10 +0200

    bareos: make client interactivly selectable

--- usr/share/rear/prep/BAREOS/default/500_check_BAREOS_bconsole_results.sh
+++ usr/share/rear/prep/BAREOS/default/500_check_BAREOS_bconsole_results.sh
@@ -1,20 +1,36 @@
 # Check that Bareos is installed and configured
 #
 
-if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
+if [ "$BAREOS_RESTORE_MODE" != "bconsole" ]; then
    ### restore using bextract, no need for a director
    return
 fi
 
-# Does the director allow connections from this client? bconsole knows!
-#
-# We want these two lines to show that we can connect to the director
-# and that the director can connect to the file daemon on this system.
-# "Connecting to Director 'director_name-fd:9101'"
-# "Connecting to Client 'bareos_client_name-fd at FQDN:9102"
-if [ -z "$BAREOS_CLIENT" ]; then
-   BAREOS_CLIENT="$HOSTNAME-fd"
-   echo "BAREOS_CLIENT=$BAREOS_CLIENT" >> $VAR_DIR/bareos.conf
+mapfile -t clients < <( bcommand ".clients" )
+
+if (( ${#clients[@]} == 0 )); then
+    Error "No Bareos clients found"
 fi
 
+if [ "$BAREOS_CLIENT" ]; then
+    LogPrint "BAREOS_CLIENT=$BAREOS_CLIENT"
+    if ! IsInArray "$BAREOS_CLIENT" "${clients[@]}"; then
+        Error "Bareos Client ($BAREOS_CLIENT) is not available. Available clients:" "${clients[@]}"
+    fi
+    return
+fi
+
+local userinput_default
+if (( ${#clients[@]} == 1 )); then
+    userinput_default="-D ${clients[0]}"
+elif IsInArray "$HOSTNAME-fd" "${clients[@]}"; then
+    userinput_default="-D $HOSTNAME-fd"
+fi
+
+until IsInArray "$BAREOS_CLIENT" "${clients[@]}" ; do
+    BAREOS_CLIENT="$( UserInput -I BAREOS_CLIENT $userinput_default -p "Choose this host as Bareos Client: " "${clients[@]}" )"
+done
+
+# bcommand_check_client_status exists on error.
 bcommand_check_client_status "$BAREOS_CLIENT"
+echo "BAREOS_CLIENT=$BAREOS_CLIENT" >> "$VAR_DIR/bareos.conf"
