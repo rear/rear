865237d14a70e29b0b37fd1a5905d02348d0aaa1
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-13 15:02:30 +0200

    bareos: remove unnecessary checks

--- usr/share/rear/verify/BAREOS/default/030_bextract_tape_device.sh
+++ usr/share/rear/verify/BAREOS/default/030_bextract_tape_device.sh
@@ -3,14 +3,7 @@
 # We construct the TAPE_DEVICE based on the Bareos device name in the Bareos
 # configuration
 
-if [[ "$BACKUP" != "BAREOS" ]]; then
-    return
-fi
-
 if [[ -z "$TAPE_DEVICE" && "$BEXTRACT_DEVICE" ]]; then
-    has_binary btape
-    LogIfError "btape binary not found, unable to handle BEXTRACT_DEVICE '$BEXTRACT_DEVICE'"
-
     TAPE_DEVICE="$(echo cap | btape $BEXTRACT_DEVICE | awk '/^Device name/ { print $3 }')"
 
     [[ "$TAPE_DEVICE" ]]
