dab2a4b9113353991e0527d9720ba9bc81d0a0c2
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-13 17:47:35 +0200

    bareos: cleanup

--- usr/share/rear/prep/BAREOS/default/450_check_BAREOS_client_configured.sh
+++ usr/share/rear/prep/BAREOS/default/450_check_BAREOS_client_configured.sh
@@ -1,28 +1,21 @@
 #
-# Check that bareos is installed and configuration files exist
+# Check that bareos configuration files exist
 
-if [ "$BEXTRACT_DEVICE" -o "$BEXTRACT_VOLUME" ]; then
+if [ "$BAREOS_RESTORE_MODE" = "bextract" ]; then
 
-   ### Bareos support using bextract
-   has_binary bextract
-   StopIfError "Bareos bextract is missing"
-
-   bareos-sd -t 
-   StopIfError "Bareos-sd configuration invalid"
+    if ! bareos-sd -t; then
+        Error "Bareos-sd configuration invalid"
+    fi
 
 else
 
-   ### Bareos support using bconsole
-   has_binary bareos-fd
-   StopIfError "Bareos File Daemon is missing"
+    ### Bareos support using bconsole
+    if ! bareos-fd -t; then
+        Error "Bareos-fd configuration invalid"
+    fi
 
-   bareos-fd -t
-   StopIfError "Bareos-fd configuration invalid"
-
-   has_binary bconsole
-   StopIfError "Bareos console executable is missing"
-
-   bconsole -t
-   StopIfError "Bareos bconsole invalid"
+    if ! bconsole -t; then
+        Error "Bareos bconsole invalid"
+    fi
 
 fi
