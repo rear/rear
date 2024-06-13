65f6676b8f67268e035135fc3c6b70f6163323d3
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-13 17:46:14 +0200

    bareos: separate between bextract and bconsole restore mode

--- usr/share/rear/prep/BAREOS/default/400_prep_bareos.sh
+++ usr/share/rear/prep/BAREOS/default/400_prep_bareos.sh
@@ -1,15 +1,31 @@
 ### prepare stuff for BAREOS
+
 CLONE_GROUPS+=( bareos )
 COPY_AS_IS+=( "${COPY_AS_IS_BAREOS[@]}" )
 COPY_AS_IS_EXCLUDE+=( "${COPY_AS_IS_EXCLUDE_BAREOS[@]}" )
 PROGS+=( "${PROGS_BAREOS[@]}" )
-
-### Include mt when we are restoring from Bareos tape (for troubleshooting)
-if [[ "$TAPE_DEVICE" || "$BEXTRACT_DEVICE" ]] ; then
-    PROGS+=( mt )
-fi
-
-source $SHARE_DIR/lib/bareos-functions.sh
+REQUIRED_PROGS+=( "${REQUIRED_PROGS_BAREOS[@]}" )
 
 ### empty config file filled by "prep" (and used by restore)
-> $VAR_DIR/bareos.conf
+true > "$VAR_DIR/bareos.conf"
+
+if [ -z "$BAREOS_RESTORE_MODE" ]; then
+    if [ "$BEXTRACT_DEVICE" ] || [ "$BEXTRACT_VOLUME" ]; then
+        BAREOS_RESTORE_MODE="bextract"
+    else
+        BAREOS_RESTORE_MODE="bconsole"
+    fi
+    echo "BAREOS_RESTORE_MODE=$BAREOS_RESTORE_MODE" >> "$VAR_DIR/bareos.conf"
+fi
+
+LogPrint "BAREOS_RESTORE_MODE=$BAREOS_RESTORE_MODE"
+
+if [ "$BAREOS_RESTORE_MODE" = "bextract" ]; then
+    PROGS+=( "${PROGS_BAREOS_BEXTRACT[@]}" )
+    REQUIRED_PROGS+=( "${REQUIRED_PROGS_BAREOS_BEXTRACT[@]}" )
+else
+    PROGS+=( "${PROGS_BAREOS_BCONSOLE[@]}" )
+    REQUIRED_PROGS+=( "${REQUIRED_PROGS_BAREOS_BCONSOLE[@]}" )
+fi
+
+source "$SHARE_DIR/lib/bareos-functions.sh"
