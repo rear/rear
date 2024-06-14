144511c6420489532b79e23b28a7f5ef52bbb31a
Joerg Steffens <joerg.steffens@bareos.com>
2024-06-14 10:44:11 +0200

    shellcheck usr/share/rear/verify/BAREOS/default/040_prepare_environment_vars.sh

--- usr/share/rear/verify/BAREOS/default/040_prepare_environment_vars.sh
+++ usr/share/rear/verify/BAREOS/default/040_prepare_environment_vars.sh
@@ -1,19 +1,24 @@
 # read config vars defined during prep.
-source $VAR_DIR/bareos.conf
+source "$VAR_DIR/bareos.conf"
 
 if [ -z "$BAREOS_CLIENT" ]; then
     BAREOS_CLIENT="$HOSTNAME-fd"
+    export BAREOS_CLIENT
 fi
 
 if [ -n "$BAREOS_FILESET" ]; then
     FILESET="fileset=\"$BAREOS_FILESET\""
+    export FILESET
 fi
 
 if [ -n "$BAREOS_RESTORE_JOB" ]; then
     RESTOREJOB="restorejob=\"$BAREOS_RESTORE_JOB\""
+    export RESTOREJOB
     RESTOREJOB_AS_JOB="job=\"$BAREOS_RESTORE_JOB\""
+    export RESTOREJOB_AS_JOB
 fi
 
 if [ -n "$BAREOS_RESTORE_CLIENT" ]; then
     RESTORECLIENT="restoreclient=\"$BAREOS_RESTORE_CLIENT\""
+    export RESTORECLIENT
 fi
