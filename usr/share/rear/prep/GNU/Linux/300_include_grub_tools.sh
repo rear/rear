2478b4cf9100e77d28c414591e2ec6b330851b49
Johannes Meixner <jsmeix@suse.com>
2024-12-05 09:49:44 +0100

    Tell why 300_include_grub_tools.sh exists
    In prep/GNU/Linux/300_include_grub_tools.sh
    explain why it exists at all, see
    https://github.com/rear/rear/pull/3354#issuecomment-2519520750

--- usr/share/rear/prep/GNU/Linux/300_include_grub_tools.sh
+++ usr/share/rear/prep/GNU/Linux/300_include_grub_tools.sh
@@ -1,4 +1,26 @@
-#
+
+# GRUB tools are normally not required inside the recovery system for "rear recover"
+# because during "rear recover" GRUB is installed within the recreated target system
+# via chroot into the recreated target system.
+# There is one exception for GRUB2: grub-probe or grub2-probe
+# This is required inside the recovery system because the function
+# is_grub2_installed is also called during "rear recover"
+# in finalize/Linux-i386/630_install_grub.sh
+# and finalize/Linux-i386/660_install_grub2.sh
+# And 'type -p grub-probe || type -p grub2-probe' is called during "rear recover"
+# in finalize/Linux-ppc64le/660_install_grub2.sh
+# and finalize/SUSE_LINUX/s390/660_install_grub2_and_zipl.sh
+# Other code places that call 'has_binary grub-probe' or 'has_binary grub2-probe'
+# are not run during "rear recover"
+# (at least as far as I <jsmeix@suse.de> found out up to now dated 05 Dec. 2024)
+# cf. https://github.com/rear/rear/pull/3354#issuecomment-2519520750
+
+# But GRUB tools are useful in general inside the
+# recovery system for a different use case which is
+# using the ReaR recovery system to repair the system,
+# in particular to repair GRUB on the original system
+# when the original system fails to boot.
+
 # GRUB2 has much more commands than the legacy grub command, including modules
 
 # cf. https://github.com/rear/rear/issues/2137
@@ -10,7 +32,7 @@
 [ "$ARCH" == "Linux-s390"  ] && return 0
 
 # It is safe to assume that we are using GRUB and try to add these files to the rescue image
-# even if the assumption is wrong.
+# even if the assumption is wrong, cf. https://github.com/rear/rear/pull/3349#issuecomment-2503808679
 # Missing programs in the PROGS array are ignored:
 PROGS+=( grub-bios-setup      grub2-bios-setup
          grub-install         grub2-install
