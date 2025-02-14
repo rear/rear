66b4e50e43af69a19ca18daef9b5024ccb0c9eea
Johannes Meixner <jsmeix@suse.com>
2025-02-14 09:10:14 +0100

    Further enhance 880_check_for_mount_by_id.sh (#3399)
    In finalize/default/880_check_for_mount_by_id.sh
    only when what is mounded 'by-id' does not exist
    as block device on the replacement hardware,
    then alert the user because then he may have to
    adapt his TARGET_FS_ROOT/etc/fstab, see
    https://github.com/rear/rear/issues/3383#issuecomment-2652966699

--- usr/share/rear/finalize/default/880_check_for_mount_by_id.sh
+++ usr/share/rear/finalize/default/880_check_for_mount_by_id.sh
@@ -3,7 +3,8 @@
 # because we do not know how to set the original LUN IDs to the new / recovered hardware.
 # To help the user we check for /dev/disk/by-id entries in etc/fstab
 # and show the LUN IDs of SCSI disk devices via 'scsi_id'.
-# TODO: For non-SCSI disk devices we show nothing.
+# For non-SCSI disk devices we only show the etc/fstab '/dev/disk/by-id/...' value
+# when it does not exist as block device on the replacement hardware.
 # Non-SCSI disks are in particular NVMe devices and KVM 'VirtIO' virtual disks,
 # see https://github.com/rear/rear/issues/3383
 #
@@ -13,16 +14,55 @@ test -e "$TARGET_FS_ROOT/etc/fstab" || return 0
 
 # We ignore swap here because we treat it specially somewhere else.
 # FIXME: Where exactly is that "somewhere else"?
-# Nothing to do when there is no '/dev/disk/by-id' entry in etc/fstab (excluding 'swap'):
-grep -v -w 'swap' "$TARGET_FS_ROOT/etc/fstab" | grep -q '^/dev/disk/by-id' || return 0
+#
+# "man fstab" (from util-linux-2.39.3) tells (excerpts):
+#   Lines starting with '#' are comments. Blank lines are ignored.
+#   first field (fs_spec) ... block special device, remote filesystem or filesystem image for loop device ... or swap file or swap device
+#   second field (fs_file) ...  For swap area, this field should be specified as 'none'
+#   third field (fs_vfstype) ... An entry swap denotes a file or partition to be used for swapping
+# so only the fs_vfstype field can be used to find out if it is a swap area.
+local by_id_no_block_devices=()
+local fs_spec fs_file fs_vfstype junk
+while read fs_spec fs_file fs_vfstype junk ; do
+    # Ignore swap:
+    test "$fs_vfstype" = "swap" && continue
+    # Skip fstab entries which are not /dev/disk/by-id/...:
+    [[ $fs_spec =~ ^/dev/disk/by-id/ ]] || continue
+    # Now we have an /dev/disk/by-id/... entry which is not used as swap.
+    # All is well with this one when it exist as block device on the replacement hardware:
+    test -b "$fs_spec" && continue
+    # Remember it when it does not exist as block device on the replacement hardware:
+    by_id_no_block_devices+=( "$fs_spec" )
+done < <( grep -E -v "^#|^[[:space:]]*$" "$TARGET_FS_ROOT/etc/fstab" ) # Ignore comments and blank lines
 
-# Something is mounted via /dev/disk/by-id in etc/fstab.
+# All is well when all /dev/disk/by-id entries in etc/fstab exist as block devices in the recreated system:
+if test ${#by_id_no_block_devices[@]} -eq 0 ; then
+    DebugPrint "All /dev/disk/by-id entries in etc/fstab exist as block devices in the recreated system"
+    return 0
+fi
+
+# At this point not all is well because
+# something is mounted via /dev/disk/by-id in etc/fstab
+# where its /dev/disk/by-id/... does not exist as block device
+# in the ReaR recovery system on the replacement hardware
+# cf. https://github.com/rear/rear/issues/3383#issuecomment-2652966699
 # Tell the user that automatically adjusting /dev/disk/by-id entries is not supported:
 LogPrintError "Automatically adjusting /dev/disk/by-id entries in etc/fstab is not supported:"
-LogPrintError "  Those IDs could be hardware dependent so check $TARGET_FS_ROOT/etc/fstab"
-LogPrintError "  and verify all is correct or manually adjust $TARGET_FS_ROOT/etc/fstab"
-LogPrintError "  to the actual values of the recreated system in $TARGET_FS_ROOT"
+LogPrintError "  Check $TARGET_FS_ROOT/etc/fstab and manually adjust /dev/disk/by-id entries"
+LogPrintError "  as needed to the actual values of the recreated system in $TARGET_FS_ROOT"
 
+# Show the user the /dev/disk/by-id entries which are no block devices in the recreated system:
+LogPrintError "  Those /dev/disk/by-id entries are no block devices in the recreated system:"
+local by_id_no_block_device
+for by_id_no_block_device in "${by_id_no_block_devices[@]}" ; do
+    LogPrintError "  $by_id_no_block_device"
+done
+
+# Additional 'scsi_id' output:
+# TODO: I <jsmeix@suse.de> wonder if 'scsi_id' output is still needed nowadays
+# when neither NVMe disks nor KVM 'VirtIO' virtual disks are SCSI disks
+# so perhaps that rather complicated code for 'scsi_id' output could be dropped?
+#
 # The supported options of scsi_id changed over time, so we try two commonly known ways to call it,
 # cf. https://github.com/rear/rear/issues/3383#issuecomment-2618970742
 # scsi_id is usually either /lib/udev/scsi_id (e.g. in SLES 11 SP4 with udev-147)
@@ -31,7 +71,7 @@ LogPrintError "  to the actual values of the recreated system in $TARGET_FS_ROOT
 # and /usr/lib/udev/scsi_id supports e.g. '/usr/lib/udev/scsi_id -x -g -d /dev/sda'
 # cf. https://github.com/rear/rear/issues/3383#issuecomment-2618970742
 if ! test -x /usr/lib/udev/scsi_id -o -x /lib/udev/scsi_id ; then
-    # Give up. The above LogPrintError should be enough:
+    # Give up. The above LogPrintError messages should be enough:
     Debug "Neither /usr/lib/udev/scsi_id nor /lib/udev/scsi_id is executable"
     return 1
 fi
@@ -92,7 +132,7 @@ while read major minor blocks name ; do
     else
         # Try the older one /lib/udev/scsi_id as fallback.
         # Above was already tested that /lib/udev/scsi_id is executable when /usr/lib/udev/scsi_id is not executable.
-            scsi_id_output="$( /lib/udev/scsi_id --export --whitelisted --device=$device_path | grep -E "$egrep_pattern" | tr -s '\n' ' ' )"
+        scsi_id_output="$( /lib/udev/scsi_id --export --whitelisted --device=$device_path | grep -E "$egrep_pattern" | tr -s '\n' ' ' )"
     fi
     if ! contains_visible_char "$scsi_id_output" ; then
         # Give up with this device and continue with the next one:
