409b9ca3652815cf98447905360c98617dc7c796
prolez <40496304+prolez@users.noreply.github.com>
2025-02-26 10:07:26 +0000

    harmonization of the use of REQUIRED_PROGS

--- usr/share/rear/prep/OPALPBA/Linux-i386/001_configure_workflow.sh
+++ usr/share/rear/prep/OPALPBA/Linux-i386/001_configure_workflow.sh
@@ -40,12 +40,12 @@ PROGS+=( "${OPAL_PBA_PROGS[@]}" clear )
 COPY_AS_IS+=( "${OPAL_PBA_COPY_AS_IS[@]}" )
 LIBS+=( "${OPAL_PBA_LIBS[@]}" )
 
-is_false "$OPAL_PBA_SBWARN" || REQUIRED_PROGS+=( 'od' )
+is_false "$OPAL_PBA_SBWARN" || REQUIRED_PROGS+=( od )
 if [ -n "$OPAL_PBA_TKNPATH" ]; then # AuthToken support
-    REQUIRED_PROGS+=( 'openssl' 'base64' 'dd' 'lsblk' )
-    is_true "$OPAL_PBA_TKNBIND" && REQUIRED_PROGS+=( 'b2sum' )
+    REQUIRED_PROGS+=( openssl base64 dd lsblk )
+    is_true "$OPAL_PBA_TKNBIND" && REQUIRED_PROGS+=( b2sum )
     if [ "${OPAL_PBA_TKNKEY:0:4}" == "tpm:" ]; then # TPM2-assisted encryption
-        REQUIRED_PROGS+=( 'systemd-creds' )
+        REQUIRED_PROGS+=( systemd-creds )
         LIBS+=( /usr/lib/x86_64-linux-gnu/libtss2-*.so* )
     fi
 fi
