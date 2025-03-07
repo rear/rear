3113d6f159ff6e72b416e0d08cda6bd6e55749d1
gdha <gratien.dhaese@gmail.com>
2025-03-07 16:09:41 +0100

    #3405 update 450_check_network_files.sh for NetworkManager

--- usr/share/rear/layout/save/default/450_check_network_files.sh
+++ usr/share/rear/layout/save/default/450_check_network_files.sh
@@ -1,20 +1,37 @@
 # This file is part of Relax-and-Recover, licensed under the GNU General
 # Public License. Refer to the included COPYING for full text of license.
 
-
 # No quoting of the elements that are appended to the CHECK_CONFIG_FILES array together with
 # the bash globbing characters like '*' or the [] around the first letter make sure
 # that with 'shopt -s nullglob' files that do not exist will not appear
 # so nonexistent files are not appended to CHECK_CONFIG_FILES
 # cf. https://github.com/rear/rear/pull/2796#issuecomment-1117171070
+
 if [[ -d /etc/sysconfig/network ]] ; then
     # SUSE
-    CHECK_CONFIG_FILES+=( /etc/sysconfig/network/ifcfg-* )
-elif [[ -d /etc/sysconfig/network-scripts ]] ; then
-    # Red Hat
-    CHECK_CONFIG_FILES+=( /etc/sysconfig/network-scripts/ifcfg-* )
-elif [[ -d /etc/network ]] ; then
-    # Debian
-    CHECK_CONFIG_FILES+=( /[e]tc/network/interfaces )
+    ls /etc/sysconfig/network/ifcfg-* >/dev/null 2>&1 && CHECK_CONFIG_FILES+=( /etc/sysconfig/network/ifcfg-* )
 fi
 
+if [[ -d /etc/NetworkManager/system-connections ]] ; then
+    # Red Hat >= 8
+    # Check if the network interfaces are really present in NetworkManager style and if yes also add nmcli to PROGS array
+    ls /etc/NetworkManager/system-connections/*.nmconnection >/dev/null 2>&1 && CHECK_CONFIG_FILES+=( /etc/NetworkManager/system-connections/*.nmconnection )
+    PROGS+=( nmcli )
+fi
+
+if [[ -d /etc/sysconfig/network-scripts ]] ; then
+    # Red Hat <=8
+    # Check if the network interfaces are really present in legacy network configuration files
+    ls /etc/sysconfig/network-scripts/ifcfg-* >/dev/null 2>&1 && CHECK_CONFIG_FILES+=( /etc/sysconfig/network-scripts/ifcfg-* )
+fi
+
+if [[ -d /etc/network ]] ; then
+    # Debian
+    CHECK_CONFIG_FILES+=( /etc/network/interfaces )
+fi
+
+if [[ -d /etc/netplan ]] ; then
+   # Ubuntu using NetworkManager
+   CHECK_CONFIG_FILES+=( /etc/network/*.yaml )
+   PROGS+=( nmcli )
+fi
