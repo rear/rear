3ea6de59760664e8ac4be825277f432781fb4a55
Schlomo Schapiro <schlomo@schapiro.org>
2025-03-30 22:41:09 +0200

    fix CentOS Stream 10

--- tools/run-in-docker-build.sh
+++ tools/run-in-docker-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # TODO: Add support for GRUB2
+# TODO: Use more fine grained OS version detection to install appropriate packages, instead of trial-and-error
 
-exec </dev/null
+exec </dev/null # no interactive input
 
 function die() {
     echo "ERROR: $*" >&2
@@ -38,13 +39,15 @@ elif type -p yum &>/dev/null; then
         sed -i -e 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' -e '/^mirror/d' /etc/yum.repos.d/*.repo
     fi
     yum -q --nogpgcheck install -y \
-        kbd cpio binutils ethtool gzip iputils parted tar openssl gawk attr bc syslinux rpcbind iproute nfs-utils xorriso genisoimage util-linux psmisc procps-ng util-linux \
+        kbd cpio binutils ethtool gzip iputils parted tar openssl gawk attr bc syslinux rpcbind iproute nfs-utils xorriso util-linux psmisc procps-ng util-linux \
         make binutils git rpm-build || die "Failed to install required packages"
     # CentOS 8 doesn't have sysvinit-tools any more but it also doesn't have asciidoctor yet
+    # CentOS 10 uses asciidoc and doesn't have sysvinit-tools and also not mkisofs
     yum -q --nogpgcheck install -y sysvinit-tools mkisofs asciidoc xmlto ||
         yum -q --nogpgcheck install -y asciidoctor ||
-        yum -q --nogpgcheck install -y mkisofs asciidoc xmlto || die "Failed to install asciidoctor or asciidoc"
-
+        yum -q --nogpgcheck install -y mkisofs asciidoc xmlto ||
+        yum -q --nogpgcheck install -y asciidoc xmlto ||
+        die "Failed to install asciidoctor or asciidoc"
 fi
 
 git config --global --add safe.directory /rear || die "Failed to configure git"
