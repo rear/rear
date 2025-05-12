ea425ec8c6370bb97cfc52a7d1bba307c431c562
Johannes Meixner <jsmeix@suse.com>
2025-05-12 16:56:45 +0200

    Update 670_run_efibootmgr.sh
    Implement "full final power to the user"
    by implementing support to specify the
    EFI Boot Manager entry label
    in EFIBOOTMGR_CREATE_ENTRIES

--- usr/share/rear/finalize/Linux-i386/670_run_efibootmgr.sh
+++ usr/share/rear/finalize/Linux-i386/670_run_efibootmgr.sh
@@ -35,14 +35,15 @@ fi
 if test "${#EFIBOOTMGR_CREATE_ENTRIES}" -gt 0 ; then
     LogPrint "Creating EFI Boot Manager entries as specified in EFIBOOTMGR_CREATE_ENTRIES"
     local efibootmgr_create_failed="no"
-    local efibootmgr_create_entry efibootmgr_disk efibootmgr_part efibootmgr_loader
+    local efibootmgr_create_entry efibootmgr_disk efibootmgr_part efibootmgr_loader efibootmgr_label
     for efibootmgr_create_entry in "${EFIBOOTMGR_CREATE_ENTRIES[@]}" ; do
         efibootmgr_disk=''
         efibootmgr_part=''
         efibootmgr_loader=''
+        efibootmgr_label=''
         # The 'read -r' option is mandatory because efibootmgr_loader is e.g. "EFI\sles\shim.efi"
         # and without the '-r' option efibootmgr_loader would then become "EFIslesshim.efi"
-        read -r efibootmgr_disk efibootmgr_part efibootmgr_loader <<<"$efibootmgr_create_entry"
+        read -r efibootmgr_disk efibootmgr_part efibootmgr_loader efibootmgr_label <<<"$efibootmgr_create_entry"
         if ! test -b $efibootmgr_disk ; then
             LogPrintError "efibootmgr cannot create entry using disk '$efibootmgr_disk' (no block device)"
             continue
@@ -52,20 +53,24 @@ if test "${#EFIBOOTMGR_CREATE_ENTRIES}" -gt 0 ; then
             efibootmgr_part=1
         fi
         if ! test $efibootmgr_loader ; then
-            # FIXME: The hardcoded '-f4-' assumes UEFI_BOOTLOADER is like /boot/efi/EFI/sles/shim.efi (e.g. on SLES15-sp6)
+            # FIXME: The hardcoded '-f4-' assumes UEFI_BOOTLOADER is like /boot/efi/EFI/sles/shim.efi (e.g. on SLES15-SP6)
             # where exactly the first 3 fields are the ESP mountpoint directory like /boot/efi/ (first field is empty)
             # to extract efibootmgr_loader from UEFI_BOOTLOADER which is it without the ESP mountpoint directory
-            # and replace Linux directory separators '/' with VFAT directory separators '\'
+            # and Linux directory separators '/' replaced by VFAT directory separators '\'
             # so that efibootmgr_loader is the VFAT filesystem path of the EFI binary in the ESP
             # e.g. from UEFI_BOOTLOADER /boot/efi/EFI/sles/shim.efi efibootmgr_loader becomes EFI\sles\shim.efi
-            # which gets a leading '\' via "\\$efibootmgr_loader" so \EFI\sles\shim.efi
+            # which gets a leading '\' added via "\\$efibootmgr_loader" so in the end \EFI\sles\shim.efi
             # is the absolute VFAT filesystem path in the ESP to what the UEFI Boot Manager should launch:
             efibootmgr_loader=$( echo $UEFI_BOOTLOADER | cut -d"/" -f4- | sed -e 's;/;\\;g' )
             LogPrint "efibootmgr will use loader '$efibootmgr_loader' from UEFI_BOOTLOADER='$UEFI_BOOTLOADER' (no loader specified)"
         fi
-        LogPrint "Creating EFI Boot Manager entry '$OS_VENDOR $OS_VERSION' using '$efibootmgr_loader' on disk '$efibootmgr_disk' partition $efibootmgr_part"
-        if ! efibootmgr --create --gpt --disk $efibootmgr_disk --part $efibootmgr_part --write-signature --label "$OS_VENDOR $OS_VERSION" --loader "\\$efibootmgr_loader" ; then
-            LogPrintError "efibootmgr failed to create EFI Boot Manager entry using '$efibootmgr_loader' on disk '$efibootmgr_disk' partition $efibootmgr_part"
+        if ! contains_visible_char $efibootmgr_label ; then
+            LogPrint "efibootmgr will use default label '$OS_VENDOR $OS_VERSION' (no visible label specified)"
+            efibootmgr_label="$OS_VENDOR $OS_VERSION"
+        fi
+        LogPrint "Creating EFI Boot Manager entry '$efibootmgr_label' for '$efibootmgr_loader' on disk '$efibootmgr_disk' partition $efibootmgr_part"
+        if ! efibootmgr --create --gpt --disk $efibootmgr_disk --part $efibootmgr_part --write-signature --label "$efibootmgr_label" --loader "\\$efibootmgr_loader" ; then
+            LogPrintError "efibootmgr failed to create '$efibootmgr_label' entry for '$efibootmgr_loader' on disk '$efibootmgr_disk' partition $efibootmgr_part"
             efibootmgr_create_failed="yes"
         fi
     done
