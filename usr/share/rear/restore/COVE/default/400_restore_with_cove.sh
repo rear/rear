e88468cc51ff99ef9bd632f82bba9593a145bb6f
Barys Barysenka <barys.barysenka@n-able.com>
2025-05-15 17:53:45 -0400

    feat(restore): linux bmr
    * added progress bar for restore
    * show % of progress and ETA
    
    Jira-Ref: BCF-5318: Add a progress bar for the Linux 'Restore' process via ReaR

--- usr/share/rear/restore/COVE/default/400_restore_with_cove.sh
+++ usr/share/rear/restore/COVE/default/400_restore_with_cove.sh
@@ -72,6 +72,11 @@ function cove_get_status() {
     "${COVE_CLIENT_TOOL}" control.status.get
 }
 
+# Shows progress of running session (%, ETA)
+function cove_show_progress() {
+    "${COVE_CLIENT_TOOL}" show.progress-bar 1>&7 2>&8
+}
+
 # Downloads the Backup Manager installer
 function cove_download_bm_installer() {
     if [ -z "${COVE_INSTALLER_URL}" ]; then
@@ -308,10 +313,8 @@ cove_print "Waiting for the restore to be started... "
 cove_wait_for 'local status="$(cove_get_status)"; [ "${status}" = "Scanning" -o "${status}" = "Restore" ]' 2 \
     && cove_print_done || { cove_print_error; Error "The restore has not started."; }
 
-# Wait for the restore to be finished
-cove_print "Waiting for the restore to be finished... "
-cove_wait_for 'local status="$(cove_get_status)"; [ "${status}" = "Idle" ]' 15 \
-    && cove_print_done || { cove_print_error; Error "The restore has not finished."; }
+# Show progress bar for restore session
+cove_show_progress || Error "Restore failed."
 
 # Stop ProcessController process
 cove_stop_pc
