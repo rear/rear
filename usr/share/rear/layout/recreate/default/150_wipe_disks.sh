71acf5ab963ecfbff5d4c4facf71db0ce40730c9
Johannes Meixner <jsmeix@suse.com>
2025-07-03 15:11:02 +0200

    In 150_wipe_disks.sh fix wrong 'device_to_be_wiped_size_byte' test (#3487)
    In layout/recreate/default/150_wipe_disks.sh
    fix wrong device_to_be_wiped_size_byte test, see
    https://github.com/rear/rear/pull/3484#issuecomment-3022670414

--- usr/share/rear/layout/recreate/default/150_wipe_disks.sh
+++ usr/share/rear/layout/recreate/default/150_wipe_disks.sh
@@ -191,8 +191,13 @@ for disk_to_be_wiped in $DISKS_TO_BE_WIPED ; do
         # Get the size of the device in bytes which could be smaller than 16 MiB.
         # For example a 'bios_grub' partition could be smaller (e.g only 8 MiB on SUSE systems)
         # cf. the "lsblk -ipo NAME,KNAME,PKNAME,TYPE,FSTYPE,SIZE,MOUNTPOINT /dev/sda" output above.
+        # On some platforms like Debian 12 the command e.g. "lsblk -dbnlpo SIZE /dev/sda2"
+        # returns a string that starts with space, for example " 1024", so the subsequent
+        # wiping command "dd if=/dev/zero of=/dev/sda2 count= 1024 iflag=count_bytes" fails.
+        # Therefore strip non-digit characters with 'tr' to get a clean numeric value,
+        # see https://github.com/rear/rear/pull/3484
         device_to_be_wiped_size_bytes="$( lsblk -dbnlpo SIZE $device_to_be_wiped | tr -c -d '[:digit:]' )"
-        if ! test -b $device_to_be_wiped_size_byte ; then
+        if ! test $device_to_be_wiped_size_bytes -gt 0 ; then
             LogPrintError "Skip wiping $device_to_be_wiped (no output for 'lsblk -dbnlpo SIZE $device_to_be_wiped' or failed)"
             continue
         fi
