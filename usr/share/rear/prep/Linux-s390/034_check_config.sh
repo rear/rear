d2155c320f74402eefeeeac11e07960a4d439a7e
Lukáš Zaoral <lzaoral@redhat.com>
2025-07-10 15:27:19 +0200

    s390: remove deprecated OUTPUT=IPL value (#3492)
    Let's remove the deprecated s390-only OUTPUT=IPL value.
    The deprecation warning has been in place for the last two releases
    and a change like this should be fine for ReaR 3.0,
    see https://github.com/rear/rear/pull/3492
    Related: https://github.com/rear/rear/commit/32fe8dd7b0eac35534b28b10dd1b541397aa8bbf

--- usr/share/rear/prep/Linux-s390/034_check_config.sh
+++ usr/share/rear/prep/Linux-s390/034_check_config.sh
@@ -1,8 +1,3 @@
-if [ "$OUTPUT" = "IPL" ]; then
-    LogPrintError "Warning: OUTPUT=IPL is deprecated. Use OUTPUT=RAMDISK instead."
-    OUTPUT=RAMDISK
-fi
-
 if [ "$OUTPUT" != "RAMDISK" ] ; then
    Error "Currently, only OUTPUT=RAMDISK is supported on s390/s390x"
 fi
