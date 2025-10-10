9492b7b76af10115dbefa82ad8fb07499d5ee833
Andrus Suvalau <andrus.suvalau@n-able.com>
2025-10-10 14:50:59 +0200

    feat(rear): add logging
    Jira-Ref: BCF-5849: Support Bare Metal Recovery in Azure

--- usr/share/rear/layout/prepare/COVE/default/020_umount_in_azure.sh
+++ usr/share/rear/layout/prepare/COVE/default/020_umount_in_azure.sh
@@ -1,5 +1,6 @@
 # Unmount /mnt, which is mounted by cloud-init in the Azure environment
 
 if is_cove_in_azure; then
+    LogPrint "Unmounting '/mnt' in Azure environments"
     umount_mountpoint /mnt
 fi
