b4cdb745a411e62dfa919c69b51d8fa0abb2328b
Andrus Suvalau <andrus.suvalau@n-able.com>
2025-10-15 15:26:09 +0200

    feat(cove): add COVE_FIRMWARE variable
    Jira-Ref: BCF-5901: [ReaR] Rely on COVE_FIRMWARE variable to verify
    firmware compatibility

--- usr/share/rear/verify/COVE/default/380_check_firmware.sh
+++ usr/share/rear/verify/COVE/default/380_check_firmware.sh
@@ -2,23 +2,16 @@
 # Check that firmware type is the same on the source and target systems
 #
 
-local used_bootloader
-used_bootloader=$( cat "$VAR_DIR/recovery/bootloader" )
+if [ -z "$COVE_FIRMWARE" ]; then
+    return 0
+fi
+
+local source_firmware_type="$COVE_FIRMWARE"
 
 local target_firmware_type="BIOS"
 if [ -d "/sys/firmware/efi/vars" ] || [ -d "/sys/firmware/efi/efivars" ] ; then
     target_firmware_type="EFI"
 fi
-
-local source_firmware_type="BIOS"
-case "${used_bootloader}" in
-    "GRUB2-EFI"|"ELILO"|"EFI")
-        source_firmware_type="EFI"
-        ;;
-    "GRUB2"|"GRUB"|"LILO"|"ZIPL"|"")
-        source_firmware_type="BIOS"
-        ;;
-esac
 
 text="Firmware type mismatch detected. The source system firmware type is ${source_firmware_type}, \
 while the target system firmware type is ${target_firmware_type}. Mismatched firmware types are not allowed. \
