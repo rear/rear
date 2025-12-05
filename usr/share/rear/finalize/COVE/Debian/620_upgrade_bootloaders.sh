343b05f52c723084ad9955d6649ee7a25faf4a47
Andrus Suvalau <andrus.suvalau@n-able.com>
2025-12-05 14:15:46 +0100

    feat(cove): fix roll back
    Jira-Ref: BCF-6007: [ReaR] Debian 10 fails to boot after recovery
    with Secure Boot enabledase enter the commit message for your changes. Lines starting

--- usr/share/rear/finalize/COVE/Debian/620_upgrade_bootloaders.sh
+++ usr/share/rear/finalize/COVE/Debian/620_upgrade_bootloaders.sh
@@ -26,16 +26,18 @@ if [ "${UEFI_BOOTLOADER##*/}" != "shimx64.efi" ]; then
     return 0
 fi
 
-declare -F sb_enabled >/dev/null || function sb_enabled() {
-    local sb_var=/sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c
-    if [ ! -f "$sb_var" ]; then
-        return 1
-    fi
+declare -F upgrade_bootloaders >/dev/null || function upgrade_bootloaders() {
+    local target_bootloader_dir="${UEFI_BOOTLOADER%/*}"
+    target_bootloader_dir="$TARGET_FS_ROOT$target_bootloader_dir"
 
-    local sb_var_data=0
-    sb_var_data=$(dd if=$sb_var bs=1 skip=4 count=1 2>/dev/null | hexdump -e '1/1 "%X"')
+    local shim="$target_bootloader_dir/shimx64.efi"
+    local grub="$target_bootloader_dir/grubx64.efi"
 
-    [ "$sb_var_data" -eq 1 ]
+    cp -b /usr/lib/shim/shimx64.efi.signed "$shim" || return 1
+    cp -b /usr/lib/grub/x86_64-efi-signed/grubx64.efi.signed "$grub" || \
+        { mv "$shim~" "$shim"; return 1; }
+
+    rm "$shim~" "$grub~"
 }
 
 if upgrade_bootloaders; then
