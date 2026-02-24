1df52deb4bcf53e46c12812a7a2c535b42592578
gdha <gratien.dhaese@gmail.com>
2026-02-24 13:32:50 +0100

    adding real example conf/examples/borg-executable-example.conf

--- usr/share/rear/backup/BORG/default/410_update_exclude_files.sh
+++ usr/share/rear/backup/BORG/default/410_update_exclude_files.sh
@@ -1,5 +1,8 @@
-# /usr/share/rear/backup/BORG/default/410_update_exclude_files.sh
+# backup/BORG/default/410_update_exclude_files.sh
 
+# The BORGBACKUP_EXCLUDE_FILE is a file that could also be used by 'borg'
+# outside of ReaR. ReaR should respect the end-user exclude rules, therefore,
+# we also add these items to the $TMP_DIR/backup-exclude.txt file.
 if [[ -f "$BORGBACKUP_EXCLUDE_FILE" ]] ; then
     cat /dev/null > "$TMP_DIR/backup-excludes.txt"
     while read -r backup_exclude_item ; do
@@ -8,7 +11,7 @@ if [[ -f "$BORGBACKUP_EXCLUDE_FILE" ]] ; then
     done <<< $(cat "$BORGBACKUP_EXCLUDE_FILE" | grep -v ^\#)
 fi
 
-# We not delete the excludes created by 400_create_include_exclude_files.sh
+# We will append the excludes created by 400_create_include_exclude_files.sh
 cat "$TMP_DIR/backup-exclude.txt" >> "$TMP_DIR/backup-excludes.txt"
 
 # backup-exclude.txt is backup-excludes.txt without duplicates but keeps the ordering:
