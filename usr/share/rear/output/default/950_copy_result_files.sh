rear/rear

commit c4ddb00f1f9eba7b036a6a54788080f2197badde

code optimizations

usr/share/rear/output/default/950_copy_result_files.sh
@@ -128,28 +128,30 @@ case "$scheme" in
     (fish|ftp|ftps|hftp|http|https|sftp)
         LogPrint "Copying result files '${RESULT_FILES[*]}' to $scheme location"
 
-        if contains_visible_char "$OUTPUT_LFTP_USERNAME" ; then
-            local lftp_cmds=
-    	    read -r -d '' lftp_cmds <<EOF
+        local lftp_cmds=
+        local lftp_user_opts=
+    	read -r -d '' lftp_cmds <<EOF
 $OUTPUT_LFTP_OPTIONS
 mkdir -fp ${path}
 mput ${RESULT_FILES[*]}
 EOF
+
+        if contains_visible_char "$OUTPUT_LFTP_USERNAME" ; then
+            { lftp_user_opts="-u $OUTPUT_LFTP_USERNAME,$OUTPUT_LFTP_PASSWORD" ; } 2>/dev/null
             Log "lftp -u $OUTPUT_LFTP_USERNAME,******* $OUTPUT_URL"
-            Log "$lftp_cmds"
-
-            { lftp -u "$OUTPUT_LFTP_USERNAME","$OUTPUT_LFTP_PASSWORD" "$OUTPUT_URL" <<< $lftp_cmds 
-            } 2>/dev/null || Error "lftp failed to transfer '${RESULT_FILES[*]}' to '$OUTPUT_URL' (lftp exit code: $?)"
-
         else
-            Log "lftp -c $OUTPUT_LFTP_OPTIONS; open $OUTPUT_URL; mput ${RESULT_FILES[*]}"
-            # Make sure that destination directory exists, otherwise lftp would copy
-            # RESULT_FILES into last available directory in the path.
-            # e.g. OUTPUT_URL=sftp://<host_name>/iso/server1 and have "/iso/server1"
-            # directory missing, would upload RESULT_FILES into sftp://<host_name>/iso/
-            lftp -c "$OUTPUT_LFTP_OPTIONS; open $OUTPUT_URL; mkdir -fp ${path}"
-            lftp -c "$OUTPUT_LFTP_OPTIONS; open $OUTPUT_URL; mput ${RESULT_FILES[*]}" || Error "lftp failed to transfer '${RESULT_FILES[*]}' to '$OUTPUT_URL' (lftp exit code: $?)"
+            Log "lftp $lftp_user_opts $OUTPUT_URL"
         fi
+
+        Log "$lftp_cmds"
+        # Make sure that destination directory exists, otherwise lftp would copy
+        # RESULT_FILES into last available directory in the path.
+        # e.g. OUTPUT_URL=sftp://<host_name>/iso/server1 and have "/iso/server1"
+        # directory missing, would upload RESULT_FILES into sftp://<host_name>/iso/
+        { lftp $lftp_user_opts "$OUTPUT_URL" <<< $lftp_cmds 
+        } 2>/dev/null \
+            || Error "lftp failed to transfer '${RESULT_FILES[*]}' to '$OUTPUT_URL' (lftp exit code: $?)" || Error "lftp failed to transfer '${RESULT_FILES[*]}' to '$OUTPUT_URL' (lftp exit code: $?)"
+
 
         ;;
     (rsync)

usr/share/rear/conf/default.conf
@@ -871,7 +871,7 @@ OUTPUT_PREFIX_PXE=""
 # Example: OUTPUT_LFTP_OPTIONS="set ftp:ssl-force true; set ftp:ssl-protect-data true;"
 OUTPUT_LFTP_OPTIONS=""
 OUTPUT_LFTP_USERNAME=${OUTPUT_LFTP_USERNAME:-}
-OUTPUT_LFTP_PASSWORD=${OUTPUT_LFTP_PASSWORD:-}
+{ OUTPUT_LFTP_PASSWORD=${OUTPUT_LFTP_PASSWORD:-} ; } 2>/dev/null
 
 ####
 # OUTPUT=RAMDISK

commit da9200db491c37662f0c6dc734dcb31e1efc0275

Fix lftp command to pass options prior to opening the URL
The lftp command needs to apply the user specified options prior to
opening the URL, otherwise the options won't apply for the opening
itself.
This is problematic when users use a custom program to connect, e.g.
OUTPUT_LFTP_OPTIONS=( "set sftp:connect-program ..." )

The issue was introduced by commit c4ddb00f1f9eba7b036a6a54788080f2197badde.

Signed-off-by: Renaud Métrich <rmetrich@redhat.com>

usr/share/rear/output/default/950_copy_result_files.sh
@@ -139,25 +139,25 @@ case "$scheme" in
         local lftp_user_opts lftp_cmds_heredoc
         local lftp_cmds=("${OUTPUT_LFTP_OPTIONS[@]}")
         lftp_cmds+=(
+            "open $OUTPUT_URL"
             "mkdir -fp ${path}"
             "mput ${RESULT_FILES[*]}"
         )
 
-    	lftp_cmds_heredoc=$(IFS=$'\n'; echo "${lftp_cmds[*]}")
+        lftp_cmds_heredoc=$(IFS=$'\n'; echo "${lftp_cmds[*]}")
 
         if contains_visible_char "$OUTPUT_LFTP_USERNAME" ; then
             { lftp_user_opts="-u $OUTPUT_LFTP_USERNAME,$OUTPUT_LFTP_PASSWORD" ; } 2>>/dev/$SECRET_OUTPUT_DEV
-            Log "lftp -u $OUTPUT_LFTP_USERNAME,******* $OUTPUT_URL"
+            Log "lftp -u $OUTPUT_LFTP_USERNAME,******* <<< '$lftp_cmds_heredoc'"
         else
-            Log "lftp $lftp_user_opts $OUTPUT_URL"
+            Log "lftp $lftp_user_opts <<< '$lftp_cmds_heredoc'"
         fi
 
-        Log "$lftp_cmds_heredoc"
         # Make sure that destination directory exists, otherwise lftp would copy
         # RESULT_FILES into last available directory in the path.
         # e.g. OUTPUT_URL=sftp://<host_name>/iso/server1 and have "/iso/server1"
         # directory missing, would upload RESULT_FILES into sftp://<host_name>/iso/
-        { lftp $lftp_user_opts "$OUTPUT_URL" <<< "$lftp_cmds_heredoc" 
+        { lftp $lftp_user_opts <<< "$lftp_cmds_heredoc"
         } 2>/dev/null \
             || Error "lftp failed to transfer '${RESULT_FILES[*]}' to '$OUTPUT_URL' (lftp exit code: $?)"
         ;;